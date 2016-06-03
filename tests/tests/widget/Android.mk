5ed3c2717161a7bdb4288b64ffa92423f444e55e
Ian Rogers <irogers@google.com>
2016-06-03 15:16:22 -0700

    Fix missing dependencies.
    Bug: 27723540
    
    Change-Id: I25381d9b607d3da6b87c46a4b01b7c3b7686db81

--- tests/tests/widget/Android.mk
+++ tests/tests/widget/Android.mk
@@ -22,6 +22,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 LOCAL_STATIC_JAVA_LIBRARIES += \
+    android-support-test \
     mockito-target \
     android-common \
     ctsdeviceutil \
