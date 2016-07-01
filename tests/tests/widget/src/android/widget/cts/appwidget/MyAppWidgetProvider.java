6f4f2b905d0d8e5b41ada1456e254319727ce46e
Kirill Grouchnikov <kirillg@google.com>
2016-07-01 19:25:12 -0400

    Fifth pass for completing CTS coverage of RemoteViews
    Coverage for setScrollPosition and setRelativeScrollPosition.
    The setup is:
    * Add ListView to the content being inflated in RemoteViews
    * Make it visible for the specific test (as these two methods
    can only be called on ListView)
    * Add the host view to our activity so that it is visible on the
    screen and has non-0 bounds
    * Wait until ListView has populated its content
    * Fire off setScrollPosition call and verify with a scroll
    listener that the operation is a success
    * Fire off setRelativeScrollPosition call and verify with
    another scroll listener that this operation is a success as well
    
    Bug: 29805243
    Change-Id: Id9c59bbdcf1e89fa412d6e277bd4dccfe7a6b8a9

--- tests/tests/widget/src/android/widget/cts/appwidget/MyAppWidgetProvider.java
+++ tests/tests/widget/src/android/widget/cts/appwidget/MyAppWidgetProvider.java
@@ -21,26 +21,47 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.cts.util.PollingCheck;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 import android.widget.RemoteViews;
-import android.widget.cts.MockURLSpanTestActivity;
 import android.widget.cts.R;
 
 import java.util.concurrent.CountDownLatch;
 
 public final class MyAppWidgetProvider extends AppWidgetProvider {
+    private static final long TIME_SLICE = 100;
+
     public static final String KEY_DISPLAYED_CHILD_INDEX =
             "MyAppWidgetProvider.displayedChildIndex";
     public static final String KEY_SHOW_NEXT = "MyAppWidgetProvider.showNext";
     public static final String KEY_SHOW_PREVIOUS = "MyAppWidgetProvider.showPrevious";
+    public static final String KEY_SWITCH_TO_LIST = "MyAppWidgetProvider.switchToList";
+    public static final String KEY_SCROLL_POSITION = "MyAppWidgetProvider.scrollPosition";
+    public static final String KEY_SCROLL_OFFSET = "MyAppWidgetProvider.scrollOffset";
+
+    // This latch will be notified when onEnabled is called on our provider.
     private static CountDownLatch sCountDownLatch;
+    // Gating condition to be polled to proceed with setScrollPosition call.
+    private static PollingCheck.PollingCheckCondition sSetScrollCondition;
+    // Gating condition to be polled to proceed with setRelativeScrollPosition call.
+    private static PollingCheck.PollingCheckCondition sSetRelativeScrollCondition;
 
     private int mDisplayedChildIndex;
     private boolean mShowNext;
     private boolean mShowPrevious;
+    private boolean mSwitchToList;
+    private int mScrollPosition;
+    private int mScrollOffset;
 
-    public static void setCountDownLatch(CountDownLatch countDownLatch) {
+    public static void configure(CountDownLatch countDownLatch,
+            PollingCheck.PollingCheckCondition setScrollCondition,
+            PollingCheck.PollingCheckCondition setRelativeScrollCondition) {
         sCountDownLatch = countDownLatch;
+        sSetScrollCondition = setScrollCondition;
+        sSetRelativeScrollCondition = setRelativeScrollCondition;
     }
 
     @Override
@@ -48,6 +69,9 @@ public final class MyAppWidgetProvider extends AppWidgetProvider {
         mDisplayedChildIndex = intent.getIntExtra(KEY_DISPLAYED_CHILD_INDEX, -1);
         mShowNext = intent.getBooleanExtra(KEY_SHOW_NEXT, false);
         mShowPrevious = intent.getBooleanExtra(KEY_SHOW_PREVIOUS, false);
+        mSwitchToList = intent.getBooleanExtra(KEY_SWITCH_TO_LIST, false);
+        mScrollPosition = intent.getIntExtra(KEY_SCROLL_POSITION, -1);
+        mScrollOffset = intent.getIntExtra(KEY_SCROLL_OFFSET, 0);
 
         super.onReceive(context, intent);
     }
@@ -55,13 +79,13 @@ public final class MyAppWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int appWidgetId = appWidgetIds[0];
+        final RemoteViews widgetAdapterView = new RemoteViews(context.getPackageName(),
+                R.layout.remoteviews_adapter);
 
         final Intent stackIntent = new Intent(context, MyAppWidgetService.class);
         stackIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
         stackIntent.setData(Uri.parse(stackIntent.toUri(Intent.URI_INTENT_SCHEME)));
 
-        final RemoteViews widgetAdapterView = new RemoteViews(context.getPackageName(),
-                R.layout.remoteviews_adapter);
         widgetAdapterView.setRemoteAdapter(R.id.remoteViews_stack, stackIntent);
         widgetAdapterView.setEmptyView(R.id.remoteViews_stack, R.id.remoteViews_empty);
 
@@ -85,6 +109,61 @@ public final class MyAppWidgetProvider extends AppWidgetProvider {
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
         widgetAdapterView.setPendingIntentTemplate(R.id.remoteViews_stack, pendingIntent);
+
+        if (mSwitchToList) {
+            final Intent listIntent = new Intent(context, MyAppWidgetService.class);
+            listIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            listIntent.setData(Uri.parse(stackIntent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            widgetAdapterView.setRemoteAdapter(R.id.remoteViews_list, listIntent);
+
+            widgetAdapterView.setViewVisibility(R.id.remoteViews_stack, View.GONE);
+            widgetAdapterView.setViewVisibility(R.id.remoteViews_list, View.VISIBLE);
+        }
+
+        final Handler handler = new Handler(Looper.myLooper());
+        if (mScrollPosition >= 0) {
+            // We need to schedule the call to setScrollPosition as a separate event that runs
+            // after the underlying ListView has been laid out on the screen. Otherwise calling
+            // that API on a ListView with 0x0 dimension has no effect - the list content is only
+            // populated via the adapter when ListView has "real" bounds.
+            final Runnable setScrollRunnable = new Runnable() {
+                public void run() {
+                    if (sSetScrollCondition.canProceed()) {
+                        // Gating condition has been satisfied. Call setScrollPosition and
+                        // ask the widget manager to update our widget
+                        widgetAdapterView.setScrollPosition(R.id.remoteViews_list, mScrollPosition);
+                        appWidgetManager.partiallyUpdateAppWidget(appWidgetId, widgetAdapterView);
+                    } else {
+                        // Keep on "waiting" until the gating condition is satisfied
+                        handler.postDelayed(this, TIME_SLICE);
+                    }
+                }
+            };
+            handler.postDelayed(setScrollRunnable, TIME_SLICE);
+        }
+
+        if (mScrollOffset != 0) {
+            // We need to schedule the call to setRelativeScrollPosition as a separate event that
+            // runs after the underlying ListView has been laid out on the screen. Otherwise calling
+            // that API on a ListView with 0x0 dimension has no effect - the list content is only
+            // populated via the adapter when ListView has "real" bounds.
+            final Runnable setRelativeScrollRunnable = new Runnable() {
+                public void run() {
+                    if (sSetRelativeScrollCondition.canProceed()) {
+                        // Gating condition has been satisfied. Call setRelativeScrollPosition and
+                        // ask the widget manager to update our widget
+                        widgetAdapterView.setRelativeScrollPosition(
+                                R.id.remoteViews_list, mScrollOffset);
+                        appWidgetManager.partiallyUpdateAppWidget(appWidgetId, widgetAdapterView);
+                    } else {
+                        // Keep on "waiting" until the gating condition is satisfied
+                        handler.postDelayed(this, TIME_SLICE);
+                    }
+                }
+            };
+            handler.postDelayed(setRelativeScrollRunnable, TIME_SLICE);
+        }
 
         appWidgetManager.updateAppWidget(appWidgetId, widgetAdapterView);
 
