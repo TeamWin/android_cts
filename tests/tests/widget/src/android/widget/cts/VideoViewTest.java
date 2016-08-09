40be29baef148a630f84424a29c925107d294323
Kirill Grouchnikov <kirillg@google.com>
2016-08-09 13:54:01 -0400

    Pass #5 for bringing android.view CTS tests into the new world
    * Use Java 8 features (lambdas, method references, diamonds)
    * Use JUnit4 (@RunWith, @Before, @Test)
    * Use the latest test harness (@ActivityTestRule, size annotations)
    * Use Mockito for tracking callbacks
    
    Also removed duplicated tests in ViewTreeObserverTest
    
    Bug: 30695817
    Change-Id: Ib0b952a833a795546b822d1521c4cb53dd43c709

--- tests/tests/widget/src/android/widget/cts/VideoViewTest.java
+++ tests/tests/widget/src/android/widget/cts/VideoViewTest.java
@@ -16,7 +16,7 @@
 
 package android.widget.cts;
 
-import static android.widget.cts.util.TestUtils.within;
+import static android.cts.util.CtsMockitoUtils.within;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
