08999c61e426240bbf0f6ba89281509358699895
Janis Danisevskis <jdanis@google.com>
2016-09-01 09:30:08 +0100

    Fix auth list app ID affiliation
    The app ID must never be associated with the tee auth list,
    because the app ID cannot be assured by the tee.
    
    Change-Id: I6c494e3c63008d19afa58e27340a55820d82e0b5

--- tests/tests/keystore/src/android/keystore/cts/KeyAttestationTest.java
+++ tests/tests/keystore/src/android/keystore/cts/KeyAttestationTest.java
@@ -460,16 +460,8 @@ public class KeyAttestationTest extends AndroidTestCase {
     private void checkAttestationApplicationId(Attestation attestation)
             throws NoSuchAlgorithmException, NameNotFoundException {
         AttestationApplicationId aaid = null;
-        switch (attestation.getAttestationSecurityLevel()) {
-        case Attestation.KM_SECURITY_LEVEL_SOFTWARE:
-            assertNull(attestation.getTeeEnforced().getAttestationApplicationId());
-            aaid = attestation.getSoftwareEnforced().getAttestationApplicationId();
-            break;
-        case Attestation.KM_SECURITY_LEVEL_TRUSTED_ENVIRONMENT:
-            assertNull(attestation.getSoftwareEnforced().getAttestationApplicationId());
-            aaid = attestation.getTeeEnforced().getAttestationApplicationId();
-            break;
-        }
+        assertNull(attestation.getTeeEnforced().getAttestationApplicationId());
+        aaid = attestation.getSoftwareEnforced().getAttestationApplicationId();
         assertNotNull(aaid);
         assertEquals(new AttestationApplicationId(getContext()), aaid);
     }
