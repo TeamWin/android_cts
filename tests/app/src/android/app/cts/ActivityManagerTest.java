17de155ee72a2cd072f10d9fbd1de5eb344992d2
Dianne Hackborn <hackbod@google.com>
2016-11-03 15:05:20 -0700

    Add new CTS test for new importance monitoring API.
    Test: this is one
    Change-Id: I1ddeb06746020f0664b5f32302c42f25ca1bdec4

--- tests/app/src/android/app/cts/ActivityManagerTest.java
+++ tests/app/src/android/app/cts/ActivityManagerTest.java
@@ -15,6 +15,7 @@
  */
 package android.app.cts;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RecentTaskInfo;
@@ -32,12 +33,21 @@ import android.app.stubs.MockApplicationActivity;
 import android.app.stubs.MockService;
 import android.app.stubs.ScreenOnActivity;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.ConfigurationInfo;
+import android.cts.util.SystemUtil;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.os.SystemClock;
 import android.platform.test.annotations.RestrictedBuildTest;
 import android.test.InstrumentationTestCase;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +62,7 @@ public class ActivityManagerTest extends InstrumentationTestCase {
     private static final String SIMPLE_ACTIVITY = ".SimpleActivity";
     private static final String SIMPLE_ACTIVITY_IMMEDIATE_EXIT = ".SimpleActivityImmediateExit";
     private static final String SIMPLE_ACTIVITY_CHAIN_EXIT = ".SimpleActivityChainExit";
+    private static final String SIMPLE_SERVICE = ".SimpleService";
     // The action sent back by the SIMPLE_APP after a restart.
     private static final String ACTIVITY_LAUNCHED_ACTION =
             "com.android.cts.launchertests.LauncherAppsTests.LAUNCHED_ACTION";
@@ -339,13 +350,14 @@ public class ActivityManagerTest extends InstrumentationTestCase {
             }
         }
         // start a new process
+        // XXX would be a lot cleaner to bind instead of start.
         mIntent = new Intent("android.app.REMOTESERVICE");
         mIntent.setPackage("android.app.stubs");
         mInstrumentation.getTargetContext().startService(mIntent);
         Thread.sleep(WAITFOR_MSEC);
 
         List<RunningAppProcessInfo> listNew = mActivityManager.getRunningAppProcesses();
-        assertTrue(list.size() <= listNew.size());
+        mInstrumentation.getTargetContext().stopService(mIntent);
 
         for (RunningAppProcessInfo ra : listNew) {
             if (ra.processName.equals("android.app.stubs:remote")) {
@@ -366,7 +378,7 @@ public class ActivityManagerTest extends InstrumentationTestCase {
     }
 
     /**
-     * Simple test for {@link ActivityManager.isUserAMonkey()} - verifies its false.
+     * Simple test for {@link ActivityManager#isUserAMonkey()} - verifies its false.
      *
      * TODO: test positive case
      */
@@ -375,7 +387,7 @@ public class ActivityManagerTest extends InstrumentationTestCase {
     }
 
     /**
-     * Verify that {@link ActivityManager.isRunningInTestHarness()} is false.
+     * Verify that {@link ActivityManager#isRunningInTestHarness()} is false.
      */
     @RestrictedBuildTest
     public void testIsRunningInTestHarness() {
@@ -393,6 +405,240 @@ public class ActivityManagerTest extends InstrumentationTestCase {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(intent);
         Thread.sleep(WAIT_TIME);
+    }
+
+    static final class UidImportanceListener implements ActivityManager.OnUidImportanceListener {
+        final int mUid;
+
+        int mLastValue = -1;
+
+        UidImportanceListener(int uid) {
+            mUid = uid;
+        }
+
+        @Override
+        public void onUidImportance(int uid, int importance) {
+            synchronized (this) {
+                Log.d("XXXXX", "Got importance for uid " + uid + ": " + importance);
+                if (uid == mUid) {
+                    mLastValue = importance;
+                    notifyAll();
+                }
+            }
+        }
+
+        public int waitForValue(int minValue, int maxValue, long timeout) {
+            final long endTime = SystemClock.uptimeMillis()+timeout;
+
+            synchronized (this) {
+                while (mLastValue < minValue || mLastValue > maxValue) {
+                    final long now = SystemClock.uptimeMillis();
+                    if (now >= endTime) {
+                        throw new IllegalStateException("Timed out waiting for importance "
+                                + minValue + "-" + maxValue + ", last was " + mLastValue);
+                    }
+                    try {
+                        wait(endTime-now);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                Log.d("XXXX", "waitForValue " + minValue + "-" + maxValue + ": " + mLastValue);
+                return mLastValue;
+            }
+        }
+    }
+
+    static final class ServiceConnectionHandler implements ServiceConnection {
+        final Context mContext;
+        final Intent mIntent;
+        boolean mBound;
+        IBinder mService;
+
+        final ServiceConnection mMainBinding = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+            }
+        };
+
+        ServiceConnectionHandler(Context context, Intent intent) {
+            mContext = context;
+            mIntent = intent;
+        }
+
+        public void bind(long timeout) {
+            synchronized (this) {
+                if (mBound) {
+                    throw new IllegalStateException("Already bound");
+                }
+                // Here's the trick: the first binding allows us to to see the service come
+                // up and go down but doesn't actually cause it to run or impact process management.
+                // The second binding actually brings it up.
+                if (!mContext.bindService(mIntent, this, Context.BIND_WAIVE_PRIORITY)) {
+                    throw new IllegalStateException("Failed to bind " + mIntent);
+                }
+                if (!mContext.bindService(mIntent, mMainBinding, Context.BIND_AUTO_CREATE)) {
+                    throw new IllegalStateException("Failed to bind " + mIntent);
+                }
+                mBound = true;
+                mService = null;
+
+                final long endTime = SystemClock.uptimeMillis() + timeout;
+
+                while (mService == null) {
+                    final long now = SystemClock.uptimeMillis();
+                    if (now >= endTime) {
+                        throw new IllegalStateException("Timed out binding to " + mIntent);
+                    }
+                    try {
+                        wait(endTime - now);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+
+        public void unbind(long timeout) {
+            synchronized (this) {
+                if (!mBound) {
+                    throw new IllegalStateException("Not bound");
+                }
+                // This allows the service to go down.  We maintain the second binding to be
+                // able to see the connection go away which is what we want to wait on.
+                mContext.unbindService(mMainBinding);
+                mBound = false;
+
+                try {
+                    final long endTime = SystemClock.uptimeMillis() + timeout;
+
+                    while (mService != null) {
+                        final long now = SystemClock.uptimeMillis();
+                        if (now >= endTime) {
+                            throw new IllegalStateException("Timed out unbinding from " + mIntent);
+                        }
+                        try {
+                            wait(endTime - now);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                } finally {
+                    mContext.unbindService(this);
+                }
+            }
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            synchronized (this) {
+                mService = service;
+                notifyAll();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            synchronized (this) {
+                mService = null;
+                notifyAll();
+            }
+        }
+    }
+
+    public void testUidImportanceListener() throws Exception {
+        Intent serviceIntent = new Intent();
+        Parcel data = Parcel.obtain();
+        serviceIntent.setClassName(SIMPLE_PACKAGE_NAME,
+                SIMPLE_PACKAGE_NAME + SIMPLE_SERVICE);
+        ServiceConnectionHandler conn = new ServiceConnectionHandler(mContext, serviceIntent);
+
+        ActivityManager am = mContext.getSystemService(ActivityManager.class);
+
+        ApplicationInfo appInfo = mContext.getPackageManager().getApplicationInfo(
+                SIMPLE_PACKAGE_NAME, 0);
+        UidImportanceListener uidForegroundListener = new UidImportanceListener(appInfo.uid);
+
+        String cmd = "pm revoke " + STUB_PACKAGE_NAME + " "
+                + Manifest.permission.PACKAGE_USAGE_STATS;
+        String result = SystemUtil.runShellCommand(getInstrumentation(), cmd);
+        /*
+        Log.d("XXXX", "Invoke: " + cmd);
+        Log.d("XXXX", "Result: " + result);
+        */
+        boolean gotException = false;
+        try {
+            am.addOnUidImportanceListener(uidForegroundListener,
+                    RunningAppProcessInfo.IMPORTANCE_SERVICE);
+        } catch (SecurityException e) {
+            gotException = true;
+        }
+        assertTrue("Expected SecurityException thrown", gotException);
+
+        cmd = "pm grant " + STUB_PACKAGE_NAME + " "
+                + Manifest.permission.PACKAGE_USAGE_STATS;
+        result = SystemUtil.runShellCommand(getInstrumentation(), cmd);
+        /*
+        Log.d("XXXX", "Invoke: " + cmd);
+        Log.d("XXXX", "Result: " + result);
+        Log.d("XXXX", SystemUtil.runShellCommand(getInstrumentation(), "dumpsys package "
+                + STUB_PACKAGE_NAME));
+        */
+        am.addOnUidImportanceListener(uidForegroundListener,
+                RunningAppProcessInfo.IMPORTANCE_SERVICE);
+
+        UidImportanceListener uidGoneListener = new UidImportanceListener(appInfo.uid);
+        am.addOnUidImportanceListener(uidGoneListener,
+                RunningAppProcessInfo.IMPORTANCE_EMPTY);
+
+        // First kill the process to start out in a stable state.
+        conn.bind(WAIT_TIME);
+        try {
+            conn.mService.transact(IBinder.FIRST_CALL_TRANSACTION, data, null, 0);
+        } catch (RemoteException e) {
+        }
+        conn.unbind(WAIT_TIME);
+
+        // Wait for uid's process to go away.
+        uidGoneListener.waitForValue(RunningAppProcessInfo.IMPORTANCE_GONE,
+                RunningAppProcessInfo.IMPORTANCE_GONE, WAIT_TIME);
+        assertEquals(RunningAppProcessInfo.IMPORTANCE_GONE,
+                am.getPackageImportance(SIMPLE_PACKAGE_NAME));
+
+        // Now bind and see if we get told about the uid coming in to the foreground.
+        conn.bind(WAIT_TIME);
+        uidForegroundListener.waitForValue(RunningAppProcessInfo.IMPORTANCE_FOREGROUND,
+                RunningAppProcessInfo.IMPORTANCE_VISIBLE, WAIT_TIME);
+        assertEquals(RunningAppProcessInfo.IMPORTANCE_FOREGROUND,
+                am.getPackageImportance(SIMPLE_PACKAGE_NAME));
+
+        // Pull out the service IBinder for a kludy hack...
+        IBinder service = conn.mService;
+
+        // Now unbind and see if we get told about it going to the background.
+        conn.unbind(WAIT_TIME);
+        uidForegroundListener.waitForValue(RunningAppProcessInfo.IMPORTANCE_BACKGROUND,
+                RunningAppProcessInfo.IMPORTANCE_EMPTY, WAIT_TIME);
+        assertEquals(RunningAppProcessInfo.IMPORTANCE_BACKGROUND,
+                am.getPackageImportance(SIMPLE_PACKAGE_NAME));
+
+        // Now kill the process and see if we are told about it being gone.
+        try {
+            service.transact(IBinder.FIRST_CALL_TRANSACTION, data, null, 0);
+        } catch (RemoteException e) {
+            // It is okay if it is already gone for some reason.
+        }
+
+        uidGoneListener.waitForValue(RunningAppProcessInfo.IMPORTANCE_GONE,
+                RunningAppProcessInfo.IMPORTANCE_GONE, WAIT_TIME);
+        assertEquals(RunningAppProcessInfo.IMPORTANCE_GONE,
+                am.getPackageImportance(SIMPLE_PACKAGE_NAME));
+
+        data.recycle();
+
+        am.removeOnUidImportanceListener(uidForegroundListener);
+        am.removeOnUidImportanceListener(uidGoneListener);
     }
 
     /**
