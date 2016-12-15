7c5b9d58d748a245f51b75f10a472864d31021be
Ta-wei Yen <twyen@google.com>
2016-12-15 15:42:34 -0800

    Add CTS verifier tests for VisualVoicemailService
    This tests onCellServiceConnected() and onSimRemoved(), which requires
    the SIM to be hot swapped and must be tested manually
    
    Change-Id: Ide60d3c8f8f5500b93969cacdeea2f44a58d360a
    Test: CTS verifier -> Telephony -> VisualVociemailService Test

--- apps/CtsVerifier/src/com/android/cts/verifier/voicemail/VoicemailBroadcastActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/voicemail/VoicemailBroadcastActivity.java
@@ -17,18 +17,12 @@
 
 package com.android.cts.verifier.voicemail;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.telecom.TelecomManager;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.android.cts.verifier.PassFailButtons;
 import com.android.cts.verifier.R;
 import com.android.cts.verifier.voicemail.VoicemailBroadcastReceiver.ReceivedListener;
@@ -41,35 +35,10 @@ import com.android.cts.verifier.voicemail.VoicemailBroadcastReceiver.ReceivedLis
  */
 public class VoicemailBroadcastActivity extends PassFailButtons.Activity {
 
-    private String mDefaultDialer;
-
-    private ImageView mSetDefaultDialerImage;
     private ImageView mLeaveVoicemailImage;
     private TextView mLeaveVoicemailText;
-    private ImageView mRestoreDefaultDialerImage;
-    private TextView mRestoreDefaultDialerText;
 
-    private Button mSetDefaultDialerButton;
-    private Button mRestoreDefaultDialerButton;
-
-    private BroadcastReceiver mDefaultDialerChangedReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String packageName =
-                    intent.getStringExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME);
-            if (!getPassButton().isEnabled()) {
-                updateSetDefaultDialerState(packageName);
-            } else {
-                if (packageName.equals(getPackageName())) {
-                    mRestoreDefaultDialerImage
-                            .setImageDrawable(getDrawable(R.drawable.fs_indeterminate));
-                } else {
-                    mRestoreDefaultDialerImage.setImageDrawable(getDrawable(R.drawable.fs_good));
-                }
-            }
-        }
-    };
-
+    private DefaultDialerChanger mDefaultDialerChanger;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,60 +49,10 @@ public class VoicemailBroadcastActivity extends PassFailButtons.Activity {
         setPassFailButtonClickListeners();
         getPassButton().setEnabled(false);
 
-        mSetDefaultDialerImage = (ImageView) findViewById(R.id.set_default_dialer_image);
         mLeaveVoicemailImage = (ImageView) findViewById(R.id.leave_voicemail_image);
         mLeaveVoicemailText = (TextView) findViewById(R.id.leave_voicemail_text);
-        mRestoreDefaultDialerImage = (ImageView) findViewById(R.id.restore_default_dialer_image);
-        mRestoreDefaultDialerText = (TextView) findViewById(R.id.restore_default_dialer_text);
 
-        mSetDefaultDialerButton = (Button) view.findViewById(R.id.set_default_dialer);
-        mRestoreDefaultDialerButton = (Button) view.findViewById(R.id.restore_default_dialer);
-
-        final TelecomManager telecomManager = getSystemService(TelecomManager.class);
-        mDefaultDialer = telecomManager.getDefaultDialerPackage();
-        updateSetDefaultDialerState(mDefaultDialer);
-        if (mDefaultDialer.equals(getPackageName())) {
-            // The CTS verifier is already the default dialer (probably due to the tester exiting
-            // mid test. We don't know what the default dialer should be so just prompt the tester
-            // to restore it through settings, and remove the button.
-            mRestoreDefaultDialerText
-                    .setText(R.string.voicemail_restore_default_dialer_no_default_description);
-            mRestoreDefaultDialerButton.setVisibility(View.GONE);
-        }
-
-        mSetDefaultDialerButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (telecomManager.getDefaultDialerPackage().equals(getPackageName())) {
-                    Toast.makeText(VoicemailBroadcastActivity.this,
-                            R.string.voicemail_default_dialer_already_set, Toast.LENGTH_SHORT)
-                            .show();
-                    return;
-                }
-
-                final Intent intent = new Intent(TelecomManager.ACTION_CHANGE_DEFAULT_DIALER);
-                intent.putExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME,
-                        getPackageName());
-                startActivityForResult(intent, 0);
-            }
-        });
-
-        mRestoreDefaultDialerButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (telecomManager.getDefaultDialerPackage().equals(mDefaultDialer)) {
-                    Toast.makeText(VoicemailBroadcastActivity.this,
-                            R.string.voicemail_default_dialer_already_restored, Toast.LENGTH_SHORT)
-                            .show();
-                    return;
-                }
-
-                final Intent intent = new Intent(TelecomManager.ACTION_CHANGE_DEFAULT_DIALER);
-                intent.putExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME,
-                        mDefaultDialer);
-                startActivityForResult(intent, 0);
-            }
-        });
+        mDefaultDialerChanger = new DefaultDialerChanger(this);
 
         VoicemailBroadcastReceiver.setListener(new ReceivedListener() {
             @Override
@@ -144,25 +63,15 @@ public class VoicemailBroadcastActivity extends PassFailButtons.Activity {
                 mLeaveVoicemailImage.setImageDrawable(getDrawable(R.drawable.fs_good));
                 mLeaveVoicemailText.setText(R.string.voicemail_broadcast_received);
                 getPassButton().setEnabled(true);
+                mDefaultDialerChanger.setRestorePending(true);
             }
         });
-
-        registerReceiver(mDefaultDialerChangedReceiver,
-                new IntentFilter(TelecomManager.ACTION_DEFAULT_DIALER_CHANGED));
     }
 
     @Override
     protected void onDestroy() {
         VoicemailBroadcastReceiver.setListener(null);
-        unregisterReceiver(mDefaultDialerChangedReceiver);
+        mDefaultDialerChanger.destroy();
         super.onDestroy();
-    }
-
-    private void updateSetDefaultDialerState(String packageName) {
-        if (packageName.equals(getPackageName())) {
-            mSetDefaultDialerImage.setImageDrawable(getDrawable(R.drawable.fs_good));
-        } else {
-            mSetDefaultDialerImage.setImageDrawable(getDrawable(R.drawable.fs_indeterminate));
-        }
     }
 }
