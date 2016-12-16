2d3808e36dda2da9385213964fe26c3e9f1c9d13
sergeyv <sergeyv@google.com>
2016-12-15 19:51:53 -0800

    Tests for hardware bitmaps and Bitmap.copy
    bug:30999911
    Change-Id: I2b03a2bc7a8ba22a3b59f9c550048b2e8f3b7d37

--- tests/tests/uirendering/src/android/uirendering/cts/testclasses/HardwareBitmapTests.java
+++ tests/tests/uirendering/src/android/uirendering/cts/testclasses/HardwareBitmapTests.java
@@ -17,6 +17,7 @@
 package android.uirendering.cts.testclasses;
 
 import android.graphics.BitmapRegionDecoder;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.uirendering.cts.R;
@@ -25,9 +26,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.test.filters.MediumTest;
 import android.uirendering.cts.bitmapcomparers.ExactComparer;
+import android.uirendering.cts.bitmapcomparers.MSSIMComparer;
 import android.uirendering.cts.bitmapverifiers.GoldenImageVerifier;
 import android.uirendering.cts.testinfrastructure.ActivityTestBase;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -59,5 +62,56 @@ public class HardwareBitmapTests extends ActivityTestBase {
             canvas.drawBitmap(hardwareBitmap, 0, 0, new Paint());
         }, true).runWithVerifier(new GoldenImageVerifier(getActivity(),
                 R.drawable.golden_headless_robot, new ExactComparer()));
+    }
+
+    @Test
+    public void testBitmapConfigFromRGB565() {
+        testBitmapCopy(R.drawable.robot, Bitmap.Config.RGB_565, Bitmap.Config.HARDWARE);
+    }
+
+    @Test
+    public void testBitmapConfigFromARGB8888() {
+        testBitmapCopy(R.drawable.robot, Bitmap.Config.ARGB_8888, Bitmap.Config.HARDWARE);
+    }
+
+    @Test
+    public void testBitmapConfigFromA8() {
+        Bitmap b = Bitmap.createBitmap(32, 32, Bitmap.Config.ALPHA_8);
+        // we do not support conversion from A8
+        Assert.assertNull(b.copy(Bitmap.Config.HARDWARE, false));
+    }
+
+    @Test
+    public void testBitmapConfigFromIndex8() {
+        testBitmapCopy(R.drawable.index_8, null, Bitmap.Config.HARDWARE);
+    }
+
+    @Test
+    public void testBitmapConfigFromHardwareToHardware() {
+        testBitmapCopy(R.drawable.robot, Bitmap.Config.HARDWARE, Bitmap.Config.HARDWARE);
+    }
+
+    @Test
+    public void testBitmapConfigFromHardwareToARGB8888() {
+        testBitmapCopy(R.drawable.robot, Bitmap.Config.HARDWARE, Bitmap.Config.ARGB_8888);
+    }
+
+    private void testBitmapCopy(int id, Bitmap.Config from, Bitmap.Config to) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        options.inPreferredConfig = from;
+        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(), id, options);
+        Assert.assertEquals(from, bitmap.getConfig());
+
+        createTest().addCanvasClient((canvas, width, height) -> {
+            canvas.drawColor(Color.CYAN);
+            canvas.drawBitmap(bitmap, 0, 0, null);
+        }, true).addCanvasClient((canvas, width, height) -> {
+            canvas.drawColor(Color.CYAN);
+            Bitmap copy = bitmap.copy(to, false);
+            Assert.assertNotNull(copy);
+            Assert.assertEquals(to, copy.getConfig());
+            canvas.drawBitmap(copy, 0, 0, null);
+        }, true).runWithComparer(new MSSIMComparer(0.99));
     }
 }
