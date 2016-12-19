e1694e18dd79d27ee20a2e235eac4f48d4cf7984
Paul Duffin <paulduffin@google.com>
2016-12-19 14:01:31 +0000

    Use junit instead of junit4-target
    Bug: 30188076
    Test: make checkbuild
    Change-Id: I38f54f5f65badff1942a80cd13109dd240dcbbd5

--- apps/CtsVerifierUSBCompanion/Android.mk
+++ apps/CtsVerifierUSBCompanion/Android.mk
@@ -21,7 +21,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-annotations junit4-target
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-annotations junit
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
