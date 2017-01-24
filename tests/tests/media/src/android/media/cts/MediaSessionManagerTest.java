2f6917392eba20c2aa77762de8674a42c97f671a
Jaewan Kim <jaewan@google.com>
2017-01-23 18:44:27 -0800

    Add test for MediaSessionManager.setOnVolumeKeyLongPressListener()
    Bug: 30125811
    Change-Id: Ib3358612f2253fea78137c736c879928ec6601fd

--- tests/tests/media/src/android/media/cts/MediaSessionManagerTest.java
+++ tests/tests/media/src/android/media/cts/MediaSessionManagerTest.java
@@ -15,15 +15,33 @@
  */
 package android.media.cts;
 
+import com.android.compatibility.common.util.SystemUtil;
+
 import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.media.session.MediaController;
+import android.media.session.MediaSession;
 import android.media.session.MediaSessionManager;
+import android.media.session.PlaybackState;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.test.InstrumentationTestCase;
 import android.test.UiThreadTest;
+import android.view.KeyEvent;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class MediaSessionManagerTest extends InstrumentationTestCase {
+    private static final String TAG = "MediaSessionManagerTest";
+    private static final int TIMEOUT_MS = 3000;
+    private static final int WAIT_MS = 500;
+
     private MediaSessionManager mSessionManager;
 
     @Override
@@ -68,5 +86,70 @@ public class MediaSessionManagerTest extends InstrumentationTestCase {
 
         // TODO enable a notification listener, test again, disable, verify
         // updates stopped
+    }
+
+    private void assertKeyEventEquals(KeyEvent lhs, int keyCode, int action, int repeatCount) {
+        assertTrue(lhs.getKeyCode() == keyCode
+                && lhs.getAction() == action
+                && lhs.getRepeatCount() == repeatCount);
+    }
+
+    private void injectInputEvent(int keyCode, boolean longPress) throws IOException {
+        // Injecting key with instrumentation requires a window/view, but we don't have it.
+        // Inject key event through the adb commend to workaround.
+        final String command = "input keyevent " + (longPress ? "--longpress " : "") + keyCode;
+        SystemUtil.runShellCommand(getInstrumentation(), command);
+    }
+
+    public void testSetOnVolumeKeyLongPressListener() throws Exception {
+        HandlerThread handlerThread = new HandlerThread(TAG);
+        handlerThread.start();
+        Handler handler = new Handler(handlerThread.getLooper());
+
+        // Ensure that the listener is called for long-press.
+        VolumeKeyLongPressListener listener = new VolumeKeyLongPressListener(3, handler);
+        mSessionManager.setOnVolumeKeyLongPressListener(listener, handler);
+        injectInputEvent(KeyEvent.KEYCODE_VOLUME_DOWN, true);
+        assertTrue(listener.mCountDownLatch.await(TIMEOUT_MS, TimeUnit.MILLISECONDS));
+        assertEquals(listener.mKeyEvents.size(), 3);
+        assertKeyEventEquals(listener.mKeyEvents.get(0),
+                KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.ACTION_DOWN, 0);
+        assertKeyEventEquals(listener.mKeyEvents.get(1),
+                KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.ACTION_DOWN, 1);
+        assertKeyEventEquals(listener.mKeyEvents.get(2),
+                KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.ACTION_UP, 0);
+
+        // Ensure the the listener isn't called for short-press.
+        listener = new VolumeKeyLongPressListener(1, handler);
+        mSessionManager.setOnVolumeKeyLongPressListener(listener, handler);
+        injectInputEvent(KeyEvent.KEYCODE_VOLUME_DOWN, false);
+        assertFalse(listener.mCountDownLatch.await(WAIT_MS, TimeUnit.MILLISECONDS));
+        assertEquals(listener.mKeyEvents.size(), 0);
+
+        // Ensure that the listener isn't called anymore.
+        mSessionManager.setOnVolumeKeyLongPressListener(null, handler);
+        injectInputEvent(KeyEvent.KEYCODE_VOLUME_DOWN, true);
+        assertFalse(listener.mCountDownLatch.await(WAIT_MS, TimeUnit.MILLISECONDS));
+        assertEquals(listener.mKeyEvents.size(), 0);
+    }
+
+    private class VolumeKeyLongPressListener
+            implements MediaSessionManager.OnVolumeKeyLongPressListener {
+        private final List<KeyEvent> mKeyEvents = new ArrayList<>();
+        private final CountDownLatch mCountDownLatch;
+        private final Handler mHandler;
+
+        public VolumeKeyLongPressListener(int count, Handler handler) {
+            mCountDownLatch = new CountDownLatch(count);
+            mHandler = handler;
+        }
+
+        @Override
+        public void onVolumeKeyLongPress(KeyEvent event) {
+            mKeyEvents.add(event);
+            // Ensure the listener is called on the thread.
+            assertEquals(mHandler.getLooper(), Looper.myLooper());
+            mCountDownLatch.countDown();
+        }
     }
 }
