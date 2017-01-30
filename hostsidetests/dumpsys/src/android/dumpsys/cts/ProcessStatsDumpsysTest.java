2e71332b6c8c472659c7e08a3eca0debd29a6175
Makoto Onuki <omakoto@google.com>
2017-01-30 13:49:43 -0800

    More CTS for dumpsys procstats
    Test: cts-tradefed run cts --skip-device-info --skip-preconditions --skip-system-status-check com.android.compatibility.common.tradefed.targetprep.NetworkConnectivityChecker -a armeabi-v7a -l INFO -m CtsDumpsysHostTestCases -t android.dumpsys.cts.ProcessStatsDumpsysTest
    
    Bug: 34228624
    Change-Id: I8233751fe0afec76b86457c3d1062a19b055f123

--- hostsidetests/dumpsys/src/android/dumpsys/cts/ProcessStatsDumpsysTest.java
+++ hostsidetests/dumpsys/src/android/dumpsys/cts/ProcessStatsDumpsysTest.java
@@ -16,6 +16,8 @@
 
 package android.dumpsys.cts;
 
+import com.android.tradefed.log.LogUtil.CLog;
+
 import java.io.BufferedReader;
 import java.io.StringReader;
 import java.util.HashSet;
@@ -25,28 +27,54 @@ import java.util.Set;
  * Test to check the format of the dumps of the processstats test.
  */
 public class ProcessStatsDumpsysTest extends BaseDumpsysTest {
+    private static final String DEVICE_SIDE_TEST_APK = "CtsProcStatsApp.apk";
+    private static final String DEVICE_SIDE_TEST_PACKAGE = "com.android.server.cts.procstats";
+
+    private static final String DEVICE_SIDE_HELPER_APK = "CtsProcStatsHelperApp.apk";
+    private static final String DEVICE_SIDE_HELPER_PACKAGE = "com.android.server.cts.procstatshelper";
+
+    private static final boolean UNINSTALL_APPS_AFTER_TESTS = true; // DON'T SUBMIT WITH TRUE
+
     /**
      * Tests the output of "dumpsys procstats -c". This is a proxy for testing "dumpsys procstats
      * --checkin", since the latter is not idempotent.
-     *
-     * @throws Exception
      */
     public void testProcstatsOutput() throws Exception {
+        // First, run the helper app so that we have some interesting records in the output.
+        checkWithProcStatsApp();
+
         String procstats = mDevice.executeShellCommand("dumpsys procstats -c");
         assertNotNull(procstats);
         assertTrue(procstats.length() > 0);
 
-        Set<String> seenTags = new HashSet<>();
-        int version = -1;
+        final int sep24h = procstats.indexOf("AGGREGATED OVER LAST 24 HOURS:");
+        final int sep3h = procstats.indexOf("AGGREGATED OVER LAST 3 HOURS:");
+
+        assertTrue("24 hour stats not found.", sep24h > 1);
+        assertTrue("3 hour stats not found.", sep3h > 1);
+
+        // Current
+        checkProcStateOutput(procstats.substring(0, sep24h), /*checkAvg=*/ true);
+
+        // Last 24 hours
+        checkProcStateOutput(procstats.substring(sep24h, sep3h), /*checkAvg=*/ false);
+
+        // Last 3 hours
+        checkProcStateOutput(procstats.substring(sep3h), /*checkAvg=*/ false);
+    }
+
+    private void checkProcStateOutput(String text, boolean checkAvg) throws Exception {
+        final Set<String> seenTags = new HashSet<>();
 
         try (BufferedReader reader = new BufferedReader(
-                new StringReader(procstats))) {
+                new StringReader(text))) {
 
             String line;
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty()) {
                     continue;
                 }
+                CLog.d("Checking line: " + line);
 
                 // extra space to make sure last column shows up.
                 if (line.endsWith(",")) {
@@ -58,34 +86,34 @@ public class ProcessStatsDumpsysTest extends BaseDumpsysTest {
                 switch (parts[0]) {
                     case "vers":
                         assertEquals(2, parts.length);
-                        version = Integer.parseInt(parts[1]);
+                        assertEquals(5, Integer.parseInt(parts[1]));
                         break;
                     case "period":
                         checkPeriod(parts);
                         break;
                     case "pkgproc":
-                        checkPkgProc(parts, version);
+                        checkPkgProc(parts);
                         break;
                     case "pkgpss":
-                        checkPkgPss(parts, version);
+                        checkPkgPss(parts, checkAvg);
                         break;
                     case "pkgsvc-bound":
                     case "pkgsvc-exec":
                     case "pkgsvc-run":
                     case "pkgsvc-start":
-                        checkPkgSvc(parts, version);
+                        checkPkgSvc(parts);
                         break;
                     case "pkgkills":
-                        checkPkgKills(parts, version);
+                        checkPkgKills(parts, checkAvg);
                         break;
                     case "proc":
                         checkProc(parts);
                         break;
                     case "pss":
-                        checkPss(parts);
+                        checkPss(parts, checkAvg);
                         break;
                     case "kills":
-                        checkKills(parts);
+                        checkKills(parts, checkAvg);
                         break;
                     case "total":
                         checkTotal(parts);
@@ -96,60 +124,45 @@ public class ProcessStatsDumpsysTest extends BaseDumpsysTest {
             }
         }
 
-/* more kinds
-total   0n:4426040  0m:5779 1n:6067104  1m:74489
-sysmemusage 0np1791668:791668:791668:40080:40080:40080:1176:1176:1176:147068:147068:147068:230682:230682:230682 ...
-weights 10573412    6.133572860728E12:40  ...
-availablepages  Unmovable   0   69  37  31  14  20  28  16  5   7   0   0
-availablepages  Reclaimable 0   0   0   0   1   1   1   1   0   0   1   0
-availablepages  Movable 0   1   0   1   1   1   2   0   1   1   0   173
-availablepages  Reserve 0   0   0   0   0   0   0   0   0   0   0   2
-availablepages  CMA 0   1   1   98  111 70  35  20  7   9   3   18
-availablepages  Isolate
- */
-
-        // spot check a few tags
+        assertSeenTag(seenTags, "vers");
+        assertSeenTag(seenTags, "period");
         assertSeenTag(seenTags, "pkgproc");
         assertSeenTag(seenTags, "proc");
         assertSeenTag(seenTags, "pss");
         assertSeenTag(seenTags, "total");
+        assertSeenTag(seenTags, "weights");
+        assertSeenTag(seenTags, "availablepages");
     }
 
     private void checkPeriod(String[] parts) {
         assertTrue("Expected 5 or 6, found: " + parts.length,
                 parts.length == 5 || parts.length == 6);
         assertNotNull(parts[1]); // date
-        assertInteger(parts[2]); // start time (msec)
-        assertInteger(parts[3]); // end time (msec)
+        assertNonNegativeInteger(parts[2]); // start time (msec)
+        assertNonNegativeInteger(parts[3]); // end time (msec)
+
+        // TODO Check the values.
         assertNotNull(parts[4]); // status
         if (parts.length == 6) {
             assertNotNull(parts[5]); // swapped-out-pss
         }
     }
 
-    private void checkPkgProc(String[] parts, int version) {
+    private void checkPkgProc(String[] parts) {
         int statesStartIndex;
 
-        if (version < 4) {
-            assertTrue(parts.length >= 4);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertNotNull(parts[3]); // process
-            statesStartIndex = 4;
-        } else {
-            assertTrue(parts.length >= 5);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertInteger(parts[3]); // app version
-            assertNotNull(parts[4]); // process
-            statesStartIndex = 5;
-        }
+        assertTrue(parts.length >= 5);
+        assertNotNull(parts[1]); // package name
+        assertNonNegativeInteger(parts[2]); // uid
+        assertNonNegativeInteger(parts[3]); // app version
+        assertNotNull(parts[4]); // process
+        statesStartIndex = 5;
 
         for (int i = statesStartIndex; i < parts.length; i++) {
             String[] subparts = parts[i].split(":");
             assertEquals(2, subparts.length);
             checkTag(subparts[0], true); // tag
-            assertInteger(subparts[1]); // duration (msec)
+            assertNonNegativeInteger(subparts[1]); // duration (msec)
         }
     }
 
@@ -170,147 +183,107 @@ availablepages  Isolate
 
         if (hasProcess) {
             char p = tag.charAt(2);
-            assertTrue("malformed tag: " + tag, p >= 'a' && p <= 'z');
+            assertTrue("malformed tag: " + tag, "ptfbuwsxrhlace".indexOf(p) >= 0);
         }
     }
 
-    private void checkPkgPss(String[] parts, int version) {
+    private void checkPkgPss(String[] parts, boolean checkAvg) {
         int statesStartIndex;
 
-        if (version < 4) {
-            assertTrue(parts.length >= 4);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertNotNull(parts[3]); // process
-            statesStartIndex = 4;
-        } else {
-            assertTrue(parts.length >= 5);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertInteger(parts[3]); // app version
-            assertNotNull(parts[4]); // process
-            statesStartIndex = 5;
-        }
+        assertTrue(parts.length >= 5);
+        assertNotNull(parts[1]); // package name
+        assertNonNegativeInteger(parts[2]); // uid
+        assertNonNegativeInteger(parts[3]); // app version
+        assertNotNull(parts[4]); // process
+        statesStartIndex = 5;
 
         for (int i = statesStartIndex; i < parts.length; i++) {
             String[] subparts = parts[i].split(":");
             assertEquals(8, subparts.length);
             checkTag(subparts[0], true); // tag
-            assertInteger(subparts[1]); // sample size
-            assertInteger(subparts[2]); // pss min
-            assertInteger(subparts[3]); // pss avg
-            assertInteger(subparts[4]); // pss max
-            assertInteger(subparts[5]); // uss min
-            assertInteger(subparts[6]); // uss avg
-            assertInteger(subparts[7]); // uss max
+            assertNonNegativeInteger(subparts[1]); // sample size
+            assertMinAvgMax(subparts[2], subparts[3], subparts[4], checkAvg); // pss
+            assertMinAvgMax(subparts[5], subparts[6], subparts[7], checkAvg); // uss
         }
     }
 
-    private void checkPkgSvc(String[] parts, int version) {
+    private void checkPkgSvc(String[] parts) {
         int statesStartIndex;
 
-        if (version < 4) {
-            assertTrue(parts.length >= 5);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertNotNull(parts[3]); // service name
-            assertInteger(parts[4]); // count
-            statesStartIndex = 5;
-        } else {
-            assertTrue(parts.length >= 6);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertInteger(parts[3]); // app version
-            assertNotNull(parts[4]); // service name
-            assertInteger(parts[5]); // count
-            statesStartIndex = 6;
-        }
+        assertTrue(parts.length >= 6);
+        assertNotNull(parts[1]); // package name
+        assertNonNegativeInteger(parts[2]); // uid
+        assertNonNegativeInteger(parts[3]); // app version
+        assertNotNull(parts[4]); // service name
+        assertNonNegativeInteger(parts[5]); // count
+        statesStartIndex = 6;
 
         for (int i = statesStartIndex; i < parts.length; i++) {
             String[] subparts = parts[i].split(":");
             assertEquals(2, subparts.length);
             checkTag(subparts[0], false); // tag
-            assertInteger(subparts[1]); // duration (msec)
+            assertNonNegativeInteger(subparts[1]); // duration (msec)
         }
     }
 
-    private void checkPkgKills(String[] parts, int version) {
+    private void checkPkgKills(String[] parts, boolean checkAvg) {
         String pssStr;
 
-        if (version < 4) {
-            assertEquals(8, parts.length);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertNotNull(parts[3]); // process
-            assertInteger(parts[4]); // wakes
-            assertInteger(parts[5]); // cpu
-            assertInteger(parts[6]); // cached
-            pssStr = parts[7];
-        } else {
-            assertEquals(9, parts.length);
-            assertNotNull(parts[1]); // package name
-            assertInteger(parts[2]); // uid
-            assertInteger(parts[3]); // app version
-            assertNotNull(parts[4]); // process
-            assertInteger(parts[5]); // wakes
-            assertInteger(parts[6]); // cpu
-            assertInteger(parts[7]); // cached
-            pssStr = parts[8];
-        }
+        assertEquals(9, parts.length);
+        assertNotNull(parts[1]); // package name
+        assertNonNegativeInteger(parts[2]); // uid
+        assertNonNegativeInteger(parts[3]); // app version
+        assertNotNull(parts[4]); // process
+        assertNonNegativeInteger(parts[5]); // wakes
+        assertNonNegativeInteger(parts[6]); // cpu
+        assertNonNegativeInteger(parts[7]); // cached
+        pssStr = parts[8];
 
         String[] subparts = pssStr.split(":");
         assertEquals(3, subparts.length);
-        assertInteger(subparts[0]); // pss min
-        assertInteger(subparts[1]); // pss avg
-        assertInteger(subparts[2]); // pss max
+        assertMinAvgMax(subparts[0], subparts[1], subparts[2], checkAvg); // pss
     }
 
     private void checkProc(String[] parts) {
         assertTrue(parts.length >= 3);
         assertNotNull(parts[1]); // package name
-        assertInteger(parts[2]); // uid
+        assertNonNegativeInteger(parts[2]); // uid
 
         for (int i = 3; i < parts.length; i++) {
             String[] subparts = parts[i].split(":");
             assertEquals(2, subparts.length);
             checkTag(subparts[0], true); // tag
-            assertInteger(subparts[1]); // duration (msec)
+            assertNonNegativeInteger(subparts[1]); // duration (msec)
         }
     }
 
-    private void checkPss(String[] parts) {
+    private void checkPss(String[] parts, boolean checkAvg) {
         assertTrue(parts.length >= 3);
         assertNotNull(parts[1]); // package name
-        assertInteger(parts[2]); // uid
+        assertNonNegativeInteger(parts[2]); // uid
 
         for (int i = 3; i < parts.length; i++) {
             String[] subparts = parts[i].split(":");
             assertEquals(8, subparts.length);
             checkTag(subparts[0], true); // tag
-            assertInteger(subparts[1]); // sample size
-            assertInteger(subparts[2]); // pss min
-            assertInteger(subparts[3]); // pss avg
-            assertInteger(subparts[4]); // pss max
-            assertInteger(subparts[5]); // uss min
-            assertInteger(subparts[6]); // uss avg
-            assertInteger(subparts[7]); // uss max
+            assertNonNegativeInteger(subparts[1]); // sample size
+            assertMinAvgMax(subparts[2], subparts[3], subparts[4], checkAvg); // pss
+            assertMinAvgMax(subparts[5], subparts[6], subparts[7], checkAvg); // uss
         }
     }
 
-    private void checkKills(String[] parts) {
+    private void checkKills(String[] parts, boolean checkAvg) {
         assertEquals(7, parts.length);
         assertNotNull(parts[1]); // package name
-        assertInteger(parts[2]); // uid
-        assertInteger(parts[3]); // wakes
-        assertInteger(parts[4]); // cpu
-        assertInteger(parts[5]); // cached
+        assertNonNegativeInteger(parts[2]); // uid
+        assertNonNegativeInteger(parts[3]); // wakes
+        assertNonNegativeInteger(parts[4]); // cpu
+        assertNonNegativeInteger(parts[5]); // cached
         String pssStr = parts[6];
 
         String[] subparts = pssStr.split(":");
         assertEquals(3, subparts.length);
-        assertInteger(subparts[0]); // pss min
-        assertInteger(subparts[1]); // pss avg
-        assertInteger(subparts[2]); // pss max
+        assertMinAvgMax(subparts[0], subparts[1], subparts[2], checkAvg); // pss
     }
 
     private void checkTotal(String[] parts) {
@@ -319,11 +292,32 @@ availablepages  Isolate
             String[] subparts = parts[i].split(":");
             checkTag(subparts[0], false); // tag
 
-            if (subparts[1].contains("sysmemusage")) {
-                break; // see b/18340771
-            }
-            assertInteger(subparts[1]); // duration (msec)
+            assertNonNegativeInteger(subparts[1]); // duration (msec)
         }
     }
 
+    private void checkWithProcStatsApp() throws Exception {
+        getDevice().uninstallPackage(DEVICE_SIDE_TEST_PACKAGE);
+        getDevice().uninstallPackage(DEVICE_SIDE_HELPER_PACKAGE);
+        installPackage(DEVICE_SIDE_TEST_APK, /* grantPermissions= */ true);
+
+        installPackage(DEVICE_SIDE_HELPER_APK, /* grantPermissions= */ true);
+
+        final int helperAppUid = Integer.parseInt(execCommandAndGetFirstGroup(
+                "dumpsys package " + DEVICE_SIDE_HELPER_PACKAGE, "userId=(\\d+)"));
+        CLog.i("Helper app UID: " + helperAppUid);
+
+        try {
+            // Run the device side test which makes some network requests.
+            runDeviceTests(DEVICE_SIDE_TEST_PACKAGE,
+                    "com.android.server.cts.procstats.ProcStatsTest", "testLaunchApp");
+        } finally {
+            if (UNINSTALL_APPS_AFTER_TESTS) {
+                getDevice().uninstallPackage(DEVICE_SIDE_TEST_PACKAGE);
+                getDevice().uninstallPackage(DEVICE_SIDE_HELPER_PACKAGE);
+            }
+        }
+
+        // TODO Check all the lines related to the helper.
+    }
 }
