a744ec3924e221f0ac152269b48fdc2ce7b1ab11
Chris Craik <ccraik@google.com>
2017-02-15 14:58:55 -0800

    Prevent transparent region in SurfaceViewSyncTests
    Cherry-pick CTS fix from O
    
    Fixes: 35314369
    Bug: 74403491
    Test: cts-tradefed run singleCommand cts-dev --module CtsViewTestCases --test android.view.cts.SurfaceViewSyncTest
    
    Change-Id: I31d29382fd7ee071811fa7bb50cfe4972ced966a
    Merged-In: I31d29382fd7ee071811fa7bb50cfe4972ced966a

--- tests/tests/view/src/android/view/cts/SurfaceViewSyncTests.java
+++ tests/tests/view/src/android/view/cts/SurfaceViewSyncTests.java
@@ -112,10 +112,21 @@ public class SurfaceViewSyncTests {
     // ViewFactories
     ///////////////////////////////////////////////////////////////////////////
 
-    private ViewFactory sEmptySurfaceViewFactory = SurfaceView::new;
+    private ViewFactory sEmptySurfaceViewFactory = context -> {
+        SurfaceView surfaceView = new SurfaceView(context);
+
+        // prevent transparent region optimization, which is invalid for a SurfaceView moving around
+        surfaceView.setWillNotDraw(false);
+
+        return surfaceView;
+    };
 
     private ViewFactory sGreenSurfaceViewFactory = context -> {
         SurfaceView surfaceView = new SurfaceView(context);
+
+        // prevent transparent region optimization, which is invalid for a SurfaceView moving around
+        surfaceView.setWillNotDraw(false);
+
         surfaceView.getHolder().setFixedSize(640, 480);
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
@@ -136,6 +147,10 @@ public class SurfaceViewSyncTests {
 
     private ViewFactory sVideoViewFactory = context -> {
         SurfaceView surfaceView = new SurfaceView(context);
+
+        // prevent transparent region optimization, which is invalid for a SurfaceView moving around
+        surfaceView.setWillNotDraw(false);
+
         surfaceView.getHolder().setFixedSize(640, 480);
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
