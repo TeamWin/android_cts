135d71e9c3deee788c35a7c6540cd07bbd30319f
Felipe Leme <felipeal@google.com>
2017-02-14 18:19:42 -0800

    Make sure auto-fill works when FLAG_SECURE is set.
    BUG: 33802548
    BUG: 34077687
    Test: CtsAutoFillServiceTestCases passes
    
    Change-Id: Ib9f03efbe86fd25ec6de252ccd89e165f0e6c730

--- tests/autofillservice/src/android/autofillservice/cts/LoginActivity.java
+++ tests/autofillservice/src/android/autofillservice/cts/LoginActivity.java
@@ -20,7 +20,6 @@ import static android.autofillservice.cts.Helper.FILL_TIMEOUT_MS;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import android.app.Activity;
-import android.autofillservice.cts.CannedFillResponse.CannedDataset;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -28,7 +27,6 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.autofill.AutoFillValue;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -199,6 +197,16 @@ public class LoginActivity extends Activity {
         assertWithMessage("Timeout (%s ms) waiting for login", LOGIN_TIMEOUT_MS)
                 .that(called).isTrue();
         return mLoginMessage;
+    }
+
+    /**
+     * Sets the window flags.
+     */
+    void setFlags(int flags) {
+        Log.d(TAG, "setFlags():" + flags);
+        runOnUiThread(() -> {
+            getWindow().setFlags(flags, flags);
+        });
     }
 
     /**
