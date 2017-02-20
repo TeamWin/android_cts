55dd1f26a00c42bbcda9ba49cecd5e00103b35c1
Robin Lee <rgl@google.com>
2017-02-20 14:11:07 +0000

    Misc fixes to CtsVerifier keyguard test
    Test: manual, open BYOD Managed Provisioning > Keyguard, shouldn't crash.
    Bug: 35487318
    Change-Id: Idf382b9eae4e039fb82ef7dab844639c6dfe66fd

--- apps/CtsVerifier/src/com/android/cts/verifier/managedprovisioning/ByodHelperActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/managedprovisioning/ByodHelperActivity.java
@@ -173,6 +173,7 @@ public class ByodHelperActivity extends LocationListenerActivity
         final Notification notification = new Notification.Builder(this)
                 .setSmallIcon(R.drawable.icon)
                 .setContentTitle(getString(R.string.provisioning_byod_notification_title))
+                .setContentText(getString(R.string.provisioning_byod_notification_title))
                 .setVisibility(visibility)
                 .setAutoCancel(true)
                 .build();
