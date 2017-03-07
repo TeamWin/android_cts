88ba52c278c67439f0be69e1b8807b87a2e6e639
Romain Guy <romainguy@google.com>
2017-03-07 15:28:58 -0800

    Test writing Bitmap color space to parcels
    Bug: 32072280
    Test: CtsGraphicsTestCases
    Change-Id: I609b42841a5b9ea7c0288cf686d1d108dbd20449

--- tests/tests/graphics/src/android/graphics/cts/BitmapColorSpaceTest.java
+++ tests/tests/graphics/src/android/graphics/cts/BitmapColorSpaceTest.java
@@ -16,10 +16,12 @@
 
 package android.graphics.cts;
 
+import android.annotation.NonNull;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ColorSpace;
+import android.os.Parcel;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
 import android.support.test.runner.AndroidJUnit4;
@@ -99,5 +101,72 @@ public class BitmapColorSpaceTest {
         } catch (IOException e) {
             fail();
         }
+    }
+
+    @Test
+    public void reuse() {
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inMutable = true;
+
+        Bitmap bitmap1 = null;
+        try (InputStream in = mResources.getAssets().open("green-srgb.png")) {
+            bitmap1 = BitmapFactory.decodeStream(in, null, opts);
+            ColorSpace cs = bitmap1.getColorSpace();
+            assertNotNull(cs);
+            assertSame(ColorSpace.get(ColorSpace.Named.SRGB), cs);
+        } catch (IOException e) {
+            fail();
+        }
+
+        try (InputStream in = mResources.getAssets().open("green-p3.png")) {
+            opts.inBitmap = bitmap1;
+
+            Bitmap bitmap2 = BitmapFactory.decodeStream(in, null, opts);
+            assertSame(bitmap1, bitmap2);
+            ColorSpace cs = bitmap2.getColorSpace();
+            assertNotNull(cs);
+            assertSame(ColorSpace.get(ColorSpace.Named.DISPLAY_P3), cs);
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void writeColorSpace() {
+        testColorSpaceMarshalling("green-srgb.png", ColorSpace.get(ColorSpace.Named.SRGB));
+        testColorSpaceMarshalling("green-p3.png", ColorSpace.get(ColorSpace.Named.DISPLAY_P3));
+        testColorSpaceMarshalling("prophoto-rgba16f.png",
+                ColorSpace.get(ColorSpace.Named.LINEAR_EXTENDED_SRGB));
+
+        // Special case where the color space will be null in native
+        Bitmap bitmapIn = BitmapFactory.decodeResource(mResources, R.drawable.robot);
+        testParcelUnparcel(bitmapIn, ColorSpace.get(ColorSpace.Named.SRGB));
+    }
+
+    private void testColorSpaceMarshalling(
+            @NonNull String fileName, @NonNull ColorSpace colorSpace) {
+        try (InputStream in = mResources.getAssets().open(fileName)) {
+            Bitmap bitmapIn = BitmapFactory.decodeStream(in);
+            testParcelUnparcel(bitmapIn, colorSpace);
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    private void testParcelUnparcel(Bitmap bitmapIn, ColorSpace expected) {
+        ColorSpace cs = bitmapIn.getColorSpace();
+        assertNotNull(cs);
+        assertSame(expected, cs);
+
+        Parcel p = Parcel.obtain();
+        bitmapIn.writeToParcel(p, 0);
+        p.setDataPosition(0);
+
+        Bitmap bitmapOut = Bitmap.CREATOR.createFromParcel(p);
+        cs = bitmapOut.getColorSpace();
+        assertNotNull(cs);
+        assertSame(expected, cs);
+
+        p.recycle();
     }
 }
