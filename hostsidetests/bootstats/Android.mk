c4803918ac86bb29cbf7bd5b11cf43397c2a7b27
Chris Wren <cwren@android.com>
2017-03-15 14:59:33 -0400

    port bootstats CTS to v2 format
    Also clean up the test to make reusable parts more obvious,
    and to eliminate magic constants where possible.
    
    Bug: 36232290
    Test: cts-tradefed run cts-dev --module CtsBootStatsTestCases
    Change-Id: I49d14540bef731089cc88f79949bee77f6952f6c

--- hostsidetests/bootstats/Android.mk
+++ hostsidetests/bootstats/Android.mk
@@ -20,8 +20,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE := CtsBootStatsTestCases
-
-LOCAL_JAVA_LIBRARIES := cts-tradefed tradefed compatibility-host-util
+LOCAL_STATIC_JAVA_LIBRARIES := host-framework-protos
+LOCAL_JAVA_LIBRARIES := cts-tradefed tradefed compatibility-host-util host-libprotobuf-java-full
 
 LOCAL_CTS_TEST_PACKAGE := android.bootstats
 
