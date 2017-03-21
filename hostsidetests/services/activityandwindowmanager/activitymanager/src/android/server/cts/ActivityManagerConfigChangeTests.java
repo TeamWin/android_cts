df22aa46d748511e8d1420cb95d298dc853f1d2a
Andrii Kulian <akulian@google.com>
2017-03-20 18:26:57 -0700

    Test updating application info
    Test that updating app info when application is running
    restarts an activity with matching package name and
    increments applied asset sequence number.
    
    Bug: 35911519
    Test: android.server.cts.ActivityManagerConfigChangeTests
    Test: #testUpdateApplicationInfo
    Change-Id: Ie3951ca029e42c88b8027fd7269e29758a6e8d32

--- hostsidetests/services/activityandwindowmanager/activitymanager/src/android/server/cts/ActivityManagerConfigChangeTests.java
+++ hostsidetests/services/activityandwindowmanager/activitymanager/src/android/server/cts/ActivityManagerConfigChangeTests.java
@@ -16,6 +16,11 @@
 
 package android.server.cts;
 
+import static android.server.cts.ActivityManagerState.STATE_RESUMED;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Build: mmma -j32 cts/hostsidetests/services
  * Run: cts/hostsidetests/services/activityandwindowmanager/util/run-test CtsServicesHostTestCases android.server.cts.ActivityManagerConfigChangeTests
@@ -89,5 +94,57 @@ public class ActivityManagerConfigChangeTests extends ActivityManagerTestBase {
             mAmWmState.computeState(mDevice, waitForActivitiesVisible);
             assertRelaunchOrConfigChanged(activityName, relaunch ? 1 : 0, relaunch ? 0 : 1);
         }
+    }
+
+    /**
+     * Test updating application info when app is running. An activity with matching package name
+     * must be recreated and its asset sequence number must be incremented.
+     */
+    public void testUpdateApplicationInfo() throws Exception {
+        clearLogcat();
+
+        // Launch an activity that prints applied config.
+        launchActivity(TEST_ACTIVITY_NAME);
+        final int assetSeq = readAssetSeqNumber(TEST_ACTIVITY_NAME);
+        clearLogcat();
+
+        // Update package info.
+        executeShellCommand("am update-appinfo all " + componentName);
+        mAmWmState.waitForWithAmState(mDevice, (amState) -> {
+            // Wait for activity to be resumed and asset seq number to be updated.
+            try {
+                return readAssetSeqNumber(TEST_ACTIVITY_NAME) == assetSeq + 1
+                        && amState.hasActivityState(TEST_ACTIVITY_NAME, STATE_RESUMED);
+            } catch (Exception e) {
+                return false;
+            }
+        }, "Waiting asset sequence number to be updated and for activity to be resumed.");
+
+        // Check if activity is relaunched and asset seq is updated.
+        assertRelaunchOrConfigChanged(TEST_ACTIVITY_NAME, 1 /* numRelaunch */,
+                0 /* numConfigChange */);
+        final int newAssetSeq = readAssetSeqNumber(TEST_ACTIVITY_NAME);
+        assertEquals("Asset sequence number must be incremented.", assetSeq + 1, newAssetSeq);
+    }
+
+    private static final Pattern sConfigurationPattern = Pattern.compile(
+            "(.+): Configuration: \\{(.*) as.(\\d+)(.*)\\}");
+
+    /** Read asset sequence number in last applied configuration from logs. */
+    private int readAssetSeqNumber(String activityName) throws Exception {
+        final String[] lines = getDeviceLogsForComponent(activityName);
+        for (int i = lines.length - 1; i >= 0; i--) {
+            final String line = lines[i].trim();
+            final Matcher matcher = sConfigurationPattern.matcher(line);
+            if (matcher.matches()) {
+                final String assetSeqNumber = matcher.group(3);
+                try {
+                    return Integer.valueOf(assetSeqNumber);
+                } catch (NumberFormatException e) {
+                    // Ignore, asset seq number is not printed when not set.
+                }
+            }
+        }
+        return 0;
     }
 }
