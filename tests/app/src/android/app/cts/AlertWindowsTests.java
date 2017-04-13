d11574de4b42422e6b009a324e6909d079264095
Makoto Onuki <omakoto@google.com>
2017-04-13 14:58:06 -0700

    Fix the wrong IMPORTANCE_ values.
    Bug: 36697597
    Test: cts-tradefed run cts-dev --skip-device-info --skip-preconditions --skip-system-status-check com.android.compatibility.common.tradefed.targetprep.NetworkConnectivityChecker -a armeabi-v7a -l INFO -m CtsAppTestCases -t android.app.cts.AlertWindowsTests
    Change-Id: I15c5e9ff9e4b9e335914753e0ec599e875c61542

--- tests/app/src/android/app/cts/AlertWindowsTests.java
+++ tests/app/src/android/app/cts/AlertWindowsTests.java
@@ -17,40 +17,47 @@
 package android.app.cts;
 
 import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_PERCEPTIBLE;
+import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_PERCEPTIBLE_DEPRECATED;
 import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_VISIBLE;
 import static android.content.Context.BIND_ALLOW_OOM_MANAGEMENT;
 import static android.content.Context.BIND_AUTO_CREATE;
 import static android.content.Context.BIND_NOT_FOREGROUND;
+
 import static com.android.app2.AlertWindowService.MSG_ADD_ALERT_WINDOW;
 import static com.android.app2.AlertWindowService.MSG_ON_ALERT_WINDOW_ADDED;
 import static com.android.app2.AlertWindowService.MSG_ON_ALERT_WINDOW_REMOVED;
 import static com.android.app2.AlertWindowService.MSG_REMOVE_ALERT_WINDOW;
 import static com.android.app2.AlertWindowService.MSG_REMOVE_ALL_ALERT_WINDOWS;
 import static com.android.app2.AlertWindowService.NOTIFICATION_MESSENGER_EXTRA;
+
 import static org.junit.Assert.assertEquals;
-
-import android.platform.test.annotations.Presubmit;
-import com.android.app2.AlertWindowService;
-import com.android.compatibility.common.util.SystemUtil;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.junit.Test;
 
 import android.app.ActivityManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
+import android.platform.test.annotations.Presubmit;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+
+import com.android.app2.AlertWindowService;
+import com.android.compatibility.common.util.SystemUtil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.function.Function;
 
 /**
  * Build: mmma -j32 cts/tests/app
@@ -61,12 +68,20 @@ import android.util.Log;
 public class AlertWindowsTests {
 
     private static final String TAG = "AlertWindowsTests";
+
     private static final boolean DEBUG = false;
     private static final long WAIT_TIME_MS = 2 * 1000;
 
+    private static final String SDK25_PACKAGE_NAME = "com.android.appSdk25";
+
     private Messenger mService;
     private String mServicePackageName;
+    private int mServiceUid;
+
+    private PackageManager mPm;
+
     private ActivityManager mAm;
+    private ActivityManager mAm25; // ActivityManager created for an SDK 25 app context.
 
     private final Messenger mMessenger = new Messenger(new IncomingHandler(Looper.getMainLooper()));
     private final Object mAddedLock = new Object();
@@ -76,7 +91,12 @@ public class AlertWindowsTests {
     public void setUp() throws Exception {
         if (DEBUG) Log.e(TAG, "setUp");
         final Context context = InstrumentationRegistry.getTargetContext();
+
+        mPm = context.getPackageManager();
+
         mAm = context.getSystemService(ActivityManager.class);
+        mAm25 = context.createPackageContext(SDK25_PACKAGE_NAME, 0)
+                .getSystemService(ActivityManager.class);
 
         final Intent intent = new Intent();
         intent.setClassName(AlertWindowService.class.getPackage().getName(),
@@ -107,27 +127,34 @@ public class AlertWindowsTests {
     public void testAlertWindowOomAdj() throws Exception {
         setAlertWindowPermission(true /* allow */);
 
-        assertImportance(IMPORTANCE_PERCEPTIBLE);
+        assertPackageImportance(IMPORTANCE_PERCEPTIBLE, IMPORTANCE_PERCEPTIBLE_DEPRECATED);
+        assertUidImportance(IMPORTANCE_PERCEPTIBLE, IMPORTANCE_PERCEPTIBLE_DEPRECATED);
+
         addAlertWindow();
         // Process importance should be increased to visible when the service has an alert window.
-        assertImportance(IMPORTANCE_VISIBLE);
+        assertPackageImportance(IMPORTANCE_VISIBLE, IMPORTANCE_VISIBLE);
+
+        // TODO: Somehow getUidImportance still returns 230 (IMPORTANCE_PERCEPTIBLE) instead of
+        // IMPORTANCE_VISIBLE(200)
+        // assertUidImportance(IMPORTANCE_VISIBLE, IMPORTANCE_VISIBLE);
+
         addAlertWindow();
-        assertImportance(IMPORTANCE_VISIBLE);
+        assertPackageImportance(IMPORTANCE_VISIBLE, IMPORTANCE_VISIBLE);
 
         setAlertWindowPermission(false /* allow */);
         // Process importance should no longer be visible since its alert windows are not allowed to
         // be visible.
-        assertImportance(IMPORTANCE_PERCEPTIBLE);
+        assertPackageImportance(IMPORTANCE_PERCEPTIBLE, IMPORTANCE_PERCEPTIBLE_DEPRECATED);
         setAlertWindowPermission(true /* allow */);
         // They can show again so importance should be visible again.
-        assertImportance(IMPORTANCE_VISIBLE);
+        assertPackageImportance(IMPORTANCE_VISIBLE, IMPORTANCE_VISIBLE);
 
         removeAlertWindow();
-        assertImportance(IMPORTANCE_VISIBLE);
+        assertPackageImportance(IMPORTANCE_VISIBLE, IMPORTANCE_VISIBLE);
         removeAlertWindow();
         // Process importance should no longer be visible when the service no longer as alert
         // windows.
-        assertImportance(IMPORTANCE_PERCEPTIBLE);
+        assertPackageImportance(IMPORTANCE_PERCEPTIBLE, IMPORTANCE_PERCEPTIBLE_DEPRECATED);
     }
 
     private void addAlertWindow() throws Exception {
@@ -152,7 +179,8 @@ public class AlertWindowsTests {
         SystemUtil.runShellCommand(InstrumentationRegistry.getInstrumentation(), cmd);
     }
 
-    private void assertImportance(int expected) throws Exception {
+    private void assertImportance(Function<ActivityManager, Integer> apiCaller,
+            int expectedForO, int expectedForPreO) throws Exception {
         int retry = 3;
         int actual;
 
@@ -161,10 +189,29 @@ public class AlertWindowsTests {
             // for changes in the uid importance. However, the way it is currently structured
             // doesn't really work for this use case right now...
             Thread.sleep(500);
-            actual = mAm.getPackageImportance(mServicePackageName);
-        } while (actual != expected && --retry > 0);
+            actual = apiCaller.apply(mAm);
+        } while (actual != expectedForO && --retry > 0);
 
-        assertEquals(expected, actual);
+        assertEquals(expectedForO, actual);
+
+        // Check the result for pre-O apps.
+        assertEquals(expectedForPreO, (int) apiCaller.apply(mAm25));
+    }
+
+    /**
+     * Make sure {@link ActivityManager#getPackageImportance} returns the expected value.
+     */
+    private void assertPackageImportance(int expectedForO, int expectedForPreO) throws Exception {
+        assertImportance(am -> am.getPackageImportance(mServicePackageName),
+                expectedForO, expectedForPreO);
+    }
+
+    /**
+     * Make sure {@link ActivityManager#getUidImportance(int)} returns the expected value.
+     */
+    private void assertUidImportance(int expectedForO, int expectedForPreO) throws Exception {
+        assertImportance(am -> am.getUidImportance(mServiceUid),
+                expectedForO, expectedForPreO);
     }
 
     private final ServiceConnection mConnection = new ServiceConnection() {
@@ -173,6 +220,11 @@ public class AlertWindowsTests {
             if (DEBUG) Log.e(TAG, "onServiceConnected");
             mService = new Messenger(service);
             mServicePackageName = name.getPackageName();
+            try {
+                mServiceUid = mPm.getPackageUid(mServicePackageName, 0);
+            } catch (NameNotFoundException e) {
+                throw new RuntimeException("getPackageUid() failed.", e);
+            }
             synchronized (mConnection) {
                 notifyAll();
             }
@@ -183,6 +235,7 @@ public class AlertWindowsTests {
             if (DEBUG) Log.e(TAG, "onServiceDisconnected");
             mService = null;
             mServicePackageName = null;
+            mServiceUid = 0;
         }
     };
 
