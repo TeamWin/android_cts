4835757dc358a66d430be78e0818bd278e6e68b6
Wei Jia <wjia@google.com>
2017-04-25 11:01:22 -0700

    DO NOT MERGE: StagefrightTest: add testStagefright_bug_21443020.
    Bug: 37225828
    Change-Id: Iabedd7d465d61fbd54cbd820d36b5fe8657615a8
    (cherry picked from commit 07ffed936278fa924b1b5e8a1cbc2e99102e744b)
    (cherry picked from commit c204bfaf2762dcf798a41c9e99cd704b2e4cca97)

--- tests/tests/security/src/android/security/cts/StagefrightTest.java
+++ tests/tests/security/src/android/security/cts/StagefrightTest.java
@@ -71,6 +71,10 @@ public class StagefrightTest extends InstrumentationTestCase {
      before any existing test methods
      ***********************************************************/
 
+    public void testStagefright_bug_21443020() throws Exception {
+        doStagefrightTest(R.raw.bug_21443020_webm);
+    }
+
     public void testStagefright_bug_34360591() throws Exception {
         doStagefrightTest(R.raw.bug_34360591);
     }
