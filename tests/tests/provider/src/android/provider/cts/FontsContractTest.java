d241df7eb93d991a36b891af8aef587b31b75b52
Clara Bayarri <clarabayarri@google.com>
2017-04-25 16:27:30 +0100

    Tests for FontsContract#requestFont
    Plus additional tests for FontsContract, based on the existing
    tests in supportlib.
    
    Improves coverage also for
    FontsContract.FontRequestCallback#onTypefaceRetrieved
    FontsContract.FontRequestCallback#onTypefaceRequestFailed
    FontsContract.FontInfo#getAxes
    FontsContract.FontInfo#getWeight
    FontsContract.FontInfo#isItalic
    
    Pending is FontsContract.FontInfo#getTtcIndex as we need a
    test .ttc file
    
    Test: bit CtsProviderTestCases:android.provider.cts.FontsContractTest
    Bug: 37656717
    Change-Id: Iae129957c2e0d2f595ee1baab267d8ebf638bfc5

--- tests/tests/provider/src/android/provider/cts/FontsContractTest.java
+++ tests/tests/provider/src/android/provider/cts/FontsContractTest.java
@@ -17,43 +17,44 @@
 package android.provider.cts;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 import android.app.Instrumentation;
-import android.content.pm.Signature;
+import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.PackageInfo;
-import android.content.Context;
+import android.content.pm.Signature;
 import android.graphics.Typeface;
-import android.graphics.fonts.FontVariationAxis;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.FontRequest;
 import android.provider.FontsContract;
+import android.provider.FontsContract.Columns;
 import android.provider.FontsContract.FontFamilyResult;
 import android.provider.FontsContract.FontInfo;
-import android.provider.FontsContract.Columns;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
 import android.support.test.runner.AndroidJUnit4;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+
+import com.android.internal.annotations.GuardedBy;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public class FontsContractTest {
     private static final String AUTHORITY = "android.provider.fonts.cts.font";
     private static final String PACKAGE = "android.provider.cts";
-
-    private static long TIMEOUT_MILLIS = 1000;
 
     // Signature to be used for authentication to access content provider.
     // In this test case, the content provider and consumer live in the same package, self package's
@@ -76,12 +77,115 @@ public class FontsContractTest {
         }
     }
 
+    private Instrumentation mInstrumentation;
+    private Context mContext;
+    private Handler mMainThreadHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        mMainThreadHandler = new Handler(Looper.getMainLooper());
+        mInstrumentation = InstrumentationRegistry.getInstrumentation();
+        mContext = mInstrumentation.getTargetContext();
+        MockFontProvider.prepareFontFiles(
+                InstrumentationRegistry.getInstrumentation().getTargetContext());
+    }
+
+    @After
+    public void tearDown() {
+        mMainThreadHandler = null;
+        MockFontProvider.cleanUpFontFiles(
+                InstrumentationRegistry.getInstrumentation().getTargetContext());
+    }
+
+    private static class TestCallback extends FontsContract.FontRequestCallback {
+        private final Object mLock = new Object();
+        @GuardedBy("mLock")
+        private Typeface mTypeface;
+        @GuardedBy("mLock")
+        private int mFailedReason;
+        @GuardedBy("mLock")
+        private int mSuccessCallCount;
+        @GuardedBy("mLock")
+        private int mFailedCallCount;
+
+        public void onTypefaceRetrieved(Typeface typeface) {
+            synchronized(mLock) {
+                mTypeface = typeface;
+                mSuccessCallCount++;
+            }
+        }
+
+        public void onTypefaceRequestFailed(int reason) {
+            synchronized(mLock) {
+                mFailedCallCount++;
+                mFailedReason = reason;
+            }
+        }
+
+        public Typeface getTypeface() {
+            synchronized(mLock) {
+                return mTypeface;
+            }
+        }
+
+        public int getFailedReason() {
+            synchronized(mLock) {
+                return mFailedReason;
+            }
+        }
+
+        public int getSuccessCallCount() {
+            synchronized(mLock) {
+                return mSuccessCallCount;
+            }
+        }
+
+        public int getFailedCallCount() {
+            synchronized(mLock) {
+                return mFailedCallCount;
+            }
+        }
+    }
+
+
+    @Test
+    public void requestFont() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.SINGLE_FONT_FAMILY_QUERY, SIGNATURE);
+        TestCallback callback = new TestCallback();
+
+        mInstrumentation.runOnMainSync(() -> FontsContract.requestFonts(
+                mContext, request, mMainThreadHandler, null, callback));
+
+        mInstrumentation.waitForIdleSync();
+        assertEquals(1, callback.getSuccessCallCount());
+        assertEquals(0, callback.getFailedCallCount());
+        assertNotNull(callback.mTypeface);
+    }
+
+    @Test
+    public void requestFontNegativeErrorCode() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.NEGATIVE_ERROR_CODE_QUERY, SIGNATURE);
+        TestCallback callback = new TestCallback();
+
+        mInstrumentation.runOnMainSync(() -> FontsContract.requestFonts(
+                mContext, request, mMainThreadHandler, null, callback));
+
+        mInstrumentation.waitForIdleSync();
+        assertNull(callback.mTypeface);
+        assertEquals(1, callback.getFailedCallCount());
+        assertEquals(0, callback.getSuccessCallCount());
+        assertEquals(FontsContract.FontRequestCallback.FAIL_REASON_FONT_LOAD_ERROR,
+                callback.getFailedReason());
+    }
+
     @Test
     public void querySingleFont() throws NameNotFoundException {
-        Context ctx = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        FontRequest request = new FontRequest(AUTHORITY, PACKAGE, "singleFontFamily", SIGNATURE);
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.SINGLE_FONT_FAMILY_QUERY, SIGNATURE);
         FontFamilyResult result = FontsContract.fetchFonts(
-                ctx, null /* cancellation signal */, request);
+                mContext, null /* cancellation signal */, request);
         assertNotNull(result);
         assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
 
@@ -90,17 +194,15 @@ public class FontsContractTest {
         FontInfo font = fonts[0];
         assertNotNull(font.getUri());
         assertEquals(Columns.RESULT_CODE_OK, font.getResultCode());
-        // TODO: add more test cases for FontInfo members once the MockFontProvider becomes
-        // configurable.
-        assertNotNull(FontsContract.buildTypeface(ctx, null /* cancellation signal */, fonts));
+        assertNotNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
     }
 
     @Test
     public void queryMultipleFont() throws NameNotFoundException {
-        Context ctx = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        FontRequest request = new FontRequest(AUTHORITY, PACKAGE, "multipleFontFamily", SIGNATURE);
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.MULTIPLE_FAMILY_QUERY, SIGNATURE);
         FontFamilyResult result = FontsContract.fetchFonts(
-                ctx, null /* cancellation signal */, request);
+                mContext, null /* cancellation signal */, request);
         assertNotNull(result);
         assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
 
@@ -110,17 +212,122 @@ public class FontsContractTest {
             assertNotNull(font.getUri());
             assertEquals(Columns.RESULT_CODE_OK, font.getResultCode());
         }
-        // TODO: add more test cases for FontInfo members once the MockFontProvider becomes
-        // configuarable.
-        assertNotNull(FontsContract.buildTypeface(ctx, null /* cancellation signal */, fonts));
+        assertNotNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
+    }
+
+    @Test
+    public void queryAttributes() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.SINGLE_FONT_FAMILY2_QUERY, SIGNATURE);
+        FontFamilyResult result = FontsContract.fetchFonts(
+                mContext, null /* cancellation signal */, request);
+        assertNotNull(result);
+        assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
+
+        FontInfo[] fonts = result.getFonts();
+        assertEquals(1, fonts.length);
+        FontInfo font = fonts[0];
+        assertNotNull(font.getUri());
+        assertEquals(700, font.getWeight());
+        assertEquals(1, font.getAxes().length);
+        assertTrue(font.isItalic());
+        assertEquals(Columns.RESULT_CODE_OK, font.getResultCode());
+        assertNotNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
+    }
+
+    @Test
+    public void queryNotFound() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.NOT_FOUND_QUERY, SIGNATURE);
+        FontFamilyResult result = FontsContract.fetchFonts(
+                mContext, null /* cancellation signal */, request);
+        assertNotNull(result);
+        assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
+
+        FontInfo[] fonts = result.getFonts();
+        assertEquals(1, fonts.length);
+        FontInfo font = fonts[0];
+        assertEquals(Columns.RESULT_CODE_FONT_NOT_FOUND, font.getResultCode());
+        assertNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
+    }
+
+    @Test
+    public void queryUnavailable() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.UNAVAILABLE_QUERY, SIGNATURE);
+        FontFamilyResult result = FontsContract.fetchFonts(
+                mContext, null /* cancellation signal */, request);
+        assertNotNull(result);
+        assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
+
+        FontInfo[] fonts = result.getFonts();
+        assertEquals(1, fonts.length);
+        FontInfo font = fonts[0];
+        assertEquals(Columns.RESULT_CODE_FONT_UNAVAILABLE, font.getResultCode());
+        assertNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
+    }
+
+    @Test
+    public void queryMalformed() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.MALFORMED_QUERY, SIGNATURE);
+        FontFamilyResult result = FontsContract.fetchFonts(
+                mContext, null /* cancellation signal */, request);
+        assertNotNull(result);
+        assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
+
+        FontInfo[] fonts = result.getFonts();
+        assertEquals(1, fonts.length);
+        FontInfo font = fonts[0];
+        assertEquals(Columns.RESULT_CODE_MALFORMED_QUERY, font.getResultCode());
+        assertNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
+    }
+
+    @Test
+    public void queryMultipleOneNotFound() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.NOT_FOUND_SECOND_QUERY, SIGNATURE);
+        FontFamilyResult result = FontsContract.fetchFonts(
+                mContext, null /* cancellation signal */, request);
+        assertNotNull(result);
+        assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
+
+        FontInfo[] fonts = result.getFonts();
+        assertEquals(2, fonts.length);
+        FontInfo font = fonts[0];
+        assertEquals(Columns.RESULT_CODE_OK, font.getResultCode());
+        FontInfo font2 = fonts[1];
+        assertEquals(Columns.RESULT_CODE_FONT_NOT_FOUND, font2.getResultCode());
+        assertNotNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
+    }
+
+    @Test
+    public void queryMandatoryFieldsOnly() throws NameNotFoundException {
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.MANDATORY_FIELDS_ONLY_QUERY, SIGNATURE);
+        FontFamilyResult result = FontsContract.fetchFonts(
+                mContext, null /* cancellation signal */, request);
+        assertNotNull(result);
+        assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
+
+        FontInfo[] fonts = result.getFonts();
+        assertEquals(1, fonts.length);
+        FontInfo font = fonts[0];
+        assertNotNull(font.getUri());
+        assertEquals(400, font.getWeight());
+        assertNull(font.getAxes());
+        assertFalse(font.isItalic());
+        assertEquals(Columns.RESULT_CODE_OK, font.getResultCode());
+        assertNotNull(FontsContract.buildTypeface(mContext, null /* cancellation signal */, fonts));
     }
 
     @Test
     public void restrictContextRejection() throws NameNotFoundException {
-        Context ctx = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        Context restrictedContext = ctx.createPackageContext(PACKAGE, Context.CONTEXT_RESTRICTED);
+        Context restrictedContext = mContext.createPackageContext(
+                PACKAGE, Context.CONTEXT_RESTRICTED);
 
-        FontRequest request = new FontRequest(AUTHORITY, PACKAGE, "singleFontFamily", SIGNATURE);
+        FontRequest request = new FontRequest(AUTHORITY, PACKAGE,
+                MockFontProvider.SINGLE_FONT_FAMILY_QUERY, SIGNATURE);
 
         // Rejected if restricted context is used.
         FontFamilyResult result = FontsContract.fetchFonts(
@@ -128,11 +335,9 @@ public class FontsContractTest {
         assertEquals(FontFamilyResult.STATUS_REJECTED, result.getStatusCode());
 
         // Even if you have a result, buildTypeface should fail with restricted context.
-        result = FontsContract.fetchFonts(ctx, null /* cancellation signal */, request);
+        result = FontsContract.fetchFonts(mContext, null /* cancellation signal */, request);
         assertEquals(FontFamilyResult.STATUS_OK, result.getStatusCode());
         assertNull(FontsContract.buildTypeface(
                 restrictedContext, null /* cancellation signal */, result.getFonts()));
     }
-
-    // TODO: Add more test case.
 }
