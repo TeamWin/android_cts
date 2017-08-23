7414d0ac41596d81d4700be406df58b0b5053947
Aaron Holden <aaronholden@google.com>
2017-08-23 15:03:30 -0700

    DO NOT MERGE Improve DynamicConfig file copying
    Copied from ag/2757817
    - Generate one host config file per DynamicConfigPusher, and store it in the build
    - Iterate through BuildInfo for every shard when copying host config files to the result
    - Write host config file directly to /tmp/, rather than creating a persisting tmp directory first
    
    bug: 64606432
    Test: run cts-dev -m CtsMediaTestCases
    Change-Id: Ifc42d47e940404c79e990d541ec5c32e540f4454

--- common/host-side/tradefed/src/com/android/compatibility/common/tradefed/result/ResultReporter.java
+++ common/host-side/tradefed/src/com/android/compatibility/common/tradefed/result/ResultReporter.java
@@ -483,7 +483,7 @@ public class ResultReporter implements ILogSaverListener, ITestInvocationListene
         long startTime = mResult.getStartTime();
         try {
             // Zip the full test results directory.
-            copyDynamicConfigFiles(mBuildHelper.getDynamicConfigFiles(), mResultDir);
+            copyDynamicConfigFiles();
             copyFormattingFiles(mResultDir, mBuildHelper.getSuiteName());
 
             File resultFile = ResultHandler.writeResults(mBuildHelper.getSuiteName(),
@@ -702,18 +702,32 @@ public class ResultReporter implements ILogSaverListener, ITestInvocationListene
      * @param configFiles
      * @param resultsDir
      */
-    static void copyDynamicConfigFiles(Map<String, File> configFiles, File resultsDir) {
-        if (configFiles.size() == 0) return;
+    private void copyDynamicConfigFiles() {
+        File configDir = new File(mResultDir, "config");
+        if (!configDir.mkdir()) {
+            warn("Failed to make dynamic config directory \"%s\" in the result",
+                    configDir.getAbsolutePath());
+        }
 
-        File folder = new File(resultsDir, "config");
-        folder.mkdir();
-        for (String moduleName : configFiles.keySet()) {
-            File resultFile = new File(folder, moduleName+".dynamic");
-            try {
-                FileUtil.copyFile(configFiles.get(moduleName), resultFile);
-                FileUtil.deleteFile(configFiles.get(moduleName));
-            } catch (IOException e) {
-                warn("Failed to copy config file for %s to file", moduleName);
+        Set<String> uniqueModules = new HashSet<>();
+        for (IBuildInfo buildInfo : mMasterBuildInfos) {
+            CompatibilityBuildHelper helper = new CompatibilityBuildHelper(buildInfo);
+            Map<String, File> dcFiles = helper.getDynamicConfigFiles();
+            for (String moduleName : dcFiles.keySet()) {
+                File srcFile = dcFiles.get(moduleName);
+                if (!uniqueModules.contains(moduleName)) {
+                    // have not seen config for this module yet, copy into result
+                    File destFile = new File(configDir, moduleName + ".dynamic");
+                    try {
+                        FileUtil.copyFile(srcFile, destFile);
+                        uniqueModules.add(moduleName); // Add to uniqueModules if copy succeeds
+                    } catch (IOException e) {
+                        warn("Failure when copying config file \"%s\" to \"%s\" for module %s",
+                                srcFile.getAbsolutePath(), destFile.getAbsolutePath(), moduleName);
+                        CLog.e(e);
+                    }
+                }
+                FileUtil.deleteFile(srcFile);
             }
         }
     }
--- common/host-side/util/tests/src/com/android/compatibility/common/util/DynamicConfigHandlerTest.java
+++ common/host-side/util/tests/src/com/android/compatibility/common/util/DynamicConfigHandlerTest.java
@@ -16,6 +16,9 @@
 
 package com.android.compatibility.common.util;
 
+import com.android.tradefed.util.FileUtil;
+import com.android.tradefed.util.StreamUtil;
+
 import junit.framework.TestCase;
 
 import java.io.File;
@@ -92,26 +95,31 @@ public class DynamicConfigHandlerTest extends TestCase {
     public void testDynamicConfigHandler() throws Exception {
         String module = "test1";
         File localConfigFile = createFileFromStr(localConfig, module);
+        File mergedFile = null;
+        try {
+            mergedFile = DynamicConfigHandler
+                    .getMergedDynamicConfigFile(localConfigFile, overrideJson, module);
 
-        File mergedFile = DynamicConfigHandler
-                .getMergedDynamicConfigFile(localConfigFile, overrideJson, module);
+            Map<String, List<String>> configMap = DynamicConfig.createConfigMap(mergedFile);
 
-        Map<String, List<String>> configMap = DynamicConfig.createConfigMap(mergedFile);
+            assertEquals("override-config-val-1", configMap.get("override-config-1").get(0));
+            assertTrue(configMap.get("override-config-list-1")
+                    .contains("override-config-list-val-1-1"));
+            assertTrue(configMap.get("override-config-list-1")
+                    .contains("override-config-list-val-1-2"));
+            assertTrue(configMap.get("override-config-list-3").size() == 0);
 
-        assertEquals("override-config-val-1", configMap.get("override-config-1").get(0));
-        assertTrue(configMap.get("override-config-list-1")
-                .contains("override-config-list-val-1-1"));
-        assertTrue(configMap.get("override-config-list-1")
-                .contains("override-config-list-val-1-2"));
-        assertTrue(configMap.get("override-config-list-3").size() == 0);
+            assertEquals("test config 1", configMap.get("test-config-1").get(0));
+            assertTrue(configMap.get("config-list").contains("config2"));
 
-        assertEquals("test config 1", configMap.get("test-config-1").get(0));
-        assertTrue(configMap.get("config-list").contains("config2"));
-
-        assertEquals("override-config-val-2", configMap.get("override-config-2").get(0));
-        assertEquals(1, configMap.get("override-config-list-2").size());
-        assertTrue(configMap.get("override-config-list-2")
-                .contains("override-config-list-val-2-1"));
+            assertEquals("override-config-val-2", configMap.get("override-config-2").get(0));
+            assertEquals(1, configMap.get("override-config-list-2").size());
+            assertTrue(configMap.get("override-config-list-2")
+                    .contains("override-config-list-val-2-1"));
+        } finally {
+            FileUtil.deleteFile(localConfigFile);
+            FileUtil.recursiveDelete(mergedFile);
+        }
     }
 
 
@@ -123,9 +131,7 @@ public class DynamicConfigHandlerTest extends TestCase {
             stream.write(configStr.getBytes());
             stream.flush();
         } finally {
-            if (stream != null) {
-                stream.close();
-            }
+            StreamUtil.close(stream);
         }
         return file;
     }
