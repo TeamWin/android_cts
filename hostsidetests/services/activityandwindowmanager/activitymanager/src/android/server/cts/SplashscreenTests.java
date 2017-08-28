6f102400da05e6da4af8a4be71e6261d3797e158
Keith Craigie <kcraigie@google.com>
2017-08-28 12:02:56 -0700

    CTS: Fixed SplashscreenTests for circular displays
    SplashscreenTests doesn't account for circular displays and so always
    fails on round watches. Fixed test to accommodate non-rectangular displays.
    
    Bug: 65025979
    Test: cts-tradefed run singleCommand cts-dev -m CtsServicesHostTestCases
    --test android.server.cts.SplashscreenTests#testSplashscreenContent -l
    DEBUG -s TKQ7N17415000138 -a armeabi-v7a
    Test: Also visually verified only the relevant pixels were being examined
    Change-Id: I5ea0457a21179815fcc8e93713d93945ad689ad0

--- hostsidetests/services/activityandwindowmanager/activitymanager/src/android/server/cts/SplashscreenTests.java
+++ hostsidetests/services/activityandwindowmanager/activitymanager/src/android/server/cts/SplashscreenTests.java
@@ -31,30 +31,49 @@ public class SplashscreenTests extends ActivityManagerTestBase {
         mAmWmState.waitForAppTransitionIdle(mDevice);
         mAmWmState.getWmState().getStableBounds();
         final BufferedImage image = takeScreenshot();
-        assertAllColor(image, mAmWmState.getWmState().getStableBounds(), Color.RED.getRGB());
+
+        // Use ratios to flexibly accomodate circular or not quite rectangular displays
+        // Note: Color.BLACK is the pixel color outside of the display region
+        assertColors(image, mAmWmState.getWmState().getStableBounds(),
+            Color.RED.getRGB(), 0.50f, Color.BLACK.getRGB(), 0.01f);
     }
 
-    private void assertAllColor(BufferedImage img, Rectangle bounds, int expectedColor) {
-        int correctPixels = 0;
+    private void assertColors(BufferedImage img, Rectangle bounds, int primaryColor,
+        float expectedPrimaryRatio, int secondaryColor, float acceptableWrongRatio) {
+
+        int primaryPixels = 0;
+        int secondaryPixels = 0;
         int wrongPixels = 0;
         for (int x = bounds.x; x < bounds.x + bounds.width; x++) {
             for (int y = bounds.y; y < bounds.y + bounds.height; y++) {
                 assertTrue(x < img.getWidth());
                 assertTrue(y < img.getHeight());
                 final int color = img.getRGB(x, y);
-                if (expectedColor == color) {
-                    correctPixels++;
+                if (primaryColor == color) {
+                    primaryPixels++;
+                } else if (secondaryColor == color) {
+                    secondaryPixels++;
                 } else {
                     wrongPixels++;
                 }
             }
         }
 
+        final int totalPixels = bounds.width * bounds.height;
+        final float primaryRatio = (float) primaryPixels / totalPixels;
+        if (primaryRatio < expectedPrimaryRatio) {
+            fail("Less than " + (expectedPrimaryRatio * 100.0f)
+                    + "% of pixels have non-primary color primaryPixels=" + primaryPixels
+                    + " secondaryPixels=" + secondaryPixels + " wrongPixels=" + wrongPixels);
+        }
+
         // Some pixels might be covered by screen shape decorations, like rounded corners.
-        final float ratio = (float) wrongPixels / (correctPixels + wrongPixels);
-        if (ratio > 0.01f) {
-            fail("More than 1% of pixels have wrong color correctPixels=" + correctPixels
-                    + " wrongPixels=" + wrongPixels);
+        // On circular displays, there is an antialiased edge.
+        final float wrongRatio = (float) wrongPixels / totalPixels;
+        if (wrongRatio > acceptableWrongRatio) {
+            fail("More than " + (acceptableWrongRatio * 100.0f)
+                    + "% of pixels have wrong color primaryPixels=" + primaryPixels
+                    + " secondaryPixels=" + secondaryPixels + " wrongPixels=" + wrongPixels);
         }
     }
 }
