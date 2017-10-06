8e9c9594eb3aa884fb27ebb841fff09991866906
Ray Essick <essick@google.com>
2017-10-06 10:46:15 -0700

    DO NOT MERGE: CTS test for fix of b/65717533
    test case and data to verify patch is applied.
    
    Bug: 65717533
    Test: ran before/after to get fail/pass respectively
    Change-Id: I8e6442a31a874cff319c501740e45d47003e2666
    (cherry picked from commit b4ed39389b6b4cfacbf2d3235881ff2813425aff)

--- tests/tests/security/src/android/security/cts/StagefrightTest.java
+++ tests/tests/security/src/android/security/cts/StagefrightTest.java
@@ -191,6 +191,11 @@ public class StagefrightTest extends InstrumentationTestCase {
      before any existing test methods
      ***********************************************************/
 
+    @SecurityTest
+    public void testStagefright_bug_65717533() throws Exception {
+        doStagefrightTest(R.raw.bug_65717533_header_corrupt);
+    }
+
     public void testStagefright_bug_33818508() throws Exception {
         doStagefrightTest(R.raw.bug_33818508);
     }
