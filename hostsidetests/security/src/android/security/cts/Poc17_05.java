8be51ebd88d0f2035b302b6a604ac638096d12bd
Moriah Tobin <mtobin@google.com>
2017-10-19 14:01:02 -0700

    STS test for Android Security b/33898330
    Test: successful run of newly introduced CTS test case.
    Bug:33898330
    Change-Id: If3a39737631cd78ac7052ccdac7b622a4789e8ee

--- hostsidetests/security/src/android/security/cts/Poc17_05.java
+++ hostsidetests/security/src/android/security/cts/Poc17_05.java
@@ -52,4 +52,15 @@ public class Poc17_05 extends SecurityTestCase {
             AdbUtils.runPocNoOutput("CVE-2016-10289", getDevice(), 60);
         }
      }
+
+    /**
+     *  b/33898330
+     */
+    @SecurityTest
+    public void testPocCve_2016_10290() throws Exception {
+        enableAdbRoot(getDevice());
+        if (containsDriver(getDevice(), "/sys/kernel/debug/rmt_storage/info")) {
+          AdbUtils.runPocNoOutput("CVE-2016-10290", getDevice(), 60);
+        }
+    }
 }
