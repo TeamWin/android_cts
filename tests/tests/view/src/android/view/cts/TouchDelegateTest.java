61487f4078afbae515b79ce7a28c4992feb20882
Siarhei Vishniakou <svv@google.com>
2017-10-20 17:25:18 -0700

    Prevent touch delegate from blocking parent events
    Make sure that TouchDelegate does not interfere with
    handling of parent's events, such as onClick().
    Check that ACTION_CANCEL is received by the TouchDelegate.
    
    Test: bit CtsViewTestCases:.TouchDelegateTest
    Bug: 65392236
    Change-Id: I45a239f66d1dc6466d5c6a47b269d457c6e980b9

--- tests/tests/view/src/android/view/cts/TouchDelegateTest.java
+++ tests/tests/view/src/android/view/cts/TouchDelegateTest.java
@@ -16,24 +16,19 @@
 
 package android.view.cts;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import android.app.Activity;
 import android.app.Instrumentation;
+import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.annotation.UiThreadTest;
 import android.support.test.filters.MediumTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.InputDevice;
 import android.view.MotionEvent;
-import android.view.TouchDelegate;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
+
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -44,37 +39,89 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class TouchDelegateTest {
     private Instrumentation mInstrumentation;
-    private Activity mActivity;
-    private Button mButton;
+    private TouchDelegateTestActivity mActivity;
 
     @Rule
-    public ActivityTestRule<MockActivity> mActivityRule =
-            new ActivityTestRule<>(MockActivity.class);
+    public ActivityTestRule<TouchDelegateTestActivity> mActivityRule =
+            new ActivityTestRule<>(TouchDelegateTestActivity.class);
 
     @Before
     public void setup() throws Throwable {
         mActivity = mActivityRule.getActivity();
+        mActivity.resetCounters();
         mInstrumentation = InstrumentationRegistry.getInstrumentation();
-
-        mButton = new Button(mActivity);
-        mActivityRule.runOnUiThread(() -> mActivity.addContentView(
-                mButton, new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)));
         mInstrumentation.waitForIdleSync();
     }
 
-    @UiThreadTest
     @Test
-    public void testOnTouchEvent() {
-        // test callback of onTouchEvent
-        final View view = new View(mActivity);
-        final TouchDelegate touchDelegate = mock(TouchDelegate.class);
-        view.setTouchDelegate(touchDelegate);
+    public void testParentClick() {
+        // If only clicking parent, button should not receive click
+        clickParent();
+        assertEquals(0, mActivity.getButtonClickCount());
+        assertEquals(1, mActivity.getParentClickCount());
 
-        final int xInside = (mButton.getLeft() + mButton.getRight()) / 3;
-        final int yInside = (mButton.getTop() + mButton.getBottom()) / 3;
+        // When clicking TouchDelegate area, both parent and button
+        // should receive DOWN and UP events. However, click will only be generated for the button
+        mActivity.resetCounters();
+        clickTouchDelegateArea();
+        assertEquals(1, mActivity.getButtonClickCount());
+        assertEquals(0, mActivity.getParentClickCount());
 
-        view.onTouchEvent(MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, xInside, yInside, 0));
-        verify(touchDelegate, times(1)).onTouchEvent(any(MotionEvent.class));
+        // Ensure parent can still receive clicks after TouchDelegate has been activated once
+        mActivity.resetCounters();
+        clickParent();
+        assertEquals(0, mActivity.getButtonClickCount());
+        assertEquals(1, mActivity.getParentClickCount());
+    }
+
+    @Test
+    public void testCancelEvent() {
+        // Ensure events with ACTION_CANCEL are received by the TouchDelegate
+        final long downTime = SystemClock.uptimeMillis();
+        dispatchMotionEventToActivity(MotionEvent.ACTION_DOWN, mActivity.touchDelegateY,
+                downTime);
+        dispatchMotionEventToActivity(MotionEvent.ACTION_CANCEL, mActivity.touchDelegateY,
+                downTime);
+        mInstrumentation.waitForIdleSync();
+
+        MotionEvent event = mActivity.removeOldestButtonEvent();
+        assertNotNull(event);
+        assertEquals(MotionEvent.ACTION_DOWN, event.getAction());
+        event.recycle();
+        event = mActivity.removeOldestButtonEvent();
+        assertNotNull(event);
+        assertEquals(MotionEvent.ACTION_CANCEL, event.getAction());
+        event.recycle();
+        assertNull(mActivity.removeOldestButtonEvent());
+        assertEquals(0, mActivity.getButtonClickCount());
+        assertEquals(0, mActivity.getParentClickCount());
+    }
+
+    private void clickParent() {
+        click(mActivity.parentViewY);
+    }
+
+    private void clickTouchDelegateArea() {
+        click(mActivity.touchDelegateY);
+    }
+
+    // Low-level input-handling functions for the activity
+
+    private void click(int y) {
+        final long downTime = SystemClock.uptimeMillis();
+        dispatchMotionEventToActivity(MotionEvent.ACTION_DOWN, y, downTime);
+        dispatchMotionEventToActivity(MotionEvent.ACTION_UP, y, downTime);
+        mInstrumentation.waitForIdleSync();
+    }
+
+    private void dispatchMotionEventToActivity(int action, int y, long downTime) {
+        mActivity.runOnUiThread(() -> {
+            final long eventTime = SystemClock.uptimeMillis();
+            final MotionEvent event = MotionEvent.obtain(downTime, eventTime, action,
+                    mActivity.x, y, 0);
+            event.setSource(InputDevice.SOURCE_TOUCHSCREEN);
+            mActivity.dispatchTouchEvent(event);
+            event.recycle();
+        });
     }
 }
