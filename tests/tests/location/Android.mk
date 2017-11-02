9420ca8e95bc55dfeead774ba8906c5573fef351
tccyp <tccyp@google.com>
2017-11-02 15:29:55 -0700

    Emergency call function cts verifier tests
    Added Cts verifer tests for emergency call function test.
    During the 911 call, we should have still have the access to GPS,
    Wifi, SMS and MMS.
    
    Bug: 36658388, 36659518, 36659220, 36659413
    Test:  make cts-verifier -j64,  run the cts verifier app
    
    Change-Id: Idf93bd53e305a04c64d9db3a3e26b76faa2107ad

--- tests/tests/location/Android.mk
+++ tests/tests/location/Android.mk
@@ -26,6 +26,8 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
+LOCAL_JAVA_LIBRARIES := telephony-common
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
     compatibility-device-util ctstestrunner apache-commons-math
 
@@ -49,6 +51,8 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 # Tag this module as a cts test artifact
 LOCAL_COMPATIBILITY_SUITE := cts vts general-tests
+
+LOCAL_JAVA_LIBRARIES := telephony-common
 
 LOCAL_STATIC_JAVA_LIBRARIES := compatibility-device-util ctstestrunner  apache-commons-math
 
