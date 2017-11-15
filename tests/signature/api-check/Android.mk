2e90ecc6680dab7b774b36151c5796372f4db031
Paul Duffin <paulduffin@google.com>
2017-11-15 11:39:14 +0000

    Separate android.test.mock from test-runner source
    Extracts the source for the android.test.mock library from the
    frameworks/base/test-runner directory into its own
    frameworks/base/test-mock directory. They are already treated separately
    at runtime and compile time so this just makes the separation complete.
    
    Bug: 30188076
    Test: make checkbuild
    Change-Id: I0489e33fc9aeac64420ec749fdd54b23f7f61222
    Merged-In: I0489e33fc9aeac64420ec749fdd54b23f7f61222
    (cherry picked from commit 490c6e403632962650c03b5b8ce1361f1848fe6e)

--- tests/signature/api-check/Android.mk
+++ tests/signature/api-check/Android.mk
@@ -32,6 +32,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     cts-signature-common \
     repackaged-legacy-test \
     repackaged.android.test.runner \
+    repackaged.android.test.mock \
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
