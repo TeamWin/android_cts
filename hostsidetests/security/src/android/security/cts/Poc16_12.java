761d1b2cfd72cdc7555f886f3badbd04aa14b62e
Andres Celis <celisa@google.com>
2017-11-30 13:45:25 -0800

    Remove all hostside elevated permissions tests that were introduced in nyc-mr1 or mr2.
    Test: successful build.
    Bug:67456333
    Merged-In: Id916ea90464deee5c2edb520d77553a6c4105532
    Change-Id: Id916ea90464deee5c2edb520d77553a6c4105532

--- hostsidetests/security/src/android/security/cts/Poc16_12.java
+++ hostsidetests/security/src/android/security/cts/Poc16_12.java
@@ -21,7 +21,6 @@ import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.testtype.DeviceTestCase;
 
-import android.platform.test.annotations.RootPermissionTest;
 import android.platform.test.annotations.SecurityTest;
 
 import java.io.BufferedOutputStream;
