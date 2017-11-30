719f272cd22335caf626657ea9f6c08937250743
Pat Plunkett <patplunkett@google.com>
2017-11-30 13:45:08 -0800

    Relax timeout duration
    On certain platforms, starting and immediately stopping an Activity
    may take slightly longer than one second. This relaxes the timeout
    duration to two seconds.
    
    Bug: 64489223
    Test: The affected timeouts pass.
    Change-Id: Iab4b6fbf1186787415ebe9952c713790541f4009

--- tests/app/src/android/app/cts/ActivityManagerTest.java
+++ tests/app/src/android/app/cts/ActivityManagerTest.java
@@ -190,7 +190,7 @@ public class ActivityManagerTest extends InstrumentationTestCase {
         private String mActivityToFilter;
         private int result = RESULT_TIMEOUT;
         public long mTimeUsed = 0;
-        private static final int TIMEOUT_IN_MS = 1000;
+        private static final int TIMEOUT_IN_MS = 2000;
 
         // Create the filter with the intent to look for.
         public ActivityReceiverFilter(String activityToFilter) {
