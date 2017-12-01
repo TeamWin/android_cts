41380b3d50c70a499160d8dd08fb57bfddf1e4c4
Felipe Leme <felipeal@google.com>
2017-11-30 17:26:00 -0800

    Added autofill test to check apps cannot bypass package name on AssistStructure
    Test: cts-tradefed run commandAndExit cts-dev -m CtsAutoFillServiceTestCases -t android.autofillservice.cts.VirtualContainerActivityTest#testAppCannotFakePackageName
    
    Bug: 69981710
    
    Change-Id: I3abf999eb6056c1df7982780bae43b58337c0668
    Merged-In: I3abf999eb6056c1df7982780bae43b58337c0668

--- tests/autofillservice/src/android/autofillservice/cts/AutoFillServiceTestCase.java
+++ tests/autofillservice/src/android/autofillservice/cts/AutoFillServiceTestCase.java
@@ -27,6 +27,7 @@ import static android.provider.Settings.Secure.AUTOFILL_SERVICE;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import android.autofillservice.cts.InstrumentedAutoFillService.Replier;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
@@ -60,10 +61,17 @@ abstract class AutoFillServiceTestCase {
     public final RequiredFeatureRule mRequiredFeatureRule =
             new RequiredFeatureRule(PackageManager.FEATURE_AUTOFILL);
 
+    protected final Context mContext;
+    protected final String mPackageName;
     /**
      * Stores the previous logging level so it's restored after the test.
      */
     private String mLoggingLevel;
+
+    protected AutoFillServiceTestCase() {
+        mContext = InstrumentationRegistry.getTargetContext();
+        mPackageName = mContext.getPackageName();
+    }
 
     @BeforeClass
     public static void removeLockScreen() {
