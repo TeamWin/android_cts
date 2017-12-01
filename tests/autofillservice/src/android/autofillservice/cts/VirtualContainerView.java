b6e8cc1010b115361541c77e512433a179baef31
Felipe Leme <felipeal@google.com>
2017-11-30 17:26:00 -0800

    Added autofill test to check apps cannot bypass package name on AssistStructure
    Test: atest CtsAutoFillServiceTestCases:VirtualContainerActivityTest#testAppCannotFakePackageName
    Bug: 69981710
    
    Change-Id: I3abf999eb6056c1df7982780bae43b58337c0668

--- tests/autofillservice/src/android/autofillservice/cts/VirtualContainerView.java
+++ tests/autofillservice/src/android/autofillservice/cts/VirtualContainerView.java
@@ -20,7 +20,9 @@ import static android.autofillservice.cts.Helper.FILL_TIMEOUT_MS;
 
 import static com.google.common.truth.Truth.assertWithMessage;
 
+import android.app.assist.AssistStructure;
 import android.app.assist.AssistStructure.ViewNode;
+import android.content.ComponentName;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -72,6 +74,7 @@ class VirtualContainerView extends View {
     private int mUnfocusedColor;
     private boolean mSync = true;
     private boolean mOverrideDispatchProvideAutofillStructure = false;
+    private ComponentName mFackedComponentName;
 
     public VirtualContainerView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -192,6 +195,18 @@ class VirtualContainerView extends View {
         Log.d(TAG, "onProvideAutofillVirtualStructure(): flags = " + flags);
         super.onProvideAutofillVirtualStructure(structure, flags);
 
+        if (mFackedComponentName != null) {
+            Log.d(TAG, "Faking package name to " + mFackedComponentName);
+            try {
+                final AssistStructure assistStructure = Helper.getField(structure, "mAssist");
+                if (assistStructure != null) {
+                    Helper.setField(assistStructure, "mActivityComponent", mFackedComponentName);
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Could not fake package name to " + mFackedComponentName, e);
+            }
+        }
+
         final String packageName = getContext().getPackageName();
         structure.setClassName(getClass().getName());
         final int childrenSize = mItems.size();
@@ -253,6 +268,11 @@ class VirtualContainerView extends View {
     void setSync(boolean sync) {
         mSync = sync;
     }
+
+    void fakePackageName(ComponentName name) {
+        mFackedComponentName = name;
+    }
+
 
     void setOverrideDispatchProvideAutofillStructure(boolean flag) {
         mOverrideDispatchProvideAutofillStructure = flag;
