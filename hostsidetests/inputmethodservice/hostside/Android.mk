e7e6cbf6b58cc51c924b2081499d8994ea24698c
Yohei Yukawa <yukawa@google.com>
2017-12-06 17:29:19 -0800

    Revert "Fix too strict requirements in IME state handling"
    This reverts commit 6cf3ccbf68865f7cde7fda5c0648588f3df60477 [1].
    
    Reason for revert:
    The change is still valid, but we want to backport this CL to
    oreo-cts-dev branch, where 'hamcrest-library' is not available yet
    in the host side.
    
    We just want to re-implement the same test without hamcrest.
    
     [1]: I2b6563e9276e380dca485dbe5e3782f09e0dc445
    
    Bug: 69276863
    Bug: 69893109
    Bug: 70283683
    Test: atest CtsInputMethodServiceHostTestCases
    Change-Id: I9ffc91e5a0db99010ee0a2803ad6629b5f035c16

--- hostsidetests/inputmethodservice/hostside/Android.mk
+++ hostsidetests/inputmethodservice/hostside/Android.mk
@@ -30,7 +30,6 @@ LOCAL_JAVA_LIBRARIES := \
     cts-tradefed \
     tradefed
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    cts-inputmethodservice-common-host \
-    hamcrest-library
+    cts-inputmethodservice-common-host
 
 include $(BUILD_CTS_HOST_JAVA_LIBRARY)
