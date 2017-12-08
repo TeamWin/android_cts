65774d41dc34f75d758618026a297819955dfab1
Bookatz <bookatz@google.com>
2017-12-08 13:32:27 -0800

    Device part of Statsd CTS test
    Moves statsd atom tests to its own directory.
    Adds infrastructure for uid-based atom tests (via an
    installed device-side app).
    Adds CTS tests for the following:
    testBleScan
    testBleUnoptimizedScan
    testBleScanResult
    testGpsScan
    testWifiScan
    
    Test: run cts-dev -m CtsStatsdHostTestCases -t android.cts.statsd
    Fixes: 70679311
    Change-Id: I0c2201dbed68ece048a5f106d8b8eb90a4d51636

--- common/host-side/tradefed/tests/src/com/android/compatibility/common/tradefed/presubmit/CtsConfigLoadingTest.java
+++ common/host-side/tradefed/tests/src/com/android/compatibility/common/tradefed/presubmit/CtsConfigLoadingTest.java
@@ -69,6 +69,7 @@ public class CtsConfigLoadingTest {
             "neuralnetworks",
             "renderscript",
             "security",
+            "statsd",
             "systems",
             "sysui",
             "telecom",
