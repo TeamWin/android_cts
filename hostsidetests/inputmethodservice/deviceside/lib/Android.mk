4b08d2e86e266656ec5d348cab787f9b1cf97622
Yohei Yukawa <yukawa@google.com>
2017-12-12 15:28:47 +0000

    Revert "Add CTS to verify IMS.onStartInput() is called once."
    This reverts commit c0bd71ea57614aafaf9eb287e5e4c3f308221fec [1].
    
    Reason for revert:
    In Bug 69845539, we successfully migrated this test from host-side to
    device-side [2] hence we no longer need this test in
    InputMethodServiceLifecycleTest.
    
    Also this test is a bit flaky (Bug 70538611).
    
     [1]: I5fd7d8a535d66b7599bb03d7192668d00e10d81c
     [2]: I90c306ec3ce15662717d3b17b833db6c2072c2ca
          e3d0c68f1bdc72f4d9ebfc08840344a1bc1bbe6f
    
    Bug: 35599628
    Change-Id: Iacc677864b111e56d0308cc85bd2a0353978aeeb
    Fixes: 70538611
    Test: atest CtsInputMethodServiceHostTestCases

--- hostsidetests/inputmethodservice/deviceside/lib/Android.mk
+++ hostsidetests/inputmethodservice/deviceside/lib/Android.mk
@@ -19,7 +19,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-annotations \
-    json \
     CtsInputMethodServiceCommon
 
 LOCAL_MODULE_TAGS := tests
