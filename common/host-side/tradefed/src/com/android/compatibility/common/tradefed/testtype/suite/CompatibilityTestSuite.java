39f7aeb99f896d1ff58852d8dc3a97a8cf4ad222
jdesprez <jdesprez@google.com>
2017-12-21 02:31:54 -0800

    Keep only the minimal specialization of CTS specific
    - Only keep what is really CTS specific in the suite
    implementation.
    - Move to TF the supporting class.
    
    Test: unit tests, run cts-suite -m CtsGesture
    Bug: 65303193
    Change-Id: I6ca065a7589977ff13d399aa08cf33d831556a2d

--- common/host-side/tradefed/src/com/android/compatibility/common/tradefed/testtype/suite/CompatibilityTestSuite.java
+++ common/host-side/tradefed/src/com/android/compatibility/common/tradefed/testtype/suite/CompatibilityTestSuite.java
@@ -19,42 +19,27 @@ import com.android.compatibility.common.tradefed.build.CompatibilityBuildHelper;
 import com.android.compatibility.common.tradefed.testtype.ISubPlan;
 import com.android.compatibility.common.tradefed.testtype.SubPlan;
 import com.android.compatibility.common.tradefed.testtype.retry.RetryFactoryTest;
-import com.android.compatibility.common.util.TestFilter;
 import com.android.tradefed.build.IBuildInfo;
 import com.android.tradefed.config.IConfiguration;
 import com.android.tradefed.config.Option;
 import com.android.tradefed.config.Option.Importance;
 import com.android.tradefed.config.OptionClass;
-import com.android.tradefed.device.DeviceNotAvailableException;
-import com.android.tradefed.testtype.IAbi;
-import com.android.tradefed.testtype.suite.ITestSuite;
-import com.android.tradefed.util.ArrayUtil;
+import com.android.tradefed.testtype.suite.BaseTestSuite;
 import com.android.tradefed.util.xml.AbstractXmlParser.ParseException;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Set;
 
 /**
  * A Test for running Compatibility Test Suite with new suite system.
  */
 @OptionClass(alias = "compatibility")
-public class CompatibilityTestSuite extends ITestSuite {
+public class CompatibilityTestSuite extends BaseTestSuite {
 
-    public static final String INCLUDE_FILTER_OPTION = "include-filter";
-    public static final String EXCLUDE_FILTER_OPTION = "exclude-filter";
     public static final String SUBPLAN_OPTION = "subplan";
-    public static final String MODULE_OPTION = "module";
-    public static final String TEST_ARG_OPTION = "test-arg";
-    public static final String TEST_OPTION = "test";
-    public static final char TEST_OPTION_SHORT_NAME = 't';
-    private static final String MODULE_ARG_OPTION = "module-arg";
 
     // TODO: remove this option when CompatibilityTest goes away
     @Option(name = RetryFactoryTest.RETRY_OPTION,
@@ -67,78 +52,34 @@ public class CompatibilityTestSuite extends ITestSuite {
             importance = Importance.IF_UNSET)
     private String mSubPlan;
 
-    @Option(name = INCLUDE_FILTER_OPTION,
-            description = "the include module filters to apply.",
-            importance = Importance.ALWAYS)
-    private Set<String> mIncludeFilters = new HashSet<>();
-
-    @Option(name = EXCLUDE_FILTER_OPTION,
-            description = "the exclude module filters to apply.",
-            importance = Importance.ALWAYS)
-    private Set<String> mExcludeFilters = new HashSet<>();
-
-    @Option(name = MODULE_OPTION,
-            shortName = 'm',
-            description = "the test module to run.",
-            importance = Importance.IF_UNSET)
-    private String mModuleName = null;
-
-    @Option(name = TEST_OPTION,
-            shortName = TEST_OPTION_SHORT_NAME,
-            description = "the test to run.",
-            importance = Importance.IF_UNSET)
-    private String mTestName = null;
-
-    @Option(name = MODULE_ARG_OPTION,
-            description = "the arguments to pass to a module. The expected format is"
-                    + "\"<module-name>:<arg-name>:[<arg-key>:=]<arg-value>\"",
-            importance = Importance.ALWAYS)
-    private List<String> mModuleArgs = new ArrayList<>();
-
-    @Option(name = TEST_ARG_OPTION,
-            description = "the arguments to pass to a test. The expected format is"
-                    + "\"<test-class>:<arg-name>:[<arg-key>:=]<arg-value>\"",
-            importance = Importance.ALWAYS)
-    private List<String> mTestArgs = new ArrayList<>();
-
-    private ModuleRepoSuite mModuleRepo = new ModuleRepoSuite();
     private CompatibilityBuildHelper mBuildHelper;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public LinkedHashMap<String, IConfiguration> loadTests() {
-        if (mRetrySessionId != null) {
-            throw new IllegalArgumentException("--retry cannot be specified with cts-suite.xml. "
-                    + "Use 'run cts --retry <session id>' instead.");
-        }
-        try {
-            setupFilters();
-            Set<IAbi> abis = getAbis(getDevice());
-            // Initialize the repository, {@link CompatibilityBuildHelper#getTestsDir} can
-            // throw a {@link FileNotFoundException}
-            return mModuleRepo.loadConfigs(mBuildHelper.getTestsDir(),
-                    abis, mTestArgs, mModuleArgs, mIncludeFilters, mExcludeFilters);
-        } catch (DeviceNotAvailableException | FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void setBuild(IBuildInfo buildInfo) {
         super.setBuild(buildInfo);
         mBuildHelper = new CompatibilityBuildHelper(buildInfo);
     }
 
+    @Override
+    public File getTestsDir() throws FileNotFoundException {
+        return mBuildHelper.getTestsDir();
+    }
+
+    @Override
+    public LinkedHashMap<String, IConfiguration> loadTests() {
+        if (mRetrySessionId != null) {
+            throw new IllegalArgumentException("--retry cannot be specified with cts-suite.xml. "
+                    + "Use 'run cts --retry <session id>' instead.");
+        }
+        return super.loadTests();
+    }
+
     /**
      * Sets the include/exclude filters up based on if a module name was given or whether this is a
      * retry run.
      */
-    void setupFilters() throws FileNotFoundException {
+    @Override
+    final protected void setupFilters(File testDir) throws FileNotFoundException {
         if (mSubPlan != null) {
             try {
                 File subPlanFile = new File(mBuildHelper.getSubPlansDir(), mSubPlan + ".xml");
@@ -149,66 +90,21 @@ public class CompatibilityTestSuite extends ITestSuite {
                 InputStream subPlanInputStream = new FileInputStream(subPlanFile);
                 ISubPlan subPlan = new SubPlan();
                 subPlan.parse(subPlanInputStream);
-                mIncludeFilters.addAll(subPlan.getIncludeFilters());
-                mExcludeFilters.addAll(subPlan.getExcludeFilters());
+                // Set include/exclude filter is additive
+                setIncludeFilter(subPlan.getIncludeFilters());
+                setExcludeFilter(subPlan.getExcludeFilters());
             } catch (ParseException e) {
                 throw new RuntimeException(
                         String.format("Unable to find or parse subplan %s", mSubPlan), e);
             }
         }
-        if (mModuleName != null) {
-            List<String> modules = ModuleRepoSuite.getModuleNamesMatching(
-                    mBuildHelper.getTestsDir(), mModuleName);
-            if (modules.size() == 0) {
-                throw new IllegalArgumentException(
-                        String.format("No modules found matching %s", mModuleName));
-            } else if (modules.size() > 1) {
-                throw new IllegalArgumentException(String.format(
-                        "Multiple modules found matching %s:\n%s\nWhich one did you mean?\n",
-                        mModuleName, ArrayUtil.join("\n", modules)));
-            } else {
-                String moduleName = modules.get(0);
-                checkFilters(mIncludeFilters, moduleName);
-                checkFilters(mExcludeFilters, moduleName);
-                mIncludeFilters.add(new TestFilter(getRequestedAbi(), moduleName, mTestName)
-                        .toString());
-            }
-        } else if (mTestName != null) {
-            throw new IllegalArgumentException(
-                    "Test name given without module name. Add --module <module-name>");
-        }
-    }
-
-    /* Helper method designed to remove filters in a list not applicable to the given module */
-    private static void checkFilters(Set<String> filters, String moduleName) {
-        Set<String> cleanedFilters = new HashSet<String>();
-        for (String filter : filters) {
-            if (moduleName.equals(TestFilter.createFrom(filter).getName())) {
-                cleanedFilters.add(filter); // Module name matches, filter passes
-            }
-        }
-        filters.clear();
-        filters.addAll(cleanedFilters);
-    }
-
-    /**
-     * Sets include-filters for the compatibility test
-     */
-    public void setIncludeFilter(Set<String> includeFilters) {
-        mIncludeFilters.addAll(includeFilters);
-    }
-
-    /**
-     * Sets exclude-filters for the compatibility test
-     */
-    public void setExcludeFilter(Set<String> excludeFilters) {
-        mExcludeFilters.addAll(excludeFilters);
+        super.setupFilters(testDir);
     }
 
     /**
      * Allow to reset the requested session id for retry.
      */
-    public void resetRetryId() {
+    public final void resetRetryId() {
         mRetrySessionId = null;
     }
 }
