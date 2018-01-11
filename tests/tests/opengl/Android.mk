389721df22993108b75b0ad3a5f5737c455f0c15
Saurav Subedi <subedi@google.com>
2018-01-11 15:48:07 -0800

    Annotated Cts Tests with CDD Sections 7.9.2/7.1.4.2/7.1.4.1
    7.1.4.1/C-5-1: Method checks to see if aep(android extension pack)
    is supported for OpenGl ES 3.2
    
    7.1.4.2/C-2-1: Method checks if feature flags are reported correctly
    
    Added compatibility device util(local static libs) to Android.mk
    
    Tests: none
    
    Bug: 80476704
    
    Merged-In: I26efb765cc76f12621cc1a533b0a6d70ffc426dd
    Change-Id: I26efb765cc76f12621cc1a533b0a6d70ffc426dd

--- tests/tests/opengl/Android.mk
+++ tests/tests/opengl/Android.mk
@@ -29,7 +29,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 LOCAL_JNI_SHARED_LIBRARIES := libopengltest_jni
 
-LOCAL_STATIC_JAVA_LIBRARIES := ctstestrunner
+LOCAL_STATIC_JAVA_LIBRARIES := compatibility-device-util ctstestrunner
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
