10a251bb248c61a8ea2bb6c10c5aff32c9299bff
Bartosz Fabianowski <bartfab@google.com>
2018-01-15 18:16:09 +0100

    Tag ArcTS artifacts
    ArcTS reuses a number of helper apps from CTS.
    
    Bug: 71981242
    Test: arcts-tradefed run arcts --module CtsDevicePolicyManagerTestCases
    Change-Id: I00c9089de6caae33cdc79131d5a15829c483bde3

--- hostsidetests/devicepolicy/app/DeviceAdmin/api24/Android.mk
+++ hostsidetests/devicepolicy/app/DeviceAdmin/api24/Android.mk
@@ -35,6 +35,6 @@ LOCAL_JAVA_LIBRARIES := android.test.runner.stubs android.test.base.stubs
 LOCAL_SDK_VERSION := current
 
 # tag this module as a cts test artifact
-LOCAL_COMPATIBILITY_SUITE := cts vts general-tests
+LOCAL_COMPATIBILITY_SUITE := arcts cts vts general-tests
 
 include $(BUILD_CTS_PACKAGE)
