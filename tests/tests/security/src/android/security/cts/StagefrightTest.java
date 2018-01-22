b0c80b808438adf4c04ff1fb158dacaee0814e88
Shrikara B <shrikara.b@ittiam.com>
2018-01-22 13:34:23 +0530

    DO NOT MERGE: CTS test for Android Security b/37093318
    Cherry-picked from https://partner-android-review.googlesource.com/c/platform/cts/+/1018231
    
    Bug: 37093318
    Test: ran new testcase on android-7.1.2_r2 with/without patch
    Change-Id: I160e0a81cfb0c2376bd1638f152edceaca2d7994
    Merged-In: I160e0a81cfb0c2376bd1638f152edceaca2d7994
    (cherry picked from commit 3f38fb0cec6139ffed48f6d185edfa76167fc699)
    (cherry picked from commit 457e232d876d8989eb1d33ecf486f74abaa54f75)
    (cherry picked from commit 024c03a35492bc5ec0c3c05a739d7bf1cf72cc6b)

--- tests/tests/security/src/android/security/cts/StagefrightTest.java
+++ tests/tests/security/src/android/security/cts/StagefrightTest.java
@@ -446,6 +446,10 @@ public class StagefrightTest extends InstrumentationTestCase {
         doStagefrightTest(R.raw.bug_67737022);
     }
 
+    @SecurityTest
+    public void testStagefright_bug_37093318() throws Exception {
+        doStagefrightTest(R.raw.bug_37093318, (4 * 60 * 1000));
+    }
 
     private void doStagefrightTest(final int rid) throws Exception {
         doStagefrightTestMediaPlayer(rid);
@@ -461,6 +465,19 @@ public class StagefrightTest extends InstrumentationTestCase {
         doStagefrightTestMediaCodec(url);
         doStagefrightTestMediaMetadataRetriever(url);
         server.shutdown();
+    }
+
+    private void doStagefrightTest(final int rid, int timeout) throws Exception {
+        runWithTimeout(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    doStagefrightTest(rid);
+                } catch (Exception e) {
+                    //unhandled exception
+                }
+            }
+        }, timeout);
     }
 
     private void doStagefrightTestANR(final int rid) throws Exception {
