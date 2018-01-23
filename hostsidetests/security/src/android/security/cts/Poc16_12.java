6d39751db786dad4fb766f7db314eb53c65a7ae6
Moriah Tobin <mtobin@google.com>
2018-01-23 14:08:25 -0800

    Remove all Android Security tests that require root from CTS.
    Test: successful build
    Bug: 67456333
    Change-Id: I096d71ebb42ea34a3f0b3e7fbd33a2f59aeed717

--- hostsidetests/security/src/android/security/cts/Poc16_12.java
+++ hostsidetests/security/src/android/security/cts/Poc16_12.java
@@ -16,20 +16,7 @@
 
 package android.security.cts;
 
-import com.android.tradefed.device.CollectingOutputReceiver;
-import com.android.tradefed.device.DeviceNotAvailableException;
-import com.android.tradefed.device.ITestDevice;
-import com.android.tradefed.testtype.DeviceTestCase;
-
 import android.platform.test.annotations.SecurityTest;
-
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Scanner;
 
 @SecurityTest
 public class Poc16_12 extends SecurityTestCase {
