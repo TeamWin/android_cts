7b464d823b502cdd57290a689c16b53eeb170cea
Fyodor Kupolov <fkupolov@google.com>
2018-01-26 14:51:42 -0800

    [DO NOT MERGE] Skip whitelist checks for updated privileged apps
    An updated system app can request more permissions than the original
    version on the system image. These permissions are never granted
    by the platform and not needed to be whitelisted.
    
    Test: self
    Bug: 71909721
    Change-Id: I49042198b310523d9c86677decefdd2ef83458f7

--- tests/tests/permission2/src/android/permission2/cts/PrivappPermissionsTest.java
+++ tests/tests/permission2/src/android/permission2/cts/PrivappPermissionsTest.java
@@ -18,6 +18,7 @@ package android.permission2.cts;
 
 import com.android.compatibility.common.util.SystemUtil;
 
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PermissionInfo;
@@ -101,6 +102,11 @@ public class PrivappPermissionsTest extends AndroidTestCase {
                         + " Not granted: " + notGranted + "."
                         + " Whitelisted: " + whitelist + "."
                         + " Denylisted: " + denylist);
+                if ((pkg.applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+                    Log.w(TAG, "Skipping whitelist checks for " + pkg.packageName
+                            + " - application was updated");
+                    continue;
+                }
 
                 Set<String> grantedNotInWhitelist = new TreeSet<>(grantedPrivPermissions);
                 grantedNotInWhitelist.removeAll(whitelist);
