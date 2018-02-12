02164f886c194cdbe26e6a3d8b3c6fc48f7cffb8
Tadashi G. Takaoka <takaoka@google.com>
2018-02-12 10:30:20 -0800

    Add Components to android.server.am.debuggable
    Bug: 70190274
    Test: No new test starts failing.
      atest CtsActivityManagerDeviceTestCases:ActivityManagerAmProfileTests
    
    Change-Id: Ie4441aa803f94950c1c57b76ce7c233fbc29a4fe

--- tests/framework/base/activitymanager/Android.mk
+++ tests/framework/base/activitymanager/Android.mk
@@ -21,7 +21,9 @@ LOCAL_MODULE_TAGS := tests optional
 # Must match the package name in CtsTestCaseList.mk
 LOCAL_PACKAGE_NAME := CtsActivityManagerDeviceTestCases
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-named-files-under,Components.java, appDebuggable) \
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-test \
