809154e9fa1508e0d1b695017d8736dc093004b3
Chenjie Yu <cjyu@google.com>
2018-02-21 09:33:41 -0800

    Add cts for picture-in-picture atom
    + Becase the logging is tied to UI elements, we don't get the dismissal
    event if the app exits by itself or is force killed.
    
    Test: cts test
    Bug: 72328541
    Change-Id: I7453162079f642b8942321b8a99ca4030505f2fa

--- hostsidetests/statsd/apps/statsdapp/src/com/android/server/cts/device/statsd/VideoPlayerActivity.java
+++ hostsidetests/statsd/apps/statsdapp/src/com/android/server/cts/device/statsd/VideoPlayerActivity.java
@@ -17,18 +17,70 @@
 package com.android.server.cts.device.statsd;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.widget.VideoView;
 
 public class VideoPlayerActivity extends Activity {
-    /** Called when the activity is first created. */
+    private static final String TAG = VideoPlayerActivity.class.getSimpleName();
+
+    public static final String KEY_ACTION = "action";
+    public static final String ACTION_PLAY_VIDEO = "action.play_video";
+    public static final String ACTION_PLAY_VIDEO_PICTURE_IN_PICTURE_MODE =
+            "action.play_video_picture_in_picture_mode";
+
+    public static final int DELAY_MILLIS = 2000;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = this.getIntent();
+        if (intent == null) {
+            Log.e(TAG, "Intent was null.");
+            finish();
+        }
+
+        String action = intent.getStringExtra(KEY_ACTION);
+        Log.i(TAG, "Starting " + action + " from foreground activity.");
+
+        switch (action) {
+            case ACTION_PLAY_VIDEO:
+                playVideo();
+                break;
+            case ACTION_PLAY_VIDEO_PICTURE_IN_PICTURE_MODE:
+                playVideo();
+                this.enterPictureInPictureMode();
+                break;
+            default:
+                Log.e(TAG, "Intent had invalid action " + action);
+                finish();
+        }
+        delay();
+    }
+
+    private void playVideo() {
         setContentView(R.layout.activity_main);
         VideoView videoView = (VideoView)findViewById(R.id.video_player_view);
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.colors_video);
         videoView.start();
+    }
+
+    private void delay() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                SystemClock.sleep(DELAY_MILLIS);
+                return null;
+            }
+            @Override
+            protected void onPostExecute(Void nothing) {
+                finish();
+            }
+        }.execute();
     }
 }
 
