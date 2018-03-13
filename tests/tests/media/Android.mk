4c5d63aaea188b8a357782cd0322f1bc81fdeeee
Inseob Kim <inseob@google.com>
2018-03-13 18:26:56 +0900

    Fix link-type warning of CtsMediaTestCases
    An app CtsMediaTestCases uses private APIs and refers some ndk jni
    libraries using STL, which generates link-type warnings(app:platform ->
    ndk:libc++:static). To remove warnings, each jni libraries should be
    built without NDK or STL. In addition, all jni libraries should use the
    same STL. They are fixed as follows:
    
    libctsimagereader_jni, libctsmediadrm_jni: None of the other apps use
    these library. Therefore, LOCAL_SDK_VERSION is removed and LOCAL_CXX_STL
    is set to libc++.
    
    libaudio_jni, libndkaudioLib: LOCAL_CXX_STL is set to libc++.
    
    libctsmediacodec_jni: This consists of several utility functions and is
    referred by an SDK app CtsVideoTestCases(cts/tests/video). If this jni
    library is built without NDK, a brand-new warning will be generated.
    Fortunatey, The functions used by CtsVideoTestCases do not depend on the
    other functions, and do not use STL except for the type std::nullptr_t.
    As a result, the functions are splitted from libctsmediacodec_jni and a
    new library libctscodecutils_jni is created. A little hack manually
    defininig std::nullptr_t is added in order to build our new library
    without STL.
    
    Bug: 73907534
    Test: mma -j does not show any link-type warnings && the result of atest
    does not changed
    
    Merged-In: I33db3277e24de2d958531b30eb50ad3d1a136475
    Change-Id: I33db3277e24de2d958531b30eb50ad3d1a136475

--- tests/tests/media/Android.mk
+++ tests/tests/media/Android.mk
@@ -57,6 +57,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_JNI_SHARED_LIBRARIES := \
     libaudio_jni \
+    libc++ \
+    libctscodecutils_jni \
     libctsimagereader_jni \
     libctsmediadrm_jni \
     libctsmediacodec_jni \
