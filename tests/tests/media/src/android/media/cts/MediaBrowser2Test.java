b717373ae0e95f73b847b5242f124e0dd4e91108
Jaewan Kim <jaewan@google.com>
2018-03-20 00:04:51 +0900

    MediaController2: Remove MediaPlaylistAgent in the ControllerCallback
    Bug: 75500592
    Test: Run CTS with MediaComponents/runcts.sh
    Change-Id: I404fc8cf6308839eed2d0b97775bd5b8b6883d65

--- tests/tests/media/src/android/media/cts/MediaBrowser2Test.java
+++ tests/tests/media/src/android/media/cts/MediaBrowser2Test.java
@@ -574,29 +574,25 @@ public class MediaBrowser2Test extends MediaController2Test {
         }
 
         @Override
-        public void onPlaylistChanged(MediaController2 controller, MediaPlaylistAgent playlistAgent,
+        public void onPlaylistChanged(MediaController2 controller,
                 List<MediaItem2> list, MediaMetadata2 metadata) {
-            mCallbackProxy.onPlaylistChanged(controller, playlistAgent, list, metadata);
+            mCallbackProxy.onPlaylistChanged(controller, list, metadata);
         }
 
         @Override
         public void onPlaylistMetadataChanged(MediaController2 controller,
-                MediaPlaylistAgent playlistAgent, MediaMetadata2 metadata) {
-            mCallbackProxy.onPlaylistMetadataChanged(controller, playlistAgent, metadata);
+                MediaMetadata2 metadata) {
+            mCallbackProxy.onPlaylistMetadataChanged(controller, metadata);
         }
 
         @Override
-        public void onShuffleModeChanged(MediaController2 controller,
-                MediaPlaylistAgent playlistAgent,
-                int shuffleMode) {
-            mCallbackProxy.onShuffleModeChanged(controller, playlistAgent, shuffleMode);
+        public void onShuffleModeChanged(MediaController2 controller, int shuffleMode) {
+            mCallbackProxy.onShuffleModeChanged(controller, shuffleMode);
         }
 
         @Override
-        public void onRepeatModeChanged(MediaController2 controller,
-                MediaPlaylistAgent playlistAgent,
-                int repeatMode) {
-            mCallbackProxy.onRepeatModeChanged(controller, playlistAgent, repeatMode);
+        public void onRepeatModeChanged(MediaController2 controller, int repeatMode) {
+            mCallbackProxy.onRepeatModeChanged(controller, repeatMode);
         }
 
         @Override
