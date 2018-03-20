e1dc38cc4270e0a9916d8ea261b29c470d387f8a
Yohei Yukawa <yukawa@google.com>
2018-03-19 22:48:55 -0700

    Ensure hostside IME tests only use public APIs
    With this CL all the APKs used for hostside IME CTS tests can be built
    only with public APIs and Test APIs at compile time.
    
    There should be no behavior / requirement change in existing CTS
    tests.
    
    Test: make -j cts
    Bug: 72399950
    Change-Id: I44afda72d321c103a8bb1310fa3bd1d76c3ae8b1

--- hostsidetests/inputmethodservice/deviceside/provider/Android.mk
+++ hostsidetests/inputmethodservice/deviceside/provider/Android.mk
@@ -35,6 +35,6 @@ LOCAL_COMPATIBILITY_SUITE := cts vts general-tests
 
 LOCAL_PACKAGE_NAME := CtsInputMethodServiceEventProvider
 
-LOCAL_SDK_VERSION := current
+LOCAL_SDK_VERSION := test_current
 
 include $(BUILD_PACKAGE)
