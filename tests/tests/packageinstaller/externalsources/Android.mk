06233629da1aeede46d101f713beae333e395f35
Philip P. Moltmann <moltmann@google.com>
2018-05-29 11:20:29 -0700

    Convert ExternalSourcesTest to kotlin.
    - Android.mk based build does not support kotlin
    - Android.bp based build does not support builing apks
    
    Hence the whole app is now a shared lib an the Android.mk is just
    turning the lib into an apk.
    
    Test: atest CtsExternalSourcesTestCases
    Change-Id: I507e7c07bc18ff43de4936d3d91a366283643555

--- tests/tests/packageinstaller/externalsources/Android.mk
+++ tests/tests/packageinstaller/externalsources/Android.mk
@@ -22,12 +22,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 LOCAL_PACKAGE_NAME := CtsExternalSourcesTestCases
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_STATIC_JAVA_LIBRARIES := ub-uiautomator \
-                               android-support-test \
-                               androidx.legacy_legacy-support-v4 \
-                               compatibility-device-util
+LOCAL_STATIC_JAVA_LIBRARIES := CtsExternalSourcesTestCases-lib
 
 LOCAL_SDK_VERSION := current
 
