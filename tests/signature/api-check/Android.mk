ce58e7438ad7d3b7d804a829ded004dddcd86685
saurav subedi <subedi@google.com>
2018-06-08 20:22:52 -0700

    DO NOT MERGE: CDD annotations for section 3.2.3.1 and 3.6
    -Annotated CDD requirements related to api signature and
    namespace compliance
    
    - Added compatibility-device-util to local_static_java_libraries
      to include the @Cddtest annotations
    
    Tests: make cts
    
    Bug: 110781422
    Change-Id: I93c3ec41cf3d4abec04b1da420bff0ae60bd3132

--- tests/signature/api-check/Android.mk
+++ tests/signature/api-check/Android.mk
@@ -32,6 +32,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     cts-signature-common \
     repackaged-legacy-test \
     repackaged.android.test.runner \
+    compatibility-device-util
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
