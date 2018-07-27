fc60bbb7cd17b7cc15a99c49db943d70f342293d
Felipe Leme <felipeal@google.com>
2018-07-27 12:56:30 -0700

    DO NOT MERGE - Changed WebViewActivityTest to set the service before the activity is launched.
    The latest WebView implementation calls AutofillManager.isEnabled() to
    disable autofill for optimization when it returns false, and unfortunately the
    value returned by that method does not change when the service is enabled /
    disabled, so we need to start enable the service before launching the activity.
    
    Bug: 111838239
    Fixes: 111880684
    Test: cts-tradefed run commandAndExit cts-dev -m CtsAutoFillServiceTestCases \
          -t android.autofillservice.cts.WebViewActivityTest
    
    Merged-In: I255d3749d7911556a8c0d863d043778ed79c2e35
    Change-Id: I255d3749d7911556a8c0d863d043778ed79c2e35
    (cherry picked from commit b9c1dc1ed1bbdd34b9c130aa7ad13daa0f2b7849)

--- tests/autofillservice/src/android/autofillservice/cts/WebViewActivity.java
+++ tests/autofillservice/src/android/autofillservice/cts/WebViewActivity.java
@@ -17,6 +17,8 @@ package android.autofillservice.cts;
 
 import static android.autofillservice.cts.Timeouts.WEBVIEW_TIMEOUT;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -112,6 +114,7 @@ public class WebViewActivity extends AbstractAutoFillActivity {
 
             });
             mWebView.loadUrl(FAKE_URL);
+            assertThat(mWebView.isAutofillEnabled()).isTrue();
         });
 
         // Wait until it's loaded.
