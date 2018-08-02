fe4f53b432c5e58611bdca107e9676bdeaa3676c
Felipe Leme <felipeal@google.com>
2018-08-02 14:42:05 -0700

    Changed Autofill tests to check for UPDATE instead of SAVE when necessary.
    Test: atest OptionalSaveActivityTest#testShowUpdateWhenUserChangedOptionalValueFromDatasetAndRequiredNotFromDataset
    Test: atest CtsAutoFillServiceTestCases # with a bunch of changes
    
    Fixes: 122848656
    Bug: 80100983
    
    Merged-In: I330de1ac7ecc313dab279254645b7fa373afc09c
    Change-Id: I330de1ac7ecc313dab279254645b7fa373afc09c
    (cherry picked from commit b08897ae8ed7d6c910b550f0c011f5da41d6743e)

--- tests/autofillservice/src/android/autofillservice/cts/LoginActivityTest.java
+++ tests/autofillservice/src/android/autofillservice/cts/LoginActivityTest.java
@@ -854,7 +854,7 @@ public class LoginActivityTest extends AbstractLoginActivityTestCase {
         assertWithMessage("Wrong welcome msg").that(actualMessage).isEqualTo(expectedMessage);
 
         // Assert the snack bar is shown and tap "Save".
-        mUiBot.saveForAutofill(true, SAVE_DATA_TYPE_PASSWORD);
+        mUiBot.updateForAutofill(true, SAVE_DATA_TYPE_PASSWORD);
 
         final SaveRequest saveRequest = sReplier.getNextSaveRequest();
 
@@ -957,7 +957,7 @@ public class LoginActivityTest extends AbstractLoginActivityTestCase {
             assertWithMessage("Wrong welcome msg").that(actualMessage).isEqualTo(expectedMessage);
 
             // Assert the snack bar is shown and tap "Save".
-            mUiBot.saveForAutofill(true, SAVE_DATA_TYPE_PASSWORD);
+            mUiBot.updateForAutofill(true, SAVE_DATA_TYPE_PASSWORD);
 
             final SaveRequest saveRequest = sReplier.getNextSaveRequest();
 
