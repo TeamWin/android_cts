ff401f0e5ef2c897bde8c97b7c3198f4dfbe3ef5
JP Sugarbroad <jpsugar@google.com>
2018-09-04 14:24:00 -0700

    [RESTRICT AUTOMERGE] Split off POC tests to separate module
    Bug: 113208166
    Change-Id: I29a98256547c8b5f26829661862db0e679a61cec

--- hostsidetests/security/Android.mk
+++ hostsidetests/security/Android.mk
@@ -18,8 +18,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_JAVA_RESOURCE_DIRS := res
-
 LOCAL_MODULE_TAGS := optional
 
 # tag this module as a cts test artifact
