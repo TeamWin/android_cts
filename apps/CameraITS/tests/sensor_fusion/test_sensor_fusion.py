134b5f338522bd3ef8b12d24128853e472e96137
MengYuan Sung <mengyuan.x.sung@sony.com>
2018-10-16 14:50:08 +0800

    CameraITS: sensor_fusion/test_sensor_fusion failed when running ITS
    Reason:
    GYRO is not supported on all Android devices.
    
    Solution:
    Add one condition to check if GYRO is supported,
    and if not, skip this test case.
    
    Bug: 117590936
    Bug: 119296003
    
    Change-Id: I9d3586904caf08590637798735a3290e846f1966
    Merged-In: I9d3586904caf08590637798735a3290e846f1966

--- apps/CameraITS/tests/sensor_fusion/test_sensor_fusion.py
+++ apps/CameraITS/tests/sensor_fusion/test_sensor_fusion.py
@@ -430,7 +430,8 @@ def collect_data(fps, w, h, test_length):
         props = cam.get_camera_properties()
         its.caps.skip_unless(its.caps.sensor_fusion(props) and
                              its.caps.manual_sensor(props) and
-                             props["android.lens.facing"] != FACING_EXTERNAL)
+                             props["android.lens.facing"] != FACING_EXTERNAL and
+                             cam.get_sensors().get("gyro"))
 
         print "Starting sensor event collection"
         cam.start_sensor_events()
