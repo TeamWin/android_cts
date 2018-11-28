bbd7ee6343021986229406debbe8e1213f46d738
Felipe Leme <felipeal@google.com>
2018-11-28 10:32:38 -0800

    Initial CTS tests for Content Capture.
    It's still rudimentar and only checks one scenario (a blank activity that is
    launched and finished), but well, even the Universe started with a Big Bang!
    
    Anyways, the main limitations are:
    
    - Everything runs in one APK; we should have at least one more for the service,
      so that project can be compiled using the system SDK, while the main test
      is using hte testing SDK.
    - The service is still set using Settings; we need to use a Shell command to
      override the service that's defined by framework resources.
    - It's not testing other scenarios like view-level events, neither multiple
      activity changes (like paused / resumed twice)
    
    Test: CtsContentCaptureServiceTestCases
    Bug: 119638528
    
    Change-Id: I62957b634fa5704fde64fb9975a6a69f6f35f633

---
+++ tests/contentcaptureservice/src/android/contentcaptureservice/cts/ActivityLifecycleTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.contentcaptureservice.cts;
+
+import static android.contentcaptureservice.cts.Helper.TAG;
+import static android.contentcaptureservice.cts.common.ActivitiesWatcher.ActivityLifecycle.DESTROYED;
+import static android.contentcaptureservice.cts.common.ActivitiesWatcher.ActivityLifecycle.RESUMED;
+import static android.view.intelligence.ContentCaptureEvent.TYPE_ACTIVITY_PAUSED;
+import static android.view.intelligence.ContentCaptureEvent.TYPE_ACTIVITY_RESUMED;
+import static android.view.intelligence.ContentCaptureEvent.TYPE_ACTIVITY_STARTED;
+import static android.view.intelligence.ContentCaptureEvent.TYPE_ACTIVITY_STOPPED;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Intent;
+import android.contentcaptureservice.cts.CtsSmartSuggestionsService.Session;
+import android.contentcaptureservice.cts.common.ActivitiesWatcher.ActivityWatcher;
+import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+import android.view.intelligence.ContentCaptureEvent;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ActivityLifecycleTest extends AbstractContentCaptureIntegrationTest {
+
+    // TODO(b/119638958): move to superclass ?
+    @Rule
+    public final ActivityTestRule<BlankActivity> activityRule = new ActivityTestRule<>(
+            BlankActivity.class, false, false);
+
+    private BlankActivity mActivity;
+
+    // TODO(b/119638958): move to superclass ?
+    private void launchActivity() {
+        Log.d(TAG, "Launching BlankActivity");
+
+        mActivity = activityRule.launchActivity(new Intent(sContext, BlankActivity.class));
+    }
+
+    // TODO(b/119638958): rename once we add moar tests
+    @Test
+    public void testIt() throws Exception {
+        enableService();
+
+        // TODO(b/119638958): move to super class
+        final ActivityWatcher watcher = mActivitiesWatcher.watch(BlankActivity.class);
+
+        launchActivity();
+        watcher.waitFor(RESUMED);
+
+        mActivity.finish();
+        watcher.waitFor(DESTROYED);
+
+        final CtsSmartSuggestionsService service = CtsSmartSuggestionsService.getInstance();
+        try {
+            final Session session = service.getFinishedSession(BlankActivity.class);
+
+            // TODO(b/119638958): create custom Truth assertions for expect component name and
+            // events
+            assertThat(session.context.getActivityComponent())
+                    .isEqualTo(mActivity.getComponentName());
+
+            final List<ContentCaptureEvent> events = session.getEvents();
+            Log.v(TAG, "events: " + events);
+            assertThat(events).hasSize(4);
+            assertLifecycleEvent(events.get(0), TYPE_ACTIVITY_STARTED);
+            assertLifecycleEvent(events.get(1), TYPE_ACTIVITY_RESUMED);
+            assertLifecycleEvent(events.get(2), TYPE_ACTIVITY_PAUSED);
+            assertLifecycleEvent(events.get(3), TYPE_ACTIVITY_STOPPED);
+        } finally {
+            // TODO(b/119638958): move to @Rule SafeCleaner
+            CtsSmartSuggestionsService.assertNoExceptions();
+        }
+    }
+}
