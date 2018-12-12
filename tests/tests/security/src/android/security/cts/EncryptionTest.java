6ec138656bd0409870a231a54333258ea7bc0601
Greg Kaiser <gkaiser@google.com>
2018-12-12 08:24:13 -0800

    EncryptionTest: Remove speed exemption
    In the Q CDD, in section 9.9, we are removing the exemption which
    allowed slower devices to avoid encryption.  We now require all
    devices to be encrypted at or above this API level.
    
    We change the test to remove this speed exemption starting in Q.
    There are further improvements/enforcements we'd ideally like for
    this test, but leave that as a TODO for now as it's more
    involved.
    
    Test: atest android.security.cts.EncryptionTest#testEncryption
    Bug: 111311698
    Change-Id: I0cfe89f0079a21ab51cbc0fe1884aa55bb1b4348

--- tests/tests/security/src/android/security/cts/EncryptionTest.java
+++ tests/tests/security/src/android/security/cts/EncryptionTest.java
@@ -23,6 +23,7 @@ import android.test.AndroidTestCase;
 import junit.framework.TestCase;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -38,6 +39,9 @@ public class EncryptionTest extends AndroidTestCase {
 
     private static final int MIN_API_LEVEL = 23;
 
+    // First API level where there are no speed exemptions.
+    private static final int MIN_ALL_SPEEDS_API_LEVEL = Build.VERSION_CODES.Q;
+
     private static final String TAG = "EncryptionTest";
 
     private static native boolean deviceIsEncrypted();
@@ -49,12 +53,28 @@ public class EncryptionTest extends AndroidTestCase {
         return PropertyUtil.getFirstApiLevel() >= MIN_API_LEVEL;
     }
 
+    private boolean isEligibleForPerformanceExemption() {
+        return PropertyUtil.getFirstApiLevel() < MIN_ALL_SPEEDS_API_LEVEL;
+    }
+
+    // Note: aesIsFast() takes ~2 second to run, so it's worth rearranging
+    //     test logic to delay calling this.
+    private boolean isExemptByPerformance() {
+        // In older API levels, we grant an exemption if AES is not fast enough.
+        return (isEligibleForPerformanceExemption() && !aesIsFast());
+    }
+
     public void testEncryption() throws Exception {
         if (!isRequired() || deviceIsEncrypted()) {
             return;
         }
 
-        // Required if performance is sufficient
-        assertFalse("Device encryption is required", aesIsFast());
+        // Required if performance is sufficient or on a Q+ build.
+        assertTrue("Device encryption is required", isExemptByPerformance());
+
+        // TODO(b/111311698): If we're able to determine if the hardware
+        //     has AES instructions, confirm that AES, and only AES,
+        //     is in use.  If the hardware does not have AES instructions,
+        //     confirm that either AES or Adiantum is in use.
     }
 }
