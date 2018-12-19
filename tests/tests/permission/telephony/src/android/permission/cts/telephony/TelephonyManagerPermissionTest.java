3cb19beddbd7caaca62f802a75cfaac9f505edc5
Polina Bondarenko <pbond@google.com>
2018-12-19 10:00:18 +0100

    Test TelephonyManager APIs without READ_PHONE_STATE perm.
    Put TelephonyManagerPermissionTest into the separated APK to not have
    READ_PHONE_STATE permission granted.
    
    Bug: 120788919
    Test: cts-tradefed run cts --module CtsPermissionTestCasesTelephony
    Change-Id: I72d7937acbab0e08c1afb41d49d29fc8ca382dca

--- tests/tests/permission/telephony/src/android/permission/cts/telephony/TelephonyManagerPermissionTest.java
+++ tests/tests/permission/telephony/src/android/permission/cts/telephony/TelephonyManagerPermissionTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.permission.cts;
+package android.permission.cts.telephony;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
