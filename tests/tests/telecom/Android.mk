db6fb6546718843b4f95297c8ed9fa0598ccadbd
Dan Willemsen <dwillemsen@google.com>
2019-01-07 14:41:45 -0800

    Fix typo in makefile
    This was caught via build warning:
    
    build/make/core/java_common.mk:188: warning: cts/tests/tests/telecom/Android.mk: CtsTelecomTestCases: Unused source files: src/android/telecom/cts/WiredHeadsetTest.java)
    
    Test: treehugger
    Change-Id: Id962870ba21210f20e4e73f72ae59a15c8273126

--- tests/tests/telecom/Android.mk
+++ tests/tests/telecom/Android.mk
@@ -31,7 +31,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_JAVA_LIBRARIES := android.test.base.stubs
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)) \
+LOCAL_SRC_FILES := $(call all-java-files-under, src) \
                    $(call all-java-files-under, ThirdPtyInCallServiceTestApp) \
                    $(call all-Iaidl-files-under, ThirdPtyInCallServiceTestApp)
 
