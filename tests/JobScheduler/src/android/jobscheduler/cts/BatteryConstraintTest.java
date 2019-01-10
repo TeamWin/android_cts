27141e10c2784a5688a20015259027d35e5a36d3
Yoshiki ISHIGE <ishige@casio.co.jp>
2019-01-10 13:39:56 +0900

    DO NOT MERGE: Make the tests permissive for P
    1. android.jobscheduler.cts.BatteryConstraintTest#testBatteryNotLowConstraintFails_withoutPower
    
    Bug:121302074
    Test: Build CTS
    Test: Device Build No.5197801 passed the following tests
    Test: run cts -o -m CtsJobSchedulerTestCases -t android.jobscheduler.cts.BatteryConstraintTest#testBatteryNotLowConstraintFails_withoutPower
    
    Change-Id: Ib59b83c8af947e7a9c95c09a58ab58dde9a5faff
    Signed-off-by: Yoshiki ISHIGE <ishige@casio.co.jp>

--- tests/JobScheduler/src/android/jobscheduler/cts/BatteryConstraintTest.java
+++ tests/JobScheduler/src/android/jobscheduler/cts/BatteryConstraintTest.java
@@ -32,6 +32,7 @@ import android.os.BatteryManager;
 import android.os.SystemClock;
 import android.provider.Settings;
 import android.util.Log;
+import android.content.res.Resources;
 
 import com.android.compatibility.common.util.SystemUtil;
 
@@ -49,6 +50,7 @@ public class BatteryConstraintTest extends ConstraintTest {
     public static final int BATTERY_JOB_ID = BatteryConstraintTest.class.hashCode();
 
     private JobInfo.Builder mBuilder;
+	private int mLowBatteryWarningLevel = 15;
     /**
      * Record of the previous state of power save mode trigger level to reset it after the test
      * finishes.
@@ -59,6 +61,9 @@ public class BatteryConstraintTest extends ConstraintTest {
     public void setUp() throws Exception {
         super.setUp();
 
+        mLowBatteryWarningLevel = Resources.getSystem().getInteger(
+                     Resources.getSystem().getIdentifier(
+                             "config_lowBatteryWarningLevel", "integer", "android"));
         // Disable power save mode as some devices may turn off Android when power save mode is
         // enabled, causing the test to fail.
         mPreviousLowPowerTriggerLevel = Settings.Global.getInt(getContext().getContentResolver(),
@@ -268,7 +273,7 @@ public class BatteryConstraintTest extends ConstraintTest {
      * the battery level is critical and not on power.
      */
     public void testBatteryNotLowConstraintFails_withoutPower() throws Exception {
-        setBatteryState(false, 5);
+        setBatteryState(false, mLowBatteryWarningLevel);
         // setBatteryState() waited for the charging/not-charging state to formally settle,
         // but battery level reporting lags behind that.  wait a moment to let that happen
         // before proceeding.
@@ -303,8 +308,8 @@ public class BatteryConstraintTest extends ConstraintTest {
                 kTestEnvironment.awaitExecution());
 
         // And check that the job is stopped if battery goes low again.
-        setBatteryState(false, 5);
-        setBatteryState(false, 4);
+        setBatteryState(false, mLowBatteryWarningLevel);
+        setBatteryState(false, mLowBatteryWarningLevel - 1);
         waitFor(2_000);
         verifyChargingState(false);
         verifyBatteryNotLowState(false);
