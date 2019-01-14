b28aa6630cf5d8b018e91b44e917a0706607e74f
Robert Carr <racarr@google.com>
2019-01-14 15:14:55 -0800

    Tests for SDK SurfaceControl API
    Modelled off ASurfaceControlTest
    
    Bug: 111297488
    Test: Indeed
    Change-Id: I45520d7e337f1976603b42f2de08d856afe58f62

---
+++ tests/tests/view/src/android/view/cts/SurfaceControlTest.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.view.cts;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+import android.view.cts.surfacevalidator.CapturedActivity;
+import android.view.cts.surfacevalidator.PixelChecker;
+import android.view.cts.surfacevalidator.PixelColor;
+import android.view.cts.surfacevalidator.SurfaceControlTestCase;
+import android.view.SurfaceControl;
+import android.view.Surface;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+@LargeTest
+public class SurfaceControlTest {
+    private static final int DEFAULT_LAYOUT_WIDTH = 100;
+    private static final int DEFAULT_LAYOUT_HEIGHT = 100;
+    private static final int DEFAULT_BUFFER_WIDTH = 640;
+    private static final int DEFAULT_BUFFER_HEIGHT = 480;
+
+    @Rule
+    public ActivityTestRule<CapturedActivity> mActivityRule =
+            new ActivityTestRule<>(CapturedActivity.class);
+
+    @Rule
+    public TestName mName = new TestName();
+    private CapturedActivity mActivity;
+
+    private void verifyTest(SurfaceControlTestCase.ParentSurfaceConsumer psc,
+            PixelChecker pixelChecker) throws Throwable {
+        mActivity.verifyTest(new SurfaceControlTestCase(psc, null,
+                        pixelChecker, DEFAULT_LAYOUT_WIDTH, DEFAULT_LAYOUT_HEIGHT,
+                        DEFAULT_BUFFER_WIDTH, DEFAULT_BUFFER_HEIGHT),
+                mName);
+    }
+
+    @Before
+    public void setup() {
+        mActivity = mActivityRule.getActivity();
+        mActivity.dismissPermissionDialog();
+    }
+
+    /**
+     * Want to be especially sure we don't leave up the permission dialog, so try and dismiss
+     * after test.
+     */
+    @After
+    public void tearDown() throws UiObjectNotFoundException {
+        mActivity.dismissPermissionDialog();
+    }
+
+    @Test
+    public void testLifecycle() {
+        final SurfaceControl.Builder b = new SurfaceControl.Builder();
+        final SurfaceControl sc = b.build();
+
+        assertTrue("Failed to build SurfaceControl", sc != null);
+        assertTrue(sc.isValid());
+        sc.release();
+        assertFalse(sc.isValid());
+    }
+
+    private SurfaceControl buildDefaultSurface(SurfaceControl parent) {
+        return new SurfaceControl.Builder()
+            .setBufferSize(DEFAULT_BUFFER_WIDTH, DEFAULT_BUFFER_HEIGHT)
+            .setParent(parent)
+            .build();
+
+    }
+
+    void fillWithColor(SurfaceControl sc, int color) {
+        Surface s = new Surface(sc);
+
+        Canvas c = s.lockHardwareCanvas();
+        c.drawColor(color);
+        s.unlockCanvasAndPost(c);
+    }
+
+    private SurfaceControl buildDefaultRedSurface(SurfaceControl parent) {
+        final SurfaceControl sc = buildDefaultSurface(parent);
+        fillWithColor(sc, Color.RED);
+        return sc;
+    }
+
+    /**
+     * Verify that showing a 100x100 surface filled with RED produces roughly 10,000 red pixels.
+     */
+    @Test
+    public void testShow() throws Throwable {
+        verifyTest(
+                new SurfaceControlTestCase.ParentSurfaceConsumer () {
+                    @Override
+                    public void addChildren(SurfaceControl parent) {
+                        final SurfaceControl sc = buildDefaultRedSurface(parent);
+
+                        new SurfaceControl.Transaction().setVisibility(sc, true).apply();
+
+                        sc.release();
+                    }
+                },
+                new PixelChecker(PixelColor.RED) { //10000
+                    @Override
+                    public boolean checkPixels(int pixelCount, int width, int height) {
+                        return pixelCount > 9000 && pixelCount < 11000;
+                    }
+                });
+    }
+
+    /**
+     * The same setup as testShow, however we hide the surface and verify that we don't see Red.
+     */
+    @Test
+    public void testHide() throws Throwable {
+        verifyTest(
+                new SurfaceControlTestCase.ParentSurfaceConsumer () {
+                    @Override
+                    public void addChildren(SurfaceControl parent) {
+                        final SurfaceControl sc = buildDefaultRedSurface(parent);
+
+                        new SurfaceControl.Transaction().setVisibility(sc, false).apply();
+
+                        sc.release();
+                    }
+                },
+                new PixelChecker(PixelColor.BLACK) { //10000
+                    @Override
+                    public boolean checkPixels(int pixelCount, int width, int height) {
+                        return pixelCount > 9000 && pixelCount < 11000;
+                    }
+                });
+    }
+
+    /**
+     * Here we use the same red-surface set up but construct it off-screen and then re-parent it.
+     */
+    @Test
+    public void testReparent() throws Throwable {
+        verifyTest(
+                new SurfaceControlTestCase.ParentSurfaceConsumer () {
+                    @Override
+                    public void addChildren(SurfaceControl parent) {
+                        final SurfaceControl sc = buildDefaultRedSurface(null);
+
+                        new SurfaceControl.Transaction().setVisibility(sc, true)
+                            .reparent(sc, parent)
+                            .apply();
+
+                        sc.release();
+                    }
+                },
+                new PixelChecker(PixelColor.RED) { //10000
+                    @Override
+                    public boolean checkPixels(int pixelCount, int width, int height) {
+                        return pixelCount > 9000 && pixelCount < 11000;
+                    }
+                });
+    }
+}
