005aa077e61bc9e527b6f949b63152c0d2158c34
Felipe Leme <felipeal@google.com>
2019-02-04 13:01:21 -0800

    Changed CtsContentCaptureServiceTestCases to use test_current
    Test: atest CtsContentCaptureServiceTestCases
    
    Fixes: 123584359
    
    Change-Id: I53a2fe5a9e08cb8548e20332cf3baaf61914d2fb

--- tests/contentcaptureservice/Android.mk
+++ tests/contentcaptureservice/Android.mk
@@ -36,6 +36,6 @@ LOCAL_COMPATIBILITY_SUITE := cts vts general-tests cts_instant
 
 LOCAL_PACKAGE_NAME := CtsContentCaptureServiceTestCases
 
-LOCAL_SDK_VERSION := system_current
+LOCAL_SDK_VERSION := test_current
 
 include $(BUILD_CTS_PACKAGE)
