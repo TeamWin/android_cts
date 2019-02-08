d6b79b4f10edee0a6b81ba9f1ee2a7b44cc66409
Aurimas Liutikas <aurimas@google.com>
2019-02-07 17:55:07 -0800

    Add more testing attribute debugging.
    Test: atest CtsViewTestCases:android.view.cts.ViewAttributeTest
          atest CtsWidgetTestCases:android.widget.cts.WidgetAttributeTest
    Change-Id: I2e57fac9099d9a7674569bf70cdccea4fdbb28c3

--- tests/tests/view/src/android/view/cts/ViewAttributeTest.java
+++ tests/tests/view/src/android/view/cts/ViewAttributeTest.java
@@ -42,7 +42,7 @@ import java.util.Map;
 
 @MediumTest
 @RunWith(AndroidJUnit4.class)
-public class ViewStyleTest {
+public class ViewAttributeTest {
 
     @Rule
     public ActivityTestRule<Activity> mActivityRule =
@@ -72,7 +72,8 @@ public class ViewStyleTest {
     public void testGetExplicitStyle() {
         Context context = InstrumentationRegistry.getTargetContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-        LinearLayout rootView = (LinearLayout) inflater.inflate(R.layout.view_style_layout, null);
+        LinearLayout rootView =
+                (LinearLayout) inflater.inflate(R.layout.view_attribute_layout, null);
         View view1 = rootView.findViewById(R.id.view1);
         assertEquals(R.style.ExplicitStyle1, view1.getExplicitStyle());
 
@@ -89,12 +90,17 @@ public class ViewStyleTest {
     @Test
     public void testGetAttributeResolutionStack() {
         LayoutInflater inflater = LayoutInflater.from(mActivityRule.getActivity());
-        LinearLayout rootView = (LinearLayout) inflater.inflate(R.layout.view_style_layout, null);
+        LinearLayout rootView =
+                (LinearLayout) inflater.inflate(R.layout.view_attribute_layout, null);
+        List<Integer> stackRootView = rootView.getAttributeResolutionStack();
+        assertEquals(1, stackRootView.size());
+        assertEquals(R.layout.view_attribute_layout, stackRootView.get(0).intValue());
+
         // View that has an explicit style ExplicitStyle1 set via style = ...
         View view1 = rootView.findViewById(R.id.view1);
         List<Integer> stackView1 = view1.getAttributeResolutionStack();
         assertEquals(3, stackView1.size());
-        assertEquals(R.layout.view_style_layout, stackView1.get(0).intValue());
+        assertEquals(R.layout.view_attribute_layout, stackView1.get(0).intValue());
         assertEquals(R.style.ExplicitStyle1, stackView1.get(1).intValue());
         assertEquals(R.style.ParentOfExplicitStyle1, stackView1.get(2).intValue());
 
@@ -103,7 +109,7 @@ public class ViewStyleTest {
         Button button1 = rootView.findViewById(R.id.button1);
         List<Integer> stackButton1 = button1.getAttributeResolutionStack();
         assertEquals(3, stackButton1.size());
-        assertEquals(R.layout.view_style_layout, stackButton1.get(0).intValue());
+        assertEquals(R.layout.view_attribute_layout, stackButton1.get(0).intValue());
         assertEquals(R.style.MyButtonStyle, stackButton1.get(1).intValue());
         assertEquals(R.style.MyButtonStyleParent, stackButton1.get(2).intValue());
 
@@ -112,7 +118,7 @@ public class ViewStyleTest {
         Button button2 = rootView.findViewById(R.id.button2);
         List<Integer> stackButton2 = button2.getAttributeResolutionStack();
         assertEquals(5, stackButton2.size());
-        assertEquals(R.layout.view_style_layout, stackButton2.get(0).intValue());
+        assertEquals(R.layout.view_attribute_layout, stackButton2.get(0).intValue());
         assertEquals(R.style.ExplicitStyle1, stackButton2.get(1).intValue());
         assertEquals(R.style.ParentOfExplicitStyle1, stackButton2.get(2).intValue());
         assertEquals(R.style.MyButtonStyle, stackButton2.get(3).intValue());
@@ -122,16 +128,20 @@ public class ViewStyleTest {
     @Test
     public void testGetAttributeSourceResourceMap() {
         LayoutInflater inflater = LayoutInflater.from(mActivityRule.getActivity());
-        LinearLayout rootView = (LinearLayout) inflater.inflate(R.layout.view_style_layout, null);
+        LinearLayout rootView =
+                (LinearLayout) inflater.inflate(R.layout.view_attribute_layout, null);
+        Map<Integer, Integer> attributeMapRootView = rootView.getAttributeSourceResourceMap();
+        assertEquals(R.layout.view_attribute_layout,
+                (attributeMapRootView.get(android.R.attr.orientation)).intValue());
+
         // View that has an explicit style ExplicitStyle1 set via style = ...
         View view1 = rootView.findViewById(R.id.view1);
         Map<Integer, Integer> attributeMapView1 = view1.getAttributeSourceResourceMap();
-        assertEquals(9, attributeMapView1.size());
         assertEquals(R.style.ExplicitStyle1,
                 (attributeMapView1.get(android.R.attr.padding)).intValue());
         assertEquals(R.style.ParentOfExplicitStyle1,
                 (attributeMapView1.get(android.R.attr.paddingLeft)).intValue());
-        assertEquals(R.layout.view_style_layout,
+        assertEquals(R.layout.view_attribute_layout,
                 (attributeMapView1.get(android.R.attr.paddingTop)).intValue());
     }
 }
