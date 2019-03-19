471c39d8fc14a766da713ddfb5fea64363c1250d
Sunny Goyal <sunnygoyal@google.com>
2019-03-19 14:08:06 -0700

    Updating CTS as per AppTarget API changes
    Test: Everything compiles
    Change-Id: I5a301856f8b13d544d73a6f6e8064c59fe42926f

--- tests/apppredictionservice/src/android/apppredictionservice/cts/AppPredictionServiceTest.java
+++ tests/apppredictionservice/src/android/apppredictionservice/cts/AppPredictionServiceTest.java
@@ -219,8 +219,10 @@ public class AppPredictionServiceTest {
         List<AppTarget> targets = new ArrayList<>();
         int n = (int) (Math.random() * 20);
         for (int i = 0; i < n; i++) {
-            targets.add(new AppTarget(new AppTargetId(String.valueOf(i)), "test.pkg",
-                    "test.class." + i, UserHandle.CURRENT));
+            targets.add(new AppTarget.Builder(new AppTargetId(String.valueOf(i)))
+                    .setTarget("test.pkg", UserHandle.CURRENT)
+                    .setClassName("test.class." + i)
+                    .build());
         }
         return targets;
     }
