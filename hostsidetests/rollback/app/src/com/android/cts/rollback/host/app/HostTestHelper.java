7fa6f6aaf9092a3ff580df582b4d9e07dfb6f880
Richard Uhler <ruhler@google.com>
2019-03-25 15:44:09 +0000

    Add host CTS test for RollbackManager APIs.
    For testing staged rollbacks, which require reboot during the test.
    
    Bug: 127933960
    Test: atest CtsRollbackManagerHostTestCases
    Change-Id: I408fcf543227a6c3db3fb8c2f161a9cec96624d1

---
+++ hostsidetests/rollback/app/src/com/android/cts/rollback/host/app/HostTestHelper.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.cts.rollback.host.app;
+
+import static com.android.cts.rollback.lib.RollbackInfoSubject.assertThat;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.Manifest;
+import android.content.rollback.RollbackInfo;
+
+import androidx.test.InstrumentationRegistry;
+
+import com.android.cts.rollback.lib.Install;
+import com.android.cts.rollback.lib.Rollback;
+import com.android.cts.rollback.lib.TestApp;
+import com.android.cts.rollback.lib.Utils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+
+/**
+ * On-device helper test methods used for host-driven rollback tests.
+ */
+@RunWith(JUnit4.class)
+public class HostTestHelper {
+
+    /**
+     * Adopts common permissions needed to test rollbacks.
+     */
+    @Before
+    public void setup() throws InterruptedException, IOException {
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                .adoptShellPermissionIdentity(
+                    Manifest.permission.INSTALL_PACKAGES,
+                    Manifest.permission.DELETE_PACKAGES,
+                    Manifest.permission.TEST_MANAGE_ROLLBACKS);
+    }
+
+    /**
+     * Drops adopted shell permissions.
+     */
+    @After
+    public void teardown() throws InterruptedException, IOException {
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                .dropShellPermissionIdentity();
+    }
+
+
+    /**
+     * Test rollbacks of staged installs involving only apks.
+     * Commits TestApp.A2 as a staged install with rollback enabled.
+     */
+    @Test
+    public void testApkOnlyEnableRollback() throws Exception {
+        Utils.uninstall(TestApp.A);
+        Install.single(TestApp.A1).commit();
+        Install.single(TestApp.A2).setStaged().setEnableRollback().commit();
+
+        // At this point, the host test driver will reboot the device and run
+        // testApkOnlyCommitRollback().
+    }
+
+    /**
+     * Test rollbacks of staged installs involving only apks.
+     * Confirms a staged rollback is available for TestApp.A2 and commits the
+     * rollback.
+     */
+    @Test
+    public void testApkOnlyCommitRollback() throws Exception {
+        assertThat(Utils.getInstalledVersion(TestApp.A)).isEqualTo(2);
+        RollbackInfo available = Utils.getAvailableRollback(TestApp.A);
+        assertThat(available).isStaged();
+        assertThat(available).packagesContainsExactly(
+                Rollback.from(TestApp.A2).to(TestApp.A1));
+        assertThat(Utils.getCommittedRollback(TestApp.A)).isNull();
+
+        Utils.rollback(available.getRollbackId(), TestApp.A2);
+        RollbackInfo committed = Utils.getCommittedRollback(TestApp.A);
+        assertThat(committed).hasRollbackId(available.getRollbackId());
+        assertThat(committed).isStaged();
+        assertThat(committed).packagesContainsExactly(
+                Rollback.from(TestApp.A2).to(TestApp.A1));
+        assertThat(committed).causePackagesContainsExactly(TestApp.A2);
+        assertThat(committed.getCommittedSessionId()).isNotEqualTo(-1);
+
+        // Note: The app is not rolled back until after the rollback is staged
+        // and the device has been rebooted.
+        Utils.waitForSessionReady(committed.getCommittedSessionId());
+        assertThat(Utils.getInstalledVersion(TestApp.A)).isEqualTo(2);
+
+        // At this point, the host test driver will reboot the device and run
+        // testApkOnlyConfirmRollback().
+    }
+
+    /**
+     * Test rollbacks of staged installs involving only apks.
+     * Confirms TestApp.A2 was rolled back.
+     */
+    @Test
+    public void testApkOnlyConfirmRollback() throws Exception {
+        assertThat(Utils.getInstalledVersion(TestApp.A)).isEqualTo(1);
+
+        RollbackInfo committed = Utils.getCommittedRollback(TestApp.A);
+        assertThat(committed).isStaged();
+        assertThat(committed).packagesContainsExactly(
+                Rollback.from(TestApp.A2).to(TestApp.A1));
+        assertThat(committed).causePackagesContainsExactly(TestApp.A2);
+        assertThat(committed.getCommittedSessionId()).isNotEqualTo(-1);
+
+        Utils.uninstall(TestApp.A);
+    }
+}
