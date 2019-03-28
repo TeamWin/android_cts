30a005b56d71586f2cda8666569676f9525b0537
Aaron Holden <aaronholden@google.com>
2019-03-27 19:20:42 -0700

    Remove client-side BL auth validation
    These checks are now performed on the server, and logic to fail
    unauthenticated tests is built into the returned remote configuration.
    
    bug:126442904
    Test: run gts -m GtsPlacementTestCases (rm auth file first)
    Change-Id: If59814e9b4a0524de1f202ff43db02ec9bd3f671

--- common/device-side/util-axt/src/com/android/compatibility/common/util/BusinessLogicTestCase.java
+++ common/device-side/util-axt/src/com/android/compatibility/common/util/BusinessLogicTestCase.java
@@ -50,7 +50,6 @@ public class BusinessLogicTestCase {
     @Before
     public void handleBusinessLogic() {
         loadBusinessLogic();
-        ensureAuthenticated();
         executeBusinessLogic();
     }
 
@@ -77,27 +76,6 @@ public class BusinessLogicTestCase {
         } else {
             mCanReadBusinessLogic = false;
         }
-    }
-
-    protected void ensureAuthenticated() {
-        if (!mCanReadBusinessLogic) {
-            // super class handles the condition that the service is unavailable.
-            return;
-        }
-
-        if (!mBusinessLogic.mConditionalTestsEnabled) {
-            skipTest("Execution of device specific tests is not enabled. "
-                    + "Enable with '--conditional-business-logic-tests-enabled'");
-        }
-
-        if (mBusinessLogic.isAuthorized()) {
-            // Run test as normal.
-            return;
-        }
-        String message = mBusinessLogic.getAuthenticationStatusMessage();
-
-        // Fail test since request was not authorized.
-        failTest(String.format("Unable to execute because %s.", message));
     }
 
     protected static Instrumentation getInstrumentation() {
