0daf4e07bfca854c989b46ef5ce1e9995824916e
Paul Duffin <paulduffin@google.com>
2019-05-10 14:28:18 +0100

    Allow MethodHandle.invoke/invokeExact to be used in tests
    If LOCAL_MIN_SDK_VERSION is set to less than 26 then attempting to use
    MethodHandle.invoke/invokeExact results in a build error:
    
    Error: MethodHandle.invoke and MethodHandle.invokeExact are only supported starting with Android O (--min-api 26)
    
    That is blocking refactoring of this code.
    
    Also, this ensures that if the minSdkVersion attribute is set in an
    AndroidManifest.xml then the LOCAL_MIN_SDK_VERSION is set to the same
    value in the corresponding build module. See change
    I3883c56394bf9b2b47e97518b57af2d1cb79cb79 for more information.
    
    Test: atest -p cts/tests/signature
    Change-Id: I4d86445ed6d4e89a91c0ca1e0159e49b40808c17

--- tests/signature/api-check/hidden-api-blacklist-27-api/Android.mk
+++ tests/signature/api-check/hidden-api-blacklist-27-api/Android.mk
@@ -19,4 +19,5 @@ LOCAL_PACKAGE_NAME := CtsHiddenApiBlacklistApi27TestCases
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 LOCAL_SIGNATURE_API_FILES := hiddenapi_flags.csv
 LOCAL_JNI_SHARED_LIBRARIES := libcts_dexchecker
+LOCAL_MIN_SDK_VERSION := 27
 include $(LOCAL_PATH)/../build_signature_apk.mk
