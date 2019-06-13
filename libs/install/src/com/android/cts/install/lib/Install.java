44eff52e9b1d745f63bc8a7cf28e964f6e4bf9c0
shafik <shafik@google.com>
2019-06-13 18:42:21 +0100

    Migrate AtomicInstallTest to cts.install.lib
    Add API to cts.install.lib that supports AtomicInstallTest needs.
    Migrate AtomicInstallTest to cts.install.lib
    Reuse test apps from cts.install.lib.
    
    Test: atest CtsAtomicInstallTestCases
    Bug: 134653708
    Fixes: 135182520
    
    Change-Id: Ia256403a8aa24f6fd121a4d82a193ad430be7af7

--- libs/install/src/com/android/cts/install/lib/Install.java
+++ libs/install/src/com/android/cts/install/lib/Install.java
@@ -16,27 +16,40 @@
 
 package com.android.cts.install.lib;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import android.content.Intent;
 import android.content.pm.PackageInstaller;
-
-import androidx.test.InstrumentationRegistry;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Helper class for installing test apps.
+ * Builder class for installing test apps and creating install sessions.
  */
 public class Install {
-    private final boolean mIsMultiPackage;
+    // The collection of apps to be installed with parameters inherited from parent Install object.
     private final TestApp[] mTestApps;
+    // The collection of apps to be installed with parameters independent of parent Install object.
+    private final Install[] mChildInstalls;
+    // Indicates whether Install represents a multiPackage install.
+    private final boolean mIsMultiPackage;
+    // PackageInstaller.Session parameters.
     private boolean mIsStaged = false;
     private boolean mEnableRollback = false;
+    private int mSessionMode = PackageInstaller.SessionParams.MODE_FULL_INSTALL;
 
     private Install(boolean isMultiPackage, TestApp... testApps) {
         mIsMultiPackage = isMultiPackage;
         mTestApps = testApps;
+        mChildInstalls = new Install[0];
+    }
+
+    private Install(boolean isMultiPackage, Install... installs) {
+        mIsMultiPackage = isMultiPackage;
+        mTestApps = new TestApp[0];
+        mChildInstalls = installs;
     }
 
     /**
@@ -51,6 +64,28 @@ public class Install {
      */
     public static Install multi(TestApp... testApps) {
         return new Install(true, testApps);
+    }
+
+    /**
+     * Creates an Install builder from separate Install builders. The newly created builder
+     * will be responsible for building the parent session, while each one of the other builders
+     * will be responsible for building one of the child sessions.
+     *
+     * <p>Modifications to the parent install are not propagated to the child installs,
+     * and vice versa. This gives more control over a multi install session,
+     * e.g. can setStaged on a subset of the child sessions or setStaged on a child session but
+     * not on the parent session.
+     *
+     * <p>It's encouraged to use {@link #multi} that receives {@link TestApp}s
+     * instead of {@link Install}s. This variation of {@link #multi} should be used only if it's
+     * necessary to modify parameters in a subset of the installed sessions.
+     */
+    public static Install multi(Install... installs) {
+        for (Install childInstall : installs) {
+            assertThat(childInstall.isMultiPackage()).isFalse();
+        }
+        Install install = new Install(true, installs);
+        return install;
     }
 
     /**
@@ -69,79 +104,25 @@ public class Install {
         return this;
     }
 
-    private static PackageInstaller getPackageInstaller() {
-        return InstrumentationRegistry.getContext().getPackageManager().getPackageInstaller();
-    }
-
     /**
-     * Creates an empty install session with appropriate install params set.
-     *
-     * @return the session id of the newly created session
+     * Sets the session mode {@link PackageInstaller.SessionParams#MODE_INHERIT_EXISTING}.
+     * If it's not set, then the default session mode is
+     * {@link PackageInstaller.SessionParams#MODE_FULL_INSTALL}
      */
-    private int createEmptyInstallSession(boolean multiPackage, boolean isApex)
-            throws IOException {
-        PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(
-                PackageInstaller.SessionParams.MODE_FULL_INSTALL);
-        if (multiPackage) {
-            params.setMultiPackage();
-        }
-        if (isApex) {
-            params.setInstallAsApex();
-        }
-        if (mIsStaged) {
-            params.setStaged();
-        }
-        params.setEnableRollback(mEnableRollback);
-        return getPackageInstaller().createSession(params);
-    }
-
-    /**
-     * Creates an install session for the given test app.
-     *
-     * @return the session id of the newly created session.
-     */
-    private int createInstallSession(TestApp app) throws IOException {
-        int sessionId = createEmptyInstallSession(/*multiPackage*/false, app.isApex());
-        PackageInstaller.Session session = getPackageInstaller().openSession(sessionId);
-
-        ClassLoader loader = TestApp.class.getClassLoader();
-        for (String resourceName : app.getResourceNames()) {
-            try (OutputStream os = session.openWrite(resourceName, 0, -1);
-                    InputStream is = loader.getResourceAsStream(resourceName);) {
-                if (is == null) {
-                    throw new IOException("Resource " + resourceName + " not found");
-                }
-                byte[] buffer = new byte[4096];
-                int n;
-                while ((n = is.read(buffer)) >= 0) {
-                    os.write(buffer, 0, n);
-                }
-            }
-        }
-        session.close();
-        return sessionId;
+    public Install setSessionMode(int sessionMode) {
+        mSessionMode = sessionMode;
+        return this;
     }
 
     /**
      * Commits the install.
      *
      * @return the session id of the install session, if the session is successful.
+     * @throws AssertionError if the install doesn't succeed.
      */
     public int commit() throws IOException, InterruptedException {
-        final int sessionId;
-        final PackageInstaller.Session session;
-        if (mIsMultiPackage) {
-            sessionId = createEmptyInstallSession(/*multiPackage*/ true, /*isApex*/false);
-            session = getPackageInstaller().openSession(sessionId);
-            for (TestApp app : mTestApps) {
-                session.addChildSessionId(createInstallSession(app));
-            }
-        } else {
-            assert mTestApps.length == 1;
-            sessionId = createInstallSession(mTestApps[0]);
-            session = getPackageInstaller().openSession(sessionId);
-        }
-
+        int sessionId = createSession();
+        PackageInstaller.Session session = InstallUtils.openPackageInstallerSession(sessionId);
         session.commit(LocalIntentSender.getIntentSender());
         Intent result = LocalIntentSender.getIntentSenderResult();
         int status = result.getIntExtra(PackageInstaller.EXTRA_STATUS,
@@ -157,5 +138,83 @@ public class Install {
             InstallUtils.waitForSessionReady(sessionId);
         }
         return sessionId;
+    }
+
+    /**
+     * Kicks off an install flow by creating an install session
+     * and, in the case of a multiPackage install, child install sessions.
+     *
+     * @return the session id of the install session, if the session is successful.
+     */
+    public int createSession() throws IOException {
+        int sessionId;
+        if (isMultiPackage()) {
+            PackageInstaller.Session session;
+            sessionId = createEmptyInstallSession(/*multiPackage*/ true, /*isApex*/false);
+            session = InstallUtils.openPackageInstallerSession(sessionId);
+            for (Install subInstall : mChildInstalls) {
+                session.addChildSessionId(subInstall.createSession());
+            }
+            for (TestApp testApp : mTestApps) {
+                session.addChildSessionId(createSingleInstallSession(testApp));
+            }
+        } else {
+            assert mTestApps.length == 1;
+            sessionId = createSingleInstallSession(mTestApps[0]);
+        }
+        return sessionId;
+    }
+
+    /**
+     * Creates an empty install session with appropriate install params set.
+     *
+     * @return the session id of the newly created session
+     */
+    private int createEmptyInstallSession(boolean multiPackage, boolean isApex)
+            throws IOException {
+        PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(mSessionMode);
+        if (multiPackage) {
+            params.setMultiPackage();
+        }
+        if (isApex) {
+            params.setInstallAsApex();
+        }
+        if (mIsStaged) {
+            params.setStaged();
+        }
+        params.setEnableRollback(mEnableRollback);
+        return InstallUtils.getPackageInstaller().createSession(params);
+    }
+
+    /**
+     * Creates an install session for the given test app.
+     *
+     * @return the session id of the newly created session.
+     */
+    private int createSingleInstallSession(TestApp app) throws IOException {
+        int sessionId = createEmptyInstallSession(/*multiPackage*/false, app.isApex());
+        PackageInstaller.Session session = InstallUtils.getPackageInstaller()
+                .openSession(sessionId);
+
+        ClassLoader loader = TestApp.class.getClassLoader();
+        for (String resourceName : app.getResourceNames()) {
+            try (OutputStream os = session.openWrite(resourceName, 0, -1);
+                 InputStream is = loader.getResourceAsStream(resourceName);) {
+                if (is == null) {
+                    throw new IOException("Resource " + resourceName + " not found");
+                }
+                byte[] buffer = new byte[4096];
+                int n;
+                while ((n = is.read(buffer)) >= 0) {
+                    os.write(buffer, 0, n);
+                }
+            }
+        }
+        session.close();
+        return sessionId;
+    }
+
+    private boolean isMultiPackage() {
+        return mIsMultiPackage;
     }
 }
