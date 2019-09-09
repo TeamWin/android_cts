9d751423a85eec7298222ce46da324b00ea77d38
Julien Desprez <jdesprez@google.com>
2019-09-09 13:11:58 -0700

    Remove cts_instant reference as it's been deleted
    hostside part of it.
    
    Test: make cts
    Bug: 140751247
    Change-Id: I16678bc4a9b2756b440be19870a956247cde5df7
    Exempt-From-Owner-Approval: Clean up of references from a deleted cts_instant suite

--- hostsidetests/testharness/app/Android.mk
+++ hostsidetests/testharness/app/Android.mk
@@ -34,7 +34,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := androidx.test.rules \
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 # tag this module as a cts test artifact
-LOCAL_COMPATIBILITY_SUITE := cts vts general-tests cts_instant
+LOCAL_COMPATIBILITY_SUITE := cts vts general-tests
 
 LOCAL_PACKAGE_NAME := CtsTestHarnessModeDeviceApp
 
