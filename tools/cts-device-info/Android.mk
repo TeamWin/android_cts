e91a22eeddba950808141ee27a6f02739d5a0b7b
Julien Desprez <jdesprez@google.com>
2019-09-25 14:52:47 -0700

    Delete last occurrence of cts_instant in cts/
    Test: make cts
    Bug: 140751247
    Change-Id: I865c2970809893f925ffbbc906374bf47d2db406
    Exempt-From-Owner-Approval: Clean up of references from a deleted cts_instant suite

--- tools/cts-device-info/Android.mk
+++ tools/cts-device-info/Android.mk
@@ -38,7 +38,7 @@ DEVICE_INFO_ACTIVITIES := \
 LOCAL_PACKAGE_NAME := CtsDeviceInfo
 
 # Tag this module as a cts test artifact
-LOCAL_COMPATIBILITY_SUITE := cts vts general-tests sts cts_instant mts
+LOCAL_COMPATIBILITY_SUITE := cts vts general-tests sts mts
 
 include $(BUILD_CTS_DEVICE_INFO_PACKAGE)
 
