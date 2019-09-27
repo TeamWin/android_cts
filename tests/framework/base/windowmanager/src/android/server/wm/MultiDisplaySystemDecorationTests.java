b13a3495756ff053a5a4bf858dc674974f034cf9
Kevin Wang <wk091218@gmail.com>
2019-09-27 16:46:26 +0800

    Skip NavBar and StatusBar test on TV, Watch and IOT devices
    TV, Watch and IOT devices don't have navigation bar and
    status bar, these test cases should be skipped.
    Also fix return value of BarTestUtils#isAssumptionViolated().
    
    Test: run cts -m CtsWindowManagerDeviceTestCases
        android.server.wm.MultiDisplaySystemDecorationTests#testNavBarNotShowingOnDisplayWithoutDecor
        android.server.wm.MultiDisplaySystemDecorationTests#testNavBarShowingOnDisplayWithDecor
        android.server.wm.MultiDisplaySystemDecorationTests#testNavBarNotShowingOnPrivateDisplay
        android.server.wm.EnsureBarContrastTest#test_dontEnsureContrast_darkBars
        android.server.wm.EnsureBarContrastTest#test_dontEnsureContrast_lightBars
    
    Bug: 141733001
    
    Change-Id: If490990aad13f8cca2e5da0333f46b9c5b0bce04
    (cherry picked from commit b37c4b45b1f819501bbe27f4990e32f2ea638e61)

--- tests/framework/base/windowmanager/src/android/server/wm/MultiDisplaySystemDecorationTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/MultiDisplaySystemDecorationTests.java
@@ -24,6 +24,7 @@ import static android.server.wm.app.Components.SINGLE_SECONDARY_HOME_ACTIVITY;
 import static android.server.wm.app.Components.TEST_LIVE_WALLPAPER_SERVICE;
 import static android.server.wm.app.Components.TestLiveWallpaperKeys.COMPONENT;
 import static android.server.wm.app.Components.TestLiveWallpaperKeys.ENGINE_DISPLAY_ID;
+import static android.server.wm.BarTestUtils.assumeHasBars;
 import static android.view.Display.DEFAULT_DISPLAY;
 import static android.view.WindowManager.LayoutParams.TYPE_WALLPAPER;
 
@@ -200,6 +201,7 @@ public class MultiDisplaySystemDecorationTests extends MultiDisplayTestBase {
      */
     @Test
     public void testNavBarShowingOnDisplayWithDecor() throws Exception {
+        assumeHasBars();
         try (final ExternalDisplaySession externalDisplaySession = new ExternalDisplaySession()) {
             final ActivityDisplay newDisplay = externalDisplaySession
                     .setPublicDisplay(true).setShowSystemDecorations(true).createVirtualDisplay();
@@ -213,6 +215,7 @@ public class MultiDisplaySystemDecorationTests extends MultiDisplayTestBase {
      */
     @Test
     public void testNavBarNotShowingOnDisplayWithoutDecor() throws Exception {
+        assumeHasBars();
         try (final ExternalDisplaySession externalDisplaySession = new ExternalDisplaySession()) {
             // Wait for system decoration showing and record current nav states.
             mAmWmState.waitForHomeActivityVisible();
@@ -231,6 +234,7 @@ public class MultiDisplaySystemDecorationTests extends MultiDisplayTestBase {
      */
     @Test
     public void testNavBarNotShowingOnPrivateDisplay() throws Exception {
+        assumeHasBars();
         try (final ExternalDisplaySession externalDisplaySession = new ExternalDisplaySession()) {
             // Wait for system decoration showing and record current nav states.
             mAmWmState.waitForHomeActivityVisible();
