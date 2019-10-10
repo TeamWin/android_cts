021617a1f1630ae7100164de7d1e7074c9579c9f
Leland Miller <lelandmiller@google.com>
2019-10-10 11:15:53 -0700

    Ignore messaging tests without telephony features
    As per the documentation in android.provider.Telephony, these features
    are only supported when the system has FEATURE_TELEPHONY. This change
    effectively ignores all related tests in this case.
    
    This also converts the TelephonyProviderTest to a JUnit test in order to
    support Assume.
    
    Test: atest CtsTelephonyProviderTestCases
    Fixed: 141382247
    Change-Id: I26aa7bf530d65071acabfceadd69ae17fded5cee

--- tests/tests/telephonyprovider/src/android/telephonyprovider/cts/MmsTest.java
+++ tests/tests/telephonyprovider/src/android/telephonyprovider/cts/MmsTest.java
@@ -16,6 +16,8 @@
 
 package android.telephonyprovider.cts;
 
+import static android.telephonyprovider.cts.DefaultSmsAppHelper.assumeTelephony;
+
 import static androidx.test.InstrumentationRegistry.getInstrumentation;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -46,6 +48,7 @@ public class MmsTest {
 
     @Before
     public void setupTestEnvironment() {
+        assumeTelephony();
         cleanup();
         mContentResolver = getInstrumentation().getContext().getContentResolver();
     }
