af25b4ad68996dfc9bfff959b4a90d60774cbc66
Amit Mahajan <amitmahajan@google.com>
2019-10-09 17:35:45 -0700

    Add CTS test for getEcNo()
    Test: atest CellInfoTest#verifyCellSignalStrengthWcdma
    Bug: 139417139
    Change-Id: I07d897f32b7fbd74658ad8024b403737c0f5834e

--- tests/tests/telephony/current/src/android/telephony/cts/CellInfoTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/CellInfoTest.java
@@ -24,9 +24,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import android.Manifest;
-import android.Manifest.permission;
-import android.app.UiAutomation;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Parcel;
@@ -56,8 +53,6 @@ import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.util.Pair;
-
-import androidx.test.InstrumentationRegistry;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -791,6 +786,10 @@ public class CellInfoTest {
         if (mRadioHalVersion >= RADIO_HAL_VERSION_1_2) {
             assertTrue("RSCP Must be valid for WCDMA", wcdma.getRscp() != CellInfo.UNAVAILABLE);
         }
+
+        int ecNo = wcdma.getEcNo();
+        assertTrue("getEcNo() out of range [-24,1], EcNo=" + ecNo,
+                (ecNo >= -24 && ecNo <= 1) || ecNo == CellInfo.UNAVAILABLE);
     }
 
     private void verifyCellSignalStrengthWcdmaParcel(CellSignalStrengthWcdma wcdma) {
