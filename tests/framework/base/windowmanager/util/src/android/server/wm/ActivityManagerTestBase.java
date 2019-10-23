b9db98805070c937ae2fb899bbfe9f9f062b7f06
Bernardo Rufino <brufino@google.com>
2019-10-23 13:09:05 +0100

    CTS for verifying toasts are not clickable
    To verify this, we:
    * Allow reflection via hidden_api_policy setting (to make sure we can construct
      a clickable toast).
    * Send broadcast to app to post a toast.
    * App posts toast and waits for it to be displayed, communicating back
      to test.
    * Test clicks toast and waits for the tap to be propagated back.
    * If click is received, test fails, otherwise, succeeds.
    
    Bug: 128674520
    Test: atest CtsWindowManagerDeviceTestCases:ToastTest
          1) Passes with ag/9585932
          2) Fails without ag/9585932
    
    Change-Id: I6d2903477f8ea86c75d6415890f443414751ab84

--- tests/framework/base/windowmanager/util/src/android/server/wm/ActivityManagerTestBase.java
+++ tests/framework/base/windowmanager/util/src/android/server/wm/ActivityManagerTestBase.java
@@ -525,11 +525,14 @@ public abstract class ActivityManagerTestBase {
         getInstrumentation().getUiAutomation().syncInputTransactions();
     }
 
+    protected void tapOnCenter(Rect bounds, int displayId) {
+        final int tapX = bounds.left + bounds.width() / 2;
+        final int tapY = bounds.top + bounds.height() / 2;
+        tapOnDisplay(tapX, tapY, displayId);
+    }
+
     protected void tapOnStackCenter(ActivityManagerState.ActivityStack stack) {
-        final Rect sideStackBounds = stack.getBounds();
-        final int tapX = sideStackBounds.left + sideStackBounds.width() / 2;
-        final int tapY = sideStackBounds.top + sideStackBounds.height() / 2;
-        tapOnDisplay(tapX, tapY, stack.mDisplayId);
+        tapOnCenter(stack.getBounds(), stack.mDisplayId);
     }
 
     private static void injectMotion(long downTime, long eventTime, int action,
