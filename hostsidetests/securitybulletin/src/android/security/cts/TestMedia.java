f2e4be969f822be93e9df0516aba9a91d66d33b2
Krishna C R <krishna.cr@ittiam.com>
2019-11-21 20:09:02 +0530

    [RESTRICT AUTOMERGE] Updated AdbUtils.java and memutils file
    Added functions in AdbUtils.java which enable testing for both hangs and crashes
    Updated memutils file to support different configurations
    
    Bug: 147497306
    Test: none
    
    Change-Id: I554ac7674317cf203206411b69e6e4ef31883e75

--- hostsidetests/securitybulletin/src/android/security/cts/TestMedia.java
+++ hostsidetests/securitybulletin/src/android/security/cts/TestMedia.java
@@ -24,69 +24,33 @@ import java.util.regex.Pattern;
 @SecurityTest
 public class TestMedia extends SecurityTestCase {
 
-    final static int TIMEOUT_SEC = 9 * 60;
-    final static String RESOURCE_ROOT = "/";
-    final static String TMP_FILE_PATH = "/data/local/tmp/";
 
-    /****************************************************************
-     * To prevent merge conflicts, add tests for N below this comment,
-     * before any existing test methods
-     ****************************************************************/
+    /******************************************************************************
+     * To prevent merge conflicts, add tests for N below this comment, before any
+     * existing test methods
+     ******************************************************************************/
 
     @SecurityTest(minPatchLevel = "2017-07")
     public void testPocCVE_2017_0684() throws Exception {
-        String errPattern[] = {
-                "name: le.h264.encoder  >>> /system/bin/mediaserver <<<"
-                        + "\n.*?(SIGSEGV)",
-                "name: media.codec  >>> omx@1.0-service <<<\n.*?(SIGSEGV)"};
-        runMediaTest("CVE-2017-0684", null, null, getDevice(), errPattern);
+        String processPatternStrings[] = {"mediaserver", "omx@1.0-service"};
+        AdbUtils.runPocAssertNoCrashesNotVulnerable("CVE-2017-0684", null, getDevice(),
+                processPatternStrings);
     }
 
-    /****************************************************************
-     * To prevent merge conflicts, add tests for O below this comment,
-     * before any existing test methods
-     ****************************************************************/
+    /******************************************************************************
+     * To prevent merge conflicts, add tests for O below this comment, before any
+     * existing test methods
+     ******************************************************************************/
 
 
-    /****************************************************************
-     * To prevent merge conflicts, add tests for P below this comment,
-     * before any existing test methods
-     ****************************************************************/
+    /******************************************************************************
+     * To prevent merge conflicts, add tests for P below this comment, before any
+     * existing test methods
+     ******************************************************************************/
 
-    /**
-     * Pushes input files, runs the PoC and checks for crash and hang
-     *
-     * @param binaryName name of the binary
-     * @param inputFiles files required as input
-     * @param arguments arguments for running the binary
-     * @param device device to be run on
-     * @param errPattern error patterns to be checked for
-     */
-    public static void runMediaTest(String binaryName,
-            String inputFiles[], String arguments, ITestDevice device,
-            String processPatternStrings[]) throws Exception {
-        if (inputFiles != null) {
-            for (String tempFile : inputFiles) {
-                AdbUtils.pushResource(RESOURCE_ROOT + tempFile,
-                        TMP_FILE_PATH + tempFile, device);
-            }
-        }
-        AdbUtils.runCommandLine("logcat -c", device);
-        AdbUtils.runWithTimeoutDeleteFiles(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    AdbUtils.runPocNoOutput(binaryName, device,
-                            TIMEOUT_SEC + 30, arguments);
-                } catch (Exception e) {
-                    CLog.w("Exception: " + e.getMessage());
-                }
-            }
-        }, TIMEOUT_SEC * 1000, device, inputFiles);
 
-        AdbUtils.assertNoCrashes(device, binaryName);
-        if (processPatternStrings != null) {
-            AdbUtils.assertNoCrashes(device, processPatternStrings);
-        }
-    }
+    /******************************************************************************
+     * To prevent merge conflicts, add tests for Q below this comment, before any
+     * existing test methods
+     ******************************************************************************/
 }
