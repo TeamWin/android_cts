78f7e001e770b11a0fa9f3d3ad86a13276eaa3f1
Stanislav Zholnin <zholnin@google.com>
2019-11-21 18:05:08 +0000

    Fix CTS test for optimized reporting or DangerousPermissionState atom.
    Test: atest UidAtomTests#testDangerousPermissionState
    
    Change-Id: I86430f278350325c09b6997c300096fa9625a941

--- hostsidetests/statsd/src/android/cts/statsd/atom/UidAtomTests.java
+++ hostsidetests/statsd/src/android/cts/statsd/atom/UidAtomTests.java
@@ -1262,8 +1262,7 @@ public class UidAtomTests extends DeviceAtomTestCase {
             assertThat(permissionState.getUid()).isAtLeast(0);
             assertThat(permissionState.getPackageName()).isNotNull();
 
-            if (permissionState.getPackageName().equals(DEVICE_SIDE_TEST_PACKAGE)) {
-                assertThat(getAppId(permissionState.getUid())).isEqualTo(testAppId);
+            if (getAppId(permissionState.getUid()) == testAppId) {
 
                 if (permissionState.getPermissionName().equals(
                         "android.permission.ACCESS_FINE_LOCATION")) {
