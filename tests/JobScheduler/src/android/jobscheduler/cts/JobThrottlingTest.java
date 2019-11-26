86fd0115d74d6306380f08161110906df9b40c71
jinho.park <jinho.park@lge.com>
2019-11-26 15:53:27 +0900

    Added reset cmd to turn off batterysaver
    Test: cts-tradefed run cts -m CtsJobSchedulerTestCases
    
    Turn off battery saver explicitly after battery saver on test is finished
    
    Change-Id: I9ec3468baeda64966b7398e5e3582573ce0ee6e0
    Signed-off-by: Jinho Park <jinho.park@lge.com>

--- tests/JobScheduler/src/android/jobscheduler/cts/JobThrottlingTest.java
+++ tests/JobScheduler/src/android/jobscheduler/cts/JobThrottlingTest.java
@@ -325,6 +325,7 @@ public class JobThrottlingTest {
             toggleDeviceIdleState(false);
         }
         mTestAppInterface.cleanup();
+        BatteryUtils.runDumpsysBatterySaverOff();
         BatteryUtils.runDumpsysBatteryReset();
         removeTestAppFromTempWhitelist();
 
