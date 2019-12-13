01114db652166f9864aef84df6b2ee172139e140
Shuo Qian <shuoq@google.com>
2019-12-13 22:09:38 +0000

    Restore CTS test for pre-q TelephonyPermssion
    Though the TelephonyPermission needs a system permission, but this permission is needed by the
    the caller which is either system or telephony, not the users of a bunch of APIs in this test.
    The new system permission in the refactored TelephonyPermission class shouldn't affect the
     previous testing result of returning null on a bunch of APIs. Therefore, it shouldn't need a
     ShellItentityUtil to adopt system permission to run call these APIs.
    
    Bug: 145688189
    Test: This CL revert a previous change of a CTS test. As long as the corresponding test
     passes Treehugger, it should be good to go.
    
    Change-Id: Ib5282d672e2b81e6d430e8ccbd4928cdc6c76809

--- tests/tests/telephony3/src/android/telephony3/cts/TelephonyManagerTest.java
+++ tests/tests/telephony3/src/android/telephony3/cts/TelephonyManagerTest.java
@@ -27,9 +27,6 @@ import android.telephony.TelephonyManager;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
-import com.android.compatibility.common.util.ApiLevelUtil;
-import com.android.compatibility.common.util.ShellIdentityUtils;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,24 +35,14 @@ import org.junit.runner.RunWith;
  * Verifies the APIs for apps with the READ_PHONE_STATE permission targeting pre-Q.
  *
  * @see android.telephony.cts.TelephonyManagerTest
+
  */
 @RunWith(AndroidJUnit4.class)
 public class TelephonyManagerTest {
     private TelephonyManager mTelephonyManager;
 
-    private static final boolean HAS_Q_API = ApiLevelUtil.isAtLeast(Build.VERSION_CODES.Q);
-
-    public static boolean isAtLeastQApi() {
-        return HAS_Q_API;
-    }
-
     @Before
     public void setUp() throws Exception {
-        // Test device identifier access for apps with the READ_PHONE_STATE
-        // permission targeting pre-Q.
-        if (HAS_Q_API) {
-            return;
-        }
         mTelephonyManager =
                 (TelephonyManager) InstrumentationRegistry.getContext().getSystemService(
                         Context.TELEPHONY_SERVICE);
@@ -63,12 +50,6 @@ public class TelephonyManagerTest {
 
     @Test
     public void testDeviceIdentifiersAreNotAccessible() throws Exception {
-        // Test device identifier access for apps with the READ_PHONE_STATE
-        // permission targeting pre-Q.
-        if (HAS_Q_API) {
-            return;
-        }
-
         // Apps with the READ_PHONE_STATE permission should no longer have access to device
         // identifiers. If an app's target SDK is less than Q and it has been granted the
         // READ_PHONE_STATE permission then a null value should be returned when querying for device
@@ -77,33 +58,27 @@ public class TelephonyManagerTest {
             assertNull(
                     "An app targeting pre-Q with the READ_PHONE_STATE permission granted must "
                             + "receive null when invoking getDeviceId",
-                    ShellIdentityUtils.invokeMethodWithShellPermissions(
-                            mTelephonyManager, tm -> tm.getDeviceId()));
+                    mTelephonyManager.getDeviceId());
             assertNull(
                     "An app targeting pre-Q with the READ_PHONE_STATE permission granted must "
                             + "receive null when invoking getImei",
-                    ShellIdentityUtils.invokeMethodWithShellPermissions(
-                            mTelephonyManager, tm -> tm.getImei()));
+                    mTelephonyManager.getImei());
             assertNull(
                     "An app targeting pre-Q with the READ_PHONE_STATE permission granted must "
                             + "receive null when invoking getMeid",
-                    ShellIdentityUtils.invokeMethodWithShellPermissions(
-                            mTelephonyManager, tm -> tm.getMeid()));
+                    mTelephonyManager.getMeid());
             assertNull(
                     "An app targeting pre-Q with the READ_PHONE_STATE permission granted must "
                             + "receive null when invoking getSubscriberId",
-                    ShellIdentityUtils.invokeMethodWithShellPermissions(
-                            mTelephonyManager, tm -> tm.getSubscriberId()));
+                    mTelephonyManager.getSubscriberId());
             assertNull(
                     "An app targeting pre-Q with the READ_PHONE_STATE permission granted must "
                             + "receive null when invoking getSimSerialNumber",
-                    ShellIdentityUtils.invokeMethodWithShellPermissions(
-                            mTelephonyManager, tm -> tm.getSimSerialNumber()));
+                    mTelephonyManager.getSimSerialNumber());
             assertNull(
                     "An app targeting pre-Q with the READ_PHONE_STATE permission granted must "
                             + "receive null when invoking getNai",
-                    ShellIdentityUtils.invokeMethodWithShellPermissions(
-                            mTelephonyManager, tm -> tm.getNai()));
+                    mTelephonyManager.getNai());
             // Since Build.getSerial is not documented to return null in previous releases this test
             // verifies that the Build.UNKNOWN value is returned when the caller does not have
             // permission to access the device identifier.
