e38854546a3d4a082b6965b0fdc17c3d37e8b66a
Steven Moreland <smoreland@google.com>
2019-12-16 17:40:32 -0800

    CtsNdkBinderTestCases: status getDescription
    Sanity 'contains' cases.
    
    Bug: 136027762
    Test: atest CtsNdkBinderTestCases
    Change-Id: I37415f7a805a038dbc9e060c1903e61fdd61c356

--- tests/tests/binder_ndk/libbinder_ndk_test/utilities.h
+++ tests/tests/binder_ndk/libbinder_ndk_test/utilities.h
@@ -48,6 +48,15 @@ inline ::testing::AssertionResult isOk(binder_status_t t) {
   return ::testing::AssertionFailure() << "Status: " << t;
 }
 
+inline ::testing::AssertionResult ContainsSubstring(const std::string& s, const std::string& ss) {
+  if (s.find(ss) != std::string::npos) {
+    return ::testing::AssertionSuccess();
+  } else {
+    return ::testing::AssertionFailure()
+           << "String: '" << s << "' does not contain substring: " << ss;
+  }
+}
+
 #define EXPECT_OK(THING) EXPECT_TRUE(isOk(THING))
 #define ASSERT_OK(THING) ASSERT_TRUE(isOk(THING))
 
