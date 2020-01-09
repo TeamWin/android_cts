55f8d725e95a0da7008d319ec8e03364e952a5c8
Joanne Chung <joannechung@google.com>
2020-01-09 19:48:32 +0800

    Add tests for InlineSuggestion APIs.
    Bug: 146453105
    Test: atest CtsInputMethodTestCases:android.view.inputmethod.cts.InlineSuggestionInfoTest
    Test: atest CtsInputMethodTestCases:android.view.inputmethod.cts.InlineSuggestionsRequestTest
    Test: atest CtsInputMethodTestCases:android.view.inputmethod.cts.InlineSuggestionsResponseTest
    Test: atest CtsInputMethodTestCases:android.view.inputmethod.cts.InlineSuggestionTest
    Change-Id: I5cb95b6847feb42ecc5eb163e0383b4db619b623

---
+++ tests/inputmethod/src/android/view/inputmethod/cts/InlineSuggestionsRequestTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.view.inputmethod.cts;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.testng.Assert.assertThrows;
+
+import android.os.Parcel;
+import android.util.Size;
+import android.view.inline.InlinePresentationSpec;
+import android.view.inputmethod.InlineSuggestionsRequest;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class InlineSuggestionsRequestTest {
+
+    @Test
+    public void testNullInlinePresentationSpecsThrowsException() {
+        assertThrows(NullPointerException.class,
+                () -> new InlineSuggestionsRequest.Builder(/* presentationSpecs */ null).build());
+    }
+
+    @Test
+    public void testInvalidPresentationSpecsCountsThrowsException() {
+        ArrayList<InlinePresentationSpec> presentationSpecs = new ArrayList<>();
+        presentationSpecs.add(new InlinePresentationSpec.Builder(new Size(100, 100),
+                new Size(400, 100)).build());
+        presentationSpecs.add(new InlinePresentationSpec.Builder(new Size(100, 100),
+                new Size(400, 100)).build());
+
+        assertThrows(IllegalStateException.class,
+                () -> new InlineSuggestionsRequest.Builder(presentationSpecs)
+                        .setMaxSuggestionCount(1).build());
+    }
+
+    @Test
+    public void testInlineSuggestionsRequestValues() {
+        final int suggestionCount = 3;
+        ArrayList<InlinePresentationSpec> presentationSpecs = new ArrayList<>();
+        InlineSuggestionsRequest request =
+                new InlineSuggestionsRequest.Builder(presentationSpecs)
+                        .setMaxSuggestionCount(suggestionCount).build();
+
+        assertThat(request.getMaxSuggestionCount()).isEqualTo(suggestionCount);
+        assertThat(request.getPresentationSpecs()).isNotNull();
+        assertThat(request.getPresentationSpecs().size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testInlineSuggestionsRequestParcelizeDeparcelize() {
+        ArrayList<InlinePresentationSpec> presentationSpecs = new ArrayList<>();
+        InlineSuggestionsRequest request =
+                new InlineSuggestionsRequest.Builder(presentationSpecs).build();
+        Parcel p = Parcel.obtain();
+        request.writeToParcel(p, 0);
+        p.setDataPosition(0);
+
+        InlineSuggestionsRequest targetRequest =
+                InlineSuggestionsRequest.CREATOR.createFromParcel(p);
+        p.recycle();
+
+        assertThat(targetRequest).isEqualTo(request);
+    }
+}
