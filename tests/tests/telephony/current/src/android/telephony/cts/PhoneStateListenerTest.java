4626f4974f401deacfe8333e6814ea60aa81e071
Nathan Harold <nharold@google.com>
2020-02-03 19:33:19 -0800

    Remove Duplicate Permission Tests
    -Remove negative permissions tests for:
     onImsCallDisconnectCauseChanged
     onCallDisconnectCauseChanged
     onPreciseDataConnectionStateChanged
    
    -Tests are migrated to Permissions tests as part of
     Ica56ebb81a3f3ea9b5c238a0e7d3a2d9c30e27f2
    Bug: 148646258
    Test: make (remove-only)
    Change-Id: I40c74966beaf6aaaf4af5b220716cca825c3488d

--- tests/tests/telephony/current/src/android/telephony/cts/PhoneStateListenerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/PhoneStateListenerTest.java
@@ -50,15 +50,15 @@ import androidx.test.InstrumentationRegistry;
 import com.android.compatibility.common.util.ShellIdentityUtils;
 import com.android.compatibility.common.util.TestThread;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 public class PhoneStateListenerTest {
 
@@ -569,59 +569,6 @@ public class PhoneStateListenerTest {
         assertThat(mOnCallDisconnectCauseChangedCalled).isTrue();
     }
 
-    /**
-     * Validate SecurityException will be thrown if listen with
-     * LISTEN_CALL_DISCONNECT_CAUSES without READ_PRECISE_PHONE_STATE permission.
-     */
-    @Test
-    public void testOnCallDisconnectCauseChangedWithoutPermission() throws Throwable {
-        if (mCm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE) == null) {
-            Log.d(TAG, "Skipping test that requires ConnectivityManager.TYPE_MOBILE");
-            return;
-        }
-
-        TestThread t = new TestThread(new Runnable() {
-            public void run() {
-                Looper.prepare();
-
-                mListener = new PhoneStateListener() {
-                    @Override
-                    public void onCallDisconnectCauseChanged(int disconnectCause,
-                                                             int preciseDisconnectCause) {
-                        synchronized (mLock) {
-                            mOnCallDisconnectCauseChangedCalled = true;
-                            mLock.notify();
-                        }
-                    }
-                };
-
-                try {
-                    mTelephonyManager.listen(mListener,
-                            PhoneStateListener.LISTEN_CALL_DISCONNECT_CAUSES);
-                } catch (SecurityException se) {
-                    mSecurityExceptionThrown = true;
-                    mLock.notify();
-                }
-                Looper.loop();
-            }
-        });
-
-        assertThat(mOnCallDisconnectCauseChangedCalled).isFalse();
-        t.start();
-
-        synchronized (mLock) {
-            if (!mSecurityExceptionThrown) {
-                mLock.wait(WAIT_TIME);
-            }
-        }
-        assertThat(mSecurityExceptionThrown).isTrue();
-        assertThat(mOnCallDisconnectCauseChangedCalled).isFalse();
-    }
-
-    /*
-     * The tests below rely on the framework to immediately call the installed listener upon
-     * registration. There is no simple way to emulate state changes for testing the listeners.
-     */
     @Test
     public void testOnImsCallDisconnectCauseChanged() throws Throwable {
         if (mCm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE) == null) {
@@ -659,54 +606,6 @@ public class PhoneStateListenerTest {
         }
         t.checkException();
         assertThat(mOnImsCallDisconnectCauseChangedCalled).isTrue();
-    }
-
-    /**
-     * Validate that SecuirtyException should be thrown if listen with
-     * LISTEN_IMS_CALL_DISCONNECT_CAUSES without permission READ_PRECISE_PHONE_STATE.
-     */
-    @Test
-    public void testOnImsCallDisconnectCauseChangedWithoutPermission() throws Throwable {
-        if (mCm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE) == null) {
-            Log.d(TAG, "Skipping test that requires ConnectivityManager.TYPE_MOBILE");
-            return;
-        }
-
-        TestThread t = new TestThread(new Runnable() {
-            public void run() {
-                Looper.prepare();
-
-                mListener = new PhoneStateListener() {
-                    @Override
-                    public void onImsCallDisconnectCauseChanged(ImsReasonInfo imsReason) {
-                        synchronized (mLock) {
-                            mOnImsCallDisconnectCauseChangedCalled = true;
-                            mLock.notify();
-                        }
-                    }
-                };
-
-                try {
-                    mTelephonyManager.listen(mListener,
-                            PhoneStateListener.LISTEN_IMS_CALL_DISCONNECT_CAUSES);
-                } catch (SecurityException se) {
-                    mSecurityExceptionThrown = true;
-                    mLock.notify();
-                }
-                Looper.loop();
-            }
-        });
-
-        assertThat(mOnImsCallDisconnectCauseChangedCalled).isFalse();
-        t.start();
-
-        synchronized (mLock) {
-            if (!mSecurityExceptionThrown) {
-                mLock.wait(WAIT_TIME);
-            }
-        }
-        assertThat(mSecurityExceptionThrown).isTrue();
-        assertThat(mOnImsCallDisconnectCauseChangedCalled).isFalse();
     }
 
     @Test
@@ -885,55 +784,6 @@ public class PhoneStateListenerTest {
         mPreciseDataConnectionState.getDataConnectionApnTypeBitMask();
         mPreciseDataConnectionState.getDataConnectionApn();
         mPreciseDataConnectionState.getDataConnectionFailCause();
-    }
-
-    /**
-     * Validate that SecurityException should be thrown when listen
-     * with LISTEN_PRECISE_DATA_CONNECTION_STATE without READ_PRECISE_PHONE_STATE permission.
-     */
-    @Test
-    public void testOnPreciseDataConnectionStateChangedWithoutPermission() throws Throwable {
-        if (mCm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE) == null) {
-            Log.d(TAG, "Skipping test that requires ConnectivityManager.TYPE_MOBILE");
-            return;
-        }
-
-        TestThread t = new TestThread(new Runnable() {
-            public void run() {
-                Looper.prepare();
-
-                mListener = new PhoneStateListener() {
-                    @Override
-                    public void onPreciseDataConnectionStateChanged(
-                            PreciseDataConnectionState state) {
-                        synchronized (mLock) {
-                            mOnPreciseDataConnectionStateChanged = true;
-                            mLock.notify();
-                        }
-                    }
-                };
-
-                try {
-                    mTelephonyManager.listen(mListener,
-                            PhoneStateListener.LISTEN_PRECISE_DATA_CONNECTION_STATE);
-                } catch (SecurityException se) {
-                    mSecurityExceptionThrown = true;
-                    mLock.notify();
-                }
-                Looper.loop();
-            }
-        });
-
-        assertThat(mOnCallDisconnectCauseChangedCalled).isFalse();
-        t.start();
-
-        synchronized (mLock) {
-            if (!mSecurityExceptionThrown) {
-                mLock.wait(WAIT_TIME);
-            }
-        }
-        assertThat(mSecurityExceptionThrown).isTrue();
-        assertThat(mOnPreciseDataConnectionStateChanged).isFalse();
     }
 
     @Test
