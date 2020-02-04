ca9614cca75582f3087dcf8b8c8346489155f8a8
Alex Johnston <acjohnston@google.com>
2020-02-04 11:09:18 +0000

    Modify DPM KEYGUARD_DISABLE_SECURE_CAMERA restriction
    * Added a new test class, KeyguardDisabledFeaturesTest, to test
      the keyguard disabled features APIs.
    * Added tests to test that the COPE PO can set specific keyguard
      disabled features on the parent profile.
    * Added tests to test that the DO and PO can set keyguard disabled
      features.
    
    Bug: 148656201
    Test: Manual testing with TestDPC
          atest com.android.cts.devicepolicy.OrgOwnedProfileOwnerTest#testSetKeyguardDisabledFeatures
          atest com.android.cts.devicepolicy.MixedDeviceOwnerTest#testSetKeyguardDisabledFeatures
          atest com.android.cts.devicepolicy.MixedManagedProfileOwnerTest#testSetKeyguardDisabledFeatures
          atest com.android.cts.devicepolicy.MixedDeviceOwnerTest#testSetKeyguardDisabledFeaturesLogged
    
    Change-Id: I00affc967944388449be28f6573b570a93242f7d

--- hostsidetests/devicepolicy/src/com/android/cts/devicepolicy/MixedManagedProfileOwnerTest.java
+++ hostsidetests/devicepolicy/src/com/android/cts/devicepolicy/MixedManagedProfileOwnerTest.java
@@ -266,6 +266,16 @@ public class MixedManagedProfileOwnerTest extends DeviceAndProfileOwnerTest {
                 "testSucceedsWithProfileOwnerIdsGrant", mUserId);
     }
 
+    @Test
+    @Override
+    public void testSetKeyguardDisabledFeatures() throws Exception {
+        if (!mHasFeature) {
+            return;
+        }
+        runDeviceTestsAsUser(DEVICE_ADMIN_PKG, ".KeyguardDisabledFeaturesTest",
+                "testSetKeyguardDisabledFeatures_onParentSilentIgnore", mUserId);
+    }
+
     @FlakyTest
     @Override
     @Test
