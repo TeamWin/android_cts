cddcdb1424e8e51faa332398872e9da9be7247a6
JP Sugarbroad <jpsugar@google.com>
2020-02-05 20:43:12 +0000

    Revert "DO NOT MERGE Verify that remote exceptions will not cause object leaks"
    This reverts commit edce354f47821b287d5a56f3019487b326bf7544.
    
    Reason for revert: b/148087431
    Bug: 148087431
    
    Change-Id: I384301ca3ac04228b9293257db4cf455d5a6fb7a

--- tests/tests/os/Android.mk
+++ tests/tests/os/Android.mk
@@ -39,8 +39,7 @@ LOCAL_SRC_FILES := \
     src/android/os/cts/IParcelFileDescriptorPeer.aidl \
     src/android/os/cts/IEmptyService.aidl \
     src/android/os/cts/ISeccompIsolatedService.aidl \
-    src/android/os/cts/ISecondary.aidl \
-    src/android/os/cts/IParcelExceptionService.aidl \
+    src/android/os/cts/ISecondary.aidl
 
 LOCAL_PACKAGE_NAME := CtsOsTestCases
 
