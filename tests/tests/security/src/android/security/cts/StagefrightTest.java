4e53241b5e53164ca5ef7e6c32bbe904e98ba8dd
Marco Nelissen <marcone@google.com>
2020-02-07 08:31:59 -0800

    Revert "CTS test for Android Security b/135207252"
    This reverts commit 08bcc80af350c1e2c88cb77510f857dc7cfd4424.
    
    Reason for revert: test crashes
    
    Bug: 13520725
    Bug: 148540357
    Change-Id: I0174d488c0a0feef8ea7cd268a2ab643c8898442
    Merged-In: I0174d488c0a0feef8ea7cd268a2ab643c8898442

--- tests/tests/security/src/android/security/cts/StagefrightTest.java
+++ tests/tests/security/src/android/security/cts/StagefrightTest.java
@@ -1087,11 +1087,6 @@ public class StagefrightTest extends InstrumentationTestCase {
         doStagefrightTest(R.raw.cve_2019_10541);
     }
 
-    @SecurityTest(minPatchLevel = "2019-08")
-    public void testStagefright_cve_2019_10489() throws Exception {
-        doStagefrightTest(R.raw.cve_2019_10489);
-    }
-
     @SecurityTest(minPatchLevel = "2019-05")
     public void testStagefright_cve_2019_2334() throws Exception {
         doStagefrightTest(R.raw.cve_2019_2334);
