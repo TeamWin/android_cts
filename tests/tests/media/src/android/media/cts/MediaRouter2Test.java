9ad0957c6c3a3a57d089ad697e373da2c8c48844
Kyunglyul Hyun <klhyun@google.com>
2020-02-12 18:30:45 +0900

    MediaRouter: Fixes a build error
    add missing import to fix build error
    
    Bug: 149367764
    Test: atest android.media.cts.MediaRouter2Test
    Change-Id: I4f361a942330a0f23df35d71f3a9ea09f4143158

--- tests/tests/media/src/android/media/cts/MediaRouter2Test.java
+++ tests/tests/media/src/android/media/cts/MediaRouter2Test.java
@@ -55,6 +55,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
