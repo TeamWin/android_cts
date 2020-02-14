35cb73567729a1759c9b5e0dfc494a62ef808439
Bram Bonné <brambonne@google.com>
2020-02-14 16:11:39 +0100

    Adds SDK-target specific networking tests.
    Adds a test verifying that apps targeting <= SDK 29
    have access to the MAC address of ethernet interfaces.
    
    Adds SELinux tests for apps targeting SDK 29.
    
    Bug: 141455849
    Change-Id: I9b9ba084dd1c58d01b97f807d7e0122f059c5121
    Fix: 149544376
    Test: atest CtsSelinuxTargetSdk27TestCases
    Test: atest CtsSelinuxTargetSdk28TestCases
    Test: atest CtsSelinuxTargetSdk29TestCases
    Test: atest CtsSelinuxTargetSdkCurrentTestCases

--- tests/tests/selinux/common/src/android/security/SELinuxTargetSdkTestBase.java
+++ tests/tests/selinux/common/src/android/security/SELinuxTargetSdkTestBase.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.net.NetworkInterface;
+import java.util.Collections;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -57,16 +59,36 @@ abstract class SELinuxTargetSdkTestBase extends AndroidTestCase
         }
     }
 
-    protected static void noNetlinkRouteGetlink() throws IOException {
-        assertEquals("RTM_GETLINK is not allowed on a netlink route sockets. Verify that the " +
-			"following patch has been applied to your kernel: " +
-			"https://android-review.googlesource.com/q/I7b44ce60ad98f858c412722d41b9842f8577151f",
-                13, checkNetlinkRouteGetlink());
+    protected static void checkNetlinkRouteGetlink(boolean expectAllowed) throws IOException {
+        if (!expectAllowed) {
+            assertEquals(
+                    "RTM_GETLINK is not allowed on a netlink route sockets. Verify that the"
+                        + " following patch has been applied to your kernel: "
+                        + "https://android-review.googlesource.com/q/I7b44ce60ad98f858c412722d41b9842f8577151f",
+                    13,
+                    checkNetlinkRouteGetlink());
+        } else {
+            assertEquals(
+                    "RTM_GETLINK should be allowed netlink route sockets for apps with "
+                            + "targetSdkVersion <= Q",
+                    -1,
+                    checkNetlinkRouteGetlink());
+        }
     }
 
-    protected static void noNetlinkRouteBind() throws IOException {
-        assertEquals("Bind() is not allowed on a netlink route sockets",
-                13, checkNetlinkRouteBind());
+    protected static void checkNetlinkRouteBind(boolean expectAllowed) throws IOException {
+        if (!expectAllowed) {
+            assertEquals(
+                    "Bind() is not allowed on a netlink route sockets",
+                    13,
+                    checkNetlinkRouteBind());
+        } else {
+            assertEquals(
+                    "Bind() should succeed for netlink route sockets for apps with "
+                            + "targetSdkVersion <= Q",
+                    -1,
+                    checkNetlinkRouteBind());
+        }
     }
 
     /**
@@ -144,6 +166,29 @@ abstract class SELinuxTargetSdkTestBase extends AndroidTestCase
             temp.delete();
         }
         return true;
+    }
+
+    /**
+     * Verify that apps having targetSdkVersion <= 29 are able to see MAC
+     * addresses of ethernet devices.
+     * The counterpart of this test (testing for targetSdkVersion > 29) is
+     * {@link libcore.java.net.NetworkInterfaceTest#testGetHardwareAddress_returnsNull()}.
+     */
+    protected static void checkNetworkInterface_returnsHardwareAddresses() throws Exception {
+        assertNotNull(NetworkInterface.getNetworkInterfaces());
+        for (NetworkInterface nif : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+            if (isEthernet(nif.getName())) {
+                assertEquals(6, nif.getHardwareAddress().length);
+            }
+        }
+    }
+
+    /**
+     * Checks whether a network interface is an ethernet interface.
+     */
+    private static Pattern ethernetNamePattern = Pattern.compile("^(eth|wlan)[0-9]+$");
+    private static boolean isEthernet(String ifName) throws Exception {
+        return ethernetNamePattern.matcher(ifName).matches();
     }
 
     private static final native int checkNetlinkRouteGetlink();
--- tests/tests/selinux/selinuxTargetSdk29/src/android/security/SELinuxTargetSdkTest.java
+++ tests/tests/selinux/selinuxTargetSdk29/src/android/security/SELinuxTargetSdkTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,8 @@ import android.test.AndroidTestCase;
 import com.android.compatibility.common.util.PropertyUtil;
 import java.io.IOException;
 
-
 /**
- * Verify the selinux domain for apps running with current targetSdkVersion
+ * Verify the selinux domain for apps running with targetSdkVersion==29
  */
 public class SELinuxTargetSdkTest extends SELinuxTargetSdkTestBase
 {
@@ -31,48 +30,6 @@ public class SELinuxTargetSdkTest extends SELinuxTargetSdkTestBase
      */
     public void testNoDns() throws IOException {
         noDns();
-    }
-
-    public void testNoNetlinkRouteGetlink() throws IOException {
-        checkNetlinkRouteGetlink(false);
-    }
-
-    public void testNoNetlinkRouteBind() throws IOException {
-        checkNetlinkRouteBind(false);
-    }
-
-    public void testCanNotExecuteFromHomeDir() throws Exception {
-        assertFalse(canExecuteFromHomeDir());
-    }
-
-    /**
-     * Verify that selinux context is the expected domain based on
-     * targetSdkVersion = current
-     */
-    public void testAppDomainContext() throws IOException {
-        String context = "u:r:untrusted_app:s0:c[0-9]+,c[0-9]+,c[0-9]+,c[0-9]+";
-        String msg = "Untrusted apps with targetSdkVersion 29 and above " +
-            "must run in the untrusted_app selinux domain and use the levelFrom=all " +
-            "selector in SELinux seapp_contexts which adds four category types " +
-            "to the app's selinux context.\n" +
-            "Example expected value: u:r:untrusted_app:s0:c89,c256,c512,c768\n" +
-            "Actual value: ";
-        appDomainContext(context, msg);
-    }
-
-    /**
-     * Verify that selinux context is the expected type based on
-     * targetSdkVersion = current
-     */
-    public void testAppDataContext() throws Exception {
-        String context = "u:object_r:app_data_file:s0:c[0-9]+,c[0-9]+,c[0-9]+,c[0-9]+";
-        String msg = "Untrusted apps with targetSdkVersion 29 and above " +
-            "must use the app_data_file selinux context and use the levelFrom=all " +
-            "selector in SELinux seapp_contexts which adds four category types " +
-            "to the app_data_file context.\n" +
-            "Example expected value: u:object_r:app_data_file:s0:c89,c256,c512,c768\n" +
-            "Actual value: ";
-        appDataContext(context, msg);
     }
 
     public void testDex2oat() throws Exception {
@@ -84,5 +41,51 @@ public class SELinuxTargetSdkTest extends SELinuxTargetSdkTestBase
         if (PropertyUtil.isVendorApiLevelNewerThan(28)) {
             checkDex2oatAccess(false);
         }
+    }
+
+    public void testNetlinkRouteGetlinkSucceeds() throws IOException {
+        checkNetlinkRouteGetlink(true);
+    }
+
+    public void testNetlinkRouteBindSucceeds() throws IOException {
+        checkNetlinkRouteBind(true);
+    }
+
+    public void testCanNotExecuteFromHomeDir() throws Exception {
+        assertFalse(canExecuteFromHomeDir());
+    }
+
+    /**
+     * Verify that selinux context is the expected domain based on
+     * targetSdkVersion = 29
+     */
+    public void testAppDomainContext() throws IOException {
+        String context = "u:r:untrusted_app_29:s0:c[0-9]+,c[0-9]+,c[0-9]+,c[0-9]+";
+        String msg = "Untrusted apps with targetSdkVersion 29 " +
+            "must run in the untrusted_app selinux domain and use the levelFrom=all " +
+            "selector in SELinux seapp_contexts which adds four category types " +
+            "to the app's selinux context.\n" +
+            "Example expected value: u:r:untrusted_app:s0:c89,c256,c512,c768\n" +
+            "Actual value: ";
+        appDomainContext(context, msg);
+    }
+
+    /**
+     * Verify that selinux context is the expected type based on
+     * targetSdkVersion = 29
+     */
+    public void testAppDataContext() throws Exception {
+        String context = "u:object_r:app_data_file:s0:c[0-9]+,c[0-9]+,c[0-9]+,c[0-9]+";
+        String msg = "Untrusted apps with targetSdkVersion 29 " +
+            "must use the app_data_file selinux context and use the levelFrom=all " +
+            "selector in SELinux seapp_contexts which adds four category types " +
+            "to the app_data_file context.\n" +
+            "Example expected value: u:object_r:app_data_file:s0:c89,c256,c512,c768\n" +
+            "Actual value: ";
+        appDataContext(context, msg);
+    }
+
+    public void testNetworkInterface() throws Exception {
+        checkNetworkInterface_returnsHardwareAddresses();
     }
 }
