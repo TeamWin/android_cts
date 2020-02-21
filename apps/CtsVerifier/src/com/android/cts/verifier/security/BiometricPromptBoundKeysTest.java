b70f7a9a3668f77f508aeb1ae5c25d9e923fa61e
Kevin Chyn <kchyn@google.com>
2020-02-21 15:03:22 -0800

    [DO NOT MERGE] Update BIOMETRIC_WEAK test on Android 10
    This is a more precise implementation. See the comments in the CL for
    more context.
    
    Bug: 149761194
    
    Test: Run BiometricPromptBoundKeysTest on various devices
    Change-Id: Ib806d9398d2a211e85cf72cb7dfb60303740f868

--- apps/CtsVerifier/src/com/android/cts/verifier/security/BiometricPromptBoundKeysTest.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/security/BiometricPromptBoundKeysTest.java
@@ -19,18 +19,26 @@ package com.android.cts.verifier.security;
 import android.content.DialogInterface;
 import android.hardware.biometrics.BiometricManager;
 import android.hardware.biometrics.BiometricPrompt;
+import android.hardware.biometrics.BiometricPrompt.CryptoObject;
 import android.os.CancellationSignal;
 import android.os.Handler;
 import android.os.Looper;
+
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.widget.Button;
 
 import com.android.cts.verifier.R;
 
+import java.lang.Exception;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.security.KeyStore;
 import java.util.concurrent.Executor;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 /**
  * Test for {@link BiometricPrompt}. This test runs twice, once with confirmation required,
@@ -89,72 +97,161 @@ public class BiometricPromptBoundKeysTest extends FingerprintBoundKeysTest {
         return TAG;
     }
 
+    private CryptoObject generateDummyCrypto() {
+        final String dummyKeyName = "dummy_key";
+
+        try {
+            KeyStore keystore = KeyStore.getInstance("AndroidKeyStore");
+            keystore.load(null);
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES,
+                "AndroidKeyStore");
+
+            // This key should, or rather must not be biometric-bound, since we depend on the
+            // operationId being non-zero to invoke the "STRONG" path of
+            // BiometricManager#canAuthenticate(CryptoObject). See the comments below, where
+            // the reflection is being checked/invoked for more details.
+            keyGenerator.init(new KeyGenParameterSpec.Builder(dummyKeyName,
+                KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                .build());
+            keyGenerator.generateKey();
+
+            SecretKey secretKey = (SecretKey) keystore.getKey(dummyKeyName, null);
+            Cipher cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                + KeyProperties.BLOCK_MODE_CBC + "/"
+                + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return new CryptoObject(cipher);
+        } catch (Exception e) {
+            showToast("Exception while generating dummy key: " + e);
+        }
+
+        return null;
+    }
+
+    private Method getHiddenCanAuthenticateApiIfExists() {
+        try {
+            return BiometricManager.class.getMethod(
+                    "canAuthenticate",
+                    BiometricPrompt.CryptoObject.class);
+        } catch (NoSuchMethodException e) {
+            return null;
+        }
+    }
+
+    private int invokeHiddenCanAuthenticateApi(BiometricManager manager, Method method,
+            CryptoObject crypto) {
+        try {
+            final Object result = method.invoke(manager, crypto);
+            if (result instanceof Integer) {
+                return (int) result;
+            }
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            // Do nothing, assume the API does not exist since something is wrong.
+        }
+        return -1;
+    }
+
     @Override
     protected void onPermissionsGranted() {
         Button startTestButton = findViewById(R.id.sec_start_test_button);
         mBiometricManager = getSystemService(BiometricManager.class);
+
+        // The Android 10 CDD allows for integration of Strong, Weak, and Convenience
+        // biometrics on the device. A subset (Strong, Weak) are allowed for application
+        // integration (e.g. with the public APIs). However, the Android 10 API surface is not
+        // wide enough to support this.
+        //
+        // Although this was addressed in Android 11 with Authenticators.BIOMETRIC_STRONG,
+        // Authenticators.BIOMETRIC_WEAK, and the canAuthenticate(int) APIs etc, we still need
+        // to patch the hole in Android 10. The POR is as follows.
+        //
+        // Devices with weak biometrics should implement a @hide API with the following
+        // signature: `int BiometricManager#canAuthenticate(BiometricPrompt.CryptoObject)`.
+        //
+        // If invoked with a CryptoObject that's initialized with a proper KeyStore key (containing
+        // a non-zero operationId), the returned result will be interpreted as "the current state
+        // of the strong biometric sensor on the device. e.g.
+        // 1) BIOMETRIC_SUCCESS will mean that a strong biometric sensor is available and enrolled,
+        // 2) BIOMETRIC_ERROR_HW_UNAVAILABLE will mean that there is a problem with the strong
+        //    biometric sensor (e.g. HAL crashed)
+        // 3) BIOMETRIC_ERROR_NONE_ENROLLED will mean that a strong biometric sensor exists, but
+        //    currently does not have any enrollments
+        // 4) BIOMETRIC_ERROR_NO_HARDWARE will mean that the device does not have a strong biometric
+        //    sensor.
+        // In other words, on devices with only a WEAK biometric sensor, the method, when invoked
+        // with a valid CryptoObject should return BIOMETRIC_ERROR_NO_HARDWARE.
+        //
+        // If invoked with a null CryptoObject, or a CryptoObject whos operationId is 0, the result
+        // will be interpreted as "the current state of the weak biometric sensor on the device".
+        // This is currently not needed, and not being tested. Since, the Android 11 API, e.g.
+        // invoking BiometricManager#canAuthenticate(Authenticators.BIOMETRIC_WEAK) means "at
+        // least one biometric sensor that meets or exceeds BIOMETRIC_WEAK is available+enrolled".
+        // That information in Android 10's case can be derived from
+        // BiometricManager#canAuthenticate() returning BIOMETRIC_SUCCESS and
+        // BiometricManager#canAuthenticate(CryptoObject) existing/returning something (which is
+        // what we're enforcing in this test here).
+        //
+        // To make this workaround available to developers, the androidx.biometric library will
+        // bring Android11-like functionality to developers by similarly checking for the
+        // existence/result of this @hide API.
+
         final int result = mBiometricManager.canAuthenticate();
-
+        // At least oneof(weak OR strong) biometrics are available. We don't know exactly which
+        // until we check for the hidden API
         if (result == BiometricManager.BIOMETRIC_SUCCESS) {
+            Method canAuthenticateCrypto = getHiddenCanAuthenticateApiIfExists();
+            if (canAuthenticateCrypto != null) {
+                showToast("canAuthenticate(CryptoObject) detected, testing @hide API");
+                // If the hidden API exists, we should use it (instead of
+                // BiometricManager#canAuthenticate() to check for availability+enrolled state of
+                // strong biometrics. In this case, BiometricManager#canAuthenticate() represents
+                // WEAK (or stronger).
 
-            // The Android 10 CDD allows for integration of Strong, Weak, and Convenience
-            // biometrics on the device. A subset (Strong, Weak) are allowed for application
-            // integration (e.g. with the public APIs). However, the Android 10 API surface is not
-            // wide enough to support this.
-            //
-            // Although this was addressed in Android 11 with Authenticators.BIOMETRIC_STRONG,
-            // Authenticators.BIOMETRIC_WEAK, and the canAuthenticate(int) APIs etc, we still need
-            // to patch the hole in Android 10. The POR is as follows.
-            //
-            // Devices with weak biometrics should implement a @hide API with the following
-            // signature: `int BiometricManager#canAuthenticate(BiometricPrompt.CryptoObject)`.
-            // On devices with weak-only, the method should return BIOMETRIC_ERROR_NO_HARDWARE. The
-            // CryptoObject does not need to be initialized/valid, and is used only to find the
-            // method signature. (Yes, in hindsight, we should have settled on the @hide API being
-            // more like `int BiometricManager#canAuthenticateWithCrypto()`
-            //
-            // The androidx.biometric library can then bring Android11-like functionality to
-            // developers by similarly checking for the existence/result of this @hide API.
+                // Override the behavior of the start button.
+                startTestButton.setOnClickListener(view -> {
+                    final int hiddenResult = invokeHiddenCanAuthenticateApi(mBiometricManager,
+                            canAuthenticateCrypto, generateDummyCrypto());
+                    boolean shouldStartCryptoTest = false;
+                    switch (hiddenResult) {
+                        case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                            showToast("Strong biometrics are available but not enrolled. Please"
+                                    + " enroll a strong biometric");
+                            break;
 
-            try {
-                final Method canAuthenticateCrypto = BiometricManager.class.getMethod(
-                        "canAuthenticate",
-                        BiometricPrompt.CryptoObject.class);
-                final Object canAuthenticateResult = canAuthenticateCrypto
-                        .invoke(mBiometricManager, (Cipher) null);
-
-                if (canAuthenticateResult instanceof Integer) {
-                    switch ((int) canAuthenticateResult) {
                         case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                            // Device does not have a strong authenticator, but has a weak one.
-                            // Allow the test to pass.
-                            startTestButton.setEnabled(false);
-                            getPassButton().setEnabled(true);
-
                             showToast("The device has WEAK biometrics but no STRONG biometrics."
                                     + " You can skip the keystore integration test.");
+                            startTestButton.setEnabled(false);
+                            getPassButton().setEnabled(true);
                             break;
+
+                        case BiometricManager.BIOMETRIC_SUCCESS:
+                            // Device reports that a Strong authenticator is available and enrolled.
+                            // Test for Keystore integration in this case.
+                            shouldStartCryptoTest = true;
+
                         case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
                             // HAL/Hardware has an issue. This is definitely not expected. In this
                             // case, we should assume it should be in the enrolled/available state.
                             // Test for Keystore integration in this case.
-                        case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                            // The user can be asked to enroll. Test for Keystore integration in
-                            // this case.
-                        case BiometricManager.BIOMETRIC_SUCCESS:
-                            // Device reports that a Strong authenticator is available and enrolled.
-                            // Test for Keystore integration in this case.
+
                         default:
-                            showToast("canAuthenticateCrypto result: " + canAuthenticateResult
-                                    + " Please proceed with the keystore integration test");
+                            showToast("canAuthenticate(CryptoObject) result: " + hiddenResult
+                                    + " Please proceed with the keystore integration test.");
                             break;
                     }
-                }
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                // Proceed with the keystore integration test as usual.
+
+                    if (shouldStartCryptoTest) {
+                        startTest();
+                    }
+                });
+            } else {
+                // Hidden API doesn't exist. BiometricManager#canAuthenticate() in this case
+                // represents only STRONG biometrics. Start button has existing behavior inherited
+                // from FingerprintBoundKeysTest.
             }
-
-
         } else if (result == BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE) {
             showToast("No biometric features, test passed.");
             getPassButton().setEnabled(true);
@@ -162,8 +259,8 @@ public class BiometricPromptBoundKeysTest extends FingerprintBoundKeysTest {
             showToast("Biometric unavailable, something is wrong with your device");
         } else if (result == BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED) {
             showToast("Error: " + result + " Please ensure you have a biometric enrolled");
-
-            startTestButton.setEnabled(false);
+        } else {
+            showToast("Unknown result: " + result);
         }
     }
 
