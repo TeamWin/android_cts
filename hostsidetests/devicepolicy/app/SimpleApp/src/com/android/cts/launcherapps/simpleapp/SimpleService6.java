29be3645996c451b03ca5aa29ffbe9a0c453c692
Jing Ji <jji@google.com>
2020-03-16 21:19:31 -0700

    Update the ActivityManagerAppExitInfoTest#testOther
    Bug: 151321953
    Test: atest CtsAppExitTestCases:ActivityManagerAppExitInfoTest
    Change-Id: I69d9efad02287156033862957b5256dc92097c31

--- hostsidetests/devicepolicy/app/SimpleApp/src/com/android/cts/launcherapps/simpleapp/SimpleService6.java
+++ hostsidetests/devicepolicy/app/SimpleApp/src/com/android/cts/launcherapps/simpleapp/SimpleService6.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package com.android.cts.launcherapps.simpleapp;
 
 /**
- * A simple service which extends SimpleService4
+ * A simple service which extends SimpleService4, but runs as isolated.
  */
-public class SimpleService5 extends SimpleService4 {
+public class SimpleService6 extends SimpleService4 {
 }
