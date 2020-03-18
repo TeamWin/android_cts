a9da422b4900d7e4233fba538b09ce53fca3fc98
Eino-Ville Talvala <etalvala@google.com>
2020-03-18 14:49:34 -0700

    RESTRICT AUTOMERGE: Camera: Remove tests for ROTATE_AND_CROP
    Feature did not get fully completed for Android 11.
    
    Test: Adjusted camera CTS continues to pass
    Bug: 150036107
    Change-Id: I6f048b3b874ce6c475aee32c555b4f2a5ca472e5

--- tools/cts-device-info/src/com/android/cts/deviceinfo/CameraDeviceInfo.java
+++ tools/cts-device-info/src/com/android/cts/deviceinfo/CameraDeviceInfo.java
@@ -491,7 +491,6 @@ public final class CameraDeviceInfo extends DeviceInfo {
         charsKeyNames.add(CameraCharacteristics.SCALER_CROPPING_TYPE.getName());
         charsKeyNames.add(CameraCharacteristics.SCALER_MANDATORY_STREAM_COMBINATIONS.getName());
         charsKeyNames.add(CameraCharacteristics.SCALER_MANDATORY_CONCURRENT_STREAM_COMBINATIONS.getName());
-        charsKeyNames.add(CameraCharacteristics.SCALER_AVAILABLE_ROTATE_AND_CROP_MODES.getName());
         charsKeyNames.add(CameraCharacteristics.SENSOR_REFERENCE_ILLUMINANT1.getName());
         charsKeyNames.add(CameraCharacteristics.SENSOR_REFERENCE_ILLUMINANT2.getName());
         charsKeyNames.add(CameraCharacteristics.SENSOR_CALIBRATION_TRANSFORM1.getName());
