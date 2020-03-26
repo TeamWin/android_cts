9318651b349add4ee0b72900ca3a899fb88c33b7
Matt Wachowski <mattwach@google.com>
2020-03-26 15:21:43 -0700

    Skip testRsaAttestation_DeviceLocked on emulator.
    The error is junit.framework.AssertionFailedError: Unexpected attestation security level: Software
    but the emualtor has no hardware key support.
    
    Bug: b/143143350
    Test: run cts -m CtsKeystoreTestCases -t android.keystore.cts.KeyAttestationTest#testRsaAttestation_DeviceLocked
    Change-Id: Ib335066417556b69fd597aa5d0426611ea77a54b

--- tests/tests/keystore/src/android/keystore/cts/KeyAttestationTest.java
+++ tests/tests/keystore/src/android/keystore/cts/KeyAttestationTest.java
@@ -68,6 +68,8 @@ import android.security.keystore.KeyProperties;
 import android.test.AndroidTestCase;
 import android.util.ArraySet;
 
+import androidx.test.filters.RequiresDevice;
+
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
 
@@ -385,6 +387,7 @@ public class KeyAttestationTest extends AndroidTestCase {
     }
 
     @RestrictedBuildTest
+    @RequiresDevice  // Emulators have no place to store the needed key
     public void testRsaAttestation_DeviceLocked() throws Exception {
         String keystoreAlias = "test_key";
         Date now = new Date();
