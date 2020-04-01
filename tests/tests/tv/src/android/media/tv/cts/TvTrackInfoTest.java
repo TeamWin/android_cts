3cc51a37786ff6e64b5b18d050afca3159dbefd0
Nick Chalko <nchalko@google.com>
2020-04-01 16:09:28 -0700

    Update TvTrackInfoTest to JUnit4 and Truth
    Change-Id: I570a67125ff353742cf26ad8168039c834ef2761
    Test: atest TvTrackInfoTest
    Bug: 152618272

--- tests/tests/tv/src/android/media/tv/cts/TvTrackInfoTest.java
+++ tests/tests/tv/src/android/media/tv/cts/TvTrackInfoTest.java
@@ -18,16 +18,22 @@ package android.media.tv.cts;
 
 import android.media.tv.TvTrackInfo;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.test.AndroidTestCase;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.core.os.Parcelables;
+
+import static android.media.tv.cts.TvTrackInfoSubject.assertThat;
+
+import org.junit.Test;
 
 /**
  * Test {@link android.media.tv.TvTrackInfo}.
  */
-public class TvTrackInfoTest extends AndroidTestCase {
+public class TvTrackInfoTest {
 
+    @Test
     public void testAudioTrackInfoOp() {
-        if (!Utils.hasTvInputFramework(getContext())) {
+        if (!Utils.hasTvInputFramework(ApplicationProvider.getApplicationContext())) {
             return;
         }
         final Bundle bundle = new Bundle();
@@ -37,31 +43,21 @@ public class TvTrackInfoTest extends AndroidTestCase {
                 .setLanguage("eng")
                 .setExtra(bundle)
                 .build();
-        assertEquals(TvTrackInfo.TYPE_AUDIO, info.getType());
-        assertEquals("id_audio", info.getId());
-        assertEquals(2, info.getAudioChannelCount());
-        assertEquals(48000, info.getAudioSampleRate());
-        assertEquals("eng", info.getLanguage());
-        assertEquals(bundle.get("testTrue"), info.getExtra().get("testTrue"));
-        assertEquals(0, info.describeContents());
-
-        // Test writeToParcel
-        Parcel p = Parcel.obtain();
-        info.writeToParcel(p, 0);
-        p.setDataPosition(0);
-        TvTrackInfo infoFromParcel = TvTrackInfo.CREATOR.createFromParcel(p);
-        assertEquals(TvTrackInfo.TYPE_AUDIO, infoFromParcel.getType());
-        assertEquals("id_audio", infoFromParcel.getId());
-        assertEquals(2, infoFromParcel.getAudioChannelCount());
-        assertEquals(48000, infoFromParcel.getAudioSampleRate());
-        assertEquals("eng", infoFromParcel.getLanguage());
-        assertEquals(bundle.get("testTrue"), infoFromParcel.getExtra().get("testTrue"));
-        assertEquals(0, infoFromParcel.describeContents());
-        p.recycle();
+        assertThat(info).hasType(TvTrackInfo.TYPE_AUDIO);
+        assertThat(info).hasId("id_audio");
+        assertThat(info).hasAudioChannelCount(2);
+        assertThat(info).hasAudioSampleRate(48000);
+        assertThat(info).hasLanguage("eng");
+        assertThat(info).extra().isEmpty();
+        assertThat(info).hasContentDescription(0);
+        assertThat(info).recreatesEqual(TvTrackInfo.CREATOR);
+        TvTrackInfo copy = Parcelables.forceParcel(info, TvTrackInfo.CREATOR);
+        assertThat(copy).extra().isEmpty();
     }
 
+    @Test
     public void testVideoTrackInfoOp() {
-        if (!Utils.hasTvInputFramework(getContext())) {
+        if (!Utils.hasTvInputFramework(ApplicationProvider.getApplicationContext())) {
             return;
         }
         final Bundle bundle = new Bundle();
@@ -75,37 +71,24 @@ public class TvTrackInfoTest extends AndroidTestCase {
                 .setLanguage("eng")
                 .setExtra(bundle)
                 .build();
-        assertEquals(TvTrackInfo.TYPE_VIDEO, info.getType());
-        assertEquals("id_video", info.getId());
-        assertEquals(1920, info.getVideoWidth());
-        assertEquals(1080, info.getVideoHeight());
-        assertEquals(29.97f, info.getVideoFrameRate());
-        assertEquals(1.0f, info.getVideoPixelAspectRatio());
-        assertEquals((byte) 8, info.getVideoActiveFormatDescription());
-        assertEquals("eng", info.getLanguage());
-        assertEquals(bundle.get("testTrue"), info.getExtra().get("testTrue"));
-        assertEquals(0, info.describeContents());
-
-        // Test writeToParcel
-        Parcel p = Parcel.obtain();
-        info.writeToParcel(p, 0);
-        p.setDataPosition(0);
-        TvTrackInfo infoFromParcel = TvTrackInfo.CREATOR.createFromParcel(p);
-        assertEquals(TvTrackInfo.TYPE_VIDEO, infoFromParcel.getType());
-        assertEquals("id_video", infoFromParcel.getId());
-        assertEquals(1920, infoFromParcel.getVideoWidth());
-        assertEquals(1080, infoFromParcel.getVideoHeight());
-        assertEquals(29.97f, infoFromParcel.getVideoFrameRate());
-        assertEquals(1.0f, infoFromParcel.getVideoPixelAspectRatio());
-        assertEquals((byte) 8, infoFromParcel.getVideoActiveFormatDescription());
-        assertEquals("eng", infoFromParcel.getLanguage());
-        assertEquals(bundle.get("testTrue"), infoFromParcel.getExtra().get("testTrue"));
-        assertEquals(0, infoFromParcel.describeContents());
-        p.recycle();
+        assertThat(info).hasType(TvTrackInfo.TYPE_VIDEO);
+        assertThat(info).hasId("id_video");
+        assertThat(info).hasVideoWidth(1920);
+        assertThat(info).hasVideoHeight(1080);
+        assertThat(info).hasVideoFrameRate(29.97f);
+        assertThat(info).hasVideoPixelAspectRatio(1.0f);
+        assertThat(info).hasVideoActiveFormatDescription((byte) 8);
+        assertThat(info).hasLanguage("eng");
+        assertThat(info).extra().bool("testTrue").isTrue();
+        assertThat(info).hasContentDescription(0);
+        assertThat(info).recreatesEqual(TvTrackInfo.CREATOR);
+        TvTrackInfo copy = Parcelables.forceParcel(info, TvTrackInfo.CREATOR);
+        assertThat(copy).extra().bool("testTrue").isTrue();
     }
 
+    @Test
     public void testSubtitleTrackInfoOp() {
-        if (!Utils.hasTvInputFramework(getContext())) {
+        if (!Utils.hasTvInputFramework(ApplicationProvider.getApplicationContext())) {
             return;
         }
         final Bundle bundle = new Bundle();
@@ -114,22 +97,13 @@ public class TvTrackInfoTest extends AndroidTestCase {
                 .setLanguage("eng")
                 .setExtra(bundle)
                 .build();
-        assertEquals(TvTrackInfo.TYPE_SUBTITLE, info.getType());
-        assertEquals("id_subtitle", info.getId());
-        assertEquals("eng", info.getLanguage());
-        assertEquals(bundle.get("testTrue"), info.getExtra().get("testTrue"));
-        assertEquals(0, info.describeContents());
-
-        // Test writeToParcel
-        Parcel p = Parcel.obtain();
-        info.writeToParcel(p, 0);
-        p.setDataPosition(0);
-        TvTrackInfo infoFromParcel = TvTrackInfo.CREATOR.createFromParcel(p);
-        assertEquals(TvTrackInfo.TYPE_SUBTITLE, infoFromParcel.getType());
-        assertEquals("id_subtitle", infoFromParcel.getId());
-        assertEquals("eng", infoFromParcel.getLanguage());
-        assertEquals(bundle.get("testTrue"), infoFromParcel.getExtra().get("testTrue"));
-        assertEquals(0, infoFromParcel.describeContents());
-        p.recycle();
+        assertThat(info).hasType(TvTrackInfo.TYPE_SUBTITLE);
+        assertThat(info).hasId("id_subtitle");
+        assertThat(info).hasLanguage("eng");
+        assertThat(info).extra().bool("testTrue").isTrue();
+        assertThat(info).hasContentDescription(0);
+        assertThat(info).recreatesEqual(TvTrackInfo.CREATOR);
+        TvTrackInfo copy = Parcelables.forceParcel(info, TvTrackInfo.CREATOR);
+        assertThat(copy).extra().bool("testTrue").isTrue();
     }
 }
