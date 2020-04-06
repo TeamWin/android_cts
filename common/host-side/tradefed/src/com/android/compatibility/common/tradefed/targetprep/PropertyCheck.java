68a3fd71f7220e10e2a81f91c215ceda8f006340
Max Spector <mspector@google.com>
2020-04-05 22:30:40 -0700

    RESTRICT AUTOMERGE Adds support for is-set-only for PropertyCheck.java
    This allows for PropertyCheck to check if a build property is set
    without specifying which value it must have.
    
    Bug: 153239170
    Test: Added new target_preparer in STS and ran it.
    Change-Id: Icf392954696d72e60b8137063bf34ce4a2ef9112

--- common/host-side/tradefed/src/com/android/compatibility/common/tradefed/targetprep/PropertyCheck.java
+++ common/host-side/tradefed/src/com/android/compatibility/common/tradefed/targetprep/PropertyCheck.java
@@ -33,9 +33,12 @@ public class PropertyCheck extends PreconditionPreparer {
             mandatory = true)
     protected String mPropertyName = null;
 
-    @Option(name = "expected-value", description = "The expected value of the property",
-            mandatory = true)
+    @Option(name = "expected-value", description = "The expected value of the property")
     protected String mExpectedPropertyValue = null;
+
+    @Option(name = "is-set-only", description = "Whether this value must be set only (don't check"
+        + "value)")
+    protected boolean mPropertyValueIsSetOnly = false;
 
     @Option(name = "throw-error",
             description = "Whether to throw an error for an unexpected property value")
@@ -44,8 +47,30 @@ public class PropertyCheck extends PreconditionPreparer {
     @Override
     public void run(ITestDevice device, IBuildInfo buildInfo) throws TargetSetupError,
             BuildError, DeviceNotAvailableException {
-
+        if (mPropertyValueIsSetOnly && mExpectedPropertyValue != null ) {
+            throw new IllegalArgumentException("is-set-only and "
+                + "expected-value cannot both be set.");
+        }
+        else if (!mPropertyValueIsSetOnly && mExpectedPropertyValue == null ) {
+            throw new IllegalArgumentException("is-set-only or "
+                + "expected-value must be set.");
+        }
         String propertyValue = device.getProperty(mPropertyName);
+
+        if (mPropertyValueIsSetOnly) {
+            if (propertyValue == null || propertyValue.equals("")) {
+                String msg = String.format("Property \"%s\" not found or not set on device",
+                    mPropertyName);
+                // Handle missing property with either exception or warning
+                if(mThrowError) {
+                    throw new TargetSetupError(msg, device.getDeviceDescriptor());
+                } else {
+                    logWarning(msg);
+                }
+            }
+            return;
+        }
+
         if (propertyValue == null) {
             logWarning("Property \"%s\" not found on device, cannot verify value \"%s\" ",
                     mPropertyName, mExpectedPropertyValue);
