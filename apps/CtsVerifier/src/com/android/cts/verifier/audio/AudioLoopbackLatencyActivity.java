a5c12e5d8f58cb1d0770d27a85a24d5b0da9611a
Paul McLean <pmclean@google.com>
2020-04-28 12:59:35 -0600

    Refactor CTSVerifier Loopback tests to use the common, Oboe-based, measurement.
    Also various additional reporting.
    Also device connection logic.
    
    Bug: 155205787
    Test: Build, install, run...
    Change-Id: I17dfb45e911ab2d36ef760015650e522b301a455

--- apps/CtsVerifier/src/com/android/cts/verifier/audio/AudioLoopbackLatencyActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/audio/AudioLoopbackLatencyActivity.java
@@ -16,21 +16,9 @@
 
 package com.android.cts.verifier.audio;
 
-import com.android.cts.verifier.R;
-import com.android.compatibility.common.util.ReportLog;
-import com.android.compatibility.common.util.ResultType;
-import com.android.compatibility.common.util.ResultUnit;
 import android.content.Context;
 
-import android.media.AudioDeviceCallback;
-import android.media.AudioDeviceInfo;
-import android.media.AudioManager;
-import android.media.AudioTrack;
-import android.media.MediaRecorder;
-
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 
 import android.util.Log;
 
@@ -39,8 +27,12 @@ import android.view.View.OnClickListener;
 
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.SeekBar;
-import android.widget.ProgressBar;
+
+import com.android.compatibility.common.util.ReportLog;
+import com.android.compatibility.common.util.ResultType;
+import com.android.compatibility.common.util.ResultUnit;
+
+import com.android.cts.verifier.R;
 
 /**
  * Tests Audio Device roundtrip latency by using a loopback plug.
@@ -48,39 +40,18 @@ import android.widget.ProgressBar;
 public class AudioLoopbackLatencyActivity extends AudioLoopbackBaseActivity {
     private static final String TAG = "AudioLoopbackLatencyActivity";
 
-    public static final int BYTES_PER_FRAME = 2;
+//    public static final int BYTES_PER_FRAME = 2;
 
-    NativeAnalyzerThread mNativeAnalyzerThread = null;
-
-    private int mSamplingRate = 44100;
     private int mMinBufferSizeInFrames = 0;
-    private static final double CONFIDENCE_THRESHOLD = 0.6;
 
     OnBtnClickListener mBtnClickListener = new OnBtnClickListener();
-    Context mContext;
-
-    TextView mAudioLevelText;
-    SeekBar mAudioLevelSeekbar;
 
     Button mTestButton;
 
-    TextView mResultText;
-    ProgressBar mProgressBar;
-
-    private double mLatencyMillis = 0.0;
-    private double mConfidence = 0.0;
-
-    int mMaxLevel;
     private class OnBtnClickListener implements OnClickListener {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.audio_loopback_plug_ready_btn:
-                    Log.i(TAG, "audio loopback plug ready");
-                    //enable all the other views.
-                    enableLayout(R.id.audio_loopback_layout, true);
-                    break;
-
                 case R.id.audio_loopback_test_btn:
                     Log.i(TAG, "audio loopback test");
                     startAudioTest();
@@ -95,193 +66,49 @@ public class AudioLoopbackLatencyActivity extends AudioLoopbackBaseActivity {
         setContentView(R.layout.audio_loopback_latency_activity);
         super.onCreate(savedInstanceState);
 
-        mContext = this;
-
-        mAudioLevelText = (TextView)findViewById(R.id.audio_loopback_level_text);
-        mAudioLevelSeekbar = (SeekBar)findViewById(R.id.audio_loopback_level_seekbar);
-        findViewById(R.id.audio_loopback_plug_ready_btn).setOnClickListener(mBtnClickListener);
         mTestButton =(Button)findViewById(R.id.audio_loopback_test_btn);
         mTestButton.setOnClickListener(mBtnClickListener);
-        mResultText = (TextView)findViewById(R.id.audio_loopback_results_text);
-        mProgressBar = (ProgressBar)findViewById(R.id.audio_loopback_progress_bar);
-        showWait(false);
-
-        enableLayout(R.id.audio_loopback_layout, false);         //disabled all content
-        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        mMaxLevel = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        mAudioLevelSeekbar.setMax(mMaxLevel);
-        am.setStreamVolume(AudioManager.STREAM_MUSIC, (int)(0.7 * mMaxLevel), 0);
-        refreshLevel();
-
-        mAudioLevelSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-                am.setStreamVolume(AudioManager.STREAM_MUSIC,
-                        progress, 0);
-                refreshLevel();
-                Log.i(TAG,"Changed stream volume to: " + progress);
-            }
-        });
 
         setPassFailButtonClickListeners();
         getPassButton().setEnabled(false);
         setInfoResources(R.string.audio_loopback_latency_test, R.string.audio_loopback_info, -1);
     }
 
-    /**
-     * refresh Audio Level seekbar and text
-     */
-    private void refreshLevel() {
-        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        int currentLevel = am.getStreamVolume(AudioManager.STREAM_MUSIC);
-        mAudioLevelSeekbar.setProgress(currentLevel);
-
-        String levelText = String.format("%s: %d/%d",
-                getResources().getString(R.string.audio_loopback_level_text),
-                currentLevel, mMaxLevel);
-        mAudioLevelText.setText(levelText);
-    }
-
-    /**
-     * show active progress bar
-     */
-    private void showWait(boolean show) {
-        if (show) {
-            mProgressBar.setVisibility(View.VISIBLE) ;
-        } else {
-            mProgressBar.setVisibility(View.INVISIBLE) ;
-        }
-    }
-
-    /**
-     *  Start the loopback audio test
-     */
-    private void startAudioTest() {
-        getPassButton().setEnabled(false);
+    protected void startAudioTest() {
         mTestButton.setEnabled(false);
-        mLatencyMillis = 0.0;
-        mConfidence = 0.0;
-
-        mNativeAnalyzerThread = new NativeAnalyzerThread();
-        if (mNativeAnalyzerThread != null) {
-            mNativeAnalyzerThread.setMessageHandler(mMessageHandler);
-            // This value matches AAUDIO_INPUT_PRESET_VOICE_RECOGNITION
-            mNativeAnalyzerThread.setInputPreset(MediaRecorder.AudioSource.VOICE_RECOGNITION);
-            mNativeAnalyzerThread.startTest();
-
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        super.startAudioTest(mMessageHandler);
     }
 
-    private void handleTestCompletion() {
-        recordTestResults();
+    protected void handleTestCompletion() {
+        super.handleTestCompletion();
+
         boolean resultValid = mConfidence >= CONFIDENCE_THRESHOLD
                 && mLatencyMillis > 1.0;
         getPassButton().setEnabled(resultValid);
 
-        // Make sure the test thread is finished. It should already be done.
-        if (mNativeAnalyzerThread != null) {
-            try {
-                mNativeAnalyzerThread.stopTest(2 * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        recordTestResults();
+
         showWait(false);
         mTestButton.setEnabled(true);
     }
 
     /**
-     * handler for messages from audio thread
-     */
-    private Handler mMessageHandler = new Handler() {
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch(msg.what) {
-                case NativeAnalyzerThread.NATIVE_AUDIO_THREAD_MESSAGE_REC_STARTED:
-                    Log.v(TAG,"got message native rec started!!");
-                    showWait(true);
-                    mResultText.setText("Test Running...");
-                    break;
-                case NativeAnalyzerThread.NATIVE_AUDIO_THREAD_MESSAGE_OPEN_ERROR:
-                    Log.v(TAG,"got message native rec can't start!!");
-                    mResultText.setText("Test Error opening streams.");
-                    handleTestCompletion();
-                    break;
-                case NativeAnalyzerThread.NATIVE_AUDIO_THREAD_MESSAGE_REC_ERROR:
-                    Log.v(TAG,"got message native rec can't start!!");
-                    mResultText.setText("Test Error while recording.");
-                    handleTestCompletion();
-                    break;
-                case NativeAnalyzerThread.NATIVE_AUDIO_THREAD_MESSAGE_REC_COMPLETE_ERRORS:
-                    mResultText.setText("Test FAILED due to errors.");
-                    handleTestCompletion();
-                    break;
-                case NativeAnalyzerThread.NATIVE_AUDIO_THREAD_MESSAGE_REC_COMPLETE:
-                    if (mNativeAnalyzerThread != null) {
-                        mLatencyMillis = mNativeAnalyzerThread.getLatencyMillis();
-                        mConfidence = mNativeAnalyzerThread.getConfidence();
-                    }
-                    mResultText.setText(String.format(
-                            "Test Finished\nLatency:%.2f ms\nConfidence: %.2f",
-                            mLatencyMillis,
-                            mConfidence));
-                    handleTestCompletion();
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-
-    /**
      * Store test results in log
      */
-    private void recordTestResults() {
+    protected void recordTestResults() {
+        super.recordTestResults();
 
-        getReportLog().addValue(
-                "Estimated Latency",
-                mLatencyMillis,
-                ResultType.LOWER_BETTER,
-                ResultUnit.MS);
-
-        getReportLog().addValue(
-                "Confidence",
-                mConfidence,
-                ResultType.HIGHER_BETTER,
-                ResultUnit.NONE);
-
+        ReportLog reportLog = getReportLog();
         int audioLevel = mAudioLevelSeekbar.getProgress();
-        getReportLog().addValue(
+        reportLog.addValue(
                 "Audio Level",
                 audioLevel,
                 ResultType.NEUTRAL,
                 ResultUnit.NONE);
 
-        getReportLog().addValue(
+        reportLog.addValue(
                 "Frames Buffer Size",
                 mMinBufferSizeInFrames,
-                ResultType.NEUTRAL,
-                ResultUnit.NONE);
-
-        getReportLog().addValue(
-                "Sampling Rate",
-                mSamplingRate,
                 ResultType.NEUTRAL,
                 ResultUnit.NONE);
 
