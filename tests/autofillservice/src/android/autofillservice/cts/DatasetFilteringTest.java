bf0f4d098adb119ecdfe2c265cb675a5c90272b6
Sergii Piatakov <sergii.piatakov@globallogic.com>
2020-05-05 16:07:43 +0300

    Skip dataset setting if the feature is disabled
    With the current implementation test runner tries to configure
    `max_visible_datasets` settings even if a DUT does not have an
    `autofill` feature. As result the command fails, the whole test
    class is skipped and module marked as incomplete.
    
    It is proposed adding the datasets configuration in the custom
    `getMainTestRule` method. In this case it will be called per test
    via base `Rule` only after feature validation.
    
    Test: run the tests on DUT with and without autofill feature:
        $ run cts-dev -m CtsAutoFillServiceTestCases \
                      -t android.autofillservice.cts.DatasetFilteringTest
    Bug: 148687014
    Change-Id: I568e080ad01ad6d5626df12ddbd6d3194922d19c
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- tests/autofillservice/src/android/autofillservice/cts/DatasetFilteringTest.java
+++ tests/autofillservice/src/android/autofillservice/cts/DatasetFilteringTest.java
@@ -37,23 +37,20 @@ import com.android.cts.mockime.ImeCommand;
 import com.android.cts.mockime.ImeEventStream;
 import com.android.cts.mockime.MockImeSession;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
 
 import java.util.regex.Pattern;
 
 public class DatasetFilteringTest extends AbstractLoginActivityTestCase {
 
-    @BeforeClass
-    public static void setMaxDatasets() throws Exception {
-        Helper.setMaxVisibleDatasets(4);
+    @Override
+    protected TestRule getMainTestRule() {
+        return RuleChain.outerRule(new MaxVisibleDatasetsRule(4))
+                        .around(super.getMainTestRule());
     }
 
-    @AfterClass
-    public static void restoreMaxDatasets() throws Exception {
-        Helper.setMaxVisibleDatasets(0);
-    }
 
     private void changeUsername(CharSequence username) {
         mActivity.onUsername((v) -> v.setText(username));
