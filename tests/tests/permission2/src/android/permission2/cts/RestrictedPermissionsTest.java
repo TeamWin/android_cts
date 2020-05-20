887a6e04528b12c46c896a24b9bd01359c67b107
Remi NGUYEN VAN <reminv@google.com>
2020-05-20 17:12:53 +0900

    Remove extra ThrowingRunnable in SystemUtil
    The package already has a ThrowingRunnable as
    com.android.compatibility.common.util.ThrowingRunnable, so there should
    not be a new one.
    
    Adding another ThrowingRunnable caused the original
    ThrowingRunnable to be shadowed, changing the arguments expected by
    runWithShellPermissionIdentity.
    This causes merge conflicts in AOSP, where the method expects one
    ThrowingRunnable in AOSP, but another one once the code is merged
    into internal branches.
    
    Test: m cts && atest RestrictedPermissionsTest \
              RestrictedStoragePermissionTest
    Bug: 157112621
    Change-Id: I5b08bd0ec67f1fa5df81bc7e9bbccc0dec00cc4f

--- tests/tests/permission2/src/android/permission2/cts/RestrictedPermissionsTest.java
+++ tests/tests/permission2/src/android/permission2/cts/RestrictedPermissionsTest.java
@@ -50,7 +50,7 @@ import android.util.ArraySet;
 import androidx.annotation.NonNull;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.android.compatibility.common.util.SystemUtil.ThrowingRunnable;
+import com.android.compatibility.common.util.ThrowingRunnable;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -691,7 +691,7 @@ public class RestrictedPermissionsTest {
         InstrumentationRegistry.getInstrumentation().getUiAutomation()
                 .adoptShellPermissionIdentity();
         try {
-            command.runOrThrow();
+            command.run();
         } finally {
             InstrumentationRegistry.getInstrumentation().getUiAutomation()
                     .dropShellPermissionIdentity();
