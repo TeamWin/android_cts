4ad2c06d3c49a2862cfb6fe5125edbf71f620f41
Anuj Joshi <anuj.joshi@ittiam.com>
2020-06-05 15:51:20 +0530

    [RESTRICT AUTOMERGE] CTS test for Android Security b/111210196
    Bug: 111210196
    Bug: 143368277
    Test: Ran the new testcase on android-8.0.0_r11 to test with/without patch
    
    Merged-In: I6b95a2a2db1cb95460b7cb2ef9ae44d7206c92c5
    Change-Id: I6b95a2a2db1cb95460b7cb2ef9ae44d7206c92c5
    (cherry picked from commit 1054364c9b096a7ab403c6b45f482b3b7d9113f8)

--- hostsidetests/securitybulletin/src/android/security/cts/TestMedia.java
+++ hostsidetests/securitybulletin/src/android/security/cts/TestMedia.java
@@ -54,6 +54,18 @@ public class TestMedia extends SecurityTestCase {
      ******************************************************************************/
 
     /**
+     * b/111210196
+     * Vulnerability Behaviour: EXIT_VULNERABLE (113)
+     */
+    @Test
+    @SecurityTest(minPatchLevel = "2019-12")
+    public void testPocCVE_2019_2228() throws Exception {
+        String inputFiles[] = {"cve_2019_2228_ipp.mp4"};
+        AdbUtils.runPocAssertNoCrashesNotVulnerable("CVE-2019-2228",
+                AdbUtils.TMP_PATH + inputFiles[0], inputFiles, AdbUtils.TMP_PATH, getDevice());
+    }
+
+    /**
      * b/157650336
      * Vulnerability Behaviour: SIGSEGV in self / EXIT_VULNERABLE (113)
      */
