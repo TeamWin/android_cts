c49c52d06f6c89285b64fe02ae6e4afcfd360ebb
junyulai <junyulai@google.com>
2020-06-09 18:51:12 +0800

    Exclude data usage atoms generated by background traffic
    When testing data usage related atoms, http request is used to
    generate some foreground data traffic. However, there are some
    background traffic generated by the same uid. Hence, tests might
    fail if unexpectedly looking into the atom for background traffic.
    
    This change also remove the misleading comments about process state.
    After ag/11030911, http traffic is generated by TestRunner instead
    of foreground activity. Thus, When the test was generating traffic,
    process state of test app will be PROCESS_STATE_FOREGROUND_SERVICE,
    which will cause NetworkStatsService count traffic into foreground
    set.
    
    Test: atest UidAtomTests#testMobileBytesTransferByFgBg \
          UidAtomTests#UidAtomTests#testDataUsageBytesTransfer \
          --generate-new-metrics 20
    Bug: 153174569
    Change-Id: Ie15fd7109e70b4046a429660ec1b034e0a96b2c0

--- hostsidetests/statsd/src/android/cts/statsd/atom/UidAtomTests.java
+++ hostsidetests/statsd/src/android/cts/statsd/atom/UidAtomTests.java
@@ -16,6 +16,7 @@
 package android.cts.statsd.atom;
 
 import static com.android.os.AtomsProto.IntegrityCheckResultReported.Response.ALLOWED;
+
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
@@ -24,6 +25,7 @@ import android.net.wifi.WifiModeEnum;
 import android.os.WakeLockLevelEnum;
 import android.server.ErrorSource;
 import android.telephony.NetworkTypeEnum;
+
 import com.android.compatibility.common.tradefed.build.CompatibilityBuildHelper;
 import com.android.compatibility.common.util.PropertyUtil;
 import com.android.internal.os.StatsdConfigProto.StatsdConfig;
@@ -77,8 +79,10 @@ import com.android.os.AtomsProto.WifiScanStateChanged;
 import com.android.os.StatsLog.EventMetricData;
 import com.android.server.notification.SmallHash;
 import com.android.tradefed.log.LogUtil;
+
 import com.google.common.collect.Range;
 import com.google.protobuf.Descriptors;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -1999,12 +2003,9 @@ public class UidAtomTests extends DeviceAtomTestCase {
         doTestMobileBytesTransferThat(Atom.MOBILE_BYTES_TRANSFER_BY_FG_BG_FIELD_NUMBER, (atom) -> {
             final AtomsProto.MobileBytesTransferByFgBg data =
                     ((Atom) atom).getMobileBytesTransferByFgBg();
-            if (data.getUid() == appUid) {
+            if (data.getUid() == appUid && data.getIsForeground()) {
                 assertDataUsageAtomDataExpected(data.getRxBytes(), data.getTxBytes(),
                         data.getRxPackets(), data.getTxPackets());
-                // IsForeground cannot be judged since foreground activity that launched
-                // while screen off (PROCESS_STATE_TOP_SLEEPING) will be treated as background
-                // in NetworkPolicyManagerService.
                 return true; // found
             }
             return false;
@@ -2017,25 +2018,27 @@ public class UidAtomTests extends DeviceAtomTestCase {
         doTestMobileBytesTransferThat(Atom.DATA_USAGE_BYTES_TRANSFER_FIELD_NUMBER, (atom) -> {
             final AtomsProto.DataUsageBytesTransfer data =
                     ((Atom) atom).getDataUsageBytesTransfer();
-            assertDataUsageAtomDataExpected(data.getRxBytes(), data.getTxBytes(),
-                    data.getRxPackets(), data.getTxPackets());
-            // TODO: verify the RAT type field with the value gotten from device.
-            if (subtypeCombined) {
-                assertThat(data.getRatType()).isEqualTo(NetworkTypeEnum.NETWORK_TYPE_UNKNOWN_VALUE);
-            } else {
-                assertThat(data.getRatType()).isGreaterThan(
-                        NetworkTypeEnum.NETWORK_TYPE_UNKNOWN_VALUE);
+            if (data.getState() == 1 /*NetworkStats.SET_FOREGROUND*/) {
+                assertDataUsageAtomDataExpected(data.getRxBytes(), data.getTxBytes(),
+                        data.getRxPackets(), data.getTxPackets());
+                // TODO: verify the RAT type field with the value gotten from device.
+                if (subtypeCombined) {
+                    assertThat(data.getRatType()).isEqualTo(
+                            NetworkTypeEnum.NETWORK_TYPE_UNKNOWN_VALUE);
+                } else {
+                    assertThat(data.getRatType()).isGreaterThan(
+                            NetworkTypeEnum.NETWORK_TYPE_UNKNOWN_VALUE);
+                }
+
+                // Assert that subscription info is valid.
+                assertThat(data.getSimMcc()).matches("^\\d{3}$");
+                assertThat(data.getSimMnc()).matches("^\\d{2,3}$");
+                assertThat(data.getCarrierId()).isNotEqualTo(
+                        -1); // TelephonyManager#UNKNOWN_CARRIER_ID
+
+                return true; // found
             }
-            // Foreground state cannot be judged since foreground activity that launched
-            // while screen off (PROCESS_STATE_TOP_SLEEPING) will be treated as background
-            // in NetworkPolicyManagerService.
-
-            // Assert that subscription info is valid.
-            assertThat(data.getSimMcc()).matches("^\\d{3}$");
-            assertThat(data.getSimMnc()).matches("^\\d{2,3}$");
-            assertThat(data.getCarrierId()).isNotEqualTo(-1); // TelephonyManager#UNKNOWN_CARRIER_ID
-
-            return true;
+            return false;
         });
     }
 
