9551599a0bc4dfd8b2ab65a8730d45170c18196d
Max Bires <jbires@google.com>
2020-06-16 21:08:58 -0700

    Updating allowed keymaster attestation versions
    With the addition of Keymaster 4.1, an attestation version of 4 must also
    be added to the list of accepted attestation versions.
    
    Bug: 149746301
    Bug: 163336591
    Test: atest KeyAttestationTest
    Change-Id: Ia38a32bf6d47e8f44e03e816058dbe1ece784f0f

--- tests/tests/keystore/src/android/keystore/cts/KeyAttestationTest.java
+++ tests/tests/keystore/src/android/keystore/cts/KeyAttestationTest.java
@@ -780,6 +780,7 @@ public class KeyAttestationTest extends AndroidTestCase {
                 case 2:
                 case 3:
                 case 4:
+                case 41:
                     assertThat(teeEnforcedDigests, is(expectedDigests));
                     break;
 
@@ -808,8 +809,8 @@ public class KeyAttestationTest extends AndroidTestCase {
 
     @SuppressWarnings("unchecked")
     private void checkAttestationSecurityLevelDependentParams(Attestation attestation) {
-        assertThat("Attestation version must be 1, 2, or 3", attestation.getAttestationVersion(),
-               either(is(1)).or(is(2)).or(is(3)));
+        assertThat("Attestation version must be 1, 2, 3, or 4", attestation.getAttestationVersion(),
+               either(is(1)).or(is(2)).or(is(3)).or(is(4)));
 
         AuthorizationList teeEnforced = attestation.getTeeEnforced();
         AuthorizationList softwareEnforced = attestation.getSoftwareEnforced();
