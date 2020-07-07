1df658529e28c51f4acede3a3f95e827b9332fc8
Alex Johnston <acjohnston@google.com>
2020-07-07 16:10:10 +0100

    Remove duplicate class CameraUtils
    * Created common folder in CTS
      device policy tests
    * Moved CameraUtils to common folder
    * Added static lib dependency to
      ManagedProfile
    
    Bug: 147356677
    Test: atest com.android.cts.devicepolicy.OrgOwnedProfileOwnerTest#testCameraDisabledOnParentIsEnforced
          atest com.android.cts.devicepolicy.ManagedProfileTest#testCameraPolicy
    Change-Id: I0a39213c69fa21457d79679e5fe78e7df6b7aecb

--- hostsidetests/devicepolicy/app/DeviceAndProfileOwner/src/com/android/cts/deviceandprofileowner/UserRestrictionsParentTest.java
+++ hostsidetests/devicepolicy/app/DeviceAndProfileOwner/src/com/android/cts/deviceandprofileowner/UserRestrictionsParentTest.java
@@ -30,6 +30,8 @@ import android.os.UserManager;
 import android.test.InstrumentationTestCase;
 import android.util.Log;
 
+import com.android.cts.devicepolicy.CameraUtils;
+
 import com.google.common.collect.ImmutableSet;
 
 import java.util.Set;
@@ -154,8 +156,7 @@ public class UserRestrictionsParentTest extends InstrumentationTestCase {
         while (successToOpen != canOpen && retries > 0) {
             retries--;
             Thread.sleep(500);
-            successToOpen = CameraUtils
-                    .blockUntilOpenCamera(mCameraManager, mBackgroundHandler);
+            successToOpen = CameraUtils.blockUntilOpenCamera(mCameraManager, mBackgroundHandler);
         }
         assertEquals(String.format("Timed out waiting the value to change to %b (actual=%b)",
                 canOpen, successToOpen), canOpen, successToOpen);
--- hostsidetests/devicepolicy/app/common/src/com/android/cts/devicepolicy/CameraUtils.java
+++ hostsidetests/devicepolicy/app/common/src/com/android/cts/devicepolicy/CameraUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,16 +11,15 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License
+ * limitations under the License.
  */
 
-package com.android.cts.managedprofile;
+package com.android.cts.devicepolicy;
 
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.os.Handler;
 import android.util.Log;
-
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -28,7 +27,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 /**
  * A util class to help open camera in a blocking way.
  */
-class CameraUtils {
+public class CameraUtils {
 
     private static final String TAG = "CameraUtils";
 
