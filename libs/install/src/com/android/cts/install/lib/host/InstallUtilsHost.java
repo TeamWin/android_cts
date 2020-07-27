7717b26025fb1c042e8ab0f4aad88c33cf0a0516
Mohammad Samiul Islam <samiul@google.com>
2020-07-27 18:51:19 +0100

    Add uninstall shim apex logic to InstallUtilsHost
    This is a partial cherry-pick of the change
    Id877ccaa5ca0030056a9bd0915a3419c0b18d6a8
    
    Test: atest sdkextensions_e2e_tests
    Change-Id: I31e2e673f5dc2f4820d12348f8c0264279c2d0c8
    Merged-In: Id877ccaa5ca0030056a9bd0915a3419c0b18d6a8
    (cherry picked from commit 6e23bc5d0f20f4ff5fcd29234efd7f704ed6a168)

--- libs/install/src/com/android/cts/install/lib/host/InstallUtilsHost.java
+++ libs/install/src/com/android/cts/install/lib/host/InstallUtilsHost.java
@@ -16,18 +16,27 @@
 
 package com.android.cts.install.lib.host;
 
+import static com.android.cts.shim.lib.ShimPackage.SHIM_APEX_PACKAGE_NAME;
+
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
+import com.android.ddmlib.Log;
+import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
 import com.android.tradefed.util.CommandResult;
 import com.android.tradefed.util.CommandStatus;
+
+import java.util.Optional;
 
 /**
  * Utilities to facilitate installation in tests on host side.
  */
 public class InstallUtilsHost {
+    private static final String TAG = InstallUtilsHost.class.getSimpleName();
 
-    private BaseHostJUnit4Test mTest;
+    private final BaseHostJUnit4Test mTest;
 
     public InstallUtilsHost(BaseHostJUnit4Test test) {
         mTest = test;
@@ -45,8 +54,49 @@ public class InstallUtilsHost {
      */
     public boolean isCheckpointSupported() throws Exception {
         CommandResult result = mTest.getDevice().executeShellV2Command("sm supports-checkpoint");
-        assertWithMessage("Failed to check if fs checkpointing is supported : %s",
+        assertWithMessage("Failed to check if file system checkpoint is supported : %s",
                 result.getStderr()).that(result.getStatus()).isEqualTo(CommandStatus.SUCCESS);
         return "true".equals(result.getStdout().trim());
+    }
+
+    /**
+     * Uninstalls a shim apex only if it's latest version is installed on /data partition (i.e.
+     * it has a version higher than {@code 1}).
+     *
+     * <p>This is purely to optimize tests run time. Since uninstalling an apex requires a reboot,
+     * and only a small subset of tests successfully install an apex, this code avoids ~10
+     * unnecessary reboots.
+     */
+    public void uninstallShimApexIfNecessary() throws Exception {
+        if (!isApexUpdateSupported()) {
+            // Device doesn't support updating apex. Nothing to uninstall.
+            return;
+        }
+        final ITestDevice.ApexInfo shimApex = getShimApex().orElseThrow(
+                () -> new AssertionError("Can't find " + SHIM_APEX_PACKAGE_NAME));
+        if (shimApex.sourceDir.startsWith("/system")) {
+            // System version is active, nothing to uninstall.
+            return;
+        }
+        // Non system version is active, need to uninstall it and reboot the device.
+        Log.i(TAG, "Uninstalling shim apex");
+        final String errorMessage = mTest.getDevice().uninstallPackage(SHIM_APEX_PACKAGE_NAME);
+        if (errorMessage != null) {
+            Log.e(TAG, "Failed to uninstall " + SHIM_APEX_PACKAGE_NAME + " : " + errorMessage);
+        } else {
+            mTest.getDevice().reboot();
+            final ITestDevice.ApexInfo shim = getShimApex().orElseThrow(
+                    () -> new AssertionError("Can't find " + SHIM_APEX_PACKAGE_NAME));
+            assertThat(shim.versionCode).isEqualTo(1L);
+            assertThat(shim.sourceDir).startsWith("/system");
+        }
+    }
+
+    /**
+     * Returns the active shim apex as optional.
+     */
+    public Optional<ITestDevice.ApexInfo> getShimApex() throws DeviceNotAvailableException {
+        return mTest.getDevice().getActiveApexes().stream().filter(
+                apex -> apex.name.equals(SHIM_APEX_PACKAGE_NAME)).findAny();
     }
 }
