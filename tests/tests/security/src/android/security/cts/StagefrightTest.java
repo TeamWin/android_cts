eb6a24d2674ca0cb0dec842eaea5d00e97bab31b
rchanchl <quic_rchanchl@quicinc.com>
2020-08-19 11:11:15 +0530

    CTS test for Android Security b/147841400
    Test: successful run of newly introduced CTS test case.
    
    Bug: b/147841400
    Change-Id: I245be109f981fe559dcbae4873c5b304ea2af7a5

--- tests/tests/security/src/android/security/cts/StagefrightTest.java
+++ tests/tests/security/src/android/security/cts/StagefrightTest.java
@@ -1241,6 +1241,12 @@ public class StagefrightTest {
      ***********************************************************/
 
     @Test
+    @SecurityTest(minPatchLevel = "2020-03")
+    public void testStagefright_cve_2019_10591() throws Exception {
+        doStagefrightTest(R.raw.cve_2019_10591);
+    }
+
+    @Test
     @SecurityTest(minPatchLevel = "2020-02")
     public void testStagefright_cve_2019_10590() throws Exception {
         doStagefrightTest(R.raw.cve_2019_10590);
