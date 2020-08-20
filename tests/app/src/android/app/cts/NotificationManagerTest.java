2b5a4a2da70513d9a83f26bb1adeed3d2a5c10be
Julia Reynolds <juliacr@google.com>
2020-08-20 14:28:05 -0400

    Reduce flakiness in test
    Test: atest --rerun-until-failure
    Fixes: 165647455
    Change-Id: I38f1fbaea1557f89c1bea625cb941d0403b6d03b

--- tests/app/src/android/app/cts/NotificationManagerTest.java
+++ tests/app/src/android/app/cts/NotificationManagerTest.java
@@ -2080,7 +2080,7 @@ public class NotificationManagerTest extends AndroidTestCase {
             mNotificationManager.setInterruptionFilter(INTERRUPTION_FILTER_PRIORITY);
 
             // delay for streams to get into correct mute states
-            Thread.sleep(50);
+            Thread.sleep(1000);
             assertTrue("Music (media) stream should be muted",
                     mAudioManager.isStreamMute(AudioManager.STREAM_MUSIC));
             assertTrue("System stream should be muted",
@@ -2121,7 +2121,7 @@ public class NotificationManagerTest extends AndroidTestCase {
             mNotificationManager.setInterruptionFilter(INTERRUPTION_FILTER_PRIORITY);
 
             // delay for streams to get into correct mute states
-            Thread.sleep(50);
+            Thread.sleep(1000);
             assertFalse("Music (media) stream should not be muted",
                     mAudioManager.isStreamMute(AudioManager.STREAM_MUSIC));
             assertTrue("System stream should be muted",
@@ -3550,8 +3550,7 @@ public class NotificationManagerTest extends AndroidTestCase {
     }
 
     public void testOriginalChannelImportance() {
-        NotificationChannel channel = new NotificationChannel(
-                "my channel", "my channel", IMPORTANCE_HIGH);
+        NotificationChannel channel = new NotificationChannel(mId, "my channel", IMPORTANCE_HIGH);
 
         mNotificationManager.createNotificationChannel(channel);
 
