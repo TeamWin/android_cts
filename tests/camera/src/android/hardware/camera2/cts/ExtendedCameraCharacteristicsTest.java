0d4bc453e5bd288e3ae26eda9466dc3ae42675a4
Sebastian Raase <sebastian.raase@sony.com>
2020-08-27 19:01:02 +0200

    Avoid checking focal lengths for external cameras
    External camera peripherals may not have a focal length,
    so it cannot be used to match such devices for API1
    compatibility.
    
    Bug: 166635639
    Test: ExtendedCameraCharacteristicsTest#testLegacyCameraDeviceParity
    Change-Id: I9b492aa4739aa85f31bc071dd4bc95ce6a4307ce

--- tests/camera/src/android/hardware/camera2/cts/ExtendedCameraCharacteristicsTest.java
+++ tests/camera/src/android/hardware/camera2/cts/ExtendedCameraCharacteristicsTest.java
@@ -2357,16 +2357,21 @@ public class ExtendedCameraCharacteristicsTest extends Camera2AndroidTestCase {
         }
 
         float legacyFocalLength = params.getFocalLength();
-        float [] focalLengths = staticMeta.getAvailableFocalLengthsChecked();
-        boolean found = false;
-        for (float focalLength : focalLengths) {
-            if (Math.abs(focalLength - legacyFocalLength) <= FOCAL_LENGTH_TOLERANCE) {
-                found = true;
-                break;
+        if (ch.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS) != null) {
+            float [] focalLengths = staticMeta.getAvailableFocalLengthsChecked();
+            boolean found = false;
+            for (float focalLength : focalLengths) {
+                if (Math.abs(focalLength - legacyFocalLength) <= FOCAL_LENGTH_TOLERANCE) {
+                    found = true;
+                    break;
+                }
             }
+            return found;
+        } else if (legacyFocalLength != -1.0f) {
+            return false;
         }
 
-        return found;
+        return true;
     }
 
     /**
