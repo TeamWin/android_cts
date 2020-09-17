65670eb01163e70872f2552e3d42a7241171a336
Garfield Tan <xutan@google.com>
2020-09-17 14:41:10 -0700

    Use fullscreen task to test orientation request
    Freeform task isn't allowed to request orientation change to display, so
    we need to use fullscreen task.
    
    Bug: 168750793
    Test: atest PinnedStackTests#testSetRequestedOrientationWhilePinned on
    freeform first device.
    Test: atest PinnedStackTests#testSetRequestedOrientationWhilePinned on
    freeform on crosshatch.
    
    Change-Id: If397d9fee650ba6989d85951af3c85eefcd115d3
    (cherry picked from commit 031ad976ab09aeb668533c87ec684e350b6e9aba)

--- tests/framework/base/windowmanager/src/android/server/wm/PinnedStackTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/PinnedStackTests.java
@@ -972,7 +972,7 @@ public class PinnedStackTests extends ActivityManagerTestBase {
     @Test
     public void testSetRequestedOrientationWhilePinned() throws Exception {
         // Launch the PiP activity fixed as portrait, and enter picture-in-picture
-        launchActivity(PIP_ACTIVITY,
+        launchActivity(PIP_ACTIVITY, WINDOWING_MODE_FULLSCREEN,
                 EXTRA_PIP_ORIENTATION, String.valueOf(ORIENTATION_PORTRAIT),
                 EXTRA_ENTER_PIP, "true");
         waitForEnterPip(PIP_ACTIVITY);
