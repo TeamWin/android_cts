1d84adb51abcccf24b8a59c42237dc8b67fffde9
Yan Yan <evitayan@google.com>
2020-10-12 15:45:01 -0700

    Migrate IPsec CTS out of tests/tests/net
    Bug: 170487836
    Test: atest CtsIkeTestCases
    Change-Id: I08f069c2a11f3daa3f0332c631055a1e0be7ce7b
    Merged-In: I08f069c2a11f3daa3f0332c631055a1e0be7ce7b
    (cherry picked from commit 51c0fa972e885d7b83bcbcce45770b57dfeac192)

--- tests/tests/ipsec/src/android/ipsec/ike/cts/IkeTestBase.java
+++ tests/tests/ipsec/src/android/ipsec/ike/cts/IkeTestBase.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.net.ipsec.ike.cts;
+package android.ipsec.ike.cts;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
