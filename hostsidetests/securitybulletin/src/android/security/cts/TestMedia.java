10d90e6cd829262310d718912f1e23f2846b9952
Aravind Kumar Angadi <aravind.angadi@ittiam.com>
2020-10-12 14:12:51 +0530

    [RESTRICT AUTOMERGE] CTS test for Android security b/156999009
    Bug: 156999009
    Bug: 163560415
    Test: Ran the new testcase on android-8.0.0_r30 with/without patch
    
    Change-Id: I7f1cbe9d555445d20a41fb60d3cf1ddbccc08605
    Merged-In: I7f1cbe9d555445d20a41fb60d3cf1ddbccc08605
    (cherry picked from commit cc325af0495b0866b88bb0b7bf4d7778bfe7704c)

--- hostsidetests/securitybulletin/src/android/security/cts/TestMedia.java
+++ hostsidetests/securitybulletin/src/android/security/cts/TestMedia.java
@@ -54,6 +54,21 @@ public class TestMedia extends SecurityTestCase {
      ******************************************************************************/
 
     /**
+     * b/156999009
+     * Vulnerability Behaviour: SIGABRT in self
+     */
+    @Test
+    @SecurityTest(minPatchLevel = "2020-10")
+    public void testPocCVE_2020_0408() throws Exception {
+        String signals[] = {CrashUtils.SIGSEGV, CrashUtils.SIGBUS, CrashUtils.SIGABRT};
+        String binaryName = "CVE-2020-0408";
+        AdbUtils.pocConfig testConfig = new AdbUtils.pocConfig(binaryName, getDevice());
+        testConfig.config = new CrashUtils.Config().setProcessPatterns(binaryName);
+        testConfig.config.setSignals(signals);
+        AdbUtils.runPocAssertNoCrashesNotVulnerable(testConfig);
+    }
+
+    /**
      * b/161894517
      * Vulnerability Behaviour: SIGABRT in self
      */
