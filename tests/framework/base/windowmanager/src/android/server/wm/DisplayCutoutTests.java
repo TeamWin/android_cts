2e58ae207f18f0dd3868a3427e2f7746fbfb7a25
shawnlin <shawnlin@google.com>
2020-10-14 15:06:42 +0800

    Fix WindowInsetsLayoutTests & DisplayCutoutTests for waterfall
    WindowInsetsLayoutTests
      - The test activity expected fullscreen bounds but the
      layoutInDisplayCutoutMode was default.
      - set layoutInDisplayCutoutMode to always mode.
    
    DisplayCutoutTests
      - There is a case that when cutout areas are on the same side with
      status bar and navigation bar and the app is not fullscreen, the
      system will still provide the cutout to the app no matter it's short
      or long edge cutout.
      - Set the test activity to fullscreen.
    
    Bug: 168590205
    Test: 1. Enable waterfall emulation in develop option
          2. atest WindowInsetsLayoutTests DisplayCutoutTests
    Change-Id: I0da7bee2df238ee110688f95b6aeee5713112ea4

--- tests/framework/base/windowmanager/src/android/server/wm/DisplayCutoutTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/DisplayCutoutTests.java
@@ -556,7 +556,9 @@ public class DisplayCutoutTests {
         public void onWindowFocusChanged(boolean hasFocus) {
             if (hasFocus) {
                 getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
             }
         }
 
