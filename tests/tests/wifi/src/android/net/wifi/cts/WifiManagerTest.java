af011df606fffa3bd14c347a79cc70afdf142ba5
Roshan Pius <rpius@google.com>
2020-10-30 09:40:23 -0700

    WifiManagerTest: Fix usage of first saved network
    This was missed instance from the previous fix.
    
    Bug: 171371509
    Test: atest android.net.wifi.cts.WifiManagerTest
    Change-Id: I1c395dc229aa83c0f3932ee9117efad4eb95cd32

--- tests/tests/wifi/src/android/net/wifi/cts/WifiManagerTest.java
+++ tests/tests/wifi/src/android/net/wifi/cts/WifiManagerTest.java
@@ -1721,10 +1721,10 @@ public class WifiManagerTest extends WifiJUnit3TestBase {
             waitForDisconnection();
 
             // Now trigger connection to the last saved network.
+            WifiConfiguration savedNetworkToConnect =
+                    savedNetworks.get(savedNetworks.size() - 1);
             synchronized (mLock) {
                 try {
-                    WifiConfiguration savedNetworkToConnect =
-                            savedNetworks.get(savedNetworks.size() - 1);
                     if (withNetworkId) {
                         mWifiManager.connect(savedNetworkToConnect.networkId, actionListener);
                     } else {
@@ -1739,7 +1739,7 @@ public class WifiManagerTest extends WifiJUnit3TestBase {
             assertTrue(actionListener.onSuccessCalled);
             // Wait for connection to complete & ensure we are connected to the saved network.
             waitForConnection();
-            assertEquals(savedNetworks.get(0).networkId,
+            assertEquals(savedNetworkToConnect.networkId,
                     mWifiManager.getConnectionInfo().getNetworkId());
         } finally {
             // Re-enable all saved networks before exiting.
