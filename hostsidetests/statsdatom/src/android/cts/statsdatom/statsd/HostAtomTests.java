86e33700a7d309f9d00670e94db4c37e1aada3bf
Jeffrey Huang <jeffreyhuang@google.com>
2020-11-03 11:45:24 -0800

    Migrate testBatteryVoltage
    Bug: 163546661
    Test: atest CtsStatsdAtomHostTestCase
    Change-Id: I2416872d76d5ad6e8fe066cdedb22378b0b89fa1

--- hostsidetests/statsdatom/src/android/cts/statsdatom/statsd/HostAtomTests.java
+++ hostsidetests/statsdatom/src/android/cts/statsdatom/statsd/HostAtomTests.java
@@ -393,22 +393,20 @@ public class HostAtomTests extends AtomTestCase {
     }
 
     public void testBatteryVoltage() throws Exception {
-        if (!hasFeature(FEATURE_WATCH, false)) return;
-        StatsdConfig.Builder config = createConfigBuilder();
-        addGaugeAtomWithDimensions(config, Atom.BATTERY_VOLTAGE_FIELD_NUMBER, null);
+        if (DeviceUtils.hasFeature(getDevice(), FEATURE_WATCH)) return;
 
-        uploadConfig(config);
+        ConfigUtils.uploadConfigForPulledAtom(getDevice(), DeviceUtils.STATSD_ATOM_TEST_PKG,
+                Atom.BATTERY_VOLTAGE_FIELD_NUMBER);
 
-        Thread.sleep(WAIT_TIME_LONG);
-        setAppBreadcrumbPredicate();
-        Thread.sleep(WAIT_TIME_LONG);
+        AtomTestUtils.sendAppBreadcrumbReportedAtom(getDevice());
+        Thread.sleep(AtomTestUtils.WAIT_TIME_LONG);
 
-        List<Atom> data = getGaugeMetricDataList();
+        List<Atom> data = ReportUtils.getGaugeMetricAtoms(getDevice());
 
         assertThat(data).isNotEmpty();
         Atom atom = data.get(0);
         assertThat(atom.getBatteryVoltage().hasVoltageMillivolt()).isTrue();
-        if (hasBattery()) {
+        if (DeviceUtils.hasBattery(getDevice())) {
             assertThat(atom.getBatteryVoltage().getVoltageMillivolt()).isGreaterThan(0);
         }
     }
