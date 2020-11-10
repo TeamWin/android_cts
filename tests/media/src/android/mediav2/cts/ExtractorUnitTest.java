4f4bdf6874c32940ac08510b35b913b00b52182a
Gopalakrishnan Nallasamy <gokrishnan@google.com>
2020-11-09 17:50:09 -0800

    MediaAppender+MediaMuxer:AppendFunction, new APIs
    Added test cases to test appending data to MPEG4 files and two new
    APIs, GetTrackFormat and GetTrackCount in NDK MediaMuxer.
    MPEG4 test files with only audio, only video and one audio and one
    video are added as inputs to the tests.
    New formats can be added to the test just by adding suitable test files.
    
    Bug: 154734325
    
    Test: atest android.mediav2.cts.MuxerTest
          -- all the new tests are passing.
    
    Change-Id: Iad22d71d80511576a7d00e4a9f807f01cdcf6d80

--- tests/media/src/android/mediav2/cts/ExtractorUnitTest.java
+++ tests/media/src/android/mediav2/cts/ExtractorUnitTest.java
@@ -963,5 +963,17 @@ public class ExtractorUnitTest {
             assertTrue(nativeTestIfNullLocationIsRejectedBySetDataSource());
         }
         private native boolean nativeTestIfNullLocationIsRejectedBySetDataSource();
+
+        @Test
+        public void testVideoSampleFileOffsetByGetSampleFormat() {
+            assertTrue(nativeTestVideoSampleFileOffsetByGetSampleFormat(mInpPrefix + mInpMedia));
+        }
+        private native boolean nativeTestVideoSampleFileOffsetByGetSampleFormat(String srcPath);
+
+        @Test
+        public void testAudioSampleFileOffsetByGetSampleFormat() {
+            assertTrue(nativeTestAudioSampleFileOffsetByGetSampleFormat(mInpPrefix + mInpMedia));
+        }
+        private native boolean nativeTestAudioSampleFileOffsetByGetSampleFormat(String srcPath);
     }
 }
