ef103f8cb61fa47fb8d6a0e20bf57e3949260058
Nandana Dutt <nandana@google.com>
2020-11-23 11:02:11 +0000

    Add a device-only test suite
    This is preparation for moving all tests that do
    not require host side support from ScopedStorageTest
    to device side in order to speed up tests.
    
    BUG: 159593019
    Test: atest ScopedStorageDeviceOnlyTest
    Change-Id: Ica41f15cac63efa0226d2c057e1fd3867e2a5a7b

---
+++ hostsidetests/scopedstorage/device/src/android/scopedstorage/cts/device/ScopedStorageDeviceTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.scopedstorage.cts.device;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Device-side test suite to verify scoped storage business logic.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ScopedStorageDeviceTest {
+
+    /**
+     * No-op test.
+     */
+    @Test
+    public void noopTest() {
+        // TODO(b/159593019): Move tests that don't require host side support from
+        // ScopedStorageTest here.
+        assertThat(5).isAtLeast(4);
+    }
+}
