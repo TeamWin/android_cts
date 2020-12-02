56be0d735dd2837012b977126ecb86b91d2983fa
Alan Stokes <alanstokes@google.com>
2020-12-02 09:39:10 +0000

    Add tests for cross-user data isolation.
    Apps running in one user should not be able to probe directories
    relating to other users.
    
    This is enforced by the app data isolation feature for apps targeting
    API level 30, and by SELinux rules for older apps, leading to slightly
    different tests.
    
    Also did some minor refactoring & added extra assertions to existing
    tests.
    
    Bug: 141677108
    Test: atest AppDataIsolationTests
    Change-Id: I56b4a2d1b62f5b02fee4809bd47cfdc480b0dbae

--- hostsidetests/appsecurity/test-apps/AppDataIsolationTestApp/common/src/com/android/cts/appdataisolation/common/FileUtils.java
+++ hostsidetests/appsecurity/test-apps/AppDataIsolationTestApp/common/src/com/android/cts/appdataisolation/common/FileUtils.java
@@ -59,13 +59,16 @@ public class FileUtils {
         });
         assertThat(exception.getMessage()).contains(JAVA_FILE_PERMISSION_DENIED_MSG);
         assertThat(exception.getMessage()).doesNotContain(JAVA_FILE_NOT_FOUND_MSG);
+
+        assertThat(new File(path).canExecute()).isFalse();
     }
 
     public static void assertDirDoesNotExist(String path) {
         // Trying to access a file/directory that does exist, but is not visible to the caller, it
         // should return file not found.
+        final File directory = new File(path);
         Exception exception = expectThrows(FileNotFoundException.class, () -> {
-            new FileInputStream(new File(path));
+            new FileInputStream(directory);
         });
         assertThat(exception.getMessage()).contains(JAVA_FILE_NOT_FOUND_MSG);
         assertThat(exception.getMessage()).doesNotContain(JAVA_FILE_PERMISSION_DENIED_MSG);
@@ -78,6 +81,8 @@ public class FileUtils {
         } catch (ErrnoException e) {
             assertEquals(e.errno, OsConstants.EACCES, "Error on path: " + path);
         }
+
+        assertThat(directory.exists()).isFalse();
     }
 
     public static void assertDirIsAccessible(String path) {
@@ -85,6 +90,8 @@ public class FileUtils {
         // if app has search permission to that directory, it should return file not found
         // and not security exception.
         assertFileDoesNotExist(path, "FILE_DOES_NOT_EXIST");
+
+        assertThat(new File(path).canExecute()).isTrue();
     }
 
     public static void assertFileIsAccessible(String path) {
