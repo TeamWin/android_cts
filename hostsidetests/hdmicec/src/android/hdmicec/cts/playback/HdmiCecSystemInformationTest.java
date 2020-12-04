2fac3094365066411375f9e9b6f7d53635d01ed0
henry-th.yeh <henry-th.yeh@mediatek.corp-partner.google.com>
2020-12-04 16:23:38 +0800

    Fix cect_11_2_6_3_SetValidMenuLanguage random fail
    Add delay in CtsHdmiCecHostTestCases
    cect_11_2_6_3_SetValidMenuLanguage to increase the pass
    rate. Let the test case to be compatible with more devices.
    
    Bug: 174816927
    Test: Build CTS
    Test: run cts -m CtsHdmiCecHostTestCases
    -t android.hdmicec.cts.playback.HdmiCecSystemInformationTest#
    cect_11_2_6_3_SetValidMenuLanguage --abi armeabi-v7a
    
    Change-Id: I6813905b6c804c1c93ea59db9d8c255c2f7c45d8

--- hostsidetests/hdmicec/src/android/hdmicec/cts/playback/HdmiCecSystemInformationTest.java
+++ hostsidetests/hdmicec/src/android/hdmicec/cts/playback/HdmiCecSystemInformationTest.java
@@ -38,6 +38,8 @@ import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 /** HDMI CEC system information tests (Section 11.2.6) */
 @RunWith(DeviceJUnit4ClassRunner.class)
 public final class HdmiCecSystemInformationTest extends BaseHostJUnit4Test {
@@ -148,6 +150,7 @@ public final class HdmiCecSystemInformationTest extends BaseHostJUnit4Test {
         try {
             hdmiCecClient.sendCecMessage(LogicalAddress.TV, LogicalAddress.BROADCAST,
                     CecOperand.SET_MENU_LANGUAGE, CecMessage.convertStringToHexParams(language));
+            TimeUnit.SECONDS.sleep(5);
             assertThat(extractLanguage(getSystemLocale())).isEqualTo(newLanguage);
         } finally {
             setSystemLocale(locale);
