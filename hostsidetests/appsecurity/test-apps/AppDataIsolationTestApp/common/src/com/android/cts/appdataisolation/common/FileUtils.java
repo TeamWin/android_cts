549094ad0416dd3675aabe149b45720a5a728a92
Martin Stjernholm <mast@google.com>
2020-12-17 15:26:41 +0000

    Revert "Add tests for cross-user data isolation."
    This reverts commit f7a3466217ee3b26771b259d06c8dec4e3bbe010.
    
    Reason for revert: Breaks aosp_crosshatch-userdebug and others
    
    Bug: 175869334
    Change-Id: I4c1246d3a79690320f9f0644b670743ff129c926

--- hostsidetests/appsecurity/test-apps/AppDataIsolationTestApp/common/src/com/android/cts/appdataisolation/common/FileUtils.java
+++ hostsidetests/appsecurity/test-apps/AppDataIsolationTestApp/common/src/com/android/cts/appdataisolation/common/FileUtils.java
@@ -59,16 +59,13 @@ public class FileUtils {
         });
         assertThat(exception.getMessage()).contains(JAVA_FILE_PERMISSION_DENIED_MSG);
         assertThat(exception.getMessage()).doesNotContain(JAVA_FILE_NOT_FOUND_MSG);
-
-        assertThat(new File(path).canExecute()).isFalse();
     }
 
     public static void assertDirDoesNotExist(String path) {
         // Trying to access a file/directory that does exist, but is not visible to the caller, it
         // should return file not found.
-        final File directory = new File(path);
         Exception exception = expectThrows(FileNotFoundException.class, () -> {
-            new FileInputStream(directory);
+            new FileInputStream(new File(path));
         });
         assertThat(exception.getMessage()).contains(JAVA_FILE_NOT_FOUND_MSG);
         assertThat(exception.getMessage()).doesNotContain(JAVA_FILE_PERMISSION_DENIED_MSG);
@@ -81,8 +78,6 @@ public class FileUtils {
         } catch (ErrnoException e) {
             assertEquals(e.errno, OsConstants.EACCES, "Error on path: " + path);
         }
-
-        assertThat(directory.exists()).isFalse();
     }
 
     public static void assertDirIsAccessible(String path) {
@@ -90,8 +85,6 @@ public class FileUtils {
         // if app has search permission to that directory, it should return file not found
         // and not security exception.
         assertFileDoesNotExist(path, "FILE_DOES_NOT_EXIST");
-
-        assertThat(new File(path).canExecute()).isTrue();
     }
 
     public static void assertFileIsAccessible(String path) {
