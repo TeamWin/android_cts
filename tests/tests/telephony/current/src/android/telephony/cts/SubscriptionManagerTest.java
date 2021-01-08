a777cf07c047b41b17246bf08bee6f6bec3c8971
Sarah Chin <sarahchin@google.com>
2021-01-07 17:24:15 -0800

    Update SubscriptionManager subscription override tests
    Changed from TEMP_NOT_METERED -> NOT_METERED
    
    Test: atest SubscriptionManagerTest
    Bug: 176836936
    Change-Id: I798ef8f7ccad78e25f89fa30cbb0a0ced2d828b6
    Merged-In: I798ef8f7ccad78e25f89fa30cbb0a0ced2d828b6
    (cherry picked from commit a0c158cd2c72f69958bae2124ceec346192ac8c1)

--- tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
@@ -18,6 +18,7 @@ package android.telephony.cts;
 
 import static android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET;
 import static android.net.NetworkCapabilities.NET_CAPABILITY_NOT_CONGESTED;
+import static android.net.NetworkCapabilities.NET_CAPABILITY_NOT_METERED;
 import static android.net.NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED;
 import static android.net.NetworkCapabilities.NET_CAPABILITY_TEMPORARILY_NOT_METERED;
 import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
@@ -355,13 +356,12 @@ public class SubscriptionManagerTest {
         if (!isUnmetered5GSupported()) return;
 
         // Cellular is metered by default
-        assertFalse(cm.getNetworkCapabilities(net).hasCapability(
-                NET_CAPABILITY_TEMPORARILY_NOT_METERED));
+        assertFalse(cm.getNetworkCapabilities(net).hasCapability(NET_CAPABILITY_NOT_METERED));
 
         // Override should make it go temporarily unmetered
         {
             final CountDownLatch latch = waitForNetworkCapabilities(net, caps -> {
-                return caps.hasCapability(NET_CAPABILITY_TEMPORARILY_NOT_METERED);
+                return caps.hasCapability(NET_CAPABILITY_NOT_METERED);
             });
             mSm.setSubscriptionOverrideUnmetered(mSubId, true, 0);
             assertTrue(latch.await(10, TimeUnit.SECONDS));
@@ -370,7 +370,7 @@ public class SubscriptionManagerTest {
         // Clearing override should make it go metered
         {
             final CountDownLatch latch = waitForNetworkCapabilities(net, caps -> {
-                return !caps.hasCapability(NET_CAPABILITY_TEMPORARILY_NOT_METERED);
+                return !caps.hasCapability(NET_CAPABILITY_NOT_METERED);
             });
             mSm.setSubscriptionOverrideUnmetered(mSubId, false, 0);
             assertTrue(latch.await(10, TimeUnit.SECONDS));
