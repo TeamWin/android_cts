fa1c92ca9057798d4f65780b0202ce38f2a30223
wilsonshih <wilsonshih@google.com>
2021-01-12 14:23:00 +0800

    Basic tests for customize splash screen exit animation.(1/N)
    Bug: 73289295
    Test: atest CtsWindowManagerDeviceTestCases:SplashscreenTests
    
    Change-Id: I6c09f2646c74106b3d01726e9562b216a486368a

---
+++ tests/framework/base/windowmanager/app/src/android/server/wm/app/HandleSplashScreenExitActivity.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.server.wm.app;
+
+import static android.server.wm.app.Components.TestStartingWindowKeys.CANCEL_HANDLE_EXIT;
+import static android.server.wm.app.Components.TestStartingWindowKeys.CONTAINS_CENTER_VIEW;
+import static android.server.wm.app.Components.TestStartingWindowKeys.HANDLE_SPLASH_SCREEN_EXIT;
+import static android.server.wm.app.Components.TestStartingWindowKeys.RECEIVE_SPLASH_SCREEN_EXIT;
+import static android.server.wm.app.Components.TestStartingWindowKeys.REQUEST_HANDLE_EXIT_ON_CREATE;
+import static android.server.wm.app.Components.TestStartingWindowKeys.REQUEST_HANDLE_EXIT_ON_RESUME;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.server.wm.TestJournalProvider;
+import android.view.View;
+import android.window.SplashScreen;
+
+public class HandleSplashScreenExitActivity extends Activity {
+    private SplashScreen mSSM;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSSM = getSplashScreen();
+        if (getIntent().getBooleanExtra(REQUEST_HANDLE_EXIT_ON_CREATE, false)) {
+            mSSM.setOnExitAnimationListener(mSplashScreenExitHandler);
+        }
+    }
+
+    private final SplashScreen.OnExitAnimationListener mSplashScreenExitHandler =
+            view -> {
+                final Context baseContext = getBaseContext();
+                final View centerView = view.getIconView();
+                TestJournalProvider.putExtras(baseContext, HANDLE_SPLASH_SCREEN_EXIT, bundle -> {
+                    bundle.putBoolean(RECEIVE_SPLASH_SCREEN_EXIT, true);
+                    bundle.putBoolean(CONTAINS_CENTER_VIEW, centerView != null);
+                });
+                view.remove();
+            };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getIntent().getBooleanExtra(REQUEST_HANDLE_EXIT_ON_RESUME, false)) {
+            mSSM.setOnExitAnimationListener(mSplashScreenExitHandler);
+        }
+        if (getIntent().getBooleanExtra(CANCEL_HANDLE_EXIT, false)) {
+            mSSM.setOnExitAnimationListener(null);
+        }
+    }
+}
