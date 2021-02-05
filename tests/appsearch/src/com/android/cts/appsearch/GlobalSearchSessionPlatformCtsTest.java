7353d677a4d4f4982034cd09ae71c6d83dbe43dd
Cassie Wang <cassiewang@google.com>
2021-02-05 00:09:23 -0800

    Add package access integration tests.
    Sideload APKs with different package names and certs during testing so
    we can verify that granting package access works. This infra is also
    useful for future package storage attribution testing.
    
    Bug: 179416188
    Bug: 179160886
    Test: atest -m -c --rebuild-module-info CtsAppSearchTestCases
    Change-Id: I7f98277fa7d04dbd61d5e5d876e1c9d8c98c421b

---
+++ tests/appsearch/src/com/android/cts/appsearch/GlobalSearchSessionPlatformCtsTest.java
@@ -0,0 +1,359 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.app.appsearch.cts;
+
+import static com.android.server.appsearch.testing.AppSearchTestUtils.checkIsBatchResultSuccess;
+import static com.android.server.appsearch.testing.AppSearchTestUtils.checkIsSetSchemaResponseSuccess;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.app.appsearch.AppSearchEmail;
+import android.app.appsearch.AppSearchManager;
+import android.app.appsearch.AppSearchSessionShim;
+import android.app.appsearch.GenericDocument;
+import android.app.appsearch.PackageIdentifier;
+import android.app.appsearch.PutDocumentsRequest;
+import android.app.appsearch.SetSchemaRequest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.android.cts.appsearch.ICommandReceiver;
+import com.android.server.appsearch.testing.AppSearchSessionShimImpl;
+
+import com.google.common.io.BaseEncoding;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This doesn't extend the {@link android.app.appsearch.cts.GlobalSearchSessionCtsTestBase} since
+ * these test cases can't be run in a non-platform environment.
+ */
+public class GlobalSearchSessionPlatformCtsTest {
+
+    private static final long TIMEOUT_BIND_SERVICE_SEC = 2;
+
+    private static final String TAG = "GlobalSearchSessionPlatformCtsTest";
+
+    private static final String PKG_A = "com.android.cts.appsearch.helper.a";
+
+    // To generate, run `apksigner` on the build APK. e.g.
+    //   ./apksigner verify --print-certs \
+    //   ~/sc-dev/out/soong/.intermediates/cts/tests/appsearch/CtsAppSearchTestHelperA/\
+    //   android_common/CtsAppSearchTestHelperA.apk`
+    // to get the SHA-256 digest. All characters need to be uppercase.
+    //
+    // Note: May need to switch the "sdk_version" of the test app from "test_current" to "30" before
+    // building the apk and running apksigner
+    private static final byte[] PKG_A_CERT_SHA256 =
+            BaseEncoding.base16()
+                    .decode("A90B80BD307B71BB4029674C5C4FE18066994E352EAC933B7B68266210CAFB53");
+
+    private static final String PKG_B = "com.android.cts.appsearch.helper.b";
+
+    // To generate, run `apksigner` on the build APK. e.g.
+    //   ./apksigner verify --print-certs \
+    //   ~/sc-dev/out/soong/.intermediates/cts/tests/appsearch/CtsAppSearchTestHelperB/\
+    //   android_common/CtsAppSearchTestHelperB.apk`
+    // to get the SHA-256 digest. All characters need to be uppercase.
+    //
+    // Note: May need to switch the "sdk_version" of the test app from "test_current" to "30" before
+    // building the apk and running apksigner
+    private static final byte[] PKG_B_CERT_SHA256 =
+            BaseEncoding.base16()
+                    .decode("88C0B41A31943D13226C3F22A86A6B4F300315575A6BC533CBF16C4EF3CFAA37");
+
+    private static final String HELPER_SERVICE =
+            "com.android.cts.appsearch.helper.AppSearchTestService";
+
+    private static final String TEXT = "foo";
+
+    private static final AppSearchEmail EMAIL_DOCUMENT =
+            new AppSearchEmail.Builder("uri1")
+                    .setFrom("from@example.com")
+                    .setTo("to1@example.com", "to2@example.com")
+                    .setSubject(TEXT)
+                    .setBody("this is the body of the email")
+                    .build();
+
+    private static final String DB_NAME = AppSearchManager.DEFAULT_DATABASE_NAME;
+
+    private AppSearchSessionShim mDb;
+
+    private Context mContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mContext = ApplicationProvider.getApplicationContext();
+
+        mDb =
+                AppSearchSessionShimImpl.createSearchSession(
+                                new AppSearchManager.SearchContext.Builder()
+                                        .setDatabaseName(DB_NAME)
+                                        .build())
+                        .get();
+        cleanup();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Cleanup whatever documents may still exist in these databases.
+        cleanup();
+    }
+
+    private void cleanup() throws Exception {
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(new SetSchemaRequest.Builder().setForceOverride(true).build()));
+    }
+
+    @Test
+    public void testNoPackageAccess_default() throws Exception {
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder().addSchemas(AppSearchEmail.SCHEMA).build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+
+        // No package has access by default
+        assertPackageCannotAccess(PKG_A);
+        assertPackageCannotAccess(PKG_B);
+    }
+
+    @Test
+    public void testNoPackageAccess_wrongPackageName() throws Exception {
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder()
+                                .addSchemas(AppSearchEmail.SCHEMA)
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ true,
+                                        new PackageIdentifier(
+                                                "some.other.package", PKG_A_CERT_SHA256))
+                                .build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+
+        assertPackageCannotAccess(PKG_A);
+    }
+
+    @Test
+    public void testNoPackageAccess_wrongCertificate() throws Exception {
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder()
+                                .addSchemas(AppSearchEmail.SCHEMA)
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ true,
+                                        new PackageIdentifier(PKG_A, new byte[] {10}))
+                                .build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+
+        assertPackageCannotAccess(PKG_A);
+    }
+
+    @Test
+    public void testAllowPackageAccess() throws Exception {
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder()
+                                .addSchemas(AppSearchEmail.SCHEMA)
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ true,
+                                        new PackageIdentifier(PKG_A, PKG_A_CERT_SHA256))
+                                .build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+
+        assertPackageCanAccess(EMAIL_DOCUMENT, PKG_A);
+        assertPackageCannotAccess(PKG_B);
+    }
+
+    @Test
+    public void testAllowMultiplePackageAccess() throws Exception {
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder()
+                                .addSchemas(AppSearchEmail.SCHEMA)
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ true,
+                                        new PackageIdentifier(PKG_A, PKG_A_CERT_SHA256))
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ true,
+                                        new PackageIdentifier(PKG_B, PKG_B_CERT_SHA256))
+                                .build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+
+        assertPackageCanAccess(EMAIL_DOCUMENT, PKG_A);
+        assertPackageCanAccess(EMAIL_DOCUMENT, PKG_B);
+    }
+
+    @Test
+    public void testNoPackageAccess_revoked() throws Exception {
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder()
+                                .addSchemas(AppSearchEmail.SCHEMA)
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ true,
+                                        new PackageIdentifier(PKG_A, PKG_A_CERT_SHA256))
+                                .build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+        assertPackageCanAccess(EMAIL_DOCUMENT, PKG_A);
+
+        // Set the schema again, but package access as false.
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder()
+                                .addSchemas(AppSearchEmail.SCHEMA)
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ false,
+                                        new PackageIdentifier(PKG_A, PKG_A_CERT_SHA256))
+                                .build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+        assertPackageCannotAccess(PKG_A);
+
+        // Set the schema again, but with default (i.e. no) access
+        checkIsSetSchemaResponseSuccess(
+                mDb.setSchema(
+                        new SetSchemaRequest.Builder()
+                                .addSchemas(AppSearchEmail.SCHEMA)
+                                .setSchemaTypeVisibilityForPackage(
+                                        AppSearchEmail.SCHEMA_TYPE,
+                                        /*visible=*/ false,
+                                        new PackageIdentifier(PKG_A, PKG_A_CERT_SHA256))
+                                .build()));
+        checkIsBatchResultSuccess(
+                mDb.put(
+                        new PutDocumentsRequest.Builder()
+                                .addGenericDocuments(EMAIL_DOCUMENT)
+                                .build()));
+        assertPackageCannotAccess(PKG_A);
+    }
+
+    private void assertPackageCannotAccess(String pkg) throws Exception {
+        final GlobalSearchSessionPlatformCtsTest.TestServiceConnection serviceConnection =
+                bindToHelperService(pkg);
+        try {
+            final ICommandReceiver commandReceiver = serviceConnection.getCommandReceiver();
+            List<String> results = commandReceiver.globalSearch(TEXT);
+            assertThat(results).isEmpty();
+        } finally {
+            serviceConnection.unbind();
+        }
+    }
+
+    private void assertPackageCanAccess(GenericDocument expectedDocument, String pkg)
+            throws Exception {
+        final GlobalSearchSessionPlatformCtsTest.TestServiceConnection serviceConnection =
+                bindToHelperService(pkg);
+        try {
+            final ICommandReceiver commandReceiver = serviceConnection.getCommandReceiver();
+            List<String> results = commandReceiver.globalSearch(TEXT);
+            assertThat(results).containsExactly(expectedDocument.toString());
+        } finally {
+            serviceConnection.unbind();
+        }
+    }
+
+    private GlobalSearchSessionPlatformCtsTest.TestServiceConnection bindToHelperService(
+            String pkg) {
+        final GlobalSearchSessionPlatformCtsTest.TestServiceConnection serviceConnection =
+                new GlobalSearchSessionPlatformCtsTest.TestServiceConnection(mContext);
+        final Intent intent = new Intent().setComponent(new ComponentName(pkg, HELPER_SERVICE));
+        mContext.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        return serviceConnection;
+    }
+
+    private class TestServiceConnection implements ServiceConnection {
+        private final Context mContext;
+        private final BlockingQueue<IBinder> mBlockingQueue = new LinkedBlockingQueue<>();
+        private ICommandReceiver mCommandReceiver;
+
+        TestServiceConnection(Context context) {
+            mContext = context;
+        }
+
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            Log.i(TAG, "Service got connected: " + componentName);
+            mBlockingQueue.offer(service);
+        }
+
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.e(TAG, "Service got disconnected: " + componentName);
+        }
+
+        private IBinder getService() throws Exception {
+            final IBinder service = mBlockingQueue.poll(TIMEOUT_BIND_SERVICE_SEC, TimeUnit.SECONDS);
+            return service;
+        }
+
+        public ICommandReceiver getCommandReceiver() throws Exception {
+            if (mCommandReceiver == null) {
+                mCommandReceiver = ICommandReceiver.Stub.asInterface(getService());
+            }
+            return mCommandReceiver;
+        }
+
+        public void unbind() {
+            mCommandReceiver = null;
+            mContext.unbindService(this);
+        }
+    }
+}
