6096d78ff3acc2b5699f964963f82cf9aa8e928e
Jonathan Scott <scottjonathan@google.com>
2021-02-09 16:55:43 +0000

    Add .switchTo() API for users.
    The switchTo test Uses ShellCommand and runWithShellPermissionIdentity
    because we currently don't have the needed APIs in nene. These will be
    replaced when the new APIs are added.
    
    This also includes a fix to EventLib because it was getting
    UnserializableExceptions.
    
    Bug: 180307892
    Test: atest NeneTest
    Test: atest EventLibTest
    Change-Id: I5989caf28a5b2645d1f84fe3a7df5808fb35ac89

--- common/device-side/bedstead/nene/src/test/java/com/android/bedstead/nene/users/UserReferenceTest.java
+++ common/device-side/bedstead/nene/src/test/java/com/android/bedstead/nene/users/UserReferenceTest.java
@@ -16,11 +16,26 @@
 
 package com.android.bedstead.nene.users;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static android.os.Build.VERSION.SDK_INT;
+
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.junit.Assume.assumeTrue;
 import static org.testng.Assert.assertThrows;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import com.android.bedstead.nene.exceptions.NeneException;
+import com.android.bedstead.nene.utils.ShellCommand;
+import com.android.compatibility.common.util.SystemUtil;
+import com.android.eventlib.EventLogs;
+import com.android.eventlib.events.activities.ActivityCreatedEvent;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,22 +46,25 @@ public class UserReferenceTest {
     private static final int NON_EXISTING_USER_ID = 10000;
     private static final int USER_ID = NON_EXISTING_USER_ID;
     private static final String USER_NAME = "userName";
+    private static final Context sContext =
+            InstrumentationRegistry.getInstrumentation().getContext();
+    private static final String TEST_ACTIVITY_NAME = "com.android.bedstead.nene.test.Activity";
 
     private final Users mUsers = new Users();
 
     @Test
     public void id_returnsId() {
-        assertThat(mUsers.user(USER_ID).id()).isEqualTo(USER_ID);
+        assertThat(mUsers.find(USER_ID).id()).isEqualTo(USER_ID);
     }
 
     @Test
     public void userHandle_referencesId() {
-        assertThat(mUsers.user(USER_ID).userHandle().getIdentifier()).isEqualTo(USER_ID);
+        assertThat(mUsers.find(USER_ID).userHandle().getIdentifier()).isEqualTo(USER_ID);
     }
 
     @Test
     public void resolve_doesNotExist_returnsNull() {
-        assertThat(mUsers.user(NON_EXISTING_USER_ID).resolve()).isNull();
+        assertThat(mUsers.find(NON_EXISTING_USER_ID).resolve()).isNull();
     }
 
     @Test
@@ -74,7 +92,7 @@ public class UserReferenceTest {
 
     @Test
     public void remove_userDoesNotExist_throwsException() {
-        assertThrows(NeneException.class, () -> mUsers.user(USER_ID).remove());
+        assertThrows(NeneException.class, () -> mUsers.find(USER_ID).remove());
     }
 
     @Test
@@ -88,7 +106,7 @@ public class UserReferenceTest {
 
     @Test
     public void start_userDoesNotExist_throwsException() {
-        assertThrows(NeneException.class, () -> mUsers.user(NON_EXISTING_USER_ID).start());
+        assertThrows(NeneException.class, () -> mUsers.find(NON_EXISTING_USER_ID).start());
     }
 
     @Test
@@ -121,7 +139,7 @@ public class UserReferenceTest {
 
     @Test
     public void stop_userDoesNotExist_throwsException() {
-        assertThrows(NeneException.class, () -> mUsers.user(NON_EXISTING_USER_ID).stop());
+        assertThrows(NeneException.class, () -> mUsers.find(NON_EXISTING_USER_ID).stop());
     }
 
     @Test
@@ -148,6 +166,45 @@ public class UserReferenceTest {
         try {
             assertThat(user.resolve().state()).isEqualTo(User.UserState.NOT_RUNNING);
         } finally {
+            user.remove();
+        }
+    }
+
+    @Test
+    public void switchTo_userIsSwitched() throws Exception {
+        assumeTrue(
+                "Install-existing only works for P+", SDK_INT >= Build.VERSION_CODES.P);
+        // TODO(scottjonathan): Might be a way of faking install-existing on older
+        //  versions (fetch the pkg and reinstall?)
+        assumeTrue(
+                "Adopting Shell Permissions only works for Q+", SDK_INT >= Build.VERSION_CODES.Q);
+        // TODO(scottjonathan): In this case we can probably grant the permission through adb?
+
+        UserReference user = createUser().start();
+        try {
+            SystemUtil.runWithShellPermissionIdentity(() -> {
+                // for INTERACT_ACROSS_USERS
+                ShellCommand.builder("pm install-existing")
+                        .addOption("--user", user.id())
+                        .addOperand(sContext.getPackageName())
+                        .executeAndValidateOutput(
+                                (output) -> output.contains("installed for user"));
+                user.switchTo();
+
+                Intent intent = new Intent();
+                intent.setPackage(sContext.getPackageName());
+                intent.setClassName(sContext.getPackageName(), TEST_ACTIVITY_NAME);
+                intent.setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TASK);
+                sContext.startActivityAsUser(intent, user.userHandle());
+
+                EventLogs<ActivityCreatedEvent> logs =
+                        ActivityCreatedEvent.queryPackage(sContext.getPackageName())
+                                .whereActivity().className().isEqualTo(TEST_ACTIVITY_NAME)
+                                .onUser(user.userHandle());
+                assertThat(logs.poll()).isNotNull();
+            });
+        } finally {
+            mUsers.system().switchTo();
             user.remove();
         }
     }
