3d615518336476378e64725ccfa00be8b492c266
Marcus Hagerott <mhagerott@google.com>
2021-02-18 15:39:42 -0800

    Fixing SimPhonebook tests for devices eSIM
    The SimPhonebookRequirementsRule was not properly handling eSIM.
    
    Test: atest CtsSimPhonebookProviderTestCases
    Bug: 154363919
    Change-Id: Ib112279ac1e7abafe53a4b145f2d5634bd3063fd

--- tests/tests/simphonebookprovider/src/android/provider/cts/simphonebook/SimPhonebookRequirementsRule.java
+++ tests/tests/simphonebookprovider/src/android/provider/cts/simphonebook/SimPhonebookRequirementsRule.java
@@ -21,20 +21,11 @@ import static com.google.common.truth.Truth.assertWithMessage;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.junit.Assume.assumeThat;
 
-import android.Manifest;
 import android.content.Context;
-import android.telephony.SubscriptionManager;
-import android.telephony.TelephonyManager;
-import android.telephony.UiccSlotInfo;
 
 import androidx.test.core.app.ApplicationProvider;
 
-import com.android.compatibility.common.util.SystemUtil;
-
 import org.junit.rules.ExternalResource;
-
-import java.util.Arrays;
-import java.util.Objects;
 
 class SimPhonebookRequirementsRule extends ExternalResource {
 
@@ -51,23 +42,12 @@ class SimPhonebookRequirementsRule extends ExternalResource {
     @Override
     protected void before() {
         Context context = ApplicationProvider.getApplicationContext();
-        TelephonyManager telephonyManager = Objects.requireNonNull(
-                context.getSystemService(TelephonyManager.class));
-        UiccSlotInfo[] uiccSlots = SystemUtil.runWithShellPermissionIdentity(
-                telephonyManager::getUiccSlotsInfo,
-                Manifest.permission.READ_PRIVILEGED_PHONE_STATE);
-        int nonEsimCount = (int) Arrays.stream(uiccSlots)
-                .filter(info -> !info.getIsEuicc()).count();
-        assumeThat(nonEsimCount, greaterThanOrEqualTo(mMinimumSimCount));
+        RemovableSims removableSims = new RemovableSims(context);
 
-        SubscriptionManager subscriptionManager = Objects.requireNonNull(
-                context.getSystemService(SubscriptionManager.class));
-        SystemUtil.runWithShellPermissionIdentity(() ->
-                // This is an assertion rather than an assumption because according to the
-                // CTS setup all slots should have a SIM installed.
-                assertWithMessage("A SIM must be installed in each SIM slot.").that(
-                        subscriptionManager.getActiveSubscriptionInfoCount())
-                        .isEqualTo(telephonyManager.getSupportedModemCount())
-        );
+        assumeThat(removableSims.getRemovableSimSlotCount(),
+                greaterThanOrEqualTo(mMinimumSimCount));
+        assertWithMessage("A SIM must be installed in each SIM slot")
+                .that(removableSims.getSubscriptionInfoForRemovableSims().size())
+                .isAtLeast(mMinimumSimCount);
     }
 }
