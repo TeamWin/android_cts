c839399d96d6c3cd4c8c32d294f14a29387d1a04
Shadab Naseem <quic_snaseem@quicinc.com>
2021-03-10 15:01:56 +0530

    cts: Add SDM660 target to whitelist in the CTS test case
    SDM660 target don't need to define CONFIG_UNMAP_KERNEL_AT_EL0
    as the hardware is not vulnerable to CVE-2017-5754.
    Hence, whitelisting it.
    
    Bug: 170457709
    Test: run cts --m CtsSecurityHostTestCases --t android.security.cts.KernelConfigTest#testConfigHardwareMitigations
    Change-Id: Ib6f83dfb0ce55bb79e53f63e05fcc58c7914be30

--- hostsidetests/security/src/android/security/cts/KernelConfigTest.java
+++ hostsidetests/security/src/android/security/cts/KernelConfigTest.java
@@ -264,6 +264,7 @@ public class KernelConfigTest extends DeviceTestCase implements IBuildReceiver, 
         put("QM215", null);
         put("ATOLL", null);
         put("ATOLL-AB", null);
+        put("SDM660", new String[]{"CONFIG_HARDEN_BRANCH_PREDICTOR=y"});
         put("BENGAL", new String[]{"CONFIG_HARDEN_BRANCH_PREDICTOR=y"});
         put("DEFAULT", new String[]{"CONFIG_HARDEN_BRANCH_PREDICTOR=y",
             "CONFIG_UNMAP_KERNEL_AT_EL0=y"});
