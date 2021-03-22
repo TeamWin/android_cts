ceb2dd3d2b4c13abd90dc45e9ecd12e234bb7cb8
Clemenz Portmann <portmannc@google.com>
2021-03-22 13:41:42 -0700

    ITS: sc-dev scene1_1 headers for gerrit <--> g3
    bug: 183226588
    Change-Id: I68743d85c1093ac6dd2547a6c4d50ad72f4c564b

--- apps/CameraITS/tests/scene1_1/test_auto_vs_manual.py
+++ apps/CameraITS/tests/scene1_1/test_auto_vs_manual.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Verifies auto and manual captures are similar with same scene."""
 
 
 import logging
