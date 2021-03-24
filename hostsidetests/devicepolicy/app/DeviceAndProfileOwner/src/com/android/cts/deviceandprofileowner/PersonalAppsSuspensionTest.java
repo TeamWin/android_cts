1597c541c33c1a9a01626a4c7eeee17a5266ecc6
Pavel Grafov <pgrafov@google.com>
2021-03-24 17:09:41 +0000

    CTS test for compliance acknowledgement.
    Bug: 181943978
    Test: atest OrgOwnedProfileOwnerTest#testWorkProfileMaximumTimeOff_complianceRequiredBroadcastDefault
    Test: atest atest OrgOwnedProfileOwnerTest#testWorkProfileMaximumTimeOff_complianceRequiredBroadcastOverride
    Change-Id: Icee29611c5d415183bf338019edbab3ffec30efb

--- hostsidetests/devicepolicy/app/DeviceAndProfileOwner/src/com/android/cts/deviceandprofileowner/PersonalAppsSuspensionTest.java
+++ hostsidetests/devicepolicy/app/DeviceAndProfileOwner/src/com/android/cts/deviceandprofileowner/PersonalAppsSuspensionTest.java
@@ -16,17 +16,24 @@
 
 package com.android.cts.deviceandprofileowner;
 
+import static com.android.cts.deviceandprofileowner.BaseDeviceAdminTest.BasicAdminReceiver.COMPLIANCE_ACK_PREF_KEY_BCAST_RECEIVED;
+import static com.android.cts.deviceandprofileowner.BaseDeviceAdminTest.BasicAdminReceiver.COMPLIANCE_ACK_PREF_KEY_OVERRIDE;
+import static com.android.cts.deviceandprofileowner.BaseDeviceAdminTest.BasicAdminReceiver.COMPLIANCE_ACK_PREF_NAME;
+
 import static com.google.common.truth.Truth.assertThat;
 
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Test for personal app suspension APIs available to POs on organization owned devices.
@@ -54,6 +61,44 @@ public class PersonalAppsSuspensionTest {
     @Test
     public void testSetManagedProfileMaximumTimeOff1Sec() {
         mDpm.setManagedProfileMaximumTimeOff(ADMIN, 1000);
+    }
+
+    @Test
+    public void testSetManagedProfileMaximumTimeOff1Year() {
+        mDpm.setManagedProfileMaximumTimeOff(ADMIN, TimeUnit.DAYS.toMillis(365));
+    }
+
+    @Test
+    public void testComplianceAcknowledgementRequiredReceived() {
+        final SharedPreferences pref =
+                mContext.getSharedPreferences(COMPLIANCE_ACK_PREF_NAME, Context.MODE_PRIVATE);
+        assertThat(pref.getBoolean(COMPLIANCE_ACK_PREF_KEY_BCAST_RECEIVED, false)).isTrue();
+    }
+
+    @Test
+    public void testSetOverrideOnComplianceAcknowledgementRequired() {
+        final SharedPreferences pref =
+                mContext.getSharedPreferences(COMPLIANCE_ACK_PREF_NAME, Context.MODE_PRIVATE);
+        pref.edit().putBoolean(COMPLIANCE_ACK_PREF_KEY_OVERRIDE, true).commit();
+    }
+
+    @Test
+    public void testComplianceAcknowledgementNotRequired() {
+        assertThat(mDpm.isComplianceAcknowledgementRequired()).isFalse();
+    }
+
+    @Test
+    public void testAcknowledgeCompliance() {
+        assertThat(mDpm.isComplianceAcknowledgementRequired()).isTrue();
+        mDpm.acknowledgeDeviceCompliant();
+        assertThat(mDpm.isComplianceAcknowledgementRequired()).isFalse();
+    }
+
+    @Test
+    public void testClearComplianceSharedPreference() {
+        final SharedPreferences pref =
+                mContext.getSharedPreferences(COMPLIANCE_ACK_PREF_NAME, Context.MODE_PRIVATE);
+        pref.edit().clear().commit();
     }
 
     @Test
