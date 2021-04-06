e7836b35eb360aafd444bee85e2dc88c6dadc8b4
Hongbo Zeng <hongbozeng@google.com>
2021-04-07 03:12:44 +0800

    Fix API review issues for 5G Slicing feature
    - Rename SlicingConfig to NetworkSlicingConfig
    
    Bug: 183714964
    Test: atest TelephonyManagerTest#testGetNetworkSlicingConfiguration
    Change-Id: I958d8a082c493c16c317fcb17cd9a13ea6355a7a

--- tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
@@ -89,7 +89,7 @@ import android.telephony.ThermalMitigationRequest;
 import android.telephony.UiccCardInfo;
 import android.telephony.UiccSlotInfo;
 import android.telephony.data.ApnSetting;
-import android.telephony.data.SlicingConfig;
+import android.telephony.data.NetworkSlicingConfig;
 import android.telephony.emergency.EmergencyNumber;
 import android.text.TextUtils;
 import android.util.Log;
@@ -4535,7 +4535,7 @@ public class TelephonyManagerTest {
         if (!mPackageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
             return;
         }
-        CompletableFuture<SlicingConfig> resultFuture = new CompletableFuture<>();
+        CompletableFuture<NetworkSlicingConfig> resultFuture = new CompletableFuture<>();
         ShellIdentityUtils.invokeMethodWithShellPermissionsNoReturn(mTelephonyManager,
                 (tm) -> tm.getNetworkSlicingConfiguration(mSimpleExecutor, resultFuture::complete));
     }
