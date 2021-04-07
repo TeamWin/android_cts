5b4e0844f874553337fed245db4b00b5b1696692
qianyou <qianyou@xiaomi.com>
2021-04-07 19:26:43 +0800

    fix wifiSlice_grantedPermissionToDefaultAssistant failure
    Currently WifiSliceTest needs a manual setup step before testing, which is not a good practice for the automated test suite.
    
    Check if the DUT has a default assistant app, if so, then check if it's granted the slice permission; if not, just auto-set a default one in SettingsProvider and then check that one. So that we can pass the test without the manual setup step.
    
    Bug:184819174
    Test:run cts -m CtsProviderTestCases -t android.provider.cts.WifiSliceTest#wifiSlice_grantedPermissionToDefaultAssistant
    
    Signed-off-by: qianyou <qianyou@xiaomi.com>
    Change-Id: Ic17d9730cea3732988a0dee1fa799840372cc47c

--- tests/tests/provider/src/android/provider/cts/WifiSliceTest.java
+++ tests/tests/provider/src/android/provider/cts/WifiSliceTest.java
@@ -17,10 +17,12 @@
 package android.provider.cts;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.provider.Settings.Secure;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assume.assumeFalse;
 import android.app.slice.Slice;
 import android.app.slice.SliceManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -29,6 +31,7 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Process;
+import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.slice.SliceConvert;
@@ -37,6 +40,7 @@ import androidx.slice.core.SliceAction;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,6 +59,7 @@ public class WifiSliceTest {
   private static final String ACTION_VOICE_ASSIST = "android.intent.action.VOICE_ASSIST";
   private static final String CATEGORY_DEFAULT = "android.intent.category.DEFAULT";
   private static final String FEATURE_VOICE_RECOGNIZERS = "android.software.voice_recognizers";
+  private static final String ASSISTANT = "assistant";
 
   private final Context mContext = InstrumentationRegistry.getContext();
   private final SliceManager mSliceManager = mContext.getSystemService(SliceManager.class);
@@ -62,12 +67,22 @@ public class WifiSliceTest {
           mContext.getPackageManager().hasSystemFeature(FEATURE_VOICE_RECOGNIZERS);
 
   private Slice mWifiSlice;
+  private String mAssistant;
 
   @Before
   public void setUp() throws Exception {
     assumeFalse("Skipping test: TV does not support provider android.settings.slices", isTv());
     assumeFalse("Skipping test: Auto does not support provider android.settings.slices", isCar());
     mWifiSlice = mSliceManager.bindSlice(WIFI_SLICE_URI, Collections.emptySet());
+    mAssistant = Secure.getString(mContext.getContentResolver(), ASSISTANT);
+  }
+
+  @After
+  public void tearDown() {
+    final String assistant = Secure.getString(mContext.getContentResolver(), ASSISTANT);
+    if (!TextUtils.equals(mAssistant, assistant)) {
+      Secure.putString(mContext.getContentResolver(), ASSISTANT, mAssistant);
+    }
   }
 
   @Test
@@ -104,18 +119,21 @@ public class WifiSliceTest {
     final List<ResolveInfo> infos = pm.queryIntentActivities(requestDefaultAssistant, 0);
 
     if (!infos.isEmpty()) {
-      final int testPid = Process.myPid();
-      boolean permissionGranted = false;
-      for (ResolveInfo info : infos) {
-        final int testUid = pm.getPackageUid(info.activityInfo.packageName, 0);
-
-        if (mSliceManager.checkSlicePermission(WIFI_SLICE_URI, testPid, testUid)
-                == PERMISSION_GRANTED) {
-          permissionGranted = true;
-          break;
-        }
+      final String packageName;
+      if (!TextUtils.isEmpty(mAssistant)) {
+        packageName = ComponentName.unflattenFromString(mAssistant).getPackageName();
+        Log.i(TAG, "Default assistant: " + packageName);
+      } else {
+        packageName = infos.get(0).activityInfo.packageName;
+        Log.i(TAG, "Set assistant: " + packageName);
+        Secure.putString(mContext.getContentResolver(), ASSISTANT,
+                new ComponentName(packageName, infos.get(0).activityInfo.name).flattenToString());
       }
-      assertThat(permissionGranted).isTrue();
+      final int testPid = Process.myPid();
+      final int testUid = pm.getPackageUid(packageName, 0);
+
+      assertThat(mSliceManager.checkSlicePermission(WIFI_SLICE_URI, testPid, testUid))
+              .isEqualTo(PERMISSION_GRANTED);
     }
   }
 
