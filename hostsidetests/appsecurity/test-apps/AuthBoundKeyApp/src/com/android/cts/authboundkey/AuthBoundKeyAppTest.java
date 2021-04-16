1ae3a9e5e7857e319a3ff4742b0f3ab22f9a7a3d
Janis Danisevskis <jdanis@google.com>
2021-04-16 09:57:29 -0700

    Keystore 2.0: Fix AuthBoundKeyTest#useInvalidatedAuthBoundKey
    In Keystore 2.0 we don't keep invalided keys around, so invalidated keys
    behave exactly like non existing keys.
    
    Bug: 183294127
    Test: atest android.appsecurity.cts.AuthBoundKeyTest
    Change-Id: I8a1ead68ef64bb99c472c1f3e49c5611df522d25

--- hostsidetests/appsecurity/test-apps/AuthBoundKeyApp/src/com/android/cts/authboundkey/AuthBoundKeyAppTest.java
+++ hostsidetests/appsecurity/test-apps/AuthBoundKeyApp/src/com/android/cts/authboundkey/AuthBoundKeyAppTest.java
@@ -74,11 +74,14 @@ public class AuthBoundKeyAppTest extends AndroidTestCase {
         keyStore.load(null);
         try {
             SecretKey secretKey = (SecretKey) keyStore.getKey(KEY_NAME, null);
+            // In Keystore 2.0 we don't keep invalidated keys around. So we cannot diagnose that
+            // a key was invalidated. It simply does not exist. Thus we expect null.
+            if (secretKey == null) return;
         } catch (UnrecoverableKeyException e) {
-            // This is correct behavior
+            // This is correct behavior for Keystore 1.0
             return;
         }
-        fail("Expected an UnrecoverableKeyException");
+        fail("Expected an UnrecoverableKeyException or null");
     }
 
 }
