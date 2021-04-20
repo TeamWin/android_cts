88c38dded047f497ee2696870d0e2fdd9fd09f60
xshu <xshu@google.com>
2021-04-19 17:34:58 -0700

    CTS - wait for verbose logging listener
    Fix a CTS flakiness issue.
    
    Bug: 184314647
    Test: atest WifiManagerTest#testVerboseLoggingEnabled
    Change-Id: Ia7352b87f7981cd08c161ec3f06be60280867d1b

--- tests/tests/wifi/src/android/net/wifi/cts/WifiManagerTest.java
+++ tests/tests/wifi/src/android/net/wifi/cts/WifiManagerTest.java
@@ -2923,13 +2923,14 @@ public class WifiManagerTest extends WifiJUnit3TestBase {
             }
             mWifiManager.setVerboseLoggingEnabled(newState);
             PollingCheck.check(
-                    "Wifi settings toggle failed!",
+                    "Wifi verbose logging toggle failed!",
                     DURATION_SETTINGS_TOGGLE,
                     () -> mWifiManager.isVerboseLoggingEnabled() == newState);
-            assertEquals(newState, mWifiManager.isVerboseLoggingEnabled());
             if (listener != null) {
-                assertEquals(newState, listener.status);
-                assertEquals(1, listener.numCalls);
+                PollingCheck.check(
+                        "Verbose logging listener timeout",
+                        DURATION_SETTINGS_TOGGLE,
+                        () -> listener.status == newState && listener.numCalls == 1);
             }
         } finally {
             if (currState != null) mWifiManager.setVerboseLoggingEnabled(currState);
