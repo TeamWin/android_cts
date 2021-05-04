b9841c4edac128f68796ea21166f027dec80b881
Luca Weiss <luca.weiss@fairphone.com>
2021-05-04 11:33:23 +0200

    Skip Dualshock test for devices with kernel < 3.18
    Those devices have an old kernel version where backporting the Dualshock
    driver is not feasable.
    
    The necessary support is present in the Pixel 1 (v3.18) kernel (e.g.
    branch android-msm-marlin-3.18-q-preview-6) or in the Android Common
    Kernel branch android-4.4-p
    
    Bug: 189143730
    Test: manual test
    Change-Id: I075de0f26a19727baf3c3318b5f1c19be8eb4c4d

--- tests/tests/hardware/src/android/hardware/input/cts/tests/SonyDualshock4TestCase.java
+++ tests/tests/hardware/src/android/hardware/input/cts/tests/SonyDualshock4TestCase.java
@@ -16,10 +16,19 @@
 
 package android.hardware.input.cts.tests;
 
+import static org.junit.Assume.assumeTrue;
+
 import android.hardware.cts.R;
+import android.os.Build;
+import android.os.VintfRuntimeInfo;
+import android.text.TextUtils;
+import android.util.Pair;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,13 +41,49 @@ public class SonyDualshock4TestCase extends InputTestCase {
         super(R.raw.sony_dualshock4_register);
     }
 
+    // Copied from cts/tests/tests/net/src/android/net/cts/ConnectivityManagerTest.java
+    private static Pair<Integer, Integer> getVersionFromString(String version) {
+        // Only gets major and minor number of the version string.
+        final Pattern versionPattern = Pattern.compile("^(\\d+)(\\.(\\d+))?.*");
+        final Matcher m = versionPattern.matcher(version);
+        if (m.matches()) {
+            final int major = Integer.parseInt(m.group(1));
+            final int minor = TextUtils.isEmpty(m.group(3)) ? 0 : Integer.parseInt(m.group(3));
+            return new Pair<>(major, minor);
+        } else {
+            return new Pair<>(0, 0);
+        }
+    }
+
+    // Copied from cts/tests/tests/net/src/android/net/cts/ConnectivityManagerTest.java
+    public static int compareMajorMinorVersion(final String s1, final String s2) {
+        final Pair<Integer, Integer> v1 = getVersionFromString(s1);
+        final Pair<Integer, Integer> v2 = getVersionFromString(s2);
+
+        if (v1.first == v2.first) {
+            return Integer.compare(v1.second, v2.second);
+        } else {
+            return Integer.compare(v1.first, v2.first);
+        }
+    }
+
+    // This test requires updates to hid-sony.c that are only available for
+    // kernels 3.18+. Skip this test for kernels older than 3.18 because it is
+    // too difficult to backport these changes to those older kernels.
+    private static boolean isDualshock4DriverSupportedByKernel() {
+        final String kVersionString = VintfRuntimeInfo.getKernelVersion();
+        return compareMajorMinorVersion(kVersionString, "3.18") >= 0;
+    }
+
     @Test
     public void testAllKeys() {
+        assumeTrue(isDualshock4DriverSupportedByKernel());
         testInputEvents(R.raw.sony_dualshock4_keyeventtests);
     }
 
     @Test
     public void testAllMotions() {
+        assumeTrue(isDualshock4DriverSupportedByKernel());
         testInputEvents(R.raw.sony_dualshock4_motioneventtests);
     }
 }
