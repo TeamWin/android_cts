af181bcd1c6fc66a562b4eee9f02b03cfbfb17d5
SongFerngWang <songferngwang@google.com>
2021-05-05 21:35:01 +0800

    [security] SubscriptionGroup is exposed to unprivileged callers
    SubscriptionInfo.mGroupUUID is not cleared in
    conditionallyRemoveIdentifiers if the caller only has READ_PHONE_STATE
    (based on a check to checkReadPhoneState) and not READ_DEVICE_IDENTIFIERS.
    Bug: 181053462
    Test: atest SubscriptionManagerTest
    
    Change-Id: Ic371faf1ded5186fd6aad022ffd3bf06066e663a
    Merged-In: Ic371faf1ded5186fd6aad022ffd3bf06066e663a

--- tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
@@ -571,6 +571,12 @@ public class SubscriptionManagerTest {
         List<SubscriptionInfo> infoList = mSm.getSubscriptionsInGroup(uuid);
         assertNotNull(infoList);
         assertEquals(1, infoList.size());
+        assertNull(infoList.get(0).getGroupUuid());
+
+        infoList = ShellIdentityUtils.invokeMethodWithShellPermissions(mSm,
+                (sm) -> sm.getSubscriptionsInGroup(uuid));
+        assertNotNull(infoList);
+        assertEquals(1, infoList.size());
         assertEquals(uuid, infoList.get(0).getGroupUuid());
 
         List<SubscriptionInfo> availableInfoList = mSm.getAvailableSubscriptionInfoList();
@@ -613,6 +619,12 @@ public class SubscriptionManagerTest {
 
         // Getting subscriptions in group.
         List<SubscriptionInfo> infoList = mSm.getSubscriptionsInGroup(uuid);
+        assertNotNull(infoList);
+        assertEquals(1, infoList.size());
+        assertNull(infoList.get(0).getGroupUuid());
+
+        infoList = ShellIdentityUtils.invokeMethodWithShellPermissions(mSm,
+                (sm) -> sm.getSubscriptionsInGroup(uuid));
         assertNotNull(infoList);
         assertEquals(1, infoList.size());
         assertEquals(uuid, infoList.get(0).getGroupUuid());
