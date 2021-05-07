2b6550f5a3434e985ff0da2fd2f9c2c6161ec3e4
Felipe Leme <felipeal@google.com>
2021-05-07 11:55:42 -0700

    Fixed some wifi-related, device owner tests on automotive.
    They were mostly failing because the test could not get the list of
    existing networks, and the fix required 2 changes:
    
    1. Replace the grantDpmWrapperPermissions() call by pre-installing
       the DO app in the current user (so it has the same privileges as
       the app in the system user).
    2. Use a custom WifiConfigCreator object that calls
       WifiManager.getConfiguredNetworks from the current user (otherwise
       it would return an empty list).
    
    A 3rd issue was revoking the location permission from the wrong user.
    
    Test: atest com.android.cts.devicepolicy.DeviceOwnerTest#testWifiSetHttpProxyTest,testWifiConfigLockdown,testWifiNetworkConfigurationWithoutFineLocationPermission  # on automotive and phone
    
    Fixes: 185268523
    Fixes: 177671379
    
    Change-Id: I5a2a15de8fc6a4a8b51b21ffb0408c2ba54c47e1

--- hostsidetests/devicepolicy/src/com/android/cts/devicepolicy/MixedDeviceOwnerTest.java
+++ hostsidetests/devicepolicy/src/com/android/cts/devicepolicy/MixedDeviceOwnerTest.java
@@ -63,26 +63,24 @@ public class MixedDeviceOwnerTest extends DeviceAndProfileOwnerTest {
                 + "mDeviceOwnerUserId=%d", getClass(), mUserId, mPrimaryUserId, mInitialUserId,
                 mDeviceOwnerUserId);
 
-        installAppAsUser(DEVICE_ADMIN_APK, mDeviceOwnerUserId);
+        installDeviceOwnerApp(DEVICE_ADMIN_APK);
         mDeviceOwnerSet = setDeviceOwner(DEVICE_ADMIN_COMPONENT_FLATTENED, mDeviceOwnerUserId,
                 /*expectFailure= */ false);
 
         if (!mDeviceOwnerSet) {
-            removeAdmin(DEVICE_ADMIN_COMPONENT_FLATTENED, mUserId);
+            removeDeviceOwnerAdmin(DEVICE_ADMIN_COMPONENT_FLATTENED);
             getDevice().uninstallPackage(DEVICE_ADMIN_PKG);
             fail("Failed to set device owner on user " + mDeviceOwnerUserId);
         }
         if (isHeadlessSystemUserMode()) {
             affiliateUsers(DEVICE_ADMIN_PKG, mDeviceOwnerUserId, mPrimaryUserId);
-            grantDpmWrapperPermissions(DEVICE_ADMIN_PKG, mPrimaryUserId);
         }
     }
 
     @Override
     public void tearDown() throws Exception {
-        if (mDeviceOwnerSet && !removeAdmin(DEVICE_ADMIN_COMPONENT_FLATTENED, mDeviceOwnerUserId)) {
-            // Don't fail as it could hide the real failure from the test method
-            CLog.e("Failed to remove device owner on user " + mDeviceOwnerUserId);
+        if (mDeviceOwnerSet) {
+            removeDeviceOwnerAdmin(DEVICE_ADMIN_COMPONENT_FLATTENED);
         }
 
         super.tearDown();
