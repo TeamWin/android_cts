a38d5eb3f88825c1ba3e39a697e81f74102bf348
Jonathan Scott <scottjonathan@google.com>
2021-05-09 22:46:54 +0100

    Add tests when policy cannot be set.
    Test: atest android.devicepolicy.cts.LockTaskTest
    Bug: 175377361
    Change-Id: Ide963d88c0bdf25b59d1ca03cb59ec6d813b2351

--- common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/BedsteadJUnit4.java
+++ common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/BedsteadJUnit4.java
@@ -18,6 +18,7 @@ package com.android.bedstead.harrier;
 
 import androidx.annotation.Nullable;
 
+import com.android.bedstead.harrier.annotations.enterprise.CannotSetPolicyTest;
 import com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy;
 import com.android.bedstead.harrier.annotations.enterprise.NegativePolicyTest;
 import com.android.bedstead.harrier.annotations.enterprise.PositivePolicyTest;
@@ -318,6 +319,17 @@ public final class BedsteadJUnit4 extends BlockJUnit4ClassRunner {
                         policy.getAnnotation(EnterprisePolicy.class);
                 List<Annotation> replacementAnnotations =
                         Policy.negativeStates(enterprisePolicy);
+
+                annotations.addAll(index, replacementAnnotations);
+                index += replacementAnnotations.size();
+            } else if (annotation instanceof CannotSetPolicyTest) {
+                annotations.remove(index);
+                Class<?> policy = ((CannotSetPolicyTest) annotation).policy();
+
+                EnterprisePolicy enterprisePolicy =
+                        policy.getAnnotation(EnterprisePolicy.class);
+                List<Annotation> replacementAnnotations =
+                        Policy.cannotSetPolicyStates(enterprisePolicy);
 
                 annotations.addAll(index, replacementAnnotations);
                 index += replacementAnnotations.size();
