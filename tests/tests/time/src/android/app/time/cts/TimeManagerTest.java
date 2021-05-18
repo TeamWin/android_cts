cf52d232cb23ee49bba059de7ecbae24c102b5be
Neil Fuller <nfuller@google.com>
2021-05-18 11:54:43 +0100

    Add CTS test: TimeManager.suggestExternalTime()
    Adds a CTS test for TimeManager.suggestExternalTime().
    
    Note: Because the test relies on device_config flags to set the device
    into a mode where it can accept "external" suggestions, this is also the
    first test that uses the new device_config command line verbs to disable
    / reset flag syncing behavior.
    
    Bug: 184947690
    Bug: 177079827
    Bug: 185786624
    Test: atest tests/tests/time/src/android/app/time/cts/TimeManagerTest.java
    Change-Id: Ibd22a6774e839898a0dfa1ceef362af041a5efdd

--- tests/tests/time/src/android/app/time/cts/TimeManagerTest.java
+++ tests/tests/time/src/android/app/time/cts/TimeManagerTest.java
@@ -16,12 +16,14 @@
 
 package android.app.time.cts;
 
+import static android.provider.DeviceConfig.NAMESPACE_SYSTEM_TIME;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import android.app.UiAutomation;
 import android.app.time.Capabilities;
 import android.app.time.ExternalTimeSuggestion;
 import android.app.time.TimeManager;
@@ -30,32 +32,63 @@ import android.app.time.TimeZoneCapabilitiesAndConfig;
 import android.app.time.TimeZoneConfiguration;
 import android.content.Context;
 import android.location.LocationManager;
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.SystemClock;
 import android.os.UserHandle;
-
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
+import android.provider.DeviceConfig;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.android.compatibility.common.util.AdoptShellPermissionsRule;
+import com.google.common.io.ByteStreams;
 
-import java.lang.reflect.Field;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/** Tests for {@link TimeManager} and associated classes. */
+/** In-process tests for {@link TimeManager} and associated classes. */
 public class TimeManagerTest {
 
     /**
      * This rule adopts the Shell process permissions, needed because MANAGE_TIME_AND_ZONE_DETECTION
-     * is a privileged permission.
+     * and SUGGEST_EXTERNAL_TIME required by {@link TimeManager} are privileged permissions.
      */
     @Rule
     public final AdoptShellPermissionsRule shellPermRule = new AdoptShellPermissionsRule();
+
+    private boolean mWasDeviceConfigSyncDisabled;
+
+    @Before
+    public void before() throws Exception {
+        // This anticipates a future state where a generally applied target preparer may disable
+        // device_config sync for all CTS tests: only suspend syncing if it isn't already suspended,
+        // and only resume it if this test suspended it.
+        mWasDeviceConfigSyncDisabled = DeviceConfigShellCommand.isSyncDisabled();
+        if (!mWasDeviceConfigSyncDisabled) {
+            DeviceConfigShellCommand.setSyncDisabled(
+                    DeviceConfigShellCommand.SYNC_DISABLED_MODE_UNTIL_REBOOT);
+        }
+    }
+
+    @After
+    public void after() throws Exception {
+        DeviceConfigShellCommand.resetToDefaults(
+                DeviceConfigShellCommand.RESET_MODE_TRUSTED_DEFAULTS, NAMESPACE_SYSTEM_TIME);
+        if (!mWasDeviceConfigSyncDisabled) {
+            // Turn syncing back on if this test disabled it.
+            DeviceConfigShellCommand.setSyncDisabled(
+                    DeviceConfigShellCommand.SYNC_DISABLED_MODE_NONE);
+        }
+    }
 
     /**
      * Registers a {@link android.app.time.TimeManager.TimeZoneDetectorListener}, makes changes
@@ -144,6 +177,64 @@ public class TimeManagerTest {
         }
     }
 
+    @Test
+    public void externalSuggestions() throws Exception {
+        long startCurrentTimeMillis = System.currentTimeMillis();
+        long elapsedRealtimeMillis = SystemClock.elapsedRealtime();
+
+        Context context = InstrumentationRegistry.getInstrumentation().getContext();
+        TimeManager timeManager = context.getSystemService(TimeManager.class);
+        assertNotNull(timeManager);
+
+        // Set the time detector to only use ORIGIN_NETWORK. The important aspect is that it isn't
+        // ORIGIN_EXTERNAL, and so suggestions from external should be ignored.
+        DeviceConfig.setProperty(NAMESPACE_SYSTEM_TIME,
+                TimeDetectorServerFlags.KEY_TIME_DETECTOR_ORIGIN_PRIORITIES_OVERRIDE,
+                TimeDetectorServerFlags.ORIGIN_NETWORK,
+                /*makeDefault=*/false);
+        sleepForAsyncOperation();
+
+        long suggestion1Millis =
+                Instant.ofEpochMilli(startCurrentTimeMillis).plus(1, ChronoUnit.DAYS).toEpochMilli();
+        ExternalTimeSuggestion futureTimeSuggestion1 =
+                new ExternalTimeSuggestion(elapsedRealtimeMillis, suggestion1Millis);
+        long suggestion2Millis =
+                Instant.ofEpochMilli(suggestion1Millis).plus(1, ChronoUnit.DAYS).toEpochMilli();
+        ExternalTimeSuggestion futureTimeSuggestion2 =
+                new ExternalTimeSuggestion(elapsedRealtimeMillis, suggestion2Millis);
+
+        // Suggest a change. It shouldn't be used.
+        timeManager.suggestExternalTime(futureTimeSuggestion1);
+        sleepForAsyncOperation();
+
+        // The suggestion should have been ignored so the system clock should not have advanced.
+        assertTrue(System.currentTimeMillis() < suggestion1Millis);
+
+        // Set the time detector to only use ORIGIN_EXTERNAL.
+        // The suggestion should have been stored and acted upon when the origin list changes.
+        DeviceConfig.setProperty(NAMESPACE_SYSTEM_TIME,
+                TimeDetectorServerFlags.KEY_TIME_DETECTOR_ORIGIN_PRIORITIES_OVERRIDE,
+                TimeDetectorServerFlags.ORIGIN_EXTERNAL,
+                /*makeDefault=*/false);
+        sleepForAsyncOperation();
+        assertTrue(System.currentTimeMillis() >= suggestion1Millis);
+
+        // Suggest a change. It should be used.
+        timeManager.suggestExternalTime(futureTimeSuggestion2);
+        sleepForAsyncOperation();
+        assertTrue(System.currentTimeMillis() >= suggestion2Millis);
+
+        // Now do our best to return the device to its original state.
+        ExternalTimeSuggestion originalTimeSuggestion =
+                new ExternalTimeSuggestion(elapsedRealtimeMillis, startCurrentTimeMillis);
+        timeManager.suggestExternalTime(
+                originalTimeSuggestion);
+        sleepForAsyncOperation();
+
+        DeviceConfigShellCommand.resetToDefaults(
+                DeviceConfigShellCommand.RESET_MODE_TRUSTED_DEFAULTS, NAMESPACE_SYSTEM_TIME);
+    }
+
     /**
      * Registers a {@link android.app.time.TimeManager.TimeZoneDetectorListener}, makes changes
      * to the "location enabled" setting and checks that the listener is called.
@@ -194,5 +285,79 @@ public class TimeManagerTest {
             Thread.sleep(250);
         }
         assertEquals(expectedValue, actualValue.get());
+    }
+
+    /**
+     * A class for interacting with the {@link DeviceConfig} service via the command line. Some
+     * behavior it supports is not available via the Android @SystemApi.
+     * See {@link com.android.providers.settings.DeviceConfigService} for the shell command
+     * implementation details.
+     */
+    private static class DeviceConfigShellCommand {
+
+        static final String RESET_MODE_TRUSTED_DEFAULTS = "trusted_defaults";
+        static final String SYNC_DISABLED_MODE_NONE = "none";
+        static final String SYNC_DISABLED_MODE_UNTIL_REBOOT = "until_reboot";
+
+        private static final String SHELL_CMD_PREFIX = "cmd device_config ";
+
+        private DeviceConfigShellCommand() {}
+
+        static boolean isSyncDisabled() throws Exception {
+            String cmd = SHELL_CMD_PREFIX + "is_sync_disabled_for_tests";
+            String result = executeShellCommandInternal(cmd);
+            return Boolean.parseBoolean(result);
+        }
+
+        static void setSyncDisabled(String syncDisabledMode) throws Exception {
+            String cmd = String.format(
+                    SHELL_CMD_PREFIX + "set_sync_disabled_for_tests %s", syncDisabledMode);
+            executeShellCommandInternal(cmd);
+        }
+
+        static void resetToDefaults(String resetMode, String namespace) throws Exception {
+            String cmd = String.format(SHELL_CMD_PREFIX + "reset %s %s", resetMode, namespace);
+            executeShellCommandInternal(cmd);
+        }
+
+        private static String executeShellCommandInternal(String cmd) throws IOException {
+            UiAutomation uiAutomation =
+                    InstrumentationRegistry.getInstrumentation().getUiAutomation();
+            try (FileInputStream output = new FileInputStream(
+                    uiAutomation.executeShellCommand(cmd).getFileDescriptor())) {
+                return new String(ByteStreams.toByteArray(output));
+            }
+        }
+    }
+
+    /**
+     * device_config service flags and values for controlling the time_detector service.
+     */
+    private interface TimeDetectorServerFlags {
+        /**
+         * See {@link
+         * com.android.server.timedetector.ServerFlags#KEY_TIME_DETECTOR_ORIGIN_PRIORITIES_OVERRIDE}
+         */
+        String KEY_TIME_DETECTOR_ORIGIN_PRIORITIES_OVERRIDE =
+                "time_detector_origin_priorities_override";
+
+        /**
+         * See {@link com.android.server.timedetector.TimeDetectorStrategy#ORIGIN_NETWORK}.
+         */
+        String ORIGIN_NETWORK = "network";
+
+        /**
+         * See {@link com.android.server.timedetector.TimeDetectorStrategy#ORIGIN_EXTERNAL}.
+         */
+        String ORIGIN_EXTERNAL = "external";
+    }
+
+    /**
+     * Sleeps for a length of time sufficient to allow async operations to complete. Many time
+     * manager APIs are or could be asynchronous and deal with time, so there are no practical
+     * alternatives.
+     */
+    private static void sleepForAsyncOperation() throws Exception{
+        Thread.sleep(5_000);
     }
 }
