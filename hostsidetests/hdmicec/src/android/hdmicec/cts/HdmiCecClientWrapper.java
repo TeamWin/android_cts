aac803cddb1d8e789b4b3e66de18d0dc05d83691
Shraddha Basantwani <shraddha.basantwani@ittiam.com>
2021-05-27 22:05:43 +0530

    CEC: Add method to send cec message with params
    Bug: 187694920
    Test: atest CtsHdmiCecHostTestCases
    Change-Id: Iefedf93ba1021dab1a6f332a2acfecd5c85b00d8

--- hostsidetests/hdmicec/src/android/hdmicec/cts/HdmiCecClientWrapper.java
+++ hostsidetests/hdmicec/src/android/hdmicec/cts/HdmiCecClientWrapper.java
@@ -185,6 +185,15 @@ public final class HdmiCecClientWrapper extends ExternalResource {
     }
 
     /**
+     * Sends a CEC message from source device to the device passed in the constructor through the
+     * output console of the cec-communication channel with the appended params.
+     */
+    public void sendCecMessage(LogicalAddress source, CecOperand message, String params)
+            throws Exception {
+        sendCecMessage(source, targetDevice, message, params);
+    }
+
+    /**
      * Sends a CEC message from source device to a destination device through the output console of
      * the cec-communication channel.
      */
