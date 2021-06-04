d96e8895b14abcc003b83ff5cc9547e7eed6ab24
Nate Jiang <qiangjiang@google.com>
2021-06-04 00:22:49 -0700

    [CTS] fix the test for P2P lazy initialization
    Bug: 187254179
    Test: cts ConcurrencyTest
    Change-Id: Ic389729c0f5c592b9f2be0f88a4453321671b72c

--- tests/tests/wifi/src/android/net/wifi/cts/ConcurrencyTest.java
+++ tests/tests/wifi/src/android/net/wifi/cts/ConcurrencyTest.java
@@ -94,7 +94,7 @@ public class ConcurrencyTest extends WifiJUnit3TestBase {
     private static final String TAG = "ConcurrencyTest";
     private static final int TIMEOUT_MSEC = 6000;
     private static final int WAIT_MSEC = 60;
-    private static final int DURATION = 10000;
+    private static final int DURATION = 5000;
     private IntentFilter mIntentFilter;
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -325,14 +325,6 @@ public class ConcurrencyTest extends WifiJUnit3TestBase {
                     + " needs Location enabled.");
         }
 
-        mWifiP2pManager =
-                (WifiP2pManager) getContext().getSystemService(Context.WIFI_P2P_SERVICE);
-        mWifiP2pChannel = mWifiP2pManager.initialize(
-                getContext(), getContext().getMainLooper(), null);
-
-        assertNotNull(mWifiP2pManager);
-        assertNotNull(mWifiP2pChannel);
-
         long timeout = System.currentTimeMillis() + TIMEOUT_MSEC;
         while (!mWifiManager.isWifiEnabled() && System.currentTimeMillis() < timeout) {
             try {
@@ -341,6 +333,14 @@ public class ConcurrencyTest extends WifiJUnit3TestBase {
         }
 
         assertTrue(mWifiManager.isWifiEnabled());
+
+        mWifiP2pManager =
+                (WifiP2pManager) getContext().getSystemService(Context.WIFI_P2P_SERVICE);
+        mWifiP2pChannel = mWifiP2pManager.initialize(
+                getContext(), getContext().getMainLooper(), null);
+
+        assertNotNull(mWifiP2pManager);
+        assertNotNull(mWifiP2pChannel);
 
         assertTrue(waitForBroadcasts(
                 new LinkedList<Integer>(
