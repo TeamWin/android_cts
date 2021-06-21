8ac56c3447258552561e75ab82194692aa29fabf
SongFerngWang <songferngwang@google.com>
2021-06-21 12:47:16 +0800

    The two testcases are skipped temporarily
    Bug: 191318997
    Test: Ignore two testcases
    Change-Id: Ia360a6da08a5b9eefc3f8f039a5305d53ccb23bd

--- tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
@@ -52,6 +52,7 @@ import com.android.internal.util.ArrayUtils;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.time.Period;
@@ -407,6 +408,7 @@ public class SubscriptionManagerTest {
     }
 
     @Test
+    @Ignore("b/181053462")
     public void testSubscriptionGroupingWithPermission() throws Exception {
         if (!isSupported()) return;
 
