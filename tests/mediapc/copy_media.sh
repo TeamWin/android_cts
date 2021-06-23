684615dddc7c73a05d86193f4cb6b5d317d719a4
Giridhar Tippabathuni <giridhar.tippabathuni@ittiam.com>
2021-06-23 14:12:02 +0530

    mediapc CTS: Update resource directory from 1.0 to 1.1
    Bug: 179084926
    Test: atest android.mediapc.cts
    
    Change-Id: I733cde1c38119b1a63fc0ca5611875a93ce288d6

--- tests/mediapc/copy_media.sh
+++ tests/mediapc/copy_media.sh
@@ -17,7 +17,7 @@
 ## script to install media performance class test files manually
 
 adbOptions=" "
-resLabel=CtsMediaPerformanceClassTestCases-1.0
+resLabel=CtsMediaPerformanceClassTestCases-1.1
 srcDir="/tmp/$resLabel"
 tgtDir="/sdcard/test"
 usage="Usage: $0 [-h] [-s serial]"
