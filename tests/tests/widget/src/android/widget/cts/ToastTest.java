96431e908fe380db99f7c52ba3c150c6c6e74cff
Dragos <drtigane@microsoft.com>
2021-07-09 14:42:59 +0300

    Support for multiple TaskDisplayAreas in ToastTest
    The Test: testCustomToastBlocked_whenBehindTranslucent
    does not take into account that in a multi-window environment,
    new Tasks can be opened in different TaskDisplayAreas.
    (With CtsActivity on one TaskDisplayArea and TranslucentActivity
    on another, the Toast launched for CtsActivity is visible)
    
    Solution:
    Ensure that TranslucentActivity is opened on top of CtsActivity,
    in the same Task and same TaskDisplayArea by removing
    FLAG_ACTIVITY_NEW_TASK and using the Context from CtsActivity.
    
    Bug: 192589485
    Test: run cts -m CtsWidgetTestCases -t android.widget.cts.
    ToastTest#testCustomToastBlocked_whenBehindTranslucentActivity
    
    Signed-off-by: Dragos <drtigane@microsoft.com>
    Change-Id: I87631a2352efd351a9a348f3cc5d91dc9563432d

--- tests/tests/widget/src/android/widget/cts/ToastTest.java
+++ tests/tests/widget/src/android/widget/cts/ToastTest.java
@@ -17,7 +17,6 @@
 package android.widget.cts;
 
 import static android.app.WindowConfiguration.WINDOWING_MODE_FULLSCREEN;
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -734,12 +733,11 @@ public class ToastTest {
                 ACTION_TRANSLUCENT_ACTIVITY_RESUMED);
         Intent intent = new Intent();
         intent.setComponent(COMPONENT_TRANSLUCENT_ACTIVITY);
-        intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
         // Launch the translucent activity in fullscreen to ensure the test activity won't resume
         // even on the freeform-first multi-window device.
         final ActivityOptions options = ActivityOptions.makeBasic();
         options.setLaunchWindowingMode(WINDOWING_MODE_FULLSCREEN);
-        mContext.startActivity(intent, options.toBundle());
+        mActivityRule.getActivity().startActivity(intent, options.toBundle());
         activityStarted.block();
         makeCustomToast();
         View view = mToast.getView();
