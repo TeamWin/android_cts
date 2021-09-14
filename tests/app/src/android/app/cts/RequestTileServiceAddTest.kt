5ad61b1ee261f5be90952c471c15a2c98376779a
Fabian Kozynski <kozynski@google.com>
2021-09-14 10:58:08 -0400

    CTS for extra checks
    Add tests for extra security checks.
    
    Test: atest RequestTileServiceAddTest
    Fixes: 199770909
    
    Change-Id: Ie53446f921bb90fdd955375c824795dcdae7f706

--- tests/app/src/android/app/cts/RequestTileServiceAddTest.kt
+++ tests/app/src/android/app/cts/RequestTileServiceAddTest.kt
@@ -16,7 +16,10 @@
 
 package android.app.cts
 
+import android.app.ActivityManager
+import android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
 import android.app.StatusBarManager
+import android.app.stubs.NotExportedTestTileService
 import android.app.stubs.TestTileService
 import android.content.ComponentName
 import android.content.Context
@@ -121,6 +124,45 @@ class RequestTileServiceAddTest {
         latch.await(TIME_OUT_SECONDS, TimeUnit.SECONDS)
 
         assertThat(consumer.result).isEqualTo(StatusBarManager.TILE_ADD_REQUEST_ERROR_BAD_COMPONENT)
+    }
+
+    @Test
+    fun testNotExportedComponent() {
+        val componentName = NotExportedTestTileService.getComponentName()
+
+        statusBarService.requestAddTileService(
+                componentName,
+                LABEL,
+                icon,
+                executor,
+                consumer
+        )
+
+        latch.await(TIME_OUT_SECONDS, TimeUnit.SECONDS)
+
+        assertThat(consumer.result).isEqualTo(StatusBarManager.TILE_ADD_REQUEST_ERROR_BAD_COMPONENT)
+    }
+
+    @Test
+    fun testAppNotInForeground() {
+        // This test is never run in foreground, so it's a good candidate for testing this
+        val componentName = TestTileService.getComponentName()
+        val activityManager = context.getSystemService(ActivityManager::class.java)!!
+        assertThat(activityManager.getPackageImportance(context.packageName))
+                .isNotEqualTo(IMPORTANCE_FOREGROUND)
+
+        statusBarService.requestAddTileService(
+                componentName,
+                LABEL,
+                icon,
+                executor,
+                consumer
+        )
+
+        latch.await(TIME_OUT_SECONDS, TimeUnit.SECONDS)
+
+        assertThat(consumer.result)
+                .isEqualTo(StatusBarManager.TILE_ADD_REQUEST_ERROR_APP_NOT_IN_FOREGROUND)
     }
 
     private class StoreIntConsumer(private val latch: CountDownLatch) : Consumer<Int> {
