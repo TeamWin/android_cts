617174855ab98da781da6059d5e32525d2c2dc50
Shivam Agrawal <shivamagrawal@google.com>
2021-09-14 15:12:52 -0700

    Activity Embedding Tests (1/n)
    1. Create test infrastructure
    2. Create simple test that launches two activities into split
    
    Bug: b/202748345
    Test: atest CtsWindowManagerJetpackTestCases:ActivityEmbeddingLaunchTests
    Change-Id: I35225ea32d39242ba180856a19411d4ee1310f60

---
+++ tests/framework/base/windowmanager/jetpack/src/android/server/wm/jetpack/ActivityEmbeddingLaunchTests.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.server.wm.jetpack;
+
+import static android.server.wm.jetpack.utils.ExtensionUtil.assumeExtensionSupportedDevice;
+import static android.server.wm.jetpack.utils.ExtensionUtil.getWindowExtensions;
+import static android.server.wm.jetpack.utils.ActivityEmbeddingUtil.TAG;
+import static android.server.wm.jetpack.utils.ActivityEmbeddingUtil.createWildcardSplitPairRule;
+import static android.server.wm.jetpack.utils.ActivityEmbeddingUtil.startActivityAndVerifySplit;
+
+import static org.junit.Assume.assumeNotNull;
+
+import android.app.Activity;
+import android.util.Log;
+import android.server.wm.jetpack.utils.WindowManagerJetpackTestBase;
+import android.server.wm.jetpack.utils.TestActivityWithId;
+import android.server.wm.jetpack.utils.TestConfigChangeHandlingActivity;
+import android.server.wm.jetpack.utils.TestFirstValueConsumer;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.window.extensions.WindowExtensions;
+import androidx.window.extensions.embedding.ActivityEmbeddingComponent;
+import androidx.window.extensions.embedding.SplitInfo;
+import androidx.window.extensions.embedding.SplitPairRule;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Tests for the {@link androidx.window.extensions} implementation provided on the device (and only
+ * if one is available) for the Activity Embedding functionality. Specifically tests activity
+ * launch scenarios.
+ *
+ * Build/Install/Run:
+ *     atest CtsWindowManagerJetpackTestCases:ActivityEmbeddingLaunchTests
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActivityEmbeddingLaunchTests extends WindowManagerJetpackTestBase {
+
+    private ActivityEmbeddingComponent mActivityEmbeddingComponent;
+    private TestFirstValueConsumer<List<SplitInfo>> mSplitInfoConsumer;
+
+    @Before
+    public void setUp() {
+        super.setUp();
+        assumeExtensionSupportedDevice();
+        WindowExtensions windowExtensions = getWindowExtensions();
+        assumeNotNull(windowExtensions);
+        mInstrumentation.runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                mActivityEmbeddingComponent = windowExtensions.getActivityEmbeddingComponent();
+            }
+        });
+        assumeNotNull(mActivityEmbeddingComponent);
+        mSplitInfoConsumer = new TestFirstValueConsumer<>();
+        mActivityEmbeddingComponent.setSplitInfoCallback(mSplitInfoConsumer);
+    }
+
+    /**
+     * Tests launching activities to the side from the primary activity.
+     */
+    @Test
+    public void testPrimaryActivityLaunchToSide() {
+        Activity primaryActivity = startActivityNewTask(TestConfigChangeHandlingActivity.class);
+
+        SplitPairRule splitPairRule = createWildcardSplitPairRule();
+        mActivityEmbeddingComponent.setEmbeddingRules(Collections.singleton(splitPairRule));
+
+        // Launch multiple activities to the side from the primary activity and verify that they
+        // all successfully split with the primary activity.
+        final int numActivitiesToLaunch = 4;
+        for (int i = 0; i < numActivitiesToLaunch; i++) {
+            Activity secondaryActivity = startActivityAndVerifySplit(mSplitInfoConsumer,
+                    primaryActivity, TestActivityWithId.class, splitPairRule);
+        }
+    }
+}
---
+++ tests/framework/base/windowmanager/jetpack/src/android/server/wm/jetpack/utils/ActivityEmbeddingUtil.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.server.wm.jetpack.utils;
+
+import static android.server.wm.jetpack.utils.ExtensionUtil.assumeExtensionSupportedDevice;
+import static android.server.wm.jetpack.utils.ExtensionUtil.getWindowExtensions;
+import static android.server.wm.jetpack.utils.WindowManagerJetpackTestBase.getActivityBounds;
+import static android.server.wm.jetpack.utils.WindowManagerJetpackTestBase.getMaximumActivityBounds;
+import static android.server.wm.jetpack.utils.WindowManagerJetpackTestBase.startActivityFromActivity;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Looper;
+import android.util.LayoutDirection;
+import android.util.Log;
+import android.util.Pair;
+import android.view.WindowMetrics;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.window.extensions.WindowExtensions;
+import androidx.window.extensions.embedding.ActivityEmbeddingComponent;
+import androidx.window.extensions.embedding.EmbeddingRule;
+import androidx.window.extensions.embedding.SplitInfo;
+import androidx.window.extensions.embedding.SplitPairRule;
+import androidx.window.extensions.embedding.SplitRule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Predicate;
+
+/**
+ * Utility class for activity embedding tests.
+ */
+public class ActivityEmbeddingUtil {
+
+    public static final String TAG = "ActivityEmbeddingTests";
+    public static final long WAIT_FOR_RESUMED_TIMEOUT_MS = 3000;
+    public static final float DEFAULT_SPLIT_RATIO = 0.5f;
+
+    @NonNull
+    public static SplitPairRule createWildcardSplitPairRule() {
+        // Any activity be split with any activity
+        final Predicate<Pair<Activity, Activity>> activityPairPredicate =
+                activityActivityPair -> true;
+        // Any activity can launch any split intent
+        final Predicate<Pair<Activity, Intent>> activityIntentPredicate =
+                activityIntentPair -> true;
+        // Allow any parent bounds to show the split containers side by side
+        Predicate<WindowMetrics> parentWindowMetricsPredicate = windowMetrics -> true;
+        // Build the split pair rule
+        return new SplitPairRule.Builder(activityPairPredicate,
+                activityIntentPredicate, parentWindowMetricsPredicate).setSplitRatio(
+                DEFAULT_SPLIT_RATIO).build();
+    }
+
+    public static Activity startActivityAndVerifySplit(
+            TestFirstValueConsumer<List<SplitInfo>> splitInfoConsumer, Activity primaryActivity,
+            Class secondActivityClass, SplitPairRule splitPairRule) {
+        // Must reset consumer so that most recent split info update can be received
+        splitInfoConsumer.reset();
+
+        // Start second activity
+        final long secondActivityId = startActivityFromActivity(primaryActivity,
+                secondActivityClass);
+
+        // Get updated split info
+        List<SplitInfo> activeSplitStates = null;
+        try {
+            activeSplitStates = splitInfoConsumer.waitAndGet();
+        } catch (InterruptedException e) {
+            fail("startActivityAndVerifySplit() InterruptedException");
+        } catch (ExecutionException e) {
+            fail("startActivityAndVerifySplit() ExecutionException");
+        } catch (TimeoutException e) {
+            fail("startActivityAndVerifySplit() TimeoutException");
+        }
+
+        // Get second activity from split info
+        Activity secondActivity = getSecondActivity(activeSplitStates, primaryActivity,
+                secondActivityId);
+        assertNotNull(secondActivity);
+
+        assertValidSplit(primaryActivity, secondActivity, splitPairRule);
+
+        // Return second activity for easy access in calling method
+        return secondActivity;
+    }
+
+    @Nullable
+    public static Activity getSecondActivity(@NonNull List<SplitInfo> activeSplitStates,
+            @NonNull Activity primaryActivity, long secondaryClassId) {
+        Log.d(TAG, "Active split states: " + activeSplitStates);
+        for (SplitInfo splitInfo : activeSplitStates) {
+            // Find the split info whose top activity in the primary container is the primary
+            // activity we are looking for
+            Activity primaryContainerTopActivity = getPrimaryStackTopActivity(splitInfo);
+            if (primaryActivity.equals(primaryContainerTopActivity)) {
+                Activity secondActivity = getSecondaryStackTopActivity(splitInfo);
+                // See if this activity is the secondary activity we expect
+                if (secondActivity != null && secondActivity instanceof TestActivityWithId
+                        && secondaryClassId == ((TestActivityWithId) secondActivity).getId()) {
+                    return secondActivity;
+                }
+            }
+        }
+        Log.d(TAG, "Second activity was not found: " + secondaryClassId);
+        return null;
+    }
+
+    public static void assertValidSplit(@NonNull Activity primaryActivity,
+            @NonNull Activity secondaryActivity, SplitRule splitRule) {
+        waitForResumed(Arrays.asList(primaryActivity, secondaryActivity));
+
+        // Compute the layout direction
+        int layoutDir = splitRule.getLayoutDirection();
+        if (layoutDir == LayoutDirection.LOCALE) {
+            layoutDir = primaryActivity.getResources().getConfiguration().getLayoutDirection();
+        }
+
+        final float splitRatio = splitRule.getSplitRatio();
+        final Rect parentBounds = getMaximumActivityBounds(primaryActivity);
+        final Rect expectedPrimaryActivityBounds = new Rect();
+        final Rect expectedSecondaryActivityBounds = new Rect();
+        getExpectedPrimaryAndSecondaryBounds(layoutDir, splitRatio, parentBounds,
+                expectedPrimaryActivityBounds, expectedSecondaryActivityBounds);
+        assertEquals(expectedPrimaryActivityBounds, getActivityBounds(primaryActivity));
+        assertEquals(expectedSecondaryActivityBounds, getActivityBounds(secondaryActivity));
+    }
+
+    public static void verifyFillsTask(Activity activity) {
+        waitForResumed(Arrays.asList(activity));
+        assertEquals(getMaximumActivityBounds(activity), getActivityBounds(activity));
+    }
+
+    public static boolean waitForResumed(
+            @NonNull List<Activity> activityList) {
+        final long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < WAIT_FOR_RESUMED_TIMEOUT_MS) {
+            boolean allActivitiesResumed = true;
+            for (Activity activity : activityList) {
+                allActivitiesResumed &= WindowManagerJetpackTestBase.isActivityResumed(activity);
+                if (!allActivitiesResumed) {
+                    break;
+                }
+            }
+            if (allActivitiesResumed) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Nullable
+    public static Activity getPrimaryStackTopActivity(SplitInfo splitInfo) {
+        List<Activity> primaryActivityStack = splitInfo.getPrimaryActivityStack().getActivities();
+        if (primaryActivityStack.isEmpty()) {
+            return null;
+        }
+        return primaryActivityStack.get(primaryActivityStack.size() - 1);
+    }
+
+    @Nullable
+    public static Activity getSecondaryStackTopActivity(SplitInfo splitInfo) {
+        List<Activity> secondaryActivityStack = splitInfo.getSecondaryActivityStack()
+                .getActivities();
+        if (secondaryActivityStack.isEmpty()) {
+            return null;
+        }
+        return secondaryActivityStack.get(secondaryActivityStack.size() - 1);
+    }
+
+    public static void getExpectedPrimaryAndSecondaryBounds(int layoutDir, float splitRatio,
+            @NonNull Rect inParentBounds, @NonNull Rect outPrimaryActivityBounds,
+            @NonNull Rect outSecondaryActivityBounds) {
+        final int expectedPrimaryWidth = (int) (inParentBounds.width() * splitRatio);
+        final int expectedSecondaryWidth = (int) (inParentBounds.width() * (1 - splitRatio));
+
+        outPrimaryActivityBounds.set(inParentBounds);
+        outSecondaryActivityBounds.set(inParentBounds);
+        if (layoutDir == LayoutDirection.LTR) {
+            /*******************|*********************
+             * primary activity | secondary activity *
+             *******************|*********************/
+            outPrimaryActivityBounds.right = inParentBounds.left + expectedPrimaryWidth;
+            outSecondaryActivityBounds.left = inParentBounds.right - expectedSecondaryWidth;
+        } else {
+            /*********************|*******************
+             * secondary activity | primary activity *
+             *********************|*******************/
+            outPrimaryActivityBounds.left = inParentBounds.right - expectedPrimaryWidth;
+            outSecondaryActivityBounds.right = inParentBounds.left + expectedSecondaryWidth;
+        }
+    }
+}
