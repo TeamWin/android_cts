b03ec45a00de5bb6b7d5eb6bafff3dc9767cc61c
Yogesh Lal <quic_ylal@quicinc.com>
2021-09-14 16:36:06 +0530

    cts: Add Khaje target to whitelist in the CTS test case
    Khaje target don't need to define CONFIG_UNMAP_KERNEL_AT_EL0,
    as the hardware is not vulnerable to CVE-2017-5754.
    Hence, whitelisting it.
    
    Bug: 198722642
    Test: run cts --m CtsSecurityHostTestCases --t
    android.security.cts.KernelConfigTest#testConfigHardwareMitigations
    
    Change-Id: Id7b523ed51be44bab07ec5756c4563e8442b02a6

--- hostsidetests/security/src/android/security/cts/KernelConfigTest.java
+++ hostsidetests/security/src/android/security/cts/KernelConfigTest.java
@@ -296,6 +296,7 @@ public class KernelConfigTest extends DeviceTestCase implements IBuildReceiver, 
         put("ATOLL-AB", null);
         put("SDM660", new String[]{"CONFIG_HARDEN_BRANCH_PREDICTOR=y"});
         put("BENGAL", new String[]{"CONFIG_HARDEN_BRANCH_PREDICTOR=y"});
+        put("KHAJE", new String[]{"CONFIG_HARDEN_BRANCH_PREDICTOR=y"});
         put("DEFAULT", new String[]{"CONFIG_HARDEN_BRANCH_PREDICTOR=y",
             "CONFIG_UNMAP_KERNEL_AT_EL0=y"});
     }};
