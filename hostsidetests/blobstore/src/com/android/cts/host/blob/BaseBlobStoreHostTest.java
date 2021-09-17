2e83ff24a66f2cccb0abca5fe20ed827aede0e80
Sudheer Shanka <sudheersai@google.com>
2021-09-17 02:53:36 +0000

    Revert "Adding assumeTrue() to setUp() instead of setUpClass() function"
    This reverts commit 4ee9b4d0c829f2e439379298c134efdfcacc4cd4.
    
    Reason for revert: The underlying issue with having assumeTrue() in
                       setUpClass() has been fixed.
    
    Bug: 200228519
    Test: adb shell setprop fw.max_users 1
          && atest CtsBlobStoreHostTestCases
    Change-Id: I6a64f7d935923049507edfdb4e1e832dcffbe3a0

--- hostsidetests/blobstore/src/com/android/cts/host/blob/BaseBlobStoreHostTest.java
+++ hostsidetests/blobstore/src/com/android/cts/host/blob/BaseBlobStoreHostTest.java
@@ -98,10 +98,6 @@ abstract class BaseBlobStoreHostTest extends BaseHostJUnit4Test {
         return device.isMultiUserSupported();
     }
 
-    protected boolean isMultiUserSupported() throws Exception {
-        return isMultiUserSupported(getDevice());
-    }
-
     protected Map<String, String> createArgsFromLastTestRun() {
         final Map<String, String> args = new HashMap<>();
         for (String key : new String[] {
