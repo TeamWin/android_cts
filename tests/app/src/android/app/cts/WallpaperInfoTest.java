ee81dfac49b1ca92773e9b2e1c492458533aaf54
Nick Chameyev <nickchameyev@google.com>
2021-09-23 17:13:41 +0000

    Rename shouldUseDefaultUnfoldTransition attribute and update docs
    Renames the shouldUseDefaultUnfoldTransition attribute
    name to use more generic
    shouldUseDefaultDeviceStateChangeTransition name as
    the transition could be used on different form factors
    depending on the OEMs.
    
    Test: manual
    Fixes: 200730817
    Fixes: 200698833
    Change-Id: Icf6e48b289c440827dd2eea42c23bfbe408d2593

--- tests/app/src/android/app/cts/WallpaperInfoTest.java
+++ tests/app/src/android/app/cts/WallpaperInfoTest.java
@@ -74,16 +74,16 @@ public class WallpaperInfoTest {
             wallpaperInfo.getSettingsSliceUri().toString());
         assertTrue(wallpaperInfo.getShowMetadataInPreview());
         assertTrue(wallpaperInfo.supportsMultipleDisplays());
-        assertTrue(wallpaperInfo.shouldUseDefaultUnfoldTransition());
+        assertTrue(wallpaperInfo.shouldUseDefaultDeviceStateChangeTransition());
         assertNotNull(wallpaperInfo.loadIcon(pm));
         assertNotNull(wallpaperInfo.loadThumbnail(pm));
     }
 
     @Test
-    public void test_defaultUnfoldTransitionDisabled() {
+    public void test_defaultDeviceStateChangeTransitionDisabled() {
         WallpaperInfo wallpaperInfo = getInfoForService(LiveWallpaperNoUnfoldTransition.class);
 
-        assertFalse(wallpaperInfo.shouldUseDefaultUnfoldTransition());
+        assertFalse(wallpaperInfo.shouldUseDefaultDeviceStateChangeTransition());
     }
 
     private <T extends WallpaperService> WallpaperInfo getInfoForService(Class<T> service) {
