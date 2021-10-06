b426bfcd6e2c77043ec909adbea8c3a8145bb073
Paul McLean <pmclean@google.com>
2021-10-06 10:22:42 -0600

    DO NOT MERGE - Cleanup/enhance AEC test. Standardize ReportLog keys.
    ReportLoging keys, Info Text.
    Refactoring SoundIO & AudioUtilities classes
    
    Bug: 202969003
    Bug: 202969274
    Test: build, install, run, pull and upload report
    
    Change-Id: I3cafa28c562542025aaebe6c9687933ab9c4c145
    Merged-In: I3cafa28c562542025aaebe6c9687933ab9c4c145

--- apps/CtsVerifier/src/com/android/cts/verifier/audio/AudioFrequencyVoiceRecognitionActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/audio/AudioFrequencyVoiceRecognitionActivity.java
@@ -30,6 +30,8 @@ import android.widget.TextView;
 
 import com.android.compatibility.common.util.ResultType;
 import com.android.compatibility.common.util.ResultUnit;
+import com.android.cts.verifier.audio.soundio.SoundPlayerObject;
+import com.android.cts.verifier.audio.soundio.SoundRecorderObject;
 import com.android.cts.verifier.CtsVerifierReportLog;
 import com.android.cts.verifier.R;
 import com.android.cts.verifier.audio.wavelib.DspBufferComplex;
