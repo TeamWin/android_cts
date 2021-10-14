3739bc26b0f8f931438f1f0a75bd8d0262af0ae5
Nicolas Geoffray <ngeoffray@google.com>
2021-10-14 13:21:31 +0000

    Fix test when the type of the field cannot be resolved.
    We should not return true/false, but instead throw an exception so that
    the test just discard this field.
    
    (cherry picked from commit f8cc0d6820e0e0bca6d3ff9149f129176e2267fb)
    
    Test: atest CtsHiddenApiBlacklistApi28TestCases
    Test: atest CtsHiddenApiKillswitchDebugClassTestCases:android.signature.cts.api.DebugClassKillswitchTest
    Bug: 202684106
    Merged-In: I5ac0f67f498de96788febafbc3195372a6d049f2
    Change-Id: Iaabbd5e8199ee447c0560a7127125900fa281e78

--- tests/signature/lib/android/src/android/signature/cts/DexMemberChecker.java
+++ tests/signature/lib/android/src/android/signature/cts/DexMemberChecker.java
@@ -97,12 +97,12 @@ public class DexMemberChecker {
             if (jni) {
                 try {
                     observer.fieldAccessibleViaJni(hasMatchingField_JNI(klass, field), field);
-                } catch (ExceptionInInitializerError | UnsatisfiedLinkError
+                } catch (ClassNotFoundException | ExceptionInInitializerError | UnsatisfiedLinkError
                         | NoClassDefFoundError e) {
                     if ((e instanceof NoClassDefFoundError)
                             && !(e.getCause() instanceof ExceptionInInitializerError)
                             && !(e.getCause() instanceof UnsatisfiedLinkError)) {
-                        throw e;
+                        throw (NoClassDefFoundError) e;
                     }
 
                     // Could not initialize the class. Skip JNI test.
@@ -167,7 +167,11 @@ public class DexMemberChecker {
         }
     }
 
-    private static boolean hasMatchingField_JNI(Class<?> klass, DexField dexField) {
+    private static boolean hasMatchingField_JNI(Class<?> klass, DexField dexField)
+            throws ClassNotFoundException {
+        // If we fail to resolve the type of the field, we will throw a ClassNotFoundException.
+        DexMember.typeToClass(dexField.getDexType());
+
         try {
             Field ifield = getField_JNI(klass, dexField.getName(), dexField.getDexType());
             if (ifield.getDeclaringClass() == klass) {
