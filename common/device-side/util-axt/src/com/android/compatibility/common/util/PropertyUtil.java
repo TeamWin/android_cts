f15438ce179cb67dd1146b26f0ff81e7bbf344b7
Justin Yun <justinyun@google.com>
2021-10-21 14:14:05 +0900

    Provide getVsrApiLevel() to read the Vsr API level of DUT
    The VSR requirements need to know the API level for both device and
    board to run or skip the tests. It is complicated to find the API
    level with existing functions.
    Provide a function to easily get the API level for the tests of VSR
    requirements.
    
    Bug: 202919753
    Test: manual test
    Change-Id: I30beeea44526ae69863e6c3eb4a14bf4d82f16aa

--- common/device-side/util-axt/src/com/android/compatibility/common/util/PropertyUtil.java
+++ common/device-side/util-axt/src/com/android/compatibility/common/util/PropertyUtil.java
@@ -38,13 +38,14 @@ public class PropertyUtil {
      * shipped. Property should be undefined for factory ROM products.
      */
     public static final String FIRST_API_LEVEL = "ro.product.first_api_level";
+    private static final String BOARD_API_LEVEL = "ro.board.api_level";
+    private static final String BOARD_FIRST_API_LEVEL = "ro.board.first_api_level";
     private static final String BUILD_TYPE_PROPERTY = "ro.build.type";
+    private static final String CAMERAX_EXTENSIONS_ENABLED = "ro.camerax.extensions.enabled";
     private static final String MANUFACTURER_PROPERTY = "ro.product.manufacturer";
     private static final String TAG_DEV_KEYS = "dev-keys";
-    private static final String VENDOR_API_LEVEL = "ro.board.api_level";
-    private static final String VENDOR_FIRST_API_LEVEL = "ro.board.first_api_level";
+    private static final String VENDOR_BUILD_VERSION_SDK = "ro.vendor.build.version.sdk";
     private static final String VNDK_VERSION = "ro.vndk.version";
-    private static final String CAMERAX_EXTENSIONS_ENABLED = "ro.camerax.extensions.enabled";
 
     public static final String GOOGLE_SETTINGS_QUERY =
             "content query --uri content://com.google.settings/partner";
@@ -89,6 +90,23 @@ public class PropertyUtil {
     }
 
     /**
+     * Return the API level that the VSR requirement must be fulfilled. It reads
+     * ro.product.first_api_level, ro.board.first_api_level, and ro.board.api_level
+     * to find the minimum required VSR api_level for the DUT.
+     */
+    public static int getVsrApiLevel() {
+        // Api level properties of the board. The order of the properties must be kept.
+        String[] boardApiLevelProps = { BOARD_API_LEVEL, BOARD_FIRST_API_LEVEL, VENDOR_BUILD_VERSION_SDK };
+        for (String apiLevelProp : boardApiLevelProps) {
+            int apiLevel = getPropertyInt(apiLevelProp);
+            if (apiLevel != INT_VALUE_IF_UNSET) {
+                return Math.min(apiLevel, getFirstApiLevel());
+            }
+        }
+        return getFirstApiLevel();
+    }
+
+    /**
      * Return the API level of the vendor partition. It will read the following properties in order
      * and returns the value of the first defined property. If none of them are defined, or the
      * value is a VERSION CODENAME, returns the current API level which is defined in
@@ -103,7 +121,7 @@ public class PropertyUtil {
     public static int getVendorApiLevel() {
         String[] vendorApiLevelProps = {
             // Use the properties in order.
-            VENDOR_API_LEVEL, VENDOR_FIRST_API_LEVEL, VNDK_VERSION,
+            BOARD_API_LEVEL, BOARD_FIRST_API_LEVEL, VNDK_VERSION,
         };
         for (String prop : vendorApiLevelProps) {
             int apiLevel = getPropertyInt(prop);
