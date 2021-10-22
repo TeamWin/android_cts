2d99c24d5256213211829fae7e8610f228ced183
Anuj Joshi <anuj.joshi@ittiam.com>
2021-10-22 14:18:00 +0530

    [RESTRICT AUTOMERGE] Updated CTS test for Android Security b/172655291
    Bug: 172655291
    Bug: 177454666
    Test: Ran the new testcase on android-9.0.0_r39 with/without patch
    
    Change-Id: I1f39c94df35a366ec1c96874b6a93d86e0e755b6

--- tests/tests/security/src/android/security/cts/CVE_2021_0394.java
+++ tests/tests/security/src/android/security/cts/CVE_2021_0394.java
@@ -16,9 +16,9 @@
 
 package android.security.cts;
 
-import android.platform.test.annotations.RequiresDevice;
 import android.platform.test.annotations.AsbSecurityTest;
 import androidx.test.runner.AndroidJUnit4;
+import dalvik.system.VMRuntime;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 import static org.junit.Assert.assertFalse;
@@ -32,13 +32,13 @@ public class CVE_2021_0394 {
     /**
      * b/172655291
      */
-    @Test
-    @RequiresDevice
-    // emulators always have checkJNI enabled which causes the test
-    // to abort the VM while passing invalid input to NewStringUTF
     @AsbSecurityTest(cveBugId = 172655291)
+    @Test
     public void testPocCVE_2021_0394() throws Exception {
-        assertFalse(poc());
+        VMRuntime vmRuntime = VMRuntime.getRuntime();
+        if (!vmRuntime.isCheckJniEnabled()) {
+            assertFalse(poc());
+        }
     }
 
     public static native boolean poc();
