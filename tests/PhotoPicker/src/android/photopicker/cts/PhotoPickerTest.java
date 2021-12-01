c2bbe7bc22cf784a28c3922231c76ddcb186d96b
Sahana Rao <sahanas@google.com>
2021-12-01 05:07:18 +0000

    Change testMultiSelect_longPress to preview video
    This assists in adding CTS coverage for video preview.
    
    Bug: 197083539
    Test: CtsPhotoPickerTest#testMultiSelect_longPress
    Change-Id: I6afd7ffcc766fdf5b56d0fb2c2862d24358dc529

--- tests/PhotoPicker/src/android/photopicker/cts/PhotoPickerTest.java
+++ tests/PhotoPicker/src/android/photopicker/cts/PhotoPickerTest.java
@@ -213,16 +213,17 @@ public class PhotoPickerTest extends PhotoPickerBaseTest {
 
     @Test
     public void testMultiSelect_longPress() throws Exception {
-        final int imageCount = 3;
-        createImages(imageCount, mContext.getUserId(), mUriList);
+        final int videoCount = 3;
+        createVideos(videoCount, mContext.getUserId(), mUriList);
         final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
         // TODO(b/205291616): Replace 100 with MediaStore.getPickImagesMaxLimit()
         intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, 100);
+        intent.setType("video/*");
         mActivity.startActivityForResult(intent, REQUEST_CODE);
 
-        final List<UiObject> itemList = findItemList(imageCount);
+        final List<UiObject> itemList = findItemList(videoCount);
         final int itemCount = itemList.size();
-        assertThat(itemCount).isEqualTo(imageCount);
+        assertThat(itemCount).isEqualTo(videoCount);
 
         // Select one item from Photo grid
         itemList.get(0).click();
