550c7203784c1bd7bccf5d74da1c1c098f5ac103
Paul Duffin <paulduffin@google.com>
2021-12-14 16:44:09 +0000

    Add DynamicConfig support for CtsHiddenApiBlacklistApiTestCases
    As all the hidden API tests verify the accessibility of the same set of
    APIs if an API that was previously inaccessible becomes accessible then
    it would likely break all the tests. Therefore, this change adds one
    set of dynamic config called CtsHiddenApiBlocklistApiTestCases.dynamic
    and uses it for all the CtsHiddenApiBlacklist*TestCases.
    
    Due to limitations in the build only one of the tests that use it can
    reference the file in the 'data' property. That test is
    CtsHiddenApiBlacklistCurrentApiTestCases. However, as all of the tests
    use it from the same location it still works. The only issue is that
    changes to the file will only have an effect when the
    CtsHiddenApiBlacklistCurrentApiTestCases test is built.
    
    Although all the tests use the same data this change follows the same
    pattern as the SignatureTest of having the name of the dynamic config
    provided by an instrumentation-arg so that a future refactoring can
    move the handling of the DynamicConfig into the AbstractApiTest class.
    
    Bug: 204723907
    Test: atest CtsHiddenApiBlacklistApi27TestCases \
                CtsHiddenApiBlacklistApi28TestCases \
                CtsHiddenApiBlacklistCurrentApiTestCases \
                CtsHiddenApiBlacklistDebugClassTestCases
    Change-Id: I058a282b6020883cc2851c6516042446ae932d59

--- tests/signature/api-check/hidden-api-blacklist-debug-class/Android.mk
+++ tests/signature/api-check/hidden-api-blacklist-debug-class/Android.mk
@@ -17,6 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := CtsHiddenApiBlacklistDebugClassTestCases
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
+LOCAL_STATIC_JAVA_LIBRARIES += cts-signature-with-dynamic-config
 LOCAL_SIGNATURE_API_FILES := hiddenapi_flags.csv
 LOCAL_JNI_SHARED_LIBRARIES := libcts_dexchecker
 include $(LOCAL_PATH)/../build_signature_apk.mk
