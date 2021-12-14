b02862eed906667bddaa142e3618354a339e4a9e
Evan Chen <evanxinchen@google.com>
2021-12-14 03:09:57 +0000

    CTS test for multiple devices connected for same application
    Test: atest CtsCompanionDevicesTestCases:SelfPresenceReportingTest
    Fix: 210131950
    Change-Id: Ib9788ef734721972237b092f6df28dd45b02a569

--- tests/tests/companion/src/android/companion/cts/TestBase.kt
+++ tests/tests/companion/src/android/companion/cts/TestBase.kt
@@ -116,7 +116,8 @@ abstract class TestBase {
         val MAC_ADDRESS_A = MacAddress.fromString("00:00:00:00:00:AA")
         val MAC_ADDRESS_B = MacAddress.fromString("00:00:00:00:00:BB")
         val MAC_ADDRESS_C = MacAddress.fromString("00:00:00:00:00:CC")
-
+        val DEVICE_DISPLAY_NAME_A = "Device A"
+        val DEVICE_DISPLAY_NAME_B = "Device B"
         val SIMPLE_EXECUTOR: Executor = Executor { it.run() }
     }
 
