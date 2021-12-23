279d43787036700884053c2c50df8de3256c8991
Michał Brzeziński <brzezinski@google.com>
2021-12-23 12:36:41 +0000

    Marking CompatChangeTests as flaky
    Bug: 211853730
    Change-Id: I3ec286e72cbb21506dcd929c013e99ef02592979
    Test: presubmit

--- tests/framework/base/windowmanager/src/android/server/wm/CompatChangeTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/CompatChangeTests.java
@@ -47,6 +47,7 @@ import android.server.wm.WindowManagerTestBase.FocusableActivity;
 import android.util.Size;
 
 import androidx.annotation.Nullable;
+import androidx.test.filters.FlakyTest;
 
 import libcore.junit.util.compat.CoreCompatChangeRule.DisableCompatChanges;
 import libcore.junit.util.compat.CoreCompatChangeRule.EnableCompatChanges;
@@ -71,6 +72,7 @@ import org.junit.rules.TestRule;
  * atest CtsWindowManagerDeviceTestCases:CompatChangeTests
  */
 @Presubmit
+@FlakyTest(bugId = 211853730)
 public final class CompatChangeTests extends MultiDisplayTestBase {
     private static final ComponentName RESIZEABLE_PORTRAIT_ACTIVITY =
             component(ResizeablePortraitActivity.class);
