e33b915f8d633c20d304dbfedf7bfd9c303dc21a
Songchun Fan <schfan@google.com>
2022-01-05 13:31:45 -0800

    [cts] fix loading progress callback test for Auto
    Loading progress listeners are only registered when there are already at
    least one LauncherApps callback registered in LauncherApps service. This
    requirement is not met on some Android Auto devices. As a result, the
    loading progress tests are failing on those devices.
    
    This tests adds a helper app that registers a LauncherApps callback in
    LauncherApps service, before the test app is installed.
    
    BUG: 208176901
    Test: atest com.android.tests.loadingprogress.host.IncrementalLoadingProgressTest
    Change-Id: I0fb87ac281242a7f1c3a36abed2be35ea25b704b
    Merged-In: I0fb87ac281242a7f1c3a36abed2be35ea25b704b
    (cherry picked from commit 91a931ee2f0001e22961a15394cdd86220d3871d)

--- hostsidetests/packagemanager/installedloadingprogess/hostside/src/com/android/tests/loadingprogress/host/IncrementalLoadingProgressTest.java
+++ hostsidetests/packagemanager/installedloadingprogess/hostside/src/com/android/tests/loadingprogress/host/IncrementalLoadingProgressTest.java
@@ -67,6 +67,11 @@ public class IncrementalLoadingProgressTest extends BaseHostJUnit4Test {
         assumeTrue("true\n".equals(getDevice().executeShellCommand(
                 "pm has-feature android.software.incremental_delivery")));
         getDevice().uninstallPackage(TEST_APP_PACKAGE_NAME);
+        // Before the test app is installed, launch a helper app to register a LauncherApps callback
+        // This ensures the loading progress listener is activated when the test app is installed
+        assertTrue(runDeviceTests(DEVICE_TEST_PACKAGE_NAME, TEST_CLASS_NAME,
+                "registerFirstLauncherAppsCallback"));
+
         CompatibilityBuildHelper buildHelper = new CompatibilityBuildHelper(getBuild());
         final File base_apk = buildHelper.getTestFile(TEST_APK);
         assertNotNull(base_apk);
