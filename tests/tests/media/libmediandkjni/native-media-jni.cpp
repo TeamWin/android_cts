3441708168069c5a8fc8c693018b4972ec5fc73b
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2022-01-06 15:07:50 -0800

    Move muxer tests from NativeDecoderTest.java to CtsMediaMuxerTestCases
    NativeDecoderTest.java in CtsMediaTestCases had some muxer tests and
    these are now moved to CtsMediaMuxerTestCases
    
    Bug: 191504968
    Test: atest CtsMediaMuxerTestCases
    Test: atest CtsMediaTestCases:NativeDecoderTest
    
    Change-Id: Ie2f41995c8ed8709ed485f32e0691b25052d3222

--- tests/tests/media/libmediandkjni/native-media-jni.cpp
+++ tests/tests/media/libmediandkjni/native-media-jni.cpp
@@ -762,69 +762,6 @@ extern "C" jboolean Java_android_media_cts_NativeDecoderTest_testPlaybackNative(
     return true;
 }
 
-extern "C" jboolean Java_android_media_cts_NativeDecoderTest_testMuxerNative(JNIEnv */*env*/,
-        jclass /*clazz*/, int infd, jlong inoffset, jlong insize, int outfd, jboolean webm) {
-
-
-    AMediaMuxer *muxer = AMediaMuxer_new(outfd,
-            webm ? AMEDIAMUXER_OUTPUT_FORMAT_WEBM : AMEDIAMUXER_OUTPUT_FORMAT_MPEG_4);
-
-    AMediaExtractor *ex = AMediaExtractor_new();
-    int err = AMediaExtractor_setDataSourceFd(ex, infd, inoffset, insize);
-    if (err != 0) {
-        ALOGE("setDataSource error: %d", err);
-        return false;
-    }
-
-    int numtracks = AMediaExtractor_getTrackCount(ex);
-    ALOGI("input tracks: %d", numtracks);
-    for (int i = 0; i < numtracks; i++) {
-        AMediaFormat *format = AMediaExtractor_getTrackFormat(ex, i);
-        const char *s = AMediaFormat_toString(format);
-        ALOGI("track %d format: %s", i, s);
-        const char *mime;
-        if (!AMediaFormat_getString(format, AMEDIAFORMAT_KEY_MIME, &mime)) {
-            ALOGE("no mime type");
-            return false;
-        } else if (!strncmp(mime, "audio/", 6) || !strncmp(mime, "video/", 6)) {
-            ssize_t tidx = AMediaMuxer_addTrack(muxer, format);
-            ALOGI("track %d -> %zd format %s", i, tidx, s);
-            AMediaExtractor_selectTrack(ex, i);
-        } else {
-            ALOGE("expected audio or video mime type, got %s", mime);
-            return false;
-        }
-        AMediaFormat_delete(format);
-        AMediaExtractor_selectTrack(ex, i);
-    }
-    AMediaMuxer_start(muxer);
-
-    int bufsize = 1024*1024;
-    uint8_t *buf = new uint8_t[bufsize];
-    AMediaCodecBufferInfo info;
-    while(true) {
-        int n = AMediaExtractor_readSampleData(ex, buf, bufsize);
-        if (n < 0) {
-            break;
-        }
-        info.offset = 0;
-        info.size = n;
-        info.presentationTimeUs = AMediaExtractor_getSampleTime(ex);
-        info.flags = AMediaExtractor_getSampleFlags(ex);
-
-        size_t idx = (size_t) AMediaExtractor_getSampleTrackIndex(ex);
-        AMediaMuxer_writeSampleData(muxer, idx, buf, &info);
-
-        AMediaExtractor_advance(ex);
-    }
-
-    AMediaExtractor_delete(ex);
-    AMediaMuxer_stop(muxer);
-    AMediaMuxer_delete(muxer);
-    return true;
-
-}
-
 extern "C" jboolean Java_android_media_cts_NativeDecoderTest_testFormatNative(JNIEnv * /*env*/,
         jclass /*clazz*/) {
     AMediaFormat* format = AMediaFormat_new();
