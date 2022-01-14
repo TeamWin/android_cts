b9613a6ff8612f6a75122121647c8dcb3a493937
Jay Thomas Sullivan <jaysullivan@google.com>
2022-01-14 14:21:16 -0800

    Fix PermissionPolicyTest and SplitPermissionsSystemTest
    PermissionPolicyTest expects all permissions defined in
    the AndroidManifest.xml (of frameworks/base) to also be defined in
    android_manifest.xml (of cts/tests/tests/permission2).
    If the files are out of sync, this test will fail. So, update
    android_manifest.xml
    
    SplitPermissionsSystemTest has a hard-coded expected list of
    split-permissions to be defined in platform.xml, so update this
    expected list.
    
    Bug: 201318587
    Test: atest PermissionPolicyTest PermissionControllerHostTest DefaultPermissionGrantPolicyTest SplitPermissionsSystemTest
    Change-Id: I8d0ae7a8ba542746c6f430edd0edd8d4e6de9bcc

--- tests/tests/permission/src/android/permission/cts/SplitPermissionsSystemTest.java
+++ tests/tests/permission/src/android/permission/cts/SplitPermissionsSystemTest.java
@@ -29,6 +29,9 @@ import static android.Manifest.permission.BODY_SENSORS_BACKGROUND;
 import static android.Manifest.permission.READ_CALL_LOG;
 import static android.Manifest.permission.READ_CONTACTS;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_MEDIA_AUDIO;
+import static android.Manifest.permission.READ_MEDIA_IMAGE;
+import static android.Manifest.permission.READ_MEDIA_VIDEO;
 import static android.Manifest.permission.READ_PHONE_STATE;
 import static android.Manifest.permission.READ_PRIVILEGED_PHONE_STATE;
 import static android.Manifest.permission.WRITE_CALL_LOG;
@@ -114,7 +117,15 @@ public class SplitPermissionsSystemTest {
                     assertSplit(split, Build.VERSION_CODES.Q, ACCESS_BACKGROUND_LOCATION);
                     break;
                 case READ_EXTERNAL_STORAGE:
-                    assertSplit(split, Build.VERSION_CODES.Q, ACCESS_MEDIA_LOCATION);
+                    if (newPermissions.contains(ACCESS_MEDIA_LOCATION)) {
+                        assertSplit(split, Build.VERSION_CODES.Q, ACCESS_MEDIA_LOCATION);
+                    } else if (newPermissions.contains(READ_MEDIA_AUDIO)) {
+                        assertSplit(split, Build.VERSION_CODES.S_V2 + 1, READ_MEDIA_AUDIO);
+                    } else if (newPermissions.contains(READ_MEDIA_VIDEO)) {
+                        assertSplit(split, Build.VERSION_CODES.S_V2 + 1, READ_MEDIA_VIDEO);
+                    } else if (newPermissions.contains(READ_MEDIA_IMAGE)) {
+                        assertSplit(split, Build.VERSION_CODES.S_V2 + 1, READ_MEDIA_IMAGE);
+                    }
                     break;
                 case READ_PRIVILEGED_PHONE_STATE:
                     assertSplit(split, NO_TARGET, READ_PHONE_STATE);
@@ -133,7 +144,7 @@ public class SplitPermissionsSystemTest {
             }
         }
 
-        assertEquals(14, seenSplits.size());
+        assertEquals(17, seenSplits.size());
     }
 
     private void assertSplit(SplitPermissionInfo split, int targetSdk, String... permission) {
