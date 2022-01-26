da24b8c904d860acdfb91412bc1c612754b67ffd
Evan Chen <evanxinchen@google.com>
2022-01-26 00:26:56 +0000

    Add cts test for COMPANION_COMPUTER profile
    Add support constant string in Constants.kt
    Ignore the COMPANION_COMPUTER profile for AssociationEndToEndTest
    
    Test: atest CtsCompanionDeviceManagerCoreTestCases
          atest CtsCompanionDeviceManagerUiAutomationTestCases
          atest CtsOsTestCases:CompanionDeviceManagerTest
    
    Bug: 216213933
    Change-Id: I3488c5efd0b42f9aa085c7877491c26405100d25

--- tests/tests/companion/uiautomation/src/android/companion/cts/uiautomation/AssociationEndToEndTest.kt
+++ tests/tests/companion/uiautomation/src/android/companion/cts/uiautomation/AssociationEndToEndTest.kt
@@ -18,6 +18,7 @@ package android.companion.cts.uiautomation
 
 import android.companion.AssociationRequest.DEVICE_PROFILE_APP_STREAMING
 import android.companion.AssociationRequest.DEVICE_PROFILE_AUTOMOTIVE_PROJECTION
+import android.companion.AssociationRequest.DEVICE_PROFILE_COMPUTER
 import android.platform.test.annotations.AppModeFull
 import org.junit.Assume.assumeFalse
 import org.junit.Test
@@ -40,8 +41,9 @@ class AssociationEndToEndTest(
     override fun setUp() {
         super.setUp()
 
-        // TODO(b/211590680): Add support for APP_STREAMING and AUTOMOTIVE_PROJECTION in the
-        // confirmation UI (the "multiple devices" flow variant).
+        // TODO(b/211590680): Add support for APP_STREAMING, AUTOMOTIVE_PROJECTION and COMPUTER
+        // in the confirmation UI (the "multiple devices" flow variant).
+        assumeFalse(profile == DEVICE_PROFILE_COMPUTER)
         assumeFalse(profile == DEVICE_PROFILE_APP_STREAMING)
         assumeFalse(profile == DEVICE_PROFILE_AUTOMOTIVE_PROJECTION)
     }
