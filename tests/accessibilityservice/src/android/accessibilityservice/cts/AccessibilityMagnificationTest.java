e912525a449d31158d6bdb1816d2874f6a5c991b
ryanlwlin <ryanlwlin@google.com>
2022-02-10 23:24:45 +0800

    test MagnificationConfig with default values
    We want to test that if the values of the target config is NAN,
    then the actual centerX, centerY and scale will be consistent with
    the former config.
    
    Bug: 218792845
    Test: atest AccessibilityMagnificationTest
    Change-Id: I206e8c26687c790d94031b3d10b69a846f5cbc3f

--- tests/accessibilityservice/src/android/accessibilityservice/cts/AccessibilityMagnificationTest.java
+++ tests/accessibilityservice/src/android/accessibilityservice/cts/AccessibilityMagnificationTest.java
@@ -19,6 +19,7 @@ package android.accessibilityservice.cts;
 import static android.accessibilityservice.MagnificationConfig.MAGNIFICATION_MODE_FULLSCREEN;
 import static android.accessibilityservice.MagnificationConfig.MAGNIFICATION_MODE_WINDOW;
 import static android.accessibilityservice.cts.utils.ActivityLaunchUtils.launchActivityAndWaitForItToBeOnscreen;
+import static android.content.pm.PackageManager.FEATURE_WINDOW_MAGNIFICATION;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -44,6 +45,7 @@ import android.accessibilityservice.cts.activities.AccessibilityWindowQueryActiv
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.app.UiAutomation;
+import android.content.Context;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.Region;
@@ -63,6 +65,7 @@ import androidx.test.runner.AndroidJUnit4;
 import com.android.compatibility.common.util.TestUtils;
 
 import org.junit.AfterClass;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -229,6 +232,49 @@ public class AccessibilityMagnificationTest {
     }
 
     @Test
+    public void testSetFullScreenConfigWithDefaultValues_windowModeEnabled_expectedConfig()
+            throws Exception {
+        final boolean windowModeSupported = isWindowModeSupported(mInstrumentation.getContext());
+        Assume.assumeTrue("window mode is not available", windowModeSupported);
+
+        final MagnificationController controller = mService.getMagnificationController();
+        final WindowManager windowManager = mInstrumentation.getContext().getSystemService(
+                WindowManager.class);
+        final float scale = 3.0f;
+        final float x = windowManager.getCurrentWindowMetrics().getBounds().centerX();
+        final float y = windowManager.getCurrentWindowMetrics().getBounds().centerY();
+        final AtomicBoolean setConfig = new AtomicBoolean();
+
+        final MagnificationConfig config = new MagnificationConfig.Builder()
+                .setMode(MAGNIFICATION_MODE_WINDOW)
+                .setScale(scale)
+                .setCenterX(x)
+                .setCenterY(y).build();
+
+        mService.runOnServiceSync(
+                () -> setConfig.set(controller.setMagnificationConfig(config, false)));
+        waitUntilMagnificationConfig(controller, config);
+
+        assertTrue("Failed to set config", setConfig.get());
+        assertConfigEquals(config, controller.getMagnificationConfig());
+
+        final MagnificationConfig newConfig = new MagnificationConfig.Builder()
+                .setMode(MAGNIFICATION_MODE_FULLSCREEN)
+                .build();
+        mService.runOnServiceSync(
+                () -> setConfig.set(controller.setMagnificationConfig(newConfig, false)));
+        final MagnificationConfig expectedConfig = new MagnificationConfig.Builder()
+                .setMode(MAGNIFICATION_MODE_FULLSCREEN)
+                .setScale(scale)
+                .setCenterX(x)
+                .setCenterY(y)
+                .build();
+        waitUntilMagnificationConfig(controller, expectedConfig);
+        assertTrue("Failed to set config", setConfig.get());
+        assertConfigEquals(expectedConfig, controller.getMagnificationConfig());
+    }
+
+    @Test
     public void testSetMagnificationConfig_legacyApiExpectedResult() {
         final MagnificationController controller = mService.getMagnificationController();
         final Region region = controller.getMagnificationRegion();
@@ -251,11 +297,7 @@ public class AccessibilityMagnificationTest {
             assertEquals("Failed to apply center X", x, controller.getCenterX(), 5.0f);
             assertEquals("Failed to apply center Y", y, controller.getCenterY(), 5.0f);
         } finally {
-            final MagnificationConfig resetConfig = new MagnificationConfig.Builder()
-                    .setScale(1).build();
-            mService.runOnServiceSync(() -> {
-                controller.setMagnificationConfig(resetConfig, false);
-            });
+            mService.runOnServiceSync(() -> controller.resetCurrentMagnification(false));
         }
     }
 
@@ -378,9 +420,7 @@ public class AccessibilityMagnificationTest {
                 .setCenterY(y).build();
 
         try {
-            mService.runOnServiceSync(() -> {
-                controller.setMagnificationConfig(config, false);
-            });
+            mService.runOnServiceSync(() -> controller.setMagnificationConfig(config, false));
             waitUntilMagnificationConfig(controller, config);
 
             final ArgumentCaptor<MagnificationConfig> configCaptor = ArgumentCaptor.forClass(
@@ -408,10 +448,8 @@ public class AccessibilityMagnificationTest {
                     eq(controller), any(Region.class), configCaptor.capture());
             assertConfigEquals(fullscreenConfig, configCaptor.getValue());
         } finally {
-            final MagnificationConfig resetConfig = new MagnificationConfig.Builder()
-                    .setScale(1).build();
             mService.runOnServiceSync(() -> {
-                controller.setMagnificationConfig(resetConfig, false);
+                controller.resetCurrentMagnification(false);
                 controller.removeListener(listener);
             });
         }
@@ -448,10 +486,8 @@ public class AccessibilityMagnificationTest {
             verify(listener, timeout(LISTENER_ANIMATION_TIMEOUT_MILLIS)).onMagnificationChanged(
                     eq(controller), any(Region.class), any(MagnificationConfig.class));
         } finally {
-            final MagnificationConfig resetConfig = new MagnificationConfig.Builder()
-                    .setScale(1).build();
             mService.runOnServiceSync(() -> {
-                controller.setMagnificationConfig(resetConfig, false);
+                controller.resetCurrentMagnification(false);
                 controller.removeListener(listener);
             });
         }
@@ -795,11 +831,11 @@ public class AccessibilityMagnificationTest {
                 "Failed to apply the config. expected: " + config + " , actual: "
                         + controller.getMagnificationConfig(), 5,
                 () -> {
-                    final MagnificationConfig tmpConfig = controller.getMagnificationConfig();
-                    return tmpConfig.getMode() == config.getMode()
-                            && tmpConfig.getScale() == config.getScale()
-                            && tmpConfig.getCenterX() == config.getCenterX()
-                            && tmpConfig.getCenterY() == config.getCenterY();
+                    final MagnificationConfig actualConfig = controller.getMagnificationConfig();
+                    return actualConfig.getMode() == config.getMode()
+                            && Float.compare(actualConfig.getScale(), config.getScale()) == 0
+                            && Float.compare(actualConfig.getCenterX(), config.getCenterX()) == 0
+                            && Float.compare(actualConfig.getCenterY(), config.getCenterY()) == 0;
                 });
     }
 
@@ -812,5 +848,9 @@ public class AccessibilityMagnificationTest {
                 result.getCenterX(), 5.0f);
         assertEquals("Failed to apply center Y", expected.getCenterY(),
                 result.getCenterY(), 5.0f);
+    }
+
+    private static boolean isWindowModeSupported(Context context) {
+        return context.getPackageManager().hasSystemFeature(FEATURE_WINDOW_MAGNIFICATION);
     }
 }
