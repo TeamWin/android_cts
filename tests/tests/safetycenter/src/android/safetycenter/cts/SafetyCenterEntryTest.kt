47c7ce3803f5ee501e895fe2ab738c89139a898d
Tyler Dewey <deweytyl@google.com>
2022-02-14 21:01:00 +0000

    Stop testing SafetyCenter APIs on non-T builds.
    Adds the previously-missing @SdkSuppress annotation to these T-only API
    tests.
    
    Bug: 219382521
    Test: atest CtsSafetyCenterTestCases
    Change-Id: I0a571c93aeb7458c8e94d81e8cedad6b6f3c680d

--- tests/tests/safetycenter/src/android/safetycenter/cts/SafetyCenterEntryTest.kt
+++ tests/tests/safetycenter/src/android/safetycenter/cts/SafetyCenterEntryTest.kt
@@ -19,15 +19,18 @@ package android.safetycenter.cts
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build.VERSION_CODES.TIRAMISU
 import android.os.Parcel
 import android.safetycenter.SafetyCenterEntry
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = TIRAMISU, codeName = "Tiramisu")
 class SafetyCenterEntryTest {
     private val context: Context = ApplicationProvider.getApplicationContext()
 
--- tests/tests/safetycenter/src/android/safetycenter/cts/SafetyCenterStaticEntryGroupTest.kt
+++ tests/tests/safetycenter/src/android/safetycenter/cts/SafetyCenterStaticEntryGroupTest.kt
@@ -19,16 +19,19 @@ package android.safetycenter.cts
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build.VERSION_CODES.TIRAMISU
 import android.os.Parcel
 import android.safetycenter.SafetyCenterStaticEntry
 import android.safetycenter.SafetyCenterStaticEntryGroup
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = TIRAMISU, codeName = "Tiramisu")
 class SafetyCenterStaticEntryGroupTest {
     private val context: Context = ApplicationProvider.getApplicationContext()
 
