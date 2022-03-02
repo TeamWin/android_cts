b41eda65826d44bbbaa79ed028146ee016b01226
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2022-03-01 16:55:33 -0800

    CtsMedia: Rename CtsMediaDrmTestCases as CtsMediaDrmFrameworkTestCases
    - Moved all the files from cts/tests/tests/media/drm to
      cts/tests/tests/media/drmframework
    
    - Changed the package name from android.media.drm.cts to
      android.media.drmframework.cts
    
    - Used CtsMediaDrmFrameworkTestCases-1.0.zip which contains only the
      files used in drmframework tests instead of CtsMediaTestCases-1.4.zip
    
    - Fixed media-folder-name in AndroidTest.xml to CtsMediaDrmFrameworkTestCases-1.0
    
    - Renamed methods in native-mediadrm-jni to remove redundant prefix
    
    Bug: 217375896
    Test: atest CtsMediaDrmFrameworkTestCases
    Merged-In: Ifb43c85a7d9fa0df52a7b5fd3fa2a44d0f929534
    Change-Id: If2d2b07384d4ab91c474032d2014a491a940602d

--- tests/tests/media/drmframework/src/android/media/drmframework/cts/MediaPlayerDrmTestBase.java
+++ tests/tests/media/drmframework/src/android/media/drmframework/cts/MediaPlayerDrmTestBase.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package android.media.drm.cts;
+package android.media.drmframework.cts;
 
 import android.app.DownloadManager;
 import android.app.DownloadManager.Request;
