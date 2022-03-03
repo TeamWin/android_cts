cec4bfb8532a9f74d720e7a6ac484266d5cb0135
Megha Sharma <megha.sharma@ittiam.com>
2022-03-03 11:45:49 +0000

    [RESTRICT AUTOMERGE] Updated CTS test for Android Security b/112161557
    Bug: 112161557
    Bug: 121266552
    Test: Ran the new testcase on android-10.0.0_r45 with/without patch
    
    Change-Id: I70c1f5428d67693a5b2d9b1c5f7d572f5902e834

--- hostsidetests/securitybulletin/src/android/security/cts/CVE_2018_9558.java
+++ hostsidetests/securitybulletin/src/android/security/cts/CVE_2018_9558.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,19 @@
  * limitations under the License.
  */
 
+
 package android.security.cts;
 
 import android.platform.test.annotations.AsbSecurityTest;
+
+import com.android.compatibility.common.util.CrashUtils;
+import com.android.compatibility.common.util.CrashUtils.Config.BacktraceFilterPattern;
 import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
-import org.junit.Test;
+
+import java.util.regex.Pattern;
+
 import org.junit.runner.RunWith;
+import org.junit.Test;
 
 @RunWith(DeviceJUnit4ClassRunner.class)
 public class CVE_2018_9558 extends SecurityTestCase {
@@ -27,6 +34,8 @@ public class CVE_2018_9558 extends SecurityTestCase {
     /**
      * b/112161557
      * Vulnerability Behaviour: EXIT_VULNERABLE (113)
+     * Vulnerable Library: libnfc-nci (As per AOSP code)
+     * Vulnerable Function: rw_t2t_handle_tlv_detect_rsp (As per AOSP code)
      */
     @Test
     @AsbSecurityTest(cveBugId = 112161557)
@@ -34,7 +43,13 @@ public class CVE_2018_9558 extends SecurityTestCase {
         AdbUtils.assumeHasNfc(getDevice());
         assumeIsSupportedNfcDevice(getDevice());
         pocPusher.only64();
-        AdbUtils.runPocAssertExitStatusNotVulnerable("CVE-2018-9558", getDevice(),
-                AdbUtils.TIMEOUT_SEC);
+        String binaryName = "CVE-2018-9558";
+        AdbUtils.pocConfig testConfig = new AdbUtils.pocConfig(binaryName, getDevice());
+        testConfig.config = new CrashUtils.Config().setProcessPatterns(Pattern.compile(binaryName))
+                .setBacktraceIncludes(new BacktraceFilterPattern("libnfc-nci",
+                        "rw_t2t_handle_tlv_detect_rsp"));
+        testConfig.config
+                .setBacktraceExcludes(new BacktraceFilterPattern("libdl", "__cfi_slowpath"));
+        AdbUtils.runPocAssertNoCrashesNotVulnerable(testConfig);
     }
 }
