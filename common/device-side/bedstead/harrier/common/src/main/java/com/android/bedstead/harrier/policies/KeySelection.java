f718fb0a7f7d514c0d4ff94b496f6a800ebafa70
Jonathan Scott <scottjonathan@google.com>
2022-03-17 14:51:51 +0000

    Fix Key Management tests when delegating.
    We needed to split the KeyManagement policy because some are about
    installing keys and some are about selecting already installed keys.
    They delegate differently.
    
    Also added `dpcOnly()` which acts like `dpc()` except when we are
    testing a delegate - in which case `dpc()` returns the delegate (so in
    most cases a test doesn't need to be modified for the delegate) but
    `dpcOnly()` returns the actual DPC. This is needed because the delegate
    doesn't have permission to install the cert but needs it to be
    installed. I don't love this, I think the API is confusing. We should
    come up with a better pattern for this.
    
    Fixes: 225033269
    Test: atest android.devicepolicy.cts.KeyManagementTest
    Change-Id: I6097bb566e29b1feeed9b91f46876f0081aad559

--- common/device-side/bedstead/harrier/common/src/main/java/com/android/bedstead/harrier/policies/KeySelection.java
+++ common/device-side/bedstead/harrier/common/src/main/java/com/android/bedstead/harrier/policies/KeySelection.java
@@ -20,19 +20,18 @@ import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePoli
 import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.APPLIED_BY_PROFILE_OWNER;
 import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.APPLIES_TO_OWN_USER;
 import static com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy.CAN_BE_DELEGATED;
-import static com.android.bedstead.nene.devicepolicy.CommonDevicePolicy.DELEGATION_CERT_INSTALL;
+import static com.android.bedstead.nene.devicepolicy.CommonDevicePolicy.DELEGATION_CERT_SELECTION;
 
 import com.android.bedstead.harrier.annotations.enterprise.EnterprisePolicy;
 
 /**
- * Policies around key management
+ * Policies around key selection
  *
  * <p>This is used by methods such as
- * {@code DevicePolicyManager#installKeyPair(ComponentName, PrivateKey, Certificate, String)} and
- * {@code DevicePolicyManager#removeKeyPair(ComponentName, String)}.
+ * {@code DevicePolicyManager#getKeyPairGrants}
  */
 @EnterprisePolicy(dpc = {APPLIED_BY_DEVICE_OWNER | APPLIES_TO_OWN_USER,
         APPLIED_BY_PROFILE_OWNER | APPLIES_TO_OWN_USER | CAN_BE_DELEGATED},
-        delegatedScopes = DELEGATION_CERT_INSTALL)
-public final class KeyManagement {
+        delegatedScopes = DELEGATION_CERT_SELECTION)
+public final class KeySelection {
 }
