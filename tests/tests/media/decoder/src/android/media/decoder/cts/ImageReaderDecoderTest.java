f14633f0d5636451e058c967630b419eb3f2203f
My Name <wonsik@google.com>
2022-03-30 16:05:57 -0700

    ImageReaderDecoderTest: fix yuv dump file name
    Bug: 210947256
    Test: atest android.media.decoder.cts.ImageReaderDecoderTest
    Change-Id: Ia7c058d08762ab2dfe8e9ab4819dadbfefd814bb

--- tests/tests/media/decoder/src/android/media/decoder/cts/ImageReaderDecoderTest.java
+++ tests/tests/media/decoder/src/android/media/decoder/cts/ImageReaderDecoderTest.java
@@ -111,6 +111,7 @@ public class ImageReaderDecoderTest {
     public String mCodecName;
     public MediaAsset mMediaAsset;
     public int mMode;
+    public String mTestId;
     MediaCodec mDecoder = null;
     MediaExtractor mExtractor = null;
 
@@ -120,6 +121,7 @@ public class ImageReaderDecoderTest {
         mCodecName = codecName;
         mMediaAsset = asset;
         mMode = mode;
+        mTestId = testId;
     }
 
     @Parameterized.Parameters(name = "{index}({0}_{1}_{4})")
@@ -500,9 +502,7 @@ public class ImageReaderDecoderTest {
 
                 if (doRender) {
                     outputFrameCount++;
-                    String fileName = DEBUG_FILE_NAME_BASE + MediaUtils.getTestName()
-                            + (mode == MODE_IMAGE ? "_image_" : "_reader_")
-                            + width + "x" + height + "_" + outputFrameCount + ".yuv";
+                    String fileName = DEBUG_FILE_NAME_BASE + mCodecName + "_" + mTestId + ".yuv";
 
                     Image image = null;
                     try {
