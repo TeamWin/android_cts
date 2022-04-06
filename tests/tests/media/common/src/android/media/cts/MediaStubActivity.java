415c20324347c33a647896246b5ef3a29ac14a45
Guillaume Chelfi <chelfi@google.com>
2022-04-06 13:27:54 +0200

    Add CTS tests to enforce PUSH_BLANK_BUFFERS_ON_STOP
    These CTS tests configure a MediaCodec with the
    PUSH_BLANK_BUFFERS_ON_STOP, decode some frames, stop the playback, and
    ensure that a blank frames is drawed on the surface on stop.
    PUSH_BLANK_BUFFERS_ON_STOP has long been implemented in OMX based
    codecs, but it was missing until recently on Codec2 based codecs.
    
    Test: atest DecoderPushBlankBuffersOnStopTest#testPushBlankBuffersOnStopAvc
    Bug: 210587592
    Change-Id: Id586b3c86d0ba0fec2b450e5bc693b9f247b18bb

--- tests/tests/media/common/src/android/media/cts/MediaStubActivity.java
+++ tests/tests/media/common/src/android/media/cts/MediaStubActivity.java
@@ -15,25 +15,41 @@
  */
 package android.media.cts;
 
-import android.media.cts.R;
-
 import android.app.Activity;
+import android.content.Intent;
+import android.media.cts.R;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
+
+import androidx.test.filters.SdkSuppress;
+
+import com.android.compatibility.common.util.ApiLevelUtil;
 
 public class MediaStubActivity extends Activity {
     private static final String TAG = "MediaStubActivity";
     private SurfaceHolder mHolder;
     private SurfaceHolder mHolder2;
 
+    public static final String INTENT_EXTRA_NO_TITLE = "NoTitle";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        if (ApiLevelUtil.isAtLeast(Build.VERSION_CODES.R)) {
+            Intent intent = getIntent();
+            if (intent.getBooleanExtra(INTENT_EXTRA_NO_TITLE, false)) {
+                hideTitle();
+            }
+        }
         setTurnScreenOn(true);
         setShowWhenLocked(true);
 
@@ -64,4 +80,26 @@ public class MediaStubActivity extends Activity {
     public SurfaceHolder getSurfaceHolder2() {
         return mHolder2;
     }
+
+    /** Note: Must be called from the thread used to create this activity. */
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.R)
+    public void hideSystemBars() {
+        var surfaceV = (SurfaceView)findViewById(R.id.surface);
+        WindowInsetsController windowInsetsController = surfaceV.getWindowInsetsController();
+        if (windowInsetsController == null) {
+            return;
+        }
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.setSystemBarsBehavior(
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsets.Type.systemBars());
+    }
+
+    /** Note: Must be called before {@code setContentView}. */
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.R)
+    private void hideTitle() {
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+    }
+
 }
