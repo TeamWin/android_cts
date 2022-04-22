98a563fa941f8af505044e4faa41fb47e305b564
Paul Duffin <paulduffin@google.com>
2022-04-22 16:39:29 +0100

    Parameterize the SignatureMultiLibsTest tests
    Creates a test for each shared library that the test can test. If a
    device does not support a specific library then the library specific
    test will fail with an assumption failed result.
    
    Bug: 228322252
    Test: atest CtsSharedLibsApiSignatureTestCases
          - removed javax.obex from the set of available shared libraries
            and verified that the tests reported ASSUMPTION_FAILED
    Merged-In: I5522091d1413638f1efc36b7137b07bd6f39968d
    Change-Id: I5522091d1413638f1efc36b7137b07bd6f39968d
    (cherry picked from commit 7962c461bd83209d3d8ad6752d189d22ecb424fc)

--- tests/signature/api-check/shared-libs-api/src/android/signature/cts/api/SignatureMultiLibsTest.java
+++ tests/signature/api-check/shared-libs-api/src/android/signature/cts/api/SignatureMultiLibsTest.java
@@ -26,12 +26,24 @@ import android.signature.cts.VirtualPath;
 import android.util.Log;
 import androidx.test.platform.app.InstrumentationRegistry;
 import com.google.common.base.Suppliers;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
+import org.junit.Assume;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 /**
  * Verifies that any shared library provided by this device and for which this test has a
@@ -41,6 +53,8 @@ import org.junit.Test;
  * {@code <uses-library>} entry for every shared library that provides an API that is contained
  * within the shared-libs-all.api.zip supplied to this test.
  */
+@RunWith(JUnitParamsRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SignatureMultiLibsTest extends AbstractSignatureTest {
 
     protected static final Supplier<String[]> EXPECTED_API_FILES =
@@ -56,6 +70,8 @@ public class SignatureMultiLibsTest extends AbstractSignatureTest {
      */
     protected static final Supplier<Set<String>> AVAILABLE_SHARED_LIBRARIES =
             Suppliers.memoize(SignatureMultiLibsTest::retrieveActiveSharedLibraries)::get;
+
+    private static final String SHARED_LIBRARY_LIST_RESOURCE_NAME = "shared-libs-names.txt";
 
     /**
      * Retrieve the names of the shared libraries that are active on the device.
@@ -80,27 +96,97 @@ public class SignatureMultiLibsTest extends AbstractSignatureTest {
     }
 
     /**
+     * A memoized supplier of the list of shared libraries that this can test.
+     */
+    protected static final Supplier<List<String>> TESTABLE_SHARED_LIBRARIES =
+            Suppliers.memoize(SignatureMultiLibsTest::retrieveTestableSharedLibraries)::get;
+
+    /**
+     * Retrieve the names of the shared libraries that are testable by this test.
+     *
+     * @return The set of shared library names.
+     */
+    private static List<String> retrieveTestableSharedLibraries() {
+        ClassLoader classLoader = SignatureMultiLibsTest.class.getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(SHARED_LIBRARY_LIST_RESOURCE_NAME)) {
+            if (is == null) {
+                throw new RuntimeException(
+                        "Resource " + SHARED_LIBRARY_LIST_RESOURCE_NAME + " could not be found");
+            }
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                return reader.lines()
+                        .filter(line -> !line.isEmpty())
+                        .sorted()
+                        .collect(Collectors.toList());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not retrieve testable shared libraries", e);
+        }
+    }
+
+    /**
+     * Convert the list of testable shared libraries into a form suitable for parameterizing a test.
+     */
+    public Object[][] getTestableSharedLibraryParameters() {
+        List<String> libraries = TESTABLE_SHARED_LIBRARIES.get();
+        Object[][] params = new Object[libraries.size()][1];
+        for (int i = 0; i < libraries.size(); i++) {
+            String name = libraries.get(i);
+            TestableLibraryParameter param = new TestableLibraryParameter(name);
+            params[i][0] = param;
+        }
+        return params;
+    }
+
+    /**
+     * Skips the test if the supplied library is unavailable on the device.
+     *
+     * <p>If the library is unavailable then this throws an
+     * {@link org.junit.AssumptionViolatedException}.</p>
+     *
+     * @param library the name of the library that needs to be available.
+     */
+    private void skipTestIfLibraryIsUnavailable(String library) {
+        Assume.assumeTrue("Shared library " + library + " is not available on this device",
+                AVAILABLE_SHARED_LIBRARIES.get().contains(library));
+    }
+
+    /**
      * Return a stream of {@link JDiffClassDescription} that are expected to be provided by the
      * shared libraries which are installed on this device.
      *
      * @param apiDocumentParser the parser to use.
      * @param apiResources the list of API resource files.
+     * @param library the name of the library whose APIs should be parsed.
      * @return a stream of {@link JDiffClassDescription}.
      */
     private Stream<JDiffClassDescription> parseActiveSharedLibraryApis(
-            ApiDocumentParser apiDocumentParser, String[] apiResources) {
+            ApiDocumentParser apiDocumentParser, String[] apiResources, String library) {
+
         return retrieveApiResourcesAsStream(getClass().getClassLoader(), apiResources)
-                .filter(this::checkLibrary)
+                .filter(path -> {
+                    String apiLibraryName = getLibraryNameFromPath(path);
+                    return apiLibraryName.equals(library);
+                })
                 .flatMap(apiDocumentParser::parseAsStream);
     }
 
     /**
-     * Tests that the device's API matches the expected set defined in xml.
-     * <p/>
-     * Will check the entire API, and then report the complete list of failures
+     * Tests that each shared library's API matches its current API.
+     *
+     * <p>One test per shared library, checks the entire API, and then reports the complete list of
+     * failures.</p>
      */
     @Test
-    public void testRuntimeCompatibilityWithCurrentApi() {
+    // Parameterize this method with the set of testable shared libraries.
+    @Parameters(method = "getTestableSharedLibraryParameters")
+    // The test name is the method name followed by and _ and the shared library name, with .s
+    // replaced with _. e.g. testRuntimeCompatibilityWithCurrentApi_android_test_base.
+    @TestCaseName("{method}_{0}")
+    public void testRuntimeCompatibilityWithCurrentApi(TestableLibraryParameter parameter) {
+        String library = parameter.getName();
+        skipTestIfLibraryIsUnavailable(library);
         runWithTestResultObserver(mResultObserver -> {
             ApiComplianceChecker complianceChecker =
                     new ApiComplianceChecker(mResultObserver, mClassProvider);
@@ -110,7 +196,7 @@ public class SignatureMultiLibsTest extends AbstractSignatureTest {
 
             ApiDocumentParser apiDocumentParser = new ApiDocumentParser(TAG);
 
-            parseActiveSharedLibraryApis(apiDocumentParser, EXPECTED_API_FILES.get())
+            parseActiveSharedLibraryApis(apiDocumentParser, EXPECTED_API_FILES.get(), library)
                     .forEach(complianceChecker::checkSignatureCompliance);
 
             // After done parsing all expected API files, perform any deferred checks.
@@ -119,17 +205,27 @@ public class SignatureMultiLibsTest extends AbstractSignatureTest {
     }
 
     /**
-     * Tests that the device's API matches the previous APIs defined in xml.
+     * Tests that each shared library's API matches its previous APIs.
+     *
+     * <p>One test per shared library, checks the entire API, and then reports the complete list of
+     * failures.</p>
      */
     @Test
-    public void testRuntimeCompatibilityWithPreviousApis() {
+    // Parameterize this method with the set of testable shared libraries.
+    @Parameters(method = "getTestableSharedLibraryParameters")
+    // The test name is the method name followed by and _ and the shared library name, with .s
+    // replaced with _. e.g. testRuntimeCompatibilityWithPreviousApis_android_test_base.
+    @TestCaseName("{method}_{0}")
+    public void testRuntimeCompatibilityWithPreviousApis(TestableLibraryParameter parameter) {
+        String library = parameter.getName();
+        skipTestIfLibraryIsUnavailable(library);
         runWithTestResultObserver(mResultObserver -> {
             ApiComplianceChecker complianceChecker =
                     new ApiComplianceChecker(mResultObserver, mClassProvider);
 
             ApiDocumentParser apiDocumentParser = new ApiDocumentParser(TAG);
 
-            parseActiveSharedLibraryApis(apiDocumentParser, PREVIOUS_API_FILES.get())
+            parseActiveSharedLibraryApis(apiDocumentParser, PREVIOUS_API_FILES.get(), library)
                     .map(clazz -> clazz.setPreviousApiFlag(true))
                     .forEach(complianceChecker::checkSignatureCompliance);
 
@@ -139,25 +235,37 @@ public class SignatureMultiLibsTest extends AbstractSignatureTest {
     }
 
     /**
-     * Check to see if the supplied name is an API file for a shared library that is available on
-     * this device.
+     * Get the library name from the API file's path.
      *
      * @param path the path of the API file.
-     * @return true if the API corresponds to a shared library on the device, false otherwise.
+     * @return the library name for the API file.
      */
-    private boolean checkLibrary (VirtualPath path) {
+    private String getLibraryNameFromPath(VirtualPath path) {
         String name = path.toString();
-        String libraryName = name.substring(name.lastIndexOf('/') + 1).split("-")[0];
-        boolean matched = AVAILABLE_SHARED_LIBRARIES.get().contains(libraryName);
-        if (matched) {
-            System.out.printf("%s: Processing API file %s, from library %s as it does match a"
-                            + " shared library on this device%n",
-                    getClass().getSimpleName(), name, libraryName);
-        } else {
-            System.out.printf("%s: Ignoring API file %s, from library %s as it does not match a"
-                    + " shared library on this device%n",
-                    getClass().getSimpleName(), name, libraryName);
+        return name.substring(name.lastIndexOf('/') + 1).split("-")[0];
+    }
+
+    /**
+     * A wrapper around a shared library name to ensure that its string representation is suitable
+     * for use in a parameterized test name, i.e. does not contain any characters that are not
+     * allowed in a test name by CTS/AndroidJUnitRunner.
+     */
+    public static class TestableLibraryParameter {
+        private final String name;
+        private final String parameter;
+
+        public TestableLibraryParameter(String name) {
+            this.name = name;
+            this.parameter = name.replace('.', '_');
         }
-        return matched;
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return parameter;
+        }
     }
 }
