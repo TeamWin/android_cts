3a665e6c71019d39d771cd400d259dae58867454
Jonathan Scott <scottjonathan@google.com>
2022-05-27 16:40:32 +0000

    Don't manage keyguard on instant app tests.
    Test: atest CtsDevicePolicyTestCases
    Fixes: 234103608
    Change-Id: Ia22923678535e04201a6f2d5c2e6c26e5046050d

--- common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/DeviceState.java
+++ common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/DeviceState.java
@@ -895,7 +895,10 @@ public final class DeviceState extends HarrierRule {
 
                 try {
                     TestApis.device().keepScreenOn(true);
-                    TestApis.device().setKeyguardEnabled(false);
+
+                    if (!Tags.hasTag(Tags.INSTANT_APP)) {
+                        TestApis.device().setKeyguardEnabled(false);
+                    }
                     TestApis.users().setStopBgUsersOnSwitch(STOP_USER_ON_SWITCH_FALSE);
 
                     try {
@@ -933,7 +936,9 @@ public final class DeviceState extends HarrierRule {
                         teardownShareableState();
                     }
 
-                    TestApis.device().setKeyguardEnabled(true);
+                    if (!Tags.hasTag(Tags.INSTANT_APP)) {
+                        TestApis.device().setKeyguardEnabled(true);
+                    }
                     TestApis.device().keepScreenOn(false);
                     TestApis.users().setStopBgUsersOnSwitch(STOP_USER_ON_SWITCH_DEFAULT);
                 }
