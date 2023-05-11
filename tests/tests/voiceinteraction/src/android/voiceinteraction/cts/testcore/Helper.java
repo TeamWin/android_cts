83d14e991e4a3a7e98c6802d9ba84ea9c8a5b525
Nicholas Ambur <nambur@google.com>
2023-05-11 18:28:56 +0000

    fix typo in testcore.Helper#createKeyphraseArray
    Test: atest CtsVoiceInteractionTestCases
    Bug: 272147641
    Change-Id: Ia687e68dbbf4f5ba3e8bfe4c88f8a310ec3ee17c

--- tests/tests/voiceinteraction/src/android/voiceinteraction/cts/testcore/Helper.java
+++ tests/tests/voiceinteraction/src/android/voiceinteraction/cts/testcore/Helper.java
@@ -216,7 +216,7 @@ public final class Helper {
     /**
      * Returns the array of {@link SoundTrigger.Keyphrase} that is used for testing.
      */
-    public static SoundTrigger.Keyphrase[] createKeyprhaseArray(Context context) {
+    public static SoundTrigger.Keyphrase[] createKeyphraseArray(Context context) {
         return new SoundTrigger.Keyphrase[] {new SoundTrigger.Keyphrase(DEFAULT_PHRASE_ID,
                 SoundTrigger.RECOGNITION_MODE_VOICE_TRIGGER,
                 KEYPHRASE_LOCALE,
