5b2c5076744002f71d6bad09fe76b7c5feb85b96
Lev Rumyantsev <levarum@google.com>
2023-05-22 21:41:44 +0000

    CtsNativeNetPlatform: remove libbinder dependency
    libbinder isn't public NDK, so we use libbinder_ndk instead.
    
    Test: CtsNativeNetPlatformTestCases x86 and arm test on x86 device
    Bug: 268440865
    Change-Id: I0fd2be1cdb93b1c811858843cdcd00737876e6d3

--- tests/tests/net/native/src/TagSocketTest.cpp
+++ tests/tests/net/native/src/TagSocketTest.cpp
@@ -15,15 +15,22 @@
  *
  */
 
+#include <thread>
+
+#include <android-base/file.h>
 #include <android-base/format.h>
+#include <android/binder_auto_utils.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <android/binder_status.h>
 #include <android/multinetwork.h>
-#include <binder/IServiceManager.h>
+#include <android-base/unique_fd.h>
 #include <bpf/BpfUtils.h>
 #include <gtest/gtest.h>
 #include <nettestutils/DumpService.h>
 
-using android::IBinder;
-using android::IServiceManager;
+using android::base::unique_fd;
+using android::base::ReadFdToString;
 using android::bpf::getSocketCookie;
 using android::bpf::NONEXISTENT_COOKIE;
 using android::sp;
@@ -33,14 +40,11 @@ using android::Vector;
 class TagSocketTest : public ::testing::Test {
  public:
   TagSocketTest() {
-    sp<IServiceManager> sm = android::defaultServiceManager();
-    mBinder = sm->getService(String16("connectivity"));
+    mBinder = ndk::SpAIBinder(AServiceManager_waitForService("connectivity"));
   }
 
-  void SetUp() override { ASSERT_NE(nullptr, mBinder.get()); }
-
  protected:
-  sp<IBinder> mBinder;
+  ndk::SpAIBinder mBinder;
 };
 
 namespace {
@@ -48,15 +52,45 @@ namespace {
 constexpr uid_t TEST_UID = 10086;
 constexpr uint32_t TEST_TAG = 42;
 
-[[maybe_unused]] void dumpBpfMaps(const sp<IBinder>& binder,
+android::status_t dumpService(const ndk::SpAIBinder& binder,
+                              const char** args,
+                              uint32_t num_args,
+                              std::vector<std::string>& outputLines) {
+  unique_fd localFd, remoteFd;
+  bool success = Pipe(&localFd, &remoteFd);
+  EXPECT_TRUE(success) << "Failed to open pipe for dumping: " << strerror(errno);
+  if (!success) return STATUS_UNKNOWN_ERROR;
+
+  // dump() blocks until another thread has consumed all its output.
+  std::thread dumpThread = std::thread([binder, remoteFd{std::move(remoteFd)}, args, num_args]() {
+    EXPECT_EQ(android::OK, AIBinder_dump(binder.get(), remoteFd, args, num_args));
+  });
+
+  std::string dumpContent;
+
+  EXPECT_TRUE(ReadFdToString(localFd.get(), &dumpContent))
+      << "Error during dump: " << strerror(errno);
+  dumpThread.join();
+
+  std::stringstream dumpStream(dumpContent);
+  std::string line;
+  while (std::getline(dumpStream, line)) {
+    outputLines.push_back(std::move(line));
+  }
+
+  return android::OK;
+}
+
+[[maybe_unused]] void dumpBpfMaps(const ndk::SpAIBinder& binder,
                                   std::vector<std::string>& output) {
   Vector<String16> vec;
-  android::status_t ret = dumpService(binder, {"trafficcontroller"}, output);
+  const char* arg = "trafficcontroller";
+  android::status_t ret = dumpService(binder, &arg, 1, output);
   ASSERT_EQ(android::OK, ret)
       << "Error dumping service: " << android::statusToString(ret);
 }
 
-[[maybe_unused]] bool socketIsTagged(const sp<IBinder>& binder, uint64_t cookie,
+[[maybe_unused]] bool socketIsTagged(const ndk::SpAIBinder& binder, uint64_t cookie,
                                      uid_t uid, uint32_t tag) {
   std::string match =
       fmt::format("cookie={} tag={:#x} uid={}", cookie, tag, uid);
@@ -68,7 +102,7 @@ constexpr uint32_t TEST_TAG = 42;
   return false;
 }
 
-[[maybe_unused]] bool socketIsNotTagged(const sp<IBinder>& binder,
+[[maybe_unused]] bool socketIsNotTagged(const ndk::SpAIBinder& binder,
                                         uint64_t cookie) {
   std::string match = fmt::format("cookie={}", cookie);
   std::vector<std::string> lines = {};
@@ -79,7 +113,7 @@ constexpr uint32_t TEST_TAG = 42;
   return true;
 }
 
-bool waitSocketIsNotTagged(const sp<IBinder>& binder, uint64_t cookie,
+bool waitSocketIsNotTagged(const ndk::SpAIBinder& binder, uint64_t cookie,
                            int maxTries) {
     for (int i = 0; i < maxTries; ++i) {
         if (socketIsNotTagged(binder, cookie)) return true;
