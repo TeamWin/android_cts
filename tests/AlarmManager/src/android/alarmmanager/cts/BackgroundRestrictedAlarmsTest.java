f97c1c756fe0cbc8fc7e17da39ca27fbacd744db
Daniel Peykov <peykov@google.com>
2023-06-08 21:24:18 +0000

    Avoid overwriting the sync disabled mode in CtsAlarmManagerTestCases
    Use DeviceConfigStateHelper (enables/reverts syncs when necessary) and
    clean up any sync mode changes. This will allow syncs to be disabled by
    default during testing to ensure deterministic platform flags.
    
    Bug: 283536230
    Test: atest CtsAlarmManagerTestCases w/ --disable-device-config-sync
    Change-Id: Id888678cb57a63d2941ef3dcd5a96169b1d30f27
    (cherry picked from commit c934bb6afaafe645b2fc018a5cbcbb006c2fafc3)
    Merged-In: Id888678cb57a63d2941ef3dcd5a96169b1d30f27

--- tests/AlarmManager/src/android/alarmmanager/cts/BackgroundRestrictedAlarmsTest.java
+++ tests/AlarmManager/src/android/alarmmanager/cts/BackgroundRestrictedAlarmsTest.java
@@ -87,6 +87,7 @@ public class BackgroundRestrictedAlarmsTest {
 
     private volatile int mAlarmCount;
     private volatile String mFgsResult;
+    private int mInitialSyncDisabledMode = -1;
 
     private final BroadcastReceiver mAlarmStateReceiver = new BroadcastReceiver() {
         @Override
@@ -114,8 +115,10 @@ public class BackgroundRestrictedAlarmsTest {
         updateAlarmManagerConstants();
         mActivityManagerDeviceConfigStateHelper
                 .set("bg_auto_restricted_bucket_on_bg_restricted", "false");
-        SystemUtil.runWithShellPermissionIdentity(() ->
-                DeviceConfig.setSyncDisabledMode(DeviceConfig.SYNC_DISABLED_MODE_UNTIL_REBOOT));
+        SystemUtil.runWithShellPermissionIdentity(() -> {
+            mInitialSyncDisabledMode = DeviceConfig.getSyncDisabledMode();
+            DeviceConfig.setSyncDisabledMode(DeviceConfig.SYNC_DISABLED_MODE_UNTIL_REBOOT);
+        });
         AppOpsUtils.setOpMode(TEST_APP_PACKAGE, OPSTR_RUN_ANY_IN_BACKGROUND, MODE_IGNORED);
         makeUidIdle();
         setAppStandbyBucket("active");
@@ -205,8 +208,10 @@ public class BackgroundRestrictedAlarmsTest {
 
     @After
     public void tearDown() throws Exception {
-        SystemUtil.runWithShellPermissionIdentity(() ->
-                DeviceConfig.setSyncDisabledMode(DeviceConfig.SYNC_DISABLED_MODE_NONE));
+        if (mInitialSyncDisabledMode != -1) {
+            SystemUtil.runWithShellPermissionIdentity(() ->
+                    DeviceConfig.setSyncDisabledMode(mInitialSyncDisabledMode));
+        }
         deleteAlarmManagerConstants();
         AppOpsUtils.reset(TEST_APP_PACKAGE);
         mActivityManagerDeviceConfigStateHelper.restoreOriginalValues();
