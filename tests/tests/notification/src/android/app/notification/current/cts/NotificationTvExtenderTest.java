6667cde1761e1add6e3a1a626eefbfba86772a49
Alexander Roederer <aroederer@google.com>
2023-06-12 19:44:43 +0000

    Move NotificationTvExtenderTest to notif module
    For test isoluation and targeted presubmit run. Other tests were moved
    in udc-dev in  https://googleplex-android-review.git.corp.google.com/c/platform/cts/+/23352505
    but this test was added in master, so needs to be moved separately.
    
    Test: atest CtsNotificationTestCases
    Test: atest CtsAppTestCases
    Bug: 280576227
    Change-Id: I30d0f8f02c7e32764c47602b554ac8a32a6b19ad

--- tests/tests/notification/src/android/app/notification/current/cts/NotificationTvExtenderTest.java
+++ tests/tests/notification/src/android/app/notification/current/cts/NotificationTvExtenderTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package android.app.cts;
+package android.app.notification.current.cts;
 
 import android.app.Notification;
 import android.app.Notification.TvExtender;
