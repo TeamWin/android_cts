520c2a9bb2d7267b21cb9bc44f03c3846b6b2351
Sumedh Sen <sumedhsen@google.com>
2023-06-28 18:49:45 -0700

    Wait till test receives preapproval request before committing
    To prevent mixing up of broadcasts intended for CONFIRM_PRE_APPROVAL and
    CONFIRM_INSTALL between the intended receivers, wait till we get the
    preapproval broadcast before committing the session.
    
    Bug: 289300824
    Test: atest CtsPackageInstallTestCases:PreapprovalInstallTest
    --iterations 100
    
    Change-Id: Id894df683ba04fb00d3315c64638ce5d25d45bb7

--- tests/tests/packageinstaller/install/src/android/packageinstaller/install/cts/PreapprovalInstallTest.kt
+++ tests/tests/packageinstaller/install/src/android/packageinstaller/install/cts/PreapprovalInstallTest.kt
@@ -28,6 +28,8 @@ import android.provider.DeviceConfig
 import androidx.test.runner.AndroidJUnit4
 import com.android.compatibility.common.util.DeviceConfigStateChangerRule
 import java.io.File
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
@@ -166,9 +168,15 @@ class PreapprovalInstallTest : PackageInstallerTestBase() {
         val (sessionId, session) = createSession(0 /* flags */, false /* isMultiPackage */,
                 null /* packageSource */)
 
+        val latch = CountDownLatch(1)
         val dummyReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
-                // Do nothing
+                // This is to make sure we receive the pre-approval intent before
+                // committing the session
+                val preapproval = intent.getBooleanExtra(PackageInstaller.EXTRA_PRE_APPROVAL, false)
+                if (preapproval) {
+                    latch.countDown()
+                }
             }
         }
 
@@ -182,6 +190,7 @@ class PreapprovalInstallTest : PackageInstallerTestBase() {
                     PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
             session.requestUserPreapproval(preparePreapprovalDetails(), pendingIntent.intentSender)
 
+            latch.await(2000, TimeUnit.MILLISECONDS)
             writeAndCommitSession(TEST_APK_NAME, session)
             clickInstallerUIButton(INSTALL_BUTTON_ID)
 
