cac518596643848a83d37f774ff0cfcc3ddb8b10
Nate(Qiang) Jiang <qiangjiang@google.com>
2023-07-19 23:23:29 +0000

    Make WifiLocationInfoBackgroundTest excluded from mainline test
    WifiLocationInfoBackgroundTest is testing a platform API which is not
    updatable by mainline. Remove this from the mainline test
    
    Bug: 288342275
    Test: TH
    Merged-In: I07f7e3664263e4903b053c525b1974f0e5c75e18
    Change-Id: I07f7e3664263e4903b053c525b1974f0e5c75e18
    (cherry picked from commit 4bc9713c5fbb9e6f14d6c9693f18aee504a9d9a6)

--- tests/tests/wifi/src/android/net/wifi/cts/WifiLocationInfoBackgroundTest.java
+++ tests/tests/wifi/src/android/net/wifi/cts/WifiLocationInfoBackgroundTest.java
@@ -39,6 +39,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.android.compatibility.common.util.ApiTest;
+import com.android.compatibility.common.util.NonMainlineTest;
 import com.android.compatibility.common.util.PollingCheck;
 import com.android.compatibility.common.util.ShellIdentityUtils;
 import com.android.compatibility.common.util.SystemUtil;
@@ -57,6 +58,7 @@ import java.util.List;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 @ApiTest(apis = {"Manifest.permission#ACCESS_BACKGROUND_LOCATION"})
+@NonMainlineTest
 public class WifiLocationInfoBackgroundTest extends WifiJUnit4TestBase{
     private static final String TAG = "WifiLocationInfoTest";
 
