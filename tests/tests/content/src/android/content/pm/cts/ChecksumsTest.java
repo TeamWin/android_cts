245d3e4de4b4e307b17308daf7f3eca57868d5ff
Victor Hsieh <victorhsieh@google.com>
2023-08-18 14:32:16 -0700

    Skip .fsv_sig related tests if deprecation flag is on
    The feature deprecation is guarded by a flag, skip the test accordingly.
    
    Replace the recently added version check (getApiLevel() <= 34 /* U */)
    with the flag check for consistency.
    
    Bug: 277351939
    Test: adb shell device_config put hardware_backed_security \
              com.android.server.security.deprecate_fsv_sig false # and true
    Test: atest CtsPackageManagerTestCases:android.content.pm.cts.ChecksumsTest
    Test: atest CtsApkVerityInstallHostTestCases
    Change-Id: I3ed21ba558c0af24b8f1f32a368323c2e14f4e5e

--- tests/tests/content/src/android/content/pm/cts/ChecksumsTest.java
+++ tests/tests/content/src/android/content/pm/cts/ChecksumsTest.java
@@ -31,6 +31,8 @@ import static android.content.pm.PackageManager.GET_SIGNING_CERTIFICATES;
 import static android.content.pm.PackageManager.TRUST_ALL;
 import static android.content.pm.PackageManager.TRUST_NONE;
 
+import static com.android.server.security.Flags.FLAG_DEPRECATE_FSV_SIG;
+
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
@@ -62,6 +64,9 @@ import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
 import android.os.SystemProperties;
 import android.platform.test.annotations.AppModeFull;
+import android.platform.test.annotations.RequiresFlagsDisabled;
+import android.platform.test.flag.junit.CheckFlagsRule;
+import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.util.ExceptionUtils;
 
 import androidx.annotation.NonNull;
@@ -191,6 +196,9 @@ public class ChecksumsTest {
     @Rule
     public AbandonAllPackageSessionsRule mAbandonSessionsRule = new AbandonAllPackageSessionsRule();
 
+    @Rule
+    public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
+
     @AfterClass
     public static void onAfterClass() throws Exception {
         uninstallPackageSilently(V4_PACKAGE_NAME);
@@ -318,6 +326,7 @@ public class ChecksumsTest {
 
     @LargeTest
     @Test
+    @RequiresFlagsDisabled(FLAG_DEPRECATE_FSV_SIG)
     public void testFixedFSVerityDefaultChecksums() throws Exception {
         assumeTrue(isApkVerityEnabled());
         uninstallPackageSilently(FIXED_FSVERITY_PACKAGE_NAME);
@@ -349,6 +358,7 @@ public class ChecksumsTest {
 
     @LargeTest
     @Test
+    @RequiresFlagsDisabled(FLAG_DEPRECATE_FSV_SIG)
     public void testFixedFSVerityDefaultChecksumsIncremental() throws Exception {
         assumeTrue(checkIncrementalDeliveryFeature());
 
