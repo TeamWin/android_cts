2acef9dbc6c299de5657eabe62b678da084e7a20
Ján Sebechlebský <jsebechlebsky@google.com>
2023-08-28 07:45:48 +0000

    Revert "Remove FlakyTest annotation from StreamedAppClipboardTest."
    This reverts commit cef1c164c8cebfcd61e2ed4c34d68ad8cf4879a6.
    
    Reason for revert: The test is still flaky due to flaky INTERNAL_SYSTEM_WINDOW permission failure.
    
    Change-Id: Ib849274e76885a78d2b3421fe695f55021ab9587

--- tests/tests/virtualdevice/core/src/android/virtualdevice/cts/core/StreamedAppClipboardTest.java
+++ tests/tests/virtualdevice/core/src/android/virtualdevice/cts/core/StreamedAppClipboardTest.java
@@ -92,6 +92,7 @@ import android.virtualdevice.cts.common.util.VirtualDeviceTestUtils;
 
 import androidx.annotation.NonNull;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.FlakyTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.android.compatibility.common.util.AdoptShellPermissionsRule;
@@ -200,6 +201,7 @@ public class StreamedAppClipboardTest {
     }
 
     @Test
+    @FlakyTest(bugId = 297288652)
     public void oneAppOnVirtualDevice_canWriteAndReadClipboard() {
         ClipboardManager clipboard = mContext.createDeviceContext(
                 DEVICE_ID_DEFAULT).getSystemService(ClipboardManager.class);
@@ -355,6 +357,7 @@ public class StreamedAppClipboardTest {
         assertThat(clipData).isNull();
     }
 
+    @FlakyTest(bugId = 293404975)
     @Test
     public void twoAppsOnVirtualDevice_firstAppWrites_secondAppCanRead() {
         final Intent firstAppIntent =
