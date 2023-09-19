6e53c375092b33cdb591d6471f5df371c184cc76
Rakesh Kumar <rakesh.kumar@ittiam.com>
2023-09-20 01:41:16 +0530

    mediacuj CTS: Add Screen Rotation Test for E2E CUJ
    Test video playback in CtsMediaShortFormPlaybackTest in
    portrait and landscape orientations.
    
    Also, Fix some of the formatting issues.
    
    Bug: 294024882
    
    Test: atest CtsMediaCUJSmallTest
    Test: atest CtsMediaCUJLargeTest
    Change-Id: Ia27f715ff0fa2b9bf82fe5fed6e2534acf05e830

--- tests/tests/mediacujtest/smalltest/src/android/media/cujsmalltest/cts/CtsMediaShortFormPlaybackTest.java
+++ tests/tests/mediacujtest/smalltest/src/android/media/cujsmalltest/cts/CtsMediaShortFormPlaybackTest.java
@@ -17,13 +17,15 @@
 package android.media.cujsmalltest.cts;
 
 import android.media.cujcommon.cts.CujTestBase;
+import android.media.cujcommon.cts.CujTestParam;
+import android.media.cujcommon.cts.PlayerListener;
 import android.platform.test.annotations.PlatinumTest;
 
-import androidx.media3.common.C;
 import androidx.test.filters.LargeTest;
 
 import com.android.compatibility.common.util.ApiTest;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -45,41 +47,68 @@ public class CtsMediaShortFormPlaybackTest extends CujTestBase {
       "android.resource://android.media.cujsmalltest.cts/raw/ForBiggerBlazes_720p_hevc_15s";
   private static final String MP4_FORBIGGERESCAPES_ASSET_720P_HEVC_URI_STRING =
       "android.resource://android.media.cujsmalltest.cts/raw/ForBiggerEscapes_720p_hevc_15s";
+  private static final String MP4_FORBIGGERJOYRIDES_ASSET_480P_HEVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/ForBiggerJoyrides_480p_hevc_5s";
+  private static final String MP4_FORBIGGERMELTDOWN_ASSET_480P_HEVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/ForBiggerMeltdowns_480p_hevc_5s";
+  private static final String MP4_FORBIGGERBLAZEA_ASSET_480P_HEVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/ForBiggerBlazes_480p_hevc_5s";
+  private static final String MP4_FORBIGGERESCAPES_ASSET_480P_HEVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/ForBiggerEscapes_480p_hevc_5s";
+  private static final String MP4_FORBIGGERMELTDOWN_ASSET_1080P_AVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/ForBiggerMeltdowns_1080p_avc_5s";
+  private static final String MP4_ELEPHANTSDREAM_ASSET_1080P_AVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/ElephantsDream_1080p_avc_5s";
+  private static final String MP4_BIGBUCKBUNNY_ASSET_1080P_AVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/BigBuckBunny_1080p_avc_5s";
+  private static final String MP4_ELEPHANTSDREAM_ASSET_360x640_AVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/ElephantsDream_360x640_avc_5s";
+  private static final String MP4_BIGBUCKBUNNY_ASSET_360x640_AVC_URI_STRING =
+      "android.resource://android.media.cujsmalltest.cts/raw/BigBuckBunny_360x640_avc_5s";
 
-  private final List<String> mMediaUrls;
-  private final long mTimeoutMilliSeconds;
-  private final boolean mIsSeekTest;
-  private final int mNumOfSeekIteration;
-  private final long mSeekStartPosition;
-  private final long mSeekTimeUs;
+  CujTestParam mCujTestParam;
+  private final String mTestType;
 
-  public CtsMediaShortFormPlaybackTest(List<String> mediaUrls, long timeoutMilliSeconds,
-      boolean isSeekTest, int numOfSeekIteration, long seekStartPosition, long seekTimeUs,
-      String testType) {
-    super();
-    this.mMediaUrls = mediaUrls;
-    this.mTimeoutMilliSeconds = timeoutMilliSeconds;
-    this.mIsSeekTest = isSeekTest;
-    this.mNumOfSeekIteration = numOfSeekIteration;
-    this.mSeekStartPosition = seekStartPosition;
-    this.mSeekTimeUs = seekTimeUs;
+  public CtsMediaShortFormPlaybackTest(CujTestParam cujTestParam, String testType) {
+    super(cujTestParam.playerListener());
+    mCujTestParam = cujTestParam;
+    this.mTestType = testType;
   }
 
   /**
    * Returns the list of parameters
    */
-  @Parameterized.Parameters(name = "{index}_{6}")
+  @Parameterized.Parameters(name = "{index}_{1}")
   public static Collection<Object[]> input() {
-    // mediaUrl, timeoutMilliSeconds, isSeekTest, numOfSeekIteration, seekStartPosition, seekTimeUs
+    // CujTestParam, testId
     final List<Object[]> exhaustiveArgsList = new ArrayList<>(Arrays.asList(new Object[][]{
-        {prepareHevc_720p_15secVideoList(), 330000, false, C.LENGTH_UNSET, C.LENGTH_UNSET,
-            C.LENGTH_UNSET, "Hevc_720p_15sec"},
-        {prepareHevc_720p_15secVideoList(), 330000, true, 1, 9000, 5000,
+        {CujTestParam.builder().setMediaUrls(prepareHevc_720p_15secVideoList())
+            .setTimeoutMilliSeconds(330000)
+            .setPlayerListener(PlayerListener.createListenerForPlaybackTest()).build(),
+            "Hevc_720p_15sec"},
+        {CujTestParam.builder().setMediaUrls(prepareHevc_720p_15secVideoList())
+            .setTimeoutMilliSeconds(330000)
+            .setPlayerListener(PlayerListener.createListenerForSeekTest(1, 5000, 9000)).build(),
             "Hevc_720p_15sec_seekTest"},
+        {CujTestParam.builder().setMediaUrls(prepareHevc_480p_5secVideoList())
+            .setTimeoutMilliSeconds(60000)
+            .setPlayerListener(PlayerListener.createListenerForOrientationTest(3000)).build(),
+            "Hevc_480p_5sec_OrientationTest"},
+        {CujTestParam.builder().setMediaUrls(prepareAvc_1080p_5secVideoList())
+            .setTimeoutMilliSeconds(60000)
+            .setPlayerListener(PlayerListener.createListenerForOrientationTest(3000)).build(),
+            "Avc_1080p_5sec_OrientationTest"},
+        {CujTestParam.builder().setMediaUrls(prepareAvc_360x640_5secVideoList())
+            .setTimeoutMilliSeconds(60000)
+            .setPlayerListener(PlayerListener.createListenerForOrientationTest(3000)).build(),
+            "Avc_360x640_5sec_OrientationTest"},
     }));
     return exhaustiveArgsList;
   }
 
+  /**
+   * Prepare Hevc 720p 15sec video list.
+   */
   public static List<String> prepareHevc_720p_15secVideoList() {
     List<String> videoInput = Arrays.asList(
         MP4_FORBIGGERJOYRIDES_ASSET_720P_HEVC_URI_STRING,
@@ -105,6 +134,41 @@ public class CtsMediaShortFormPlaybackTest extends CujTestBase {
     return videoInput;
   }
 
+  /**
+   * Prepare Hevc 480p 15sec video list.
+   */
+  public static List<String> prepareHevc_480p_5secVideoList() {
+    List<String> videoInput = Arrays.asList(
+        MP4_FORBIGGERJOYRIDES_ASSET_480P_HEVC_URI_STRING,
+        MP4_FORBIGGERMELTDOWN_ASSET_480P_HEVC_URI_STRING,
+        MP4_FORBIGGERBLAZEA_ASSET_480P_HEVC_URI_STRING,
+        MP4_FORBIGGERESCAPES_ASSET_480P_HEVC_URI_STRING);
+    return videoInput;
+  }
+
+  /**
+   * Prepare Avc 1080p 15sec video list.
+   */
+  public static List<String> prepareAvc_1080p_5secVideoList() {
+    List<String> videoInput = Arrays.asList(
+        MP4_FORBIGGERMELTDOWN_ASSET_1080P_AVC_URI_STRING,
+        MP4_ELEPHANTSDREAM_ASSET_1080P_AVC_URI_STRING,
+        MP4_BIGBUCKBUNNY_ASSET_1080P_AVC_URI_STRING);
+    return videoInput;
+  }
+
+  /**
+   * Prepare Avc 360x480 video list.
+   */
+  public static List<String> prepareAvc_360x640_5secVideoList() {
+    List<String> videoInput = Arrays.asList(
+        MP4_BIGBUCKBUNNY_ASSET_360x640_AVC_URI_STRING,
+        MP4_ELEPHANTSDREAM_ASSET_360x640_AVC_URI_STRING,
+        MP4_BIGBUCKBUNNY_ASSET_360x640_AVC_URI_STRING,
+        MP4_ELEPHANTSDREAM_ASSET_360x640_AVC_URI_STRING);
+    return videoInput;
+  }
+
   // Test to Verify video playback with and without seek
   @ApiTest(apis = {"android.media.MediaCodec#configure",
       "android.media.MediaCodec#createByCodecName",
@@ -114,7 +178,10 @@ public class CtsMediaShortFormPlaybackTest extends CujTestBase {
   @Test
   @PlatinumTest(focusArea = "media")
   public void testVideoPlayback() throws Exception {
-    play(mMediaUrls, mTimeoutMilliSeconds, mIsSeekTest, mNumOfSeekIteration, mSeekStartPosition,
-        mSeekTimeUs);
+    if (mCujTestParam.playerListener().isOrientationTest()) {
+      Assume.assumeTrue("Skipping " + mTestType + " as device doesn't support orientation.",
+          supportOrientationRequest(mActivity));
+    }
+    play(mCujTestParam.mediaUrls(), mCujTestParam.timeoutMilliSeconds());
   }
 }
