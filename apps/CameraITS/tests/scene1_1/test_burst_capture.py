89c1be4aaae0ff4aadd2c44888f19e7b535fa40e
Clemenz Portmann <portmannc@google.com>
2023-09-29 09:14:07 -0700

    ITS_VIC: test_burst_capture: eval content and frame times
    Originally, the test ran in scene0 & did not check scene content.
    Moved test to scene1_1. Test for scene brightness and frame drops.
    
    bug: 301250924
    bug: 291147208
    Change-Id: I63b94d1e0c1ae6119063b9ffa1eb7c86abfba1ae

--- apps/CameraITS/tests/scene1_1/test_burst_capture.py
+++ apps/CameraITS/tests/scene1_1/test_burst_capture.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Verify capture burst of full size images is fast enough to not timeout.
-"""
+"""Verify capture burst of full size images is fast enough to not timeout."""
 
 import logging
 import os
@@ -25,8 +24,15 @@ import capture_request_utils
 import image_processing_utils
 import its_session_utils
 
+_FRAME_TIME_DELTA_ATOL = 60  # ideal frame delta: 33ms
 _NAME = os.path.splitext(os.path.basename(__file__))[0]
+_NS_TO_MS = 1.0E-6
 _NUM_TEST_FRAMES = 15
+_PATCH_H = 0.1  # center 10% patch params
+_PATCH_W = 0.1
+_PATCH_X = 0.5 - _PATCH_W/2
+_PATCH_Y = 0.5 - _PATCH_H/2
+_THRESH_MIN_LEVEL = 0.1  # check images aren't too dark
 
 
 class BurstCaptureTest(its_base_test.ItsBaseTest):
@@ -46,14 +52,33 @@ class BurstCaptureTest(its_base_test.ItsBaseTest):
       camera_properties_utils.skip_unless(
           camera_properties_utils.backward_compatible(props))
       req = capture_request_utils.auto_capture_request()
+      cam.do_3a()
       caps = cam.do_capture([req] * _NUM_TEST_FRAMES)
-      cap = caps[0]
       img = image_processing_utils.convert_capture_to_rgb_image(
-          cap, props=props)
+          caps[0], props=props)
       name_with_log_path = os.path.join(self.log_path, _NAME)
-      img_name = f'{name_with_log_path}.jpg'
-      logging.debug('Image Name: %s', img_name)
-      image_processing_utils.write_image(img, img_name)
+      image_processing_utils.write_image(img, f'{name_with_log_path}.jpg')
+      logging.debug('Image W, H: %d, %d', caps[0]['width'], caps[0]['height'])
+
+      # Confirm center patch brightness
+      patch = image_processing_utils.get_image_patch(
+          img, _PATCH_X, _PATCH_Y, _PATCH_W, _PATCH_H)
+      r, g, b = image_processing_utils.compute_image_means(patch)
+      logging.debug('RGB levels %.3f, %.3f, %.3f', r, g, b)
+      if g < _THRESH_MIN_LEVEL:
+        raise AssertionError(f'Image is too dark! G center patch avg: {g:.3f}, '
+                             f'THRESH: {_THRESH_MIN_LEVEL}')
+
+      # Check frames are consecutive
+      frame_times = [cap['metadata']['android.sensor.timestamp']
+                     for cap in caps]
+      for i, time in enumerate(frame_times):
+        if i > 1:
+          frame_time_delta = (time - frame_times[i-1]) * _NS_TO_MS
+          if frame_time_delta > _FRAME_TIME_DELTA_ATOL:
+            raise AssertionError(
+                f'Frame drop! Frame time delta: {frame_time_delta}ms, '
+                f'ATOL: {_FRAME_TIME_DELTA_ATOL}')
 
 
 if __name__ == '__main__':
