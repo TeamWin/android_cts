af7f4aff6ea493e4799d3593deade83f4a564949
Alexander Roederer <aroederer@google.com>
2023-10-06 01:27:37 +0000

    Reenable ranking update pressure cts test
    Test: abtd run
    Bug: 300941310
    Change-Id: Ie38b06e89b9cb977b125b9a65f37d6c575531291

--- tests/tests/notification/src/android/app/notification/current/cts/NotificationManagerTest.java
+++ tests/tests/notification/src/android/app/notification/current/cts/NotificationManagerTest.java
@@ -994,8 +994,6 @@ public class NotificationManagerTest extends BaseNotificationManagerTest {
      * 50 seconds. Tests will time out after approximately 60 seconds, so binding needs to happen
      * in under 10 seconds.
      */
-// TODO (b/300941310): Restore test
-/**
     @LargeTest
     @RequiresDevice
     public void testRankingUpdateSentWithPressure() throws Exception {
@@ -1104,7 +1102,7 @@ public class NotificationManagerTest extends BaseNotificationManagerTest {
         mListener.removeTestPackage(PRESSURE_APP_06);
         mListener.removeTestPackage(PRESSURE_APP_07);
     }
-*/
+
     public void testSuspendedPackageSendsNotification() throws Exception {
         mListener = mNotificationHelper.enableListener(STUB_PACKAGE_NAME);
         assertNotNull(mListener);
