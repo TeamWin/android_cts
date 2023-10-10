be7117db7b369fb0789430d46b96aff2260b66c9
Brad Ebinger <breadley@google.com>
2023-10-10 16:01:21 -0700

    Tighten up synchronization patterns in CtsConnectionService
    1) Fixes a bug where the CountDownLatch was not reset for tests
    that use the same CtsConnectionService instance between tests.
    2) Separated locks for incoming/outgoing requests to
    ConnectionService to make deadlocks due to re-entry not possible.
    
    Test: atest CtsTelephonyTestCases
    Flag: test only fix
    Change-Id: Idcd63545f2f00b70d96a5f5fdaac2adefdc0a857

--- tests/tests/telecom/src/android/telecom/cts/BaseTelecomTestWithMockServices.java
+++ tests/tests/telecom/src/android/telecom/cts/BaseTelecomTestWithMockServices.java
@@ -1954,7 +1954,7 @@ public class BaseTelecomTestWithMockServices extends InstrumentationTestCase {
     }
 
     void assertCtsConnectionServiceUnbound() {
-        if (CtsConnectionService.isBound()) {
+        if (CtsConnectionService.isServiceRegisteredToTelecom()) {
             assertTrue("CtsConnectionService not yet unbound!",
                     CtsConnectionService.waitForUnBinding());
         }
