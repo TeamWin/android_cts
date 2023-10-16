a2da175152140495bd72dc793537e258c79cc8a9
wilsonshih <wilsonshih@google.com>
2023-10-16 05:49:22 +0000

    Fix assert on a removed window.
    Do not assert non-null case on a possible removed window.
    
    Bug: 304941504
    Test: atest BackGestureInvokedTest#testImeDismissed
    Change-Id: I3e100d7c3b6095025b9922ca4368c0ac80d789bd

--- tests/framework/base/windowmanager/util/src/android/server/wm/WindowManagerStateHelper.java
+++ tests/framework/base/windowmanager/util/src/android/server/wm/WindowManagerStateHelper.java
@@ -832,11 +832,12 @@ public class WindowManagerStateHelper extends WindowManagerState {
         final WindowState imeWinState = Condition.waitForResult("IME window",
                 condition -> condition
                         .setResultSupplier(this::getImeWindowState)
-                        .setResultValidator(
-                                w -> w != null && !w.isSurfaceShown()
-                                        && w.getDisplayId() == displayId));
+                        .setResultValidator(w -> w == null
+                                || (!w.isSurfaceShown() && w.getDisplayId() == displayId)));
 
-        assertNotNull("IME window must exist", imeWinState);
+        if (imeWinState == null) {
+            return;
+        }
         assertFalse("IME window must be hidden", imeWinState.isSurfaceShown());
         assertEquals("IME window must be on the given display", displayId,
                 imeWinState.getDisplayId());
