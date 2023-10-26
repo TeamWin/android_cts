15d328dc850e11eb049efc9e9f38fdb5e083a533
Tyler Saunders <tylersaunders@google.com>
2023-10-26 14:12:33 +0000

    Revert "Update PhotoPickerTest"
    Revert submission 24834288-get_content_non_redaction
    
    Reason for revert: b/306064780: breaks cloud picker
    
    Reverted changes: /q/submissionid:24834288-get_content_non_redaction
    
    Change-Id: I5b81f689e56be6647f698c0de901592ec66cac25

--- tests/PhotoPicker/src/android/photopicker/cts/ActionPickImagesOnlyTest.java
+++ tests/PhotoPicker/src/android/photopicker/cts/ActionPickImagesOnlyTest.java
@@ -25,7 +25,6 @@ import static android.photopicker.cts.util.PhotoPickerUiUtils.findItemList;
 import static android.photopicker.cts.util.ResultsAssertionsUtils.assertPersistedGrant;
 import static android.photopicker.cts.util.ResultsAssertionsUtils.assertPickerUriFormat;
 import static android.photopicker.cts.util.ResultsAssertionsUtils.assertRedactedReadOnlyAccess;
-import static android.provider.MediaStore.ACTION_PICK_IMAGES;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
@@ -75,7 +74,7 @@ public class ActionPickImagesOnlyTest extends PhotoPickerBaseTest {
 
     @Test
     public void testPhotoPickerIntentDelegation() throws Exception {
-        final Intent intent = new Intent(ACTION_PICK_IMAGES);
+        final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
 
         for (String mimeType: new String[] {
                 null,
@@ -94,7 +93,7 @@ public class ActionPickImagesOnlyTest extends PhotoPickerBaseTest {
 
     @Test
     public void testMultiSelect_invalidParam() throws Exception {
-        final Intent intent = new Intent(ACTION_PICK_IMAGES);
+        final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
         intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, MediaStore.getPickImagesMaxLimit() + 1);
         mActivity.startActivityForResult(intent, REQUEST_CODE);
         final GetResultActivity.Result res = mActivity.getResult();
@@ -103,7 +102,7 @@ public class ActionPickImagesOnlyTest extends PhotoPickerBaseTest {
 
     @Test
     public void testMultiSelect_invalidNegativeParam() throws Exception {
-        final Intent intent = new Intent(ACTION_PICK_IMAGES);
+        final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
         intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, -1);
         mActivity.startActivityForResult(intent, REQUEST_CODE);
         final GetResultActivity.Result res = mActivity.getResult();
@@ -116,7 +115,7 @@ public class ActionPickImagesOnlyTest extends PhotoPickerBaseTest {
         final int imageCount = maxCount + 1;
         mUriList.addAll(createImagesAndGetUris(imageCount, mContext.getUserId()));
 
-        final Intent intent = new Intent(ACTION_PICK_IMAGES);
+        final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
         intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, maxCount);
         mActivity.startActivityForResult(intent, REQUEST_CODE);
 
@@ -147,7 +146,7 @@ public class ActionPickImagesOnlyTest extends PhotoPickerBaseTest {
     public void testDoesNotRespectExtraAllowMultiple() throws Exception {
         final int imageCount = 2;
         mUriList.addAll(createImagesAndGetUris(imageCount, mContext.getUserId()));
-        final Intent intent = new Intent(ACTION_PICK_IMAGES);
+        final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         mActivity.startActivityForResult(intent, REQUEST_CODE);
 
@@ -158,14 +157,14 @@ public class ActionPickImagesOnlyTest extends PhotoPickerBaseTest {
         clickAndWait(sDevice, itemList.get(0));
 
         final Uri uri = mActivity.getResult().data.getData();
-        assertPickerUriFormat(ACTION_PICK_IMAGES, uri, mContext.getUserId());
+        assertPickerUriFormat(uri, mContext.getUserId());
         assertPersistedGrant(uri, mContext.getContentResolver());
         assertRedactedReadOnlyAccess(uri);
     }
 
     @Test
     public void testMimeTypeFilter() throws Exception {
-        final Intent intent = new Intent(ACTION_PICK_IMAGES);
+        final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
         intent.setType("audio/*");
         assertThrows(ActivityNotFoundException.class,
                 () -> mActivity.startActivityForResult(intent, REQUEST_CODE));
@@ -173,7 +172,7 @@ public class ActionPickImagesOnlyTest extends PhotoPickerBaseTest {
 
     @Test
     public void testExtraMimeTypeFilter() throws Exception {
-        final Intent intent = new Intent(ACTION_PICK_IMAGES);
+        final Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
         intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"audio/*"});
         mActivity.startActivityForResult(intent, REQUEST_CODE);
         final GetResultActivity.Result res = mActivity.getResult();
