f2832e16184b54b67d0f16624ad5f17c9136007f
Clemenz Portmann <portmannc@google.com>
2023-10-26 10:41:44 -0700

    ITS: raise lighting check THRESH for dark scenes
    files affected:
      its_session_utils.py
      test_hdr_extension.py
      test_night_extension.py
    
    bug: 307677537
    Change-Id: I2e8845b86c21061136a24ba9248a8ffd01790560

--- apps/CameraITS/tests/scene_extensions/scene_hdr/test_hdr_extension.py
+++ apps/CameraITS/tests/scene_extensions/scene_hdr/test_hdr_extension.py
@@ -209,7 +209,8 @@ class HdrExtensionTest(its_base_test.ItsBaseTest):
           capture_request_utils.auto_capture_request(), cam.CAP_YUV)
       y_plane, _, _ = image_processing_utils.convert_capture_to_planes(cap)
       its_session_utils.validate_lighting(
-          y_plane, self.scene, state='OFF', log_path=self.log_path)
+          y_plane, self.scene, state='OFF', log_path=self.log_path,
+          tablet_state='OFF')
 
       self.setup_tablet()
       self.set_screen_brightness(_TABLET_BRIGHTNESS)
