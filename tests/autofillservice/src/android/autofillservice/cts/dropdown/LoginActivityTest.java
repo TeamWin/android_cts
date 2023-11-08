765c9a5523cc953d761e97425e22b7736ae12f6d
Aditi <aditi.parikh@samsung.corp-partner.google.com>
2023-11-08 10:55:24 -0800

    Bug: 308991732
    Modified LoginActivityTest and UiBot to support higher density values.
    
    Change-Id: Idd9577ae627149de53be71240f831ed6faf02b5f
    Signed-off-by: Aditi <aditi.parikh@samsung.corp-partner.google.com>

--- tests/autofillservice/src/android/autofillservice/cts/dropdown/LoginActivityTest.java
+++ tests/autofillservice/src/android/autofillservice/cts/dropdown/LoginActivityTest.java
@@ -3029,7 +3029,7 @@ public class LoginActivityTest extends LoginActivityCommonTestCase {
     public void testSwitchInputMethod_noNewFillRequest() throws Exception {
         // TODO(b/187664861): Find better solution for small display device.
         mUiBot.assumeMinimumResolution(500);
-
+	mUiBot.setScreenResolution();
         // Set service
         enableService();
 
@@ -3060,5 +3060,6 @@ public class LoginActivityTest extends LoginActivityCommonTestCase {
 
         // No new fill request
         sReplier.assertNoUnhandledFillRequests();
+        mUiBot.resetScreenResolution();
     }
 }
