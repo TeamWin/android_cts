29399aa71b64f0ac5ed601ba6406f6d09a4f27c2
Jonathan Scott <scottjonathan@google.com>
2023-11-09 21:48:27 +0000

    Remove usage of Set.of
    Fixes: 296295120
    Test: btest CtsDevicePolicyTestCases (checking it builds primarily)
    Change-Id: I8f478add2f7918e96d2b917edcb87bce78750803

--- common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/Policy.java
+++ common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/Policy.java
@@ -468,11 +468,11 @@ public final class Policy {
                     existingAnnotations.length + 1);
             newAnnotations[newAnnotations.length - 1] = ensureHasDevicePolicyManagerRoleHolder(
                     roleHolderUser, /* isPrimary= */ true);
-            return Set.of(annotation,
+            return new HashSet<>(Arrays.asList(annotation,
                     new DynamicParameterizedAnnotation(
                     annotation.annotationType().getSimpleName() + "_DPMRH",
                     newAnnotations
-            ));
+            )));
         };
     }
 
@@ -923,7 +923,7 @@ public final class Policy {
 
         for (Annotation annotation : annotations) {
             shadowingAnnotations.addAll(
-                    sReverseShadowMap.getOrDefault(annotation.annotationType(), Set.of()));
+                    sReverseShadowMap.getOrDefault(annotation.annotationType(), new HashSet<>()));
         }
 
         annotations.removeIf(a -> shadowingAnnotations.contains(a.annotationType()));
