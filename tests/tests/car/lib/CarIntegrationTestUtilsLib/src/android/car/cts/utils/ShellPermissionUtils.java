d9ab6215fc8b6360d7975c4e23f791db138b3631
Yu Shan <shanyu@google.com>
2023-11-20 18:37:35 -0800

    Add permission check option.
    Allow client to assume/assert/no check the granted permissions
    for runWithShellPermissionIdentity.
    
    Test: Presubmit
    Bug: 310273554
    Change-Id: I4de149cafd9531fc1a777d6843b7a3be3040cc10

--- tests/tests/car/lib/CarIntegrationTestUtilsLib/src/android/car/cts/utils/ShellPermissionUtils.java
+++ tests/tests/car/lib/CarIntegrationTestUtilsLib/src/android/car/cts/utils/ShellPermissionUtils.java
@@ -16,8 +16,11 @@
 
 package android.car.cts.utils;
 
+import static com.google.common.truth.Truth.assertWithMessage;
+
 import static org.junit.Assume.assumeTrue;
 
+import android.annotation.IntDef;
 import android.app.UiAutomation;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
@@ -27,6 +30,9 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.android.compatibility.common.util.ThrowingRunnable;
 
 import org.junit.AssumptionViolatedException;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Class contains static methods to adopt all or a subset of the shell's permissions while invoking
@@ -41,6 +47,8 @@ public final class ShellPermissionUtils {
 
     /**
      * Run {@code throwingRunnable} with all the shell's permissions adopted.
+     *
+     * <p>It is possible that the adoption failed.
      */
     public static void runWithShellPermissionIdentity(ThrowingRunnable throwingRunnable) {
         UiAutomation uiAutomation = InstrumentationRegistry.getInstrumentation().getUiAutomation();
@@ -49,18 +57,65 @@ public final class ShellPermissionUtils {
     }
 
     /**
+     * No check for granted permissions.
+     */
+    public static final int CHECK_MODE_NONE = 1;
+    /**
+     * Skip the test if the required permissions are not granted after trying to adopt the shell
+     * permissions.
+     */
+    public static final int CHECK_MODE_ASSUME = 2;
+    /**
+     * Fail the test if the required permissions are not granted after trying to adopt the shell
+     * permissions.
+     */
+    public static final int CHECK_MODE_ASSERT = 3;
+
+    /** @hide */
+    @IntDef(prefix = {"CHECK_MODE_"}, value = {
+            CHECK_MODE_NONE,
+            CHECK_MODE_ASSUME,
+            CHECK_MODE_ASSERT
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface PermissionCheckMode {}
+
+    /**
      * Run {@code throwingRunnable} with a subset, {@code permissions}, of the shell's permissions
      * adopted.
+     *
+     * <p>This will skip the test case if the required permissions are not granted after trying
+     * to adopt the shell permissions.
      */
     public static void runWithShellPermissionIdentity(ThrowingRunnable throwingRunnable,
             String... permissions) {
+        runWithShellPermissionIdentity(throwingRunnable, CHECK_MODE_ASSUME, permissions);
+    }
+
+    /**
+     * Run {@code throwingRunnable} with a subset, {@code permissions}, of the shell's permissions
+     * adopted.
+     *
+     * <p>The {@code checkMode} specified whether to skip/fail/do nothing if failed to adopt the
+     * required permissions.
+     */
+    public static void runWithShellPermissionIdentity(ThrowingRunnable throwingRunnable,
+            @PermissionCheckMode int checkMode, String... permissions) {
         UiAutomation uiAutomation = InstrumentationRegistry.getInstrumentation().getUiAutomation();
         uiAutomation.adoptShellPermissionIdentity(permissions);
-        for (String permission : permissions) {
-            assumeTrue("Unable to adopt shell permission: " + permission,
-                    ContextCompat.checkSelfPermission(
-                            InstrumentationRegistry.getInstrumentation().getTargetContext(),
-                            permission) == PackageManager.PERMISSION_GRANTED);
+        if (checkMode != CHECK_MODE_NONE) {
+            for (String permission : permissions) {
+                boolean adopted = ContextCompat.checkSelfPermission(
+                        InstrumentationRegistry.getInstrumentation().getTargetContext(),
+                        permission) == PackageManager.PERMISSION_GRANTED;
+                String msg = "Unable to adopt shell permission: " + permission;
+                if (checkMode == CHECK_MODE_ASSUME) {
+                    assumeTrue(msg, adopted);
+                }
+                if (checkMode == CHECK_MODE_ASSERT) {
+                    assertWithMessage(msg).that(adopted).isTrue();
+                }
+            }
         }
         run(throwingRunnable, uiAutomation);
     }
