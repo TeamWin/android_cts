a6a5799716f1776fe39fd1b7c8de11014fe4edf0
Vladimir Komsiyski <vladokom@google.com>
2023-11-28 14:03:46 +0100

    VDM IME CTS 2/n: setInputMethodComponent test
    Bug: 287269288
    Test: atest VirtualDeviceImeTest
    Change-Id: I048e9c4b78967627819cdd4f93e1f68c9a4994cb

--- tests/tests/virtualdevice/applaunch/src/android/virtualdevice/cts/applaunch/VirtualDeviceImeTest.java
+++ tests/tests/virtualdevice/applaunch/src/android/virtualdevice/cts/applaunch/VirtualDeviceImeTest.java
@@ -21,27 +21,44 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.junit.Assume.assumeTrue;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
 
+import android.companion.virtual.VirtualDeviceManager.VirtualDevice;
+import android.companion.virtual.VirtualDeviceParams;
 import android.companion.virtual.flags.Flags;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
 import android.inputmethodservice.InputMethodService;
 import android.platform.test.annotations.AppModeFull;
 import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.view.Display;
 import android.view.inputmethod.InputMethodInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.virtualdevice.cts.applaunch.AppComponents.EmptyActivity;
 import android.virtualdevice.cts.common.VirtualDeviceRule;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.android.compatibility.common.util.ApiTest;
 import com.android.compatibility.common.util.FeatureUtil;
+import com.android.compatibility.common.util.SystemUtil;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * Tests for IME behavior on virtual devices.
@@ -51,16 +68,52 @@ import org.junit.runner.RunWith;
 @RequiresFlagsEnabled(Flags.FLAG_VDM_CUSTOM_IME)
 public class VirtualDeviceImeTest {
 
+    private static final long TIMEOUT_MILLIS = TimeUnit.SECONDS.toMillis(3);
+
     @Rule
     public VirtualDeviceRule mRule = VirtualDeviceRule.createDefault();
+
+    @Rule
+    public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     private final Context mContext = getInstrumentation().getContext();
     private final InputMethodManager mInputMethodManager =
             mContext.getSystemService(InputMethodManager.class);
 
+    private int mVirtualDisplayId;
+
+    @Mock
+    private Consumer<Integer> mDefaultDeviceImeListener;
+    @Mock
+    private Consumer<Integer> mVirtualDeviceImeListener;
+
+
     @Before
     public void setUp() throws Exception {
         assumeTrue(FeatureUtil.hasSystemFeature(PackageManager.FEATURE_INPUT_METHODS));
+
+        DefaultDeviceTestIme.sShowInputOnDisplayRequestListener = mDefaultDeviceImeListener;
+        VirtualDeviceTestIme.sShowInputOnDisplayRequestListener = mVirtualDeviceImeListener;
+
+        enableTestIme(DefaultDeviceTestIme.class, /* makeDefault= */ true);
+        enableTestIme(VirtualDeviceTestIme.class, /* makeDefault= */ false);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        SystemUtil.runShellCommandOrThrow("ime reset");
+    }
+
+    private <T extends InputMethodService> String enableTestIme(
+            Class<T> imeClass, boolean makeDefault) {
+        final String imeId =
+                new ComponentName(mContext, imeClass.getName()).flattenToShortString();
+        SystemUtil.runShellCommandOrThrow("ime enable " + imeId);
+        if (makeDefault) {
+            SystemUtil.runShellCommandOrThrow("ime set " + imeId);
+            assertThat(mInputMethodManager.getCurrentInputMethodInfo().getId()).isEqualTo(imeId);
+        }
+        return imeId;
     }
 
     /** The virtualDeviceOnly attribute is propagated to InputMethodInfo. */
@@ -80,21 +133,96 @@ public class VirtualDeviceImeTest {
         assertThat(defaultDeviceImi.isVirtualDeviceOnly()).isFalse();
     }
 
+    /** The default IME is used on virtual devices when there's no custom IME component. */
+    @Test
+    public void noCustomImeComponent_defaultImeShouldBeOnVirtualDisplay() {
+        createVirtualDeviceAndDisplay(/* imeComponent= */ Optional.empty());
+
+        showSoftInputOnDisplay(Display.DEFAULT_DISPLAY);
+        verify(mDefaultDeviceImeListener, timeout(TIMEOUT_MILLIS)).accept(Display.DEFAULT_DISPLAY);
+
+        showSoftInputOnDisplay(mVirtualDisplayId);
+        verify(mDefaultDeviceImeListener, timeout(TIMEOUT_MILLIS)).accept(mVirtualDisplayId);
+    }
+
+    /** The default IME is used on virtual devices when the custom IME component is {@code null}. */
+    @ApiTest(apis = {
+            "android.companion.virtual.VirtualDeviceParams.Builder#setInputMethodComponent"})
+    @Test
+    public void nullCustomImeComponent_defaultImeShouldBeOnVirtualDisplay() {
+        createVirtualDeviceAndDisplay(/* imeComponent= */ Optional.ofNullable(null));
+
+        showSoftInputOnDisplay(Display.DEFAULT_DISPLAY);
+        verify(mDefaultDeviceImeListener, timeout(TIMEOUT_MILLIS)).accept(Display.DEFAULT_DISPLAY);
+
+        showSoftInputOnDisplay(mVirtualDisplayId);
+        verify(mDefaultDeviceImeListener, timeout(TIMEOUT_MILLIS)).accept(mVirtualDisplayId);
+    }
+
     private InputMethodInfo getInputMethodInfo(String className) {
         final String imeId = new ComponentName(mContext, className).flattenToShortString();
         return mInputMethodManager.getInputMethodList().stream()
                 .filter(imi -> imi.getId().equals(imeId)).findFirst().orElse(null);
     }
 
-    /**
-     * Simple IME implementation forwarding the show input requests to a listener along with a
-     * display id.
-     */
-    public static class DefaultDeviceTestIme extends InputMethodService {}
+    private void showSoftInputOnDisplay(int displayId) {
+        EmptyActivity activity = mRule.startActivityOnDisplaySync(displayId, EmptyActivity.class);
+
+        InputMethodManager activityInputMethodManager =
+                activity.getSystemService(InputMethodManager.class);
+
+        getInstrumentation().runOnMainSync(
+                () -> activityInputMethodManager.showSoftInput(
+                        activity.getWindow().getDecorView(), 0));
+        getInstrumentation().waitForIdleSync();
+    }
+
+    private void createVirtualDeviceAndDisplay(Optional<ComponentName> imeComponent) {
+        VirtualDeviceParams.Builder builder = new VirtualDeviceParams.Builder();
+        imeComponent.ifPresent(componentName -> builder.setInputMethodComponent(componentName));
+        VirtualDevice virtualDevice = mRule.createManagedVirtualDevice(builder.build());
+
+        VirtualDisplay virtualDisplay = mRule.createManagedVirtualDisplayWithFlags(virtualDevice,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC
+                        | DisplayManager.VIRTUAL_DISPLAY_FLAG_TRUSTED
+                        | DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY);
+        mVirtualDisplayId = virtualDisplay.getDisplay().getDisplayId();
+    }
+
 
     /**
      * Simple IME implementation forwarding the show input requests to a listener along with a
      * display id.
      */
-    public static class VirtualDeviceTestIme extends InputMethodService {}
+    public static class DefaultDeviceTestIme extends InputMethodService {
+
+        static Consumer<Integer> sShowInputOnDisplayRequestListener = null;
+
+        @Override
+        public boolean onShowInputRequested(int flags, boolean configChange) {
+            if (sShowInputOnDisplayRequestListener != null) {
+                sShowInputOnDisplayRequestListener.accept(
+                        getWindow().getWindow().getDecorView().getDisplay().getDisplayId());
+            }
+            return true;
+        }
+    }
+
+    /**
+     * Simple IME implementation forwarding the show input requests to a listener along with a
+     * display id.
+     */
+    public static class VirtualDeviceTestIme extends InputMethodService {
+
+        static Consumer<Integer> sShowInputOnDisplayRequestListener = null;
+
+        @Override
+        public boolean onShowInputRequested(int flags, boolean configChange) {
+            if (sShowInputOnDisplayRequestListener != null) {
+                sShowInputOnDisplayRequestListener.accept(
+                        getWindow().getWindow().getDecorView().getDisplay().getDisplayId());
+            }
+            return true;
+        }
+    }
 }
