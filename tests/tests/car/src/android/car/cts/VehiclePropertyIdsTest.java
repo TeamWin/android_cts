0a6d658c62261af0c573a0d214c98e8ea0dca017
Eva Chen <evaschen@google.com>
2023-11-28 17:12:32 -0800

    Add DRIVER_DROWSINESS_ATTENTION_STATE to cts.
    Bug: 292142225
    Test: atest VtsHalAutomotiveVehicle_TargetTest
    Test: atest CtsCarTestCases:CarPropertyManagerTest
    Test: atest CtsCarTestCases:VehiclePropertyIdsTest
    Test: atest CtsCarTestCases:DriverDrowsinessAttentionStateTest
    Test: atest AndroidCarApiTest:DriverDrowsinessAttentionStateTest
    Change-Id: I62610a4b8b41e925820196aad8ca3fabb14c8771

--- tests/tests/car/src/android/car/cts/VehiclePropertyIdsTest.java
+++ tests/tests/car/src/android/car/cts/VehiclePropertyIdsTest.java
@@ -75,6 +75,10 @@ public class VehiclePropertyIdsTest extends AbstractCarLessTestCase {
                 VehiclePropertyIds.toString(
                         VehiclePropertyIds.DRIVER_DROWSINESS_ATTENTION_SYSTEM_ENABLED))
                 .isEqualTo("DRIVER_DROWSINESS_ATTENTION_SYSTEM_ENABLED");
+        assertThat(
+                VehiclePropertyIds.toString(
+                        VehiclePropertyIds.DRIVER_DROWSINESS_ATTENTION_STATE))
+                .isEqualTo("DRIVER_DROWSINESS_ATTENTION_STATE");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.IGNITION_STATE))
                 .isEqualTo("IGNITION_STATE");
         assertThat(VehiclePropertyIds.toString(VehiclePropertyIds.INFO_DRIVER_SEAT))
