cd4b2dfe68f5c931e53977244313765a9111fa92
Tim Yu <yunicorn@google.com>
2023-12-05 23:28:16 +0000

    Autofill - deflake cts
    Tests if rotation is enabled before set screen orientation.
    
    Test: this
    Bug: 311544215
    
    Change-Id: I0e41409cb97049d4f4c0998e224130a98373f368

--- tests/autofillservice/src/android/autofillservice/cts/testcore/UiBot.java
+++ tests/autofillservice/src/android/autofillservice/cts/testcore/UiBot.java
@@ -1192,13 +1192,31 @@ public class UiBot {
     }
 
     /**
+     * Trys to set the orientation, if possible. No-op if the device does not support rotation
+     *
+     * @return True if the device orientation matches the requested orientation, else false
+     */
+    public boolean maybeSetScreenOrientation(int orientation) {
+        mAutoman.setRotation(orientation);
+        final int currentRotation =
+                InstrumentationRegistry.getInstrumentation()
+                        .getContext()
+                        .getSystemService(DisplayManager.class)
+                        .getDisplay(Display.DEFAULT_DISPLAY)
+                        .getRotation();
+        return orientation == currentRotation;
+    }
+
+    /**
      * Sets the screen orientation.
      *
      * @param orientation typically {@link #LANDSCAPE} or {@link #PORTRAIT}.
-     *
+     * @throws org.junit.AssumptionViolatedException if device does not support rotation.
      * @throws RetryableException if value didn't change.
      */
     public void setScreenOrientation(int orientation) throws Exception {
+        assumeTrue("Rotation is supported", Helper.isRotationSupported(mContext));
+
         // Use the platform API instead of mDevice.getDisplayRotation(), which is slow due to
         // waitForIdle(). waitForIdle() is not needed here because in AutoFillServiceTestCase we
         // always use UiBot#setScreenOrientation() to change the screen rotation, which blocks until
