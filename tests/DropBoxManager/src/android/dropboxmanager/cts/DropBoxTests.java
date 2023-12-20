848230ebe6ff0b9478b6971d149f61c42b6243e0
John Wu <topjohnwu@google.com>
2023-12-20 17:05:59 +0000

    Do not override feature flag in test preparer
    Feature flag override is not allowed when the feature flag is read-only.
    Properly skip tests that requires the feature flag to be enabled.
    
    Bug: 310052524
    Test: atest CtsDropBoxManagerTestCases CtsDropBoxManagerTestCasesAPI34
    Change-Id: Ia975f6a24b4817b1c8f58f643cfc402f0ef28c5e

--- tests/DropBoxManager/src/android/dropboxmanager/cts/DropBoxTests.java
+++ tests/DropBoxManager/src/android/dropboxmanager/cts/DropBoxTests.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -28,12 +29,13 @@ import android.content.IntentFilter;
 import android.os.DropBoxManager;
 import android.os.SystemClock;
 
-import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.android.compatibility.common.util.AmUtils;
 import com.android.compatibility.common.util.SystemUtil;
+import com.android.server.feature.flags.Flags;
 
 import org.junit.After;
 import org.junit.Before;
@@ -108,8 +110,13 @@ public class DropBoxTests {
 
     @Before
     public void setUp() throws Exception {
-        mContext = InstrumentationRegistry.getTargetContext();
+        mContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         mDropBoxManager = mContext.getSystemService(DropBoxManager.class);
+
+        if (mContext.getApplicationInfo().targetSdkVersion > 34) {
+            // Make sure the feature flag is enabled to ensure proper behavior
+            assumeTrue(Flags.enableReadDropboxPermission());
+        }
 
         AmUtils.waitForBroadcastIdle();
 
@@ -125,7 +132,10 @@ public class DropBoxTests {
 
     @After
     public void tearDown() throws Exception {
-        mContext.unregisterReceiver(mDropBoxEntryAddedReceiver);
+        try {
+            mContext.unregisterReceiver(mDropBoxEntryAddedReceiver);
+        } catch (Exception ignored) {
+        }
 
         // Restore dropbox defaults
         restoreDropboxDefaults();
@@ -342,7 +352,7 @@ public class DropBoxTests {
 
         assertTrue(mTestPermissionLatch.await(BROADCAST_RATE_LIMIT * 3 / 2,
                 TimeUnit.MILLISECONDS));
-        assertNotNull(mDropBoxManager.getNextEntry(GET_ENTRY_TAG, currTime));
+        assertNotNull(mDropBoxManager.getNextEntry(GET_ENTRY_TAG, currTime - 1));
     }
 
     private void setTagLowPriority(String tag) throws IOException {
