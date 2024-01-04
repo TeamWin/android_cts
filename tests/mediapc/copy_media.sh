9f29251e7fd52b3c646934aad2ad44ca1af47f62
srujan vandrangi <srujan.vandrangi@ittiam.com>
2024-01-04 19:29:00 +0530

    mediapc CTS: update to CtsMediaPerformanceClassTestCases-3.0.zip
    Bug: 230682028
    Test: atest android.mediapc.cts
    
    Change-Id: Ifbc76dea7020c4b681634776abf4019b23677e0a

--- tests/mediapc/copy_media.sh
+++ tests/mediapc/copy_media.sh
@@ -17,7 +17,7 @@
 ## script to install media performance class test files manually
 
 adbOptions=" "
-resLabel=CtsMediaPerformanceClassTestCases-2.3
+resLabel=CtsMediaPerformanceClassTestCases-3.0
 srcDir="/tmp/$resLabel"
 tgtDir="/sdcard/test"
 usage="Usage: $0 [-h] [-s serial]"
