ea956f990059450fe07fa12af665a454f2af81f0
Rachit Jain <jainrachit@google.com>
2024-01-05 17:15:24 +0000

    Fix ActivityTransitionTests on auto split screen UI
    - For a few tests, do not run the tests on auto split screen UI since the tests
      expect to see a background color during the transition which will not
    be seen since the launcher activity is present but the color is being
    applied to task display area.
    
    Bug: 299971411
    
    Test: atest android.server.wm.ActivityTransitionTests
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:1bbf03c535fa956df4a111724c6c11c2c7acaded)
    Merged-In: Id561a2a5d9f571be0cf6080a4aea90b15a5a694b
    Change-Id: Id561a2a5d9f571be0cf6080a4aea90b15a5a694b

--- tests/framework/base/windowmanager/src/android/server/wm/ActivityTransitionTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/ActivityTransitionTests.java
@@ -35,6 +35,7 @@ import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeFalse;
 
 import android.app.Activity;
 import android.app.ActivityOptions;
@@ -73,7 +74,6 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -279,6 +279,11 @@ public class ActivityTransitionTests extends ActivityManagerTestBase {
      */
     @Test
     public void testAnimationBackgroundColorIsUsedDuringActivityTransition() {
+        // TODO (b/319637823): Find proper fix for background overriding in ActivityTransitionTests
+        // This test expects the default task display area to show a red color during the
+        // transition. But since in auto split screen UI, the launcher activity is always
+        // shown, no screenshot of the test would return the red background color.
+        assumeFalse(hasAutomotiveSplitscreenMultitaskingFeature());
         final int backgroundColor = Color.RED;
         final ActivityOptions activityOptions = ActivityOptions.makeCustomAnimation(mContext,
                 R.anim.alpha_0_with_red_backdrop, R.anim.alpha_0_with_red_backdrop);
@@ -296,6 +301,11 @@ public class ActivityTransitionTests extends ActivityManagerTestBase {
      */
     @Test
     public void testCustomTransitionCanOverrideBackgroundColor() {
+        // TODO (b/319637823): Find proper fix for background overriding in ActivityTransitionTests
+        // This test expects the default task display area to show a green color during the
+        // transition. But since in auto split screen UI, the launcher activity is always
+        // shown, no screenshot of the test would return the green background color.
+        assumeFalse(hasAutomotiveSplitscreenMultitaskingFeature());
         final int backgroundColor = Color.GREEN;
         final ActivityOptions activityOptions = ActivityOptions.makeCustomAnimation(mContext,
                 R.anim.alpha_0_with_backdrop, R.anim.alpha_0_with_backdrop, backgroundColor
@@ -314,6 +324,11 @@ public class ActivityTransitionTests extends ActivityManagerTestBase {
      */
     @Test
     public void testPendingTransitionCanOverrideBackgroundColor() {
+        // TODO (b/319637823): Find proper fix for background overriding in ActivityTransitionTests
+        // This test expects the default task display area to show a green color during the
+        // transition. But since in auto split screen UI, the launcher activity is always
+        // shown, no screenshot of the test would return the green background color.
+        assumeFalse(hasAutomotiveSplitscreenMultitaskingFeature());
         final int backgroundColor = Color.GREEN;
 
         final Bundle extras = new Bundle();
@@ -330,6 +345,11 @@ public class ActivityTransitionTests extends ActivityManagerTestBase {
 
     @Test
     public void testSetTransitionCanOverrideBackgroundColor() {
+        // TODO (b/319637823): Find proper fix for background overriding in ActivityTransitionTests
+        // This test expects the default task display area to show a green color during the
+        // transition. But since in auto split screen UI, the launcher activity is always
+        // shown, no screenshot of the test would return the green background color.
+        assumeFalse(hasAutomotiveSplitscreenMultitaskingFeature());
         final int backgroundColor = Color.GREEN;
 
         final Bundle extras = new Bundle();
