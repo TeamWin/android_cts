ed267380b9e0f6cc31a448b17fc5c147bb017e35
Iván Budnik <ivanbuper@google.com>
2024-01-19 22:34:13 +0000

    Add tests for new MediaRouter2 scanning APIs
    The three `MediaRouter2DeviceTest` files correspond to different
    combination of router instances + permissions:
    
    1. Local router instance
    2. Proxy router instance + MEDIA_CONTENT_CONTROL
    3. Proxy router instance + MEDIA_ROUTING_CONTROL
    
    Bug: 281072508
    Test: atest CtsMediaBetterTogeterTestCases
    Change-Id: I6c418f70342ec72d5180e91ad81979601614d7eb

--- hostsidetests/media/app/ProxyMediaRouterMediaRoutingControl/src/android/media/router/cts/proxyroutingapp/MediaRouter2DeviceTest.java
+++ hostsidetests/media/app/ProxyMediaRouterMediaRoutingControl/src/android/media/router/cts/proxyroutingapp/MediaRouter2DeviceTest.java
@@ -16,6 +16,7 @@
 
 package android.media.router.cts.proxyroutingapp;
 
+import static android.media.cts.MediaRouterTestConstants.FEATURE_SAMPLE;
 import static android.media.cts.MediaRouterTestConstants.MEDIA_ROUTER_PROVIDER_1_PACKAGE;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -27,20 +28,30 @@ import android.annotation.SuppressLint;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.media.MediaRoute2Info;
 import android.media.MediaRouter2;
+import android.media.RouteDiscoveryPreference;
 import android.platform.test.annotations.LargeTest;
 import android.platform.test.annotations.RequiresFlagsDisabled;
 import android.platform.test.annotations.RequiresFlagsEnabled;
 import android.platform.test.flag.junit.CheckFlagsRule;
 import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 
+import androidx.annotation.NonNull;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.android.media.flags.Flags;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /** Device-side test for privileged {@link MediaRouter2} functionality. */
 @LargeTest
@@ -49,13 +60,22 @@ public class MediaRouter2DeviceTest {
     @Rule
     public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
 
+    private static final int TIMEOUT_MS = 5000;
+
     private Instrumentation mInstrumentation;
     private Context mContext;
+    private Executor mExecutor;
 
     @Before
     public void setUp() {
         mInstrumentation = InstrumentationRegistry.getInstrumentation();
         mContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        mExecutor = Executors.newSingleThreadExecutor();
+    }
+
+    @After
+    public void tearDown() {
+        mInstrumentation.getUiAutomation().dropShellPermissionIdentity();
     }
 
     @SuppressLint("MissingPermission")
@@ -138,5 +158,51 @@ public class MediaRouter2DeviceTest {
                                 mContext,
                                 mContext.getPackageName(),
                                 mContext.getUser()));
+    }
+
+    @RequiresFlagsEnabled({
+        Flags.FLAG_ENABLE_SCREEN_OFF_SCANNING,
+        Flags.FLAG_ENABLE_PRIVILEGED_ROUTING_FOR_MEDIA_ROUTING_CONTROL
+    })
+    @Test
+    public void requestScan_withScreenOff_triggersScanning() throws InterruptedException {
+        mInstrumentation
+                .getUiAutomation()
+                .adoptShellPermissionIdentity(Manifest.permission.MEDIA_ROUTING_CONTROL);
+
+        MediaRouter2 localInstance = MediaRouter2.getInstance(mContext);
+        MediaRouter2.RouteCallback placeholderCallback = new MediaRouter2.RouteCallback() {};
+        localInstance.registerRouteCallback(
+                mExecutor,
+                placeholderCallback,
+                new RouteDiscoveryPreference.Builder(List.of(FEATURE_SAMPLE), false).build());
+
+        MediaRouter2 instance = MediaRouter2.getInstance(mContext, mContext.getPackageName());
+        assertThat(instance).isNotNull();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        MediaRouter2.RouteCallback onRoutesUpdated =
+                new MediaRouter2.RouteCallback() {
+                    @Override
+                    public void onRoutesUpdated(@NonNull List<MediaRoute2Info> routes) {
+                        if (routes.stream()
+                                .anyMatch(r -> r.getFeatures().contains(FEATURE_SAMPLE))) {
+                            latch.countDown();
+                        }
+                    }
+                };
+
+        instance.registerRouteCallback(mExecutor, onRoutesUpdated, RouteDiscoveryPreference.EMPTY);
+
+        MediaRouter2.ScanToken token =
+                instance.requestScan(
+                        new MediaRouter2.ScanRequest.Builder().setScreenOffScan(true).build());
+        try {
+            assertThat(latch.await(TIMEOUT_MS, TimeUnit.MILLISECONDS)).isTrue();
+        } finally {
+            instance.cancelScanRequest(token);
+            localInstance.unregisterRouteCallback(placeholderCallback);
+            instance.unregisterRouteCallback(onRoutesUpdated);
+        }
     }
 }
