4d41ecc51791acfc29503f4f0cfb35cd6a31908c
Clemenz Portmann <portmannc@google.com>
2024-01-26 17:16:18 -0800

    ITS: imu_processing_utils.py lint cleanup
    other changes:
      harden unittests by adding float test
    
    bug: 306239763
    Change-Id: I1066b372b49fc1dca04e3ae903a7205385643476

--- apps/CameraITS/utils/imu_processing_utils_tests.py
+++ apps/CameraITS/utils/imu_processing_utils_tests.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 """Tests for imu_processing_utils."""
 
+import numpy as np
 import unittest
 
 import imu_processing_utils
@@ -27,16 +28,22 @@ class ImuProcessingUtilsTest(unittest.TestCase):
     a = [-1, 0, 1, -2]
     b = [-179, 180, 179, -180, -178]
     c = [179, 180, -179, -180, 178]
+    d = [179.0, 180.0, -179.0, -180.0, 178.0]
 
     a_drift = [0, 1, 2, -1]
     b_drift = [0, -1, -2, -1, 1]
     c_drift = [0, 1, 2, 1, -1]
+    d_drift = [0.0, 1.0, 2.0, 1.0, -1.0]
 
     # Check drift
     self.assertEqual(imu_processing_utils.calc_rv_drift(a), a_drift,
                      'a_drift is incorrect')
     self.assertEqual(imu_processing_utils.calc_rv_drift(b), b_drift,
                      'b_drift is incorrect')
+    self.assertEqual(imu_processing_utils.calc_rv_drift(c), c_drift,
+                     'c_drift is incorrect')
+    self.assertTrue(np.allclose(imu_processing_utils.calc_rv_drift(d), d_drift),
+                     'd_drift is incorrect')
 
 
 if __name__ == '__main__':
