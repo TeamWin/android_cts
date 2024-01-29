3c74c89a8750150acaebbef3df174d72ef3d172c
Tony Mak <tonymak@google.com>
2024-01-29 17:07:22 +0000

    Add tests for onHandleScreenshot and onHandleAssist
    Bug: 323128958
    
    Test: android.voiceinteraction.cts.VoiceInteractionServiceTest
    
    Change-Id: Ide3ca9bf5ee03e8d37b93b7c82a484e730721f53

--- tests/tests/voiceinteraction/src/android/voiceinteraction/cts/VoiceInteractionTestReceiver.java
+++ tests/tests/voiceinteraction/src/android/voiceinteraction/cts/VoiceInteractionTestReceiver.java
@@ -22,6 +22,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.voiceinteraction.service.MainInteractionSession;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -32,6 +33,9 @@ public class VoiceInteractionTestReceiver extends BroadcastReceiver {
 
     private static CountDownLatch sServiceStartedLatch = new CountDownLatch(1);
     private static Intent sReceivedIntent;
+    private static CountDownLatch sScreenshotReceivedLatch = new CountDownLatch(1);
+
+    private static CountDownLatch sAssistDataReceivedLatch = new CountDownLatch(1);
 
     public static void waitSessionStarted(long timeout, TimeUnit unit) throws InterruptedException {
         if (!sServiceStartedLatch.await(timeout, unit)) {
@@ -43,10 +47,50 @@ public class VoiceInteractionTestReceiver extends BroadcastReceiver {
         }
     }
 
+    /** Waits for onHandleScreenshot being called. */
+    public static boolean waitScreenshotReceived(long timeout, TimeUnit unit)
+            throws InterruptedException {
+        if (!sScreenshotReceivedLatch.await(timeout, unit)) {
+            // Timeout. Assume this means no screenshot is sent to VoiceInteractionService.
+            return false;
+        }
+        if (!sReceivedIntent.hasExtra(MainInteractionSession.EXTRA_RECEIVED)) {
+            throw new IllegalStateException("Expect EXTRA_RECEIVED");
+        }
+        return sReceivedIntent.getBooleanExtra(MainInteractionSession.EXTRA_RECEIVED, false);
+    }
+
+    /** Waits for onHandleAssist being called. */
+    public static boolean waitAssistDataReceived(long timeout, TimeUnit unit)
+            throws InterruptedException {
+        if (!sAssistDataReceivedLatch.await(timeout, unit)) {
+            // Timeout. Assume this means no assist data is sent to VoiceInteractionService.
+            return false;
+        }
+        if (!sReceivedIntent.hasExtra(MainInteractionSession.EXTRA_RECEIVED)) {
+            throw new IllegalStateException("Expect EXTRA_RECEIVED");
+        }
+        return sReceivedIntent.getBooleanExtra(MainInteractionSession.EXTRA_RECEIVED, false);
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "Got broadcast that MainInteractionService started");
         sReceivedIntent = intent;
-        sServiceStartedLatch.countDown();
+        switch (intent.getAction()) {
+            case MainInteractionSession.ACTION_SESSION_STARTED -> sServiceStartedLatch.countDown();
+            case MainInteractionSession.ACTION_SCREENSHOT_RECEIVED ->
+                    sScreenshotReceivedLatch.countDown();
+            case MainInteractionSession.ACTION_ASSIST_DATA_RECEIVED ->
+                    sAssistDataReceivedLatch.countDown();
+        }
+    }
+
+    /** Resets the states. */
+    public static void reset() {
+        sReceivedIntent = null;
+        sServiceStartedLatch = new CountDownLatch(1);
+        sScreenshotReceivedLatch = new CountDownLatch(1);
+        sAssistDataReceivedLatch = new CountDownLatch(1);
     }
 }
