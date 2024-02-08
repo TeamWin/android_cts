63c546ff6eb1c8d9a4899520b36d3477121bfa55
Jeff Sharkey <jsharkey@google.com>
2024-02-08 11:39:07 -0700

    `ComponentOptions` on Ravenwood.
    These are lightweight holder objects, so they're prime candidates
    for inclusion on Ravenwood.  Also enables `BroadcastRecordTest` on
    Ravenwood too.
    
    Bring CTS to verify we're in sync with devices, and split
    dynamic integration tests into a separate file to avoid complex
    dependency build rules.
    
    Bug: 324417456
    Test: atest CtsAppTestCasesRavenwood
    Test: atest CtsAppTestCases:ActivityOptionsTest
    Test: atest CtsAppTestCases:BroadcastOptionsTest
    Test: atest CtsAppTestCases:BroadcastOptionsIntegrationTest
    Change-Id: I874fb66e2b4d3f06ae2ed98f55e2f3557837a661

--- tests/app/src/android/app/cts/BroadcastOptionsTest.java
+++ tests/app/src/android/app/cts/BroadcastOptionsTest.java
@@ -16,11 +16,6 @@
 
 package android.app.cts;
 
-import static android.app.cts.ActivityManagerFgsBgStartTest.PACKAGE_NAME_APP1;
-import static android.app.cts.ActivityManagerFgsBgStartTest.PACKAGE_NAME_APP2;
-import static android.app.cts.ActivityManagerFgsBgStartTest.WAITFOR_MSEC;
-import static android.app.stubs.LocalForegroundService.ACTION_START_FGS_RESULT;
-
 import static com.google.common.truth.Truth.assertThat;
 
 import static junit.framework.Assert.assertEquals;
@@ -28,19 +23,11 @@ import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThrows;
 
 import android.app.BroadcastOptions;
-import android.app.Instrumentation;
-import android.app.cts.android.app.cts.tools.WaitForBroadcast;
-import android.app.stubs.CommandReceiver;
-import android.content.IntentFilter;
 import android.os.Build;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.os.PowerExemptionManager;
 
-import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.android.compatibility.common.util.SystemUtil;
@@ -50,11 +37,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class BroadcastOptionsTest {
-
     /**
      * Creates a clone of BroadcastOptions, using toBundle().
      */
-    private BroadcastOptions cloneViaBundle(BroadcastOptions bo) {
+    static BroadcastOptions cloneViaBundle(BroadcastOptions bo) {
         return BroadcastOptions.fromBundle(bo.toBundle());
     }
 
@@ -220,81 +206,6 @@ public class BroadcastOptionsTest {
         assertFalse(options.isPendingIntentBackgroundActivityLaunchAllowed());
         assertThat(options.getPendingIntentBackgroundActivityStartMode()).isEqualTo(
                 BroadcastOptions.MODE_BACKGROUND_ACTIVITY_START_DENIED);
-    }
-
-    private void assertBroadcastSuccess(BroadcastOptions options) {
-        final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
-        final WaitForBroadcast waiter = new WaitForBroadcast(instrumentation.getTargetContext());
-        waiter.prepare(ACTION_START_FGS_RESULT);
-        CommandReceiver.sendCommandWithBroadcastOptions(instrumentation.getContext(),
-                CommandReceiver.COMMAND_START_FOREGROUND_SERVICE,
-                PACKAGE_NAME_APP1, PACKAGE_NAME_APP2, 0, null,
-                options.toBundle());
-        waiter.doWait(WAITFOR_MSEC);
-    }
-
-    private void assertBroadcastFailure(BroadcastOptions options) {
-        final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
-        final WaitForBroadcast waiter = new WaitForBroadcast(instrumentation.getTargetContext());
-        waiter.prepare(ACTION_START_FGS_RESULT);
-        CommandReceiver.sendCommandWithBroadcastOptions(instrumentation.getContext(),
-                CommandReceiver.COMMAND_START_FOREGROUND_SERVICE,
-                PACKAGE_NAME_APP1, PACKAGE_NAME_APP2, 0, null,
-                options.toBundle());
-        assertThrows(Exception.class, () -> waiter.doWait(WAITFOR_MSEC));
-    }
-
-    @Test
-    public void testRequireCompatChange_simple() {
-        SystemUtil.runWithShellPermissionIdentity(() -> {
-            final int uid = android.os.Process.myUid();
-            final BroadcastOptions options = BroadcastOptions.makeBasic();
-
-            // Default passes
-            assertTrue(options.testRequireCompatChange(uid));
-            assertTrue(cloneViaBundle(options).testRequireCompatChange(uid));
-
-            // Verify both enabled and disabled
-            options.setRequireCompatChange(BroadcastOptions.CHANGE_ALWAYS_ENABLED, true);
-            assertTrue(options.testRequireCompatChange(uid));
-            assertTrue(cloneViaBundle(options).testRequireCompatChange(uid));
-            options.setRequireCompatChange(BroadcastOptions.CHANGE_ALWAYS_ENABLED, false);
-            assertFalse(options.testRequireCompatChange(uid));
-            assertFalse(cloneViaBundle(options).testRequireCompatChange(uid));
-
-            // And back to default passes
-            options.clearRequireCompatChange();
-            assertTrue(options.testRequireCompatChange(uid));
-            assertTrue(cloneViaBundle(options).testRequireCompatChange(uid));
-        });
-    }
-
-    @Test
-    public void testRequireCompatChange_enabled_success() {
-        final BroadcastOptions options = BroadcastOptions.makeBasic();
-        options.setRequireCompatChange(BroadcastOptions.CHANGE_ALWAYS_ENABLED, true);
-        assertBroadcastSuccess(options);
-    }
-
-    @Test
-    public void testRequireCompatChange_enabled_failure() {
-        final BroadcastOptions options = BroadcastOptions.makeBasic();
-        options.setRequireCompatChange(BroadcastOptions.CHANGE_ALWAYS_DISABLED, true);
-        assertBroadcastFailure(options);
-    }
-
-    @Test
-    public void testRequireCompatChange_disabled_success() {
-        final BroadcastOptions options = BroadcastOptions.makeBasic();
-        options.setRequireCompatChange(BroadcastOptions.CHANGE_ALWAYS_DISABLED, false);
-        assertBroadcastSuccess(options);
-    }
-
-    @Test
-    public void testRequireCompatChange_disabled_failure() {
-        final BroadcastOptions options = BroadcastOptions.makeBasic();
-        options.setRequireCompatChange(BroadcastOptions.CHANGE_ALWAYS_ENABLED, false);
-        assertBroadcastFailure(options);
     }
 
     @Test
