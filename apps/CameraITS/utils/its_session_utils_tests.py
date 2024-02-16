d0bcb55ef781c5a7a17d2a0e0998654a89da9151
Jonathan Liu <jpliu@google.com>
2024-02-16 09:48:23 -0800

    ITS: refactor its_session_utils_tests.py
    Details: there is some logic in the tests for its_session_utils, which
    we should avoid. Also, write_image() is called for these tests, which
    may raise an exception if executed in a directory without write access.
    
    Bug: 324639366
    Test: ran locally and remotely
    Change-Id: Iadd12ceb950e9d39c550c2701156552efac07372

--- apps/CameraITS/utils/its_session_utils_tests.py
+++ apps/CameraITS/utils/its_session_utils_tests.py
@@ -13,54 +13,82 @@
 # limitations under the License.
 """Tests for its_session_utils."""
 
-import logging
 import unittest
+import unittest.mock
 
 import numpy
 
+import image_processing_utils
 import its_session_utils
+
+
+# intensity values of Y-plane array from ITS testing
+_EMPIRICAL_TABLET_OFF_BRIGHTNESS = 0.11
+_PLACEHOLDER_SCENE = 'its_session_utils_unittest_scene'
+_TEST_IMG_W = 640
+_TEST_IMG_H = 480
+
+
+def _generate_test_image(brightness):
+  """Creates a Y plane array with pixel values of brightness.
+
+  Args:
+    brightness: float between [0.0, 1.0]
+
+  Returns:
+    test_image: Y plane array with elements of value brightness.
+  """
+  test_image = numpy.zeros((_TEST_IMG_W, _TEST_IMG_H, 1), dtype=float)
+  test_image.fill(brightness)
+  return test_image
 
 
 class ItsSessionUtilsTests(unittest.TestCase):
   """Run a suite of unit tests on this module."""
 
-  _BRIGHTNESS_CHECKS = (0.0,
-                        its_session_utils._VALIDATE_LIGHTING_THRESH-0.01,
-                        its_session_utils._VALIDATE_LIGHTING_THRESH,
-                        its_session_utils._VALIDATE_LIGHTING_THRESH+0.01,
-                        1.0)
-  _TEST_IMG_W = 640
-  _TEST_IMG_H = 480
+  def setUp(self):
+    super().setUp()
+    self.test_image_zero = _generate_test_image(0.0)
+    self.test_image_half = _generate_test_image(0.5)
+    self.test_image_one = _generate_test_image(1.0)
+    self.addCleanup(unittest.mock.patch.stopall)
+    unittest.mock.patch.object(
+        image_processing_utils, 'write_image', autospec=True).start()
 
-  def _generate_test_image(self, brightness):
-    """Creates a Y plane array with pixel values of brightness.
+  def test_validate_lighting_state_on_at_zero(self):
+    with self.assertRaises(AssertionError):
+      its_session_utils.validate_lighting(
+          self.test_image_zero, _PLACEHOLDER_SCENE)
 
-    Args:
-      brightness: float between [0.0, 1.0]
+  def test_validate_lighting_state_on_at_half(self):
+    its_session_utils.validate_lighting(
+        self.test_image_half, _PLACEHOLDER_SCENE)
 
-    Returns:
-      Y plane array with elements of value brightness
-    """
-    test_image = numpy.zeros((self._TEST_IMG_W, self._TEST_IMG_H, 1),
-                             dtype=float)
-    test_image.fill(brightness)
-    return test_image
+  def test_validate_lighting_state_on_at_one(self):
+    its_session_utils.validate_lighting(
+        self.test_image_one, _PLACEHOLDER_SCENE)
 
-  def test_validate_lighting(self):
-    """Tests validate_lighting() works correctly."""
-    # Run with different brightnesses to validate.
-    for brightness in self._BRIGHTNESS_CHECKS:
-      logging.debug('Testing validate_lighting with brightness %.1f',
-                    brightness)
-      test_image = self._generate_test_image(brightness)
-      print(f'Testing brightness: {brightness}')
-      if brightness <= its_session_utils._VALIDATE_LIGHTING_THRESH:
-        self.assertRaises(
-            AssertionError, its_session_utils.validate_lighting,
-            test_image, 'unittest')
-      else:
-        self.assertTrue(its_session_utils.validate_lighting(
-            test_image, 'unittest'), f'image value {brightness} should PASS')
+  def test_validate_lighting_state_off_at_zero(self):
+    its_session_utils.validate_lighting(
+        self.test_image_zero, _PLACEHOLDER_SCENE, state='OFF')
+
+  def test_validate_lighting_state_off_at_half(self):
+    with self.assertRaises(AssertionError):
+      its_session_utils.validate_lighting(
+          self.test_image_half, _PLACEHOLDER_SCENE, state='OFF')
+
+  def test_validate_lighting_state_off_at_one(self):
+    with self.assertRaises(AssertionError):
+      its_session_utils.validate_lighting(
+          self.test_image_one, _PLACEHOLDER_SCENE, state='OFF')
+
+  def test_validate_lighting_state_off_tablet_off_handles_noise(self):
+    test_image_tablet_off = _generate_test_image(
+        _EMPIRICAL_TABLET_OFF_BRIGHTNESS)
+    its_session_utils.validate_lighting(test_image_tablet_off,
+                                        _PLACEHOLDER_SCENE,
+                                        state='OFF',
+                                        tablet_state='OFF')
 
 
 if __name__ == '__main__':
