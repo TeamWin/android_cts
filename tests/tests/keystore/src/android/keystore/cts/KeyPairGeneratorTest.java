fb30601e68865b0a541fe4883d9520f87a1cb04a
Prashant Patil <patilprashant@google.com>
2024-03-14 15:47:41 +0000

    DO NOT MERGE : Revert "Corrected CTS tests to accept Ed25519"
    This reverts commit f718aa80b2b2fd720d68a0286573009caa9e6141.
    
    Bug: 332421248
    
    Reason for revert: Ed25519 related changes are not expected to merge on android API 34.
    (cherry picked from https://android-review.googlesource.com/q/commit:436d5e31a89a28f7173b56a71436ca8d75568ada)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:060217e2817fb2d4970dea91f1b6f06b05d0302f)
    Merged-In: I1fd1b25ed0cf9638a0a124f8120fc20ac72b6808
    Change-Id: I1fd1b25ed0cf9638a0a124f8120fc20ac72b6808

--- tests/tests/keystore/src/android/keystore/cts/KeyPairGeneratorTest.java
+++ tests/tests/keystore/src/android/keystore/cts/KeyPairGeneratorTest.java
@@ -178,8 +178,6 @@ public class KeyPairGeneratorTest {
         // XDH is also a supported algorithm, but not available for other tests as the keys
         // generated with it have more limited set of uses.
         expectedAlgsLowerCase.add("xdh");
-        // AndroidKeyStore supports key generation of curve 25519
-        expectedAlgsLowerCase.add("ed25519");
 
         for (Service service : services) {
             if ("KeyPairGenerator".equalsIgnoreCase(service.getType())) {
